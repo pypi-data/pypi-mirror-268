d503dde445948f344a2920788123277750055011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:04 +0000

    Add package blackduck-lutils/0.0.11/blackduck_lutils-0.0.11.tar.gz

---
+++ packages/blackduck-lutils/blackduck_lutils-0.0.11.tar.gz/blackduck_lutils-0.0.11/environ.sh
@@ -0,0 +1,3 @@
+  export PYTHONPATH=`pwd`/src:/Users/kumykov/workspace/hub-rest-api-python 
+  export BD_URL=https://ec2-44-202-54-168.compute-1.amazonaws.com
+  export API_TOKEN=MTJlMmI5MGMtNmI0MS00ZGJiLThkMWEtMzNjNDNjMGU5ODAzOjQxN2IxNWU4LTA0OTQtNGMzNS1hNTVjLTY3NTQ2Nzk1ZjNhNw==
