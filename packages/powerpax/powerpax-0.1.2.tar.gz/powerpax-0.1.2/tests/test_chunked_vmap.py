c1c45164f60708f182946cb9824e974a46fb6170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:11 +0000

    Add package powerpax/0.1.2/powerpax-0.1.2.tar.gz

---
+++ packages/powerpax/powerpax-0.1.2.tar.gz/powerpax-0.1.2/tests/test_chunked_vmap.py
@@ -0,0 +1,35 @@
+# Copyright Karl Otness
+# SPDX-License-Identifier: MIT
+
+
+import jax
+import jax.numpy as jnp
+import powerpax as ppx
+import pytest
+
+
+@pytest.mark.parametrize("chunk_size", [1, 3, 5, 9, 10, 11])
+@pytest.mark.parametrize(
+    "use_args,use_kwargs", [(True, True), (True, False), (False, True)]
+)
+def test_matches_vmap(chunk_size, use_args, use_kwargs):
+    def fun(arg=1, arg2=1):
+        return arg * arg2
+
+    arr = jnp.arange(10)
+    args = (arr,) if use_args else ()
+    kwargs = {"arg2": arr} if use_kwargs else {}
+    jax_res = jax.vmap(fun)(*args, **kwargs)
+    chunked_res = jax.jit(ppx.chunked_vmap(fun, chunk_size=chunk_size))(*args, **kwargs)
+    assert jax_res.dtype == chunked_res.dtype
+    assert jax_res.shape == chunked_res.shape
+    assert jnp.all(jax_res == chunked_res)
+
+
+@pytest.mark.parametrize("chunk_size", [-1, 0])
+def test_error_invalid_chunk_size(chunk_size):
+    def fun(arg):
+        return arg * 2
+
+    with pytest.raises(ValueError):
+        _ = ppx.chunked_vmap(fun, chunk_size=chunk_size)
---
+++ packages/powerpax/powerpax-0.1.2.tar.gz/powerpax-0.1.2/tests/test_sliced_scan.py
@@ -0,0 +1,62 @@
+# Copyright Karl Otness
+# SPDX-License-Identifier: MIT
+
+
+import operator
+import jax
+import jax.numpy as jnp
+import powerpax as ppx
+import pytest
+
+
+@pytest.mark.parametrize(
+    "start,stop,step",
+    [
+        (None, None, None),
+        (None, None, -1),
+        (5, None, None),
+        (None, -3, None),
+        (-1, 0, 1),
+        (0, -1, -1),
+        (None, None, 2),
+        (1, None, 2),
+        (2, None, 2),
+        (1, -3, 3),
+        (-7, -2, 2),
+        (2, 3, 4),
+        (3, 2, -4),
+    ],
+)
+@pytest.mark.parametrize("reverse", [False, True])
+@pytest.mark.parametrize(
+    "use_xs,use_length", [(True, True), (True, False), (False, True)]
+)
+def test_matches_scan(start, stop, step, reverse, use_xs, use_length):
+    def scan_fn(carry, x):
+        return carry + 1, (carry, x)
+
+    length = 15
+    xs = jnp.arange(length) if use_xs else None
+    extra_args = {"length": length} if use_length else {}
+    jax_carry, jax_ys = jax.lax.scan(scan_fn, 0, xs, reverse=reverse, **extra_args)
+    jax_ys = jax.tree_util.tree_map(
+        operator.itemgetter(slice(start, stop, step)), jax_ys
+    )
+    ppx_carry, ppx_ys = jax.jit(
+        lambda init, xs: ppx.sliced_scan(
+            scan_fn,
+            init,
+            xs,
+            reverse=reverse,
+            start=start,
+            stop=stop,
+            step=step,
+            **extra_args,
+        )
+    )(0, xs)
+    assert jax.tree_util.tree_all(
+        jax.tree_util.tree_map(lambda la, lb: jnp.all(la == lb), jax_ys, ppx_ys)
+    )
+    assert jax.tree_util.tree_all(
+        jax.tree_util.tree_map(lambda la, lb: jnp.all(la == lb), jax_carry, ppx_carry)
+    )
