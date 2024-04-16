3864f0feeb7c2ebce365e366a394985d2ff9420e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:03:56 +0000

    Add package ResSimpy/2.1.0/ressimpy-2.1.0.tar.gz

---
+++ packages/ResSimpy/ressimpy-2.1.0.tar.gz/ressimpy-2.1.0/ResSimpy/__init__.py
@@ -0,0 +1,14 @@
+"""A library to help read and manipulate input decks for reservoir simulators.
+
+.. autosummary::
+    :toctree: _autosummary
+    :caption: API Reference
+    :template: custom-module-template.rst
+    :recursive:
+
+    Nexus
+    Utils
+    Enums
+"""
+
+__version__ = "2.1.0"  # Set at build time
