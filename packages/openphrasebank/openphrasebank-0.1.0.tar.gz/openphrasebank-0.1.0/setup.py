ca466ce000096724a8f02ceb35063f1313f713da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 06:03:27 +0000

    Add package openphrasebank/0.1.0/openphrasebank-0.1.0.tar.gz

---
+++ packages/openphrasebank/openphrasebank-0.1.0.tar.gz/openphrasebank-0.1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name="openphrasebank",
+    version="0.1.0",
+    author="Zhihao",
+    author_email="liuzhihao109@foxmial.com",
+    description="PhraseBank is a utility designed to help users build customized phrasebanks from various texts or corpora.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/liuh886/open_phrasebank",
+    install_requires=[
+        'nltk', 
+        'pymupdf',
+        'tqdm',  
+        'datasets', # Huggingface datasets
+    ],
+    extras_require={
+        'spacy': ['spacy>=3.0', 'en_core_web_sm']
+        },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'openphrasebank=openphrasebank.cli:main',  # Adjust according to your package structure
+        ],
+    },
+    package_dir={"": "openphrasebank"},
+    packages=find_packages(where="openphrasebank"),
+    include_package_data=True,  # This tells setuptools to check MANIFEST.in
+    package_data={
+        "": ["data/*", "phrasebanks/*"],  # include all files in the data and phrasebanks directories
+    },
+)
