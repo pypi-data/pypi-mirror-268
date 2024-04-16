43094fe2cfa76b4c339ce51296dd3bc744e2f945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:14 +0000

    Add package openadapt/0.17.1/openadapt-0.17.1-py3-none-any.whl

---
+++ packages/openadapt/openadapt-0.17.1-py3-none-any.whl/openadapt/common.py
@@ -0,0 +1,28 @@
+"""openadapt.common module.
+
+This module defines common constants and variables used in OpenAdapt.
+
+Example usage:
+    from openadapt.common import ALL_EVENTS
+
+    for event in ALL_EVENTS:
+        print(event)
+"""
+
+MOUSE_EVENTS = (
+    # raw
+    "move",
+    "click",
+    "scroll",
+    # processed
+    "doubleclick",
+    "singleclick",
+)
+KEY_EVENTS = (
+    # raw
+    "press",
+    "release",
+    # processed
+    "type",
+)
+ALL_EVENTS = tuple(list(MOUSE_EVENTS) + list(KEY_EVENTS))
