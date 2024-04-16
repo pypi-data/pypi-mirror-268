e297805e5262e3f68de3c5c22231da74f78b9828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 06:03:12 +0000

    Add package pynfe_xml2dict/0.0.4/pynfe_xml2dict-0.0.4-py3-none-any.whl

---
+++ packages/pynfe_xml2dict/pynfe_xml2dict-0.0.4-py3-none-any.whl/pynfe_xml2dict/nfe/__tag_ide__.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+dirNameSrc = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+sys.path.append(dirNameSrc)
+
+from pynfe_xml2dict.utils.functions import returnDataInDictOrArray, treatsFieldAsNumber
+from typing import Dict
+
+
+class TagIde():
+    def __init__(self, dataTagIde: dict):
+        self.__dataTagIde = dataTagIde
+        self.__objIde: Dict[str, str] = {}
+
+    def getData(self):
+        self.__objIde['numero_nf'] = treatsFieldAsNumber(returnDataInDictOrArray(self.__dataTagIde, ['nNF']))
+        self.__objIde['modelo_nf'] = treatsFieldAsNumber(returnDataInDictOrArray(self.__dataTagIde, ['mod']))
+        self.__objIde['serie_nf'] = returnDataInDictOrArray(self.__dataTagIde, ['serie'])
+        self.__objIde['data_emissao'] = returnDataInDictOrArray(self.__dataTagIde, ['dhEmi'])
+        self.__objIde['data_saida_entrada'] = returnDataInDictOrArray(self.__dataTagIde, ['dhSaiEnt'])
+        self.__objIde['tipo_documento'] = treatsFieldAsNumber(returnDataInDictOrArray(self.__dataTagIde, 'tpNF'))
+        self.__objIde['identificador_local_destino_operacao'] = treatsFieldAsNumber(
+            returnDataInDictOrArray(self.__dataTagIde, ['idDest']))
+        self.__objIde['municipio_ocorrencia_fato'] = treatsFieldAsNumber(
+            returnDataInDictOrArray(self.__dataTagIde, ['cMunFG']))
+        self.__objIde['finalidade_nf'] = treatsFieldAsNumber(returnDataInDictOrArray(self.__dataTagIde, ['finNFe']))
+
+        return self.__objIde
---
+++ packages/pynfe_xml2dict/pynfe_xml2dict-0.0.4-py3-none-any.whl/pynfe_xml2dict/utils/read_xml.py
@@ -0,0 +1,30 @@
+import xmltodict as xmldict
+import platform
+from codecs import open
+
+
+def readXml(filePath: str) -> dict:
+    """
+    # :filePath is xml file
+    """
+    try:
+        if platform.system() == 'Windows':
+            with open(filePath, 'rb', encoding='latin1') as file:
+                return xmldict.parse(file.read())
+        else:
+            with open(filePath, 'rb') as file:
+                return xmldict.parse(file.read())
+    except Exception as e:
+        print(e)
+        return {}
+
+
+def readXmlFromStr(xml: str) -> dict:
+    """
+    # :xml is str xml data
+    """
+    try:
+        return xmldict.parse(xml)
+    except Exception as e:
+        print(e)
+        return {}
