5846b84bbbfc1269bb67ed35ecc6ee3ae5d27ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:23 +0000

    Add package gptstonks-wrappers/0.0.1.post2/gptstonks_wrappers-0.0.1.post2.tar.gz

---
+++ packages/gptstonks-wrappers/gptstonks_wrappers-0.0.1.post2.tar.gz/gptstonks_wrappers-0.0.1.post2/gptstonks/wrappers/llms/guidance_wrapper.py
@@ -0,0 +1,30 @@
+import guidance
+from transformers import AutoTokenizer
+
+
+class GuidanceWrapper:
+    """Wrapper around `guidance` to be used with HF models."""
+
+    def __init__(
+        self,
+        model_id: str = "openlm-research/open_llama_3b_v2",
+        tokenizer_kwargs: dict = {},
+        model_kwargs: dict = {},
+    ):
+        """Init method.
+
+        Args:
+            model_id (`str`):
+                Name of the HF model to use.
+        """
+
+        tokenizer = AutoTokenizer.from_pretrained(model_id, **tokenizer_kwargs)
+
+        # set the default language model used to execute guidance programs
+        guidance.llm = guidance.models.Transformers(
+            model=model_id, tokenizer=tokenizer, **model_kwargs
+        )
+
+    def __call__(self, *args, **kwargs):
+        """Calls `guidance.__call__` passing all the arguments to it."""
+        return guidance(*args, **kwargs)
