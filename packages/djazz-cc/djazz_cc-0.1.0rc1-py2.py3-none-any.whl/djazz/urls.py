455bacc5436afdce9b0b71cd36fd607507d9527e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:03:20 +0000

    Add package djazz-cc/0.1.0rc1/djazz_cc-0.1.0rc1-py2.py3-none-any.whl

---
+++ packages/djazz-cc/djazz_cc-0.1.0rc1-py2.py3-none-any.whl/djazz/urls.py
@@ -0,0 +1,46 @@
+"""
+URL configuration for djazz project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+# djazz urls
+urlpatterns += [
+    path("", include("core.urls")),
+]
+
+# DEBUG only URL patterns
+if settings.DEBUG:
+    # django-debug-toolbar
+    try:
+        import debug_toolbar
+
+        urlpatterns += [
+            path("__debug__/", include(debug_toolbar.urls)),
+        ]
+    except ImportError:
+        pass
+
+    # serve static and media files in development
+    from django.conf.urls.static import static
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
