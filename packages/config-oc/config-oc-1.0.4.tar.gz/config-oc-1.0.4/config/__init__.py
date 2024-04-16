394e1c8337e523ad30c07930c06d795b003ff388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:03:56 +0000

    Add package config-oc/1.0.4/config-oc-1.0.4.tar.gz

---
+++ packages/config-oc/config-oc-1.0.4.tar.gz/config-oc-1.0.4/config/__init__.py
@@ -0,0 +1,30 @@
+# coding=utf8
+""" Config
+
+Loads configuration from ./config.json merged with ./config.`hostname`.json
+"""
+
+__author__		= "Chris Nasr"
+__copyright__	= "Ouroboros Coding Inc."
+__email__		= "chris@ouroboroscoding.com"
+__created__		= "2023-05-26"
+
+# Limit exports
+__all__ = [ 'config', 'Data' ]
+
+# Python imports
+import sys
+
+# Project imports
+from .conf import Conf
+from .data import Data
+
+# The one instance we export
+config = Conf()
+
+# Allow use of import config instead of from config import config
+if sys.modules[__name__] is config:
+	pass
+else:
+	sys.modules[__name__] = config
+	sys.modules[__name__].config = config
