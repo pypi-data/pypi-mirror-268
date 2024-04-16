e2004d3d0334070a6f6afe56c430e0d6889ead13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:03:20 +0000

    Add package nonebug/0.3.7/nonebug-0.3.7.tar.gz

---
+++ packages/nonebug/nonebug-0.3.7.tar.gz/nonebug-0.3.7/nonebug/mixin/call_api/model.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any, Dict, Union, Optional
+
+if TYPE_CHECKING:
+    from nonebot.adapters import Bot, Event, Adapter, Message, MessageSegment
+
+
+@dataclass
+class Model: ...
+
+
+@dataclass
+class Api(Model):
+    name: str
+    data: Dict[str, Any]
+    result: Any
+    exception: Optional[Exception]
+    adapter: Optional["Adapter"]
+
+
+@dataclass
+class Send(Model):
+    event: "Event"
+    message: Union[str, "Message", "MessageSegment"]
+    kwargs: Dict[str, Any]
+    result: Any
+    exception: Optional[Exception]
+    bot: Optional["Bot"]
