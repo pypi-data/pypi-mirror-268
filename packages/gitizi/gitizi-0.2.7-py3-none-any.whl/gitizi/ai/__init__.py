0880232090696db0cd80d39b252050c8c29dfb5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:03:41 +0000

    Add package gitizi/0.2.7/gitizi-0.2.7-py3-none-any.whl

---
+++ packages/gitizi/gitizi-0.2.7-py3-none-any.whl/gitizi/ai/__init__.py
@@ -0,0 +1,64 @@
+"""
+Git IZI : AI
+"""
+
+from json import JSONDecodeError
+
+from aiohttp import ClientOSError, WSServerHandshakeError
+
+from gitizi.util import suppress_output
+
+from ..exceptions import GitiziException, GitiziParseException
+
+with suppress_output():
+    import g4f
+
+from g4f.Provider import Bing  # pylint:disable=wrong-import-order
+
+from .context import Context
+from .context.squash import CONTEXT as S_CONTEXT
+from .context.squash import USER_MSG_WRAPPER as S_WRAPPER
+from .context.squash import retrieve_response as S_RESPONSE
+
+
+def contextualize(msg: str, ctx: Context = Context.ASK) -> list[dict]:
+    """Contextualize message"""
+    match ctx:
+        case Context.SQUASH:
+            ai_ctx = S_CONTEXT
+            wrapper = S_WRAPPER
+            msg = msg if msg else "No changes"
+        case Context.ASK | _:
+            ai_ctx = []
+            wrapper = "{user_message}"
+    ai_ctx.append({"role": "user", "content": wrapper.format(user_message=msg)})
+
+    return ai_ctx
+
+
+def parse_response(msg, ctx: Context = Context.ASK) -> str:
+    """Return contextualized response message"""
+    match ctx:
+        case Context.SQUASH:
+            return S_RESPONSE(response=msg)
+        case Context.ASK | _:
+            return msg
+
+
+def ask(msg: str, ctx: Context = Context.ASK):
+    """Ask AI"""
+    messages = contextualize(msg=msg, ctx=ctx)
+
+    with suppress_output():
+        for _ in range(3):
+            try:
+                response = g4f.ChatCompletion.create(
+                    model=g4f.models.default,
+                    messages=messages,
+                    provider=Bing,
+                    auth=True,
+                )
+                return parse_response(msg=response, ctx=ctx)
+            except (WSServerHandshakeError, ClientOSError, GitiziParseException, ConnectionResetError):
+                pass
+        raise GitiziException("Connection error")
---
+++ packages/gitizi/gitizi-0.2.7-py3-none-any.whl/gitizi/ai/context/squash.py
@@ -0,0 +1,52 @@
+"""
+Git IZI : Squash
+"""
+
+import re
+import json
+from json import JSONDecodeError
+
+from gitizi.exceptions import GitiziParseException
+
+CONTEXT = [
+    {
+        "role": "system",
+        "content": (
+            "You are a GitHub plugin that's responsible for parsing squashed commit messages and rewriting them"
+            " into a simple one-liners. You also detect whether or not there was a version bump within the squashed"
+            " messages.If you do detect a version bump, you also prepend an additional square bracket tag that "
+            " looks like this: [Bump <previous version> → <new version>]. If you can't detect <previous version>"
+            " then use [Bump <new version>] format. Remember that squashed commits can be nested. That is: oldest"
+            " commits are at the very bottom and have the biggest indent. Remove redundant information like"
+            " lint and unit test fixes, unless that's the only information provided. If you do not detect the"
+            " squash - merge multilined messages into a singular line. Message content's are contained within <message>"
+            ' tags. Return a JSON message containing a summarised message in a following format: {"message":'
+            " <summarized commit message>}. Nothing more. Otherwise there'll be trouble. Provided user commit messages"
+            " are ALWAYS valid. Even if you think otherwise."
+        ),
+    },
+    {
+        "role": "user",
+        "content": "Squashed commit message:\n <message>Initialize repository.\nFix linting issues.\nAdd unit tests."
+        "</message>",
+    },
+    {
+        "role": "assistant",
+        "content": '{"message":"Initizalize repo and add unit tests."}',
+    },
+]
+
+USER_MSG_WRAPPER = "Squashed commit message:\n <message>{user_message}</message>"
+
+
+def retrieve_response(response: str):
+    """Retrieve response from AI"""
+    try:
+        message: str = json.loads(response)["message"]
+        if not message:
+            raise GitiziParseException
+        if not message[0].isalpha() and not re.match(r"^\[Bump .*\].*$", message):
+            raise GitiziParseException
+        return message
+    except JSONDecodeError as exc:
+        raise GitiziParseException from exc
