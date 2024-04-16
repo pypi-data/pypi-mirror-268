5eae5dbf2107aef42017d09f8842b5643130efaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:30 +0000

    Add package pyprland/2.2.7/pyprland-2.2.7-py3-none-any.whl

---
+++ packages/pyprland/pyprland-2.2.7-py3-none-any.whl/pyprland/plugins/pyprland.py
@@ -0,0 +1,46 @@
+" Not a real Plugin - provides some core features and some caching of commonly requested structures "
+from .interface import Plugin
+from ..common import state
+
+
+class Extension(Plugin):
+    "Internal built-in plugin allowing caching states and implementing special commands"
+
+    async def init(self):
+        "initializes the plugin"
+        state.active_window = ""
+        state.active_workspace = (await self.hyprctlJSON("activeworkspace"))["name"]
+        monitors = await self.hyprctlJSON("monitors")
+        state.monitors = [mon["name"] for mon in monitors]
+        state.active_monitor = next(mon["name"] for mon in monitors if mon["focused"])
+
+    async def event_monitoradded(self, name):
+        "track monitor"
+        state.monitors.append(name)
+
+    async def event_monitorremoved(self, name):
+        "track monitor"
+        state.monitors.remove(name)
+
+    async def on_reload(self):
+        state.variables = self.config.get("variables", {})
+
+    async def event_activewindowv2(self, addr):
+        "keep track of focused client"
+        state.active_window = "0x" + addr
+        self.log.debug("active_window = %s", state.active_window)
+
+    async def event_workspace(self, wrkspace):
+        "track the active workspace"
+        state.active_workspace = wrkspace
+        self.log.debug("active_workspace = %s", state.active_workspace)
+
+    async def event_focusedmon(self, mon):
+        "track the active workspace"
+        state.active_monitor, state.active_workspace = mon.rsplit(",", 1)
+        self.log.debug("active_monitor = %s", state.active_monitor)
+
+    def set_commands(self, **cmd_map):
+        "Set some commands, made available as run_`name` methods"
+        for name, fn in cmd_map.items():
+            setattr(self, f"run_{name}", fn)
