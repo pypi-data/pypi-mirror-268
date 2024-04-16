0997e8c26ce4f6b970c42ad503eb167a09e373d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:02:57 +0000

    Add package discord-drive/0.0.4/discord_drive-0.0.4.tar.gz

---
+++ packages/discord-drive/discord_drive-0.0.4.tar.gz/discord_drive-0.0.4/setup.py
@@ -0,0 +1,28 @@
+# import os
+from setuptools import setup
+
+# print(os.curdir)
+# with open('./discord_drive/requirements.txt') as f:
+#     required = f.read().splitlines()
+    
+setup(
+    name='discord_drive',
+    version='0.0.4',
+    description='Use Google Drive via Discord!',
+    license='MIT',
+    packages=['discord_drive'],
+    author='Ryan Karch, Dominic Beyer',
+    author_email='',
+    keywords=['Discord', 'Google Drive'],
+    url='https://github.com/thedomino1313/DiscordDrive',
+    install_requires=[
+        'py-cord',
+        'google_api_python_client',
+        'google_auth_oauthlib',
+        'numpy',
+        'opencv_python',
+        'opencv_python_headless',
+        'protobuf',
+        'python-dotenv'
+    ]
+)
