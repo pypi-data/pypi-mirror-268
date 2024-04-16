c8fd80818dc3f33e2dbcc63bc2c525826e66070c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 16:03:23 +0000

    Add package pcffont/0.0.5/pcffont-0.0.5.tar.gz

---
+++ packages/pcffont/pcffont-0.0.5.tar.gz/pcffont-0.0.5/src/pcffont/header.py
@@ -0,0 +1,56 @@
+from enum import IntEnum
+
+from pcffont.error import PcfParseError
+from pcffont.internal.buffer import Buffer
+
+_FILE_VERSION = b'\x01fcp'
+
+
+class PcfTableType(IntEnum):
+    PROPERTIES = 1 << 0
+    ACCELERATORS = 1 << 1
+    METRICS = 1 << 2
+    BITMAPS = 1 << 3
+    INK_METRICS = 1 << 4
+    BDF_ENCODINGS = 1 << 5
+    SWIDTHS = 1 << 6
+    GLYPH_NAMES = 1 << 7
+    BDF_ACCELERATORS = 1 << 8
+
+
+class PcfHeader:
+    @staticmethod
+    def parse(buffer: Buffer) -> list['PcfHeader']:
+        buffer.seek(0)
+        if buffer.read(4) != _FILE_VERSION:
+            raise PcfParseError('Not PCF format')
+
+        tables_count = buffer.read_int32()
+
+        headers = []
+        for _ in range(tables_count):
+            table_type = PcfTableType(buffer.read_int32())
+            table_format = buffer.read_int32()
+            table_size = buffer.read_int32()
+            table_offset = buffer.read_int32()
+            headers.append(PcfHeader(table_type, table_format, table_size, table_offset))
+
+        return headers
+
+    @staticmethod
+    def dump(buffer: Buffer, headers: list['PcfHeader']):
+        buffer.seek(0)
+        buffer.write(_FILE_VERSION)
+
+        buffer.write_int32(len(headers))
+        for header in headers:
+            buffer.write_int32(header.table_type)
+            buffer.write_int32(header.table_format)
+            buffer.write_int32(header.table_size)
+            buffer.write_int32(header.table_offset)
+
+    def __init__(self, table_type: PcfTableType, table_format: int, table_size: int, table_offset: int):
+        self.table_type = table_type
+        self.table_format = table_format
+        self.table_size = table_size
+        self.table_offset = table_offset
---
+++ packages/pcffont/pcffont-0.0.5.tar.gz/pcffont-0.0.5/tests/test_load_save.py
@@ -0,0 +1,76 @@
+import hashlib
+import os
+from pathlib import Path
+
+from pcffont import PcfFont
+
+project_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+def _file_sha256(file_path: str) -> str:
+    with open(file_path, 'rb') as file:
+        return hashlib.sha256(file.read()).hexdigest()
+
+
+def test_unifont(tmp_path: Path):
+    load_file_path = os.path.join(project_root_dir, 'assets', 'unifont', 'unifont-15.1.05.pcf')
+    save_file_path = os.path.join(tmp_path, 'unifont-15.1.05.pcf')
+    font = PcfFont.load(load_file_path)
+    font.save(save_file_path)
+    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+
+
+def test_dweep(tmp_path: Path):
+    load_file_path = os.path.join(project_root_dir, 'assets', 'dweep', 'dweep.pcf')
+    save_file_path = os.path.join(tmp_path, 'dweep.pcf')
+    font = PcfFont.load(load_file_path)
+    font.save(save_file_path)
+    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+
+
+def test_rock36(tmp_path: Path):
+    load_file_path = os.path.join(project_root_dir, 'assets', 'sgi', 'rock36.pcf')
+    save_file_path = os.path.join(tmp_path, 'rock36.pcf')
+    font = PcfFont.load(load_file_path)
+    font.save(save_file_path)
+    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+
+
+def test_raize(tmp_path: Path):
+    load_file_path = os.path.join(project_root_dir, 'assets', 'raize', 'raize-normal-19.pcf')
+    save_file_path = os.path.join(tmp_path, 'raize-normal-19.pcf')
+    font = PcfFont.load(load_file_path)
+    font.save(save_file_path)
+    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+
+
+def test_anorexia(tmp_path: Path):
+    load_file_path = os.path.join(project_root_dir, 'assets', 'artwiz', 'anorexia.pcf')
+    save_file_path = os.path.join(tmp_path, 'anorexia.pcf')
+    font = PcfFont.load(load_file_path)
+    font.save(save_file_path)
+    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+
+
+def test_kates(tmp_path: Path):
+    load_file_path = os.path.join(project_root_dir, 'assets', 'artwiz', 'kates.pcf')
+    save_file_path = os.path.join(tmp_path, 'kates.pcf')
+    font = PcfFont.load(load_file_path)
+    font.save(save_file_path)
+    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+
+
+def test_trisk(tmp_path: Path):
+    load_file_path = os.path.join(project_root_dir, 'assets', 'trisk', 'trisk.pcf')
+    save_file_path = os.path.join(tmp_path, 'trisk.pcf')
+    font = PcfFont.load(load_file_path)
+    font.save(save_file_path)
+    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
+
+
+def test_profont(tmp_path: Path):
+    load_file_path = os.path.join(project_root_dir, 'assets', 'profont-x11', 'ProFont_r400-29.pcf')
+    save_file_path = os.path.join(tmp_path, 'ProFont_r400-29.pcf')
+    font = PcfFont.load(load_file_path)
+    font.save(save_file_path)
+    assert _file_sha256(load_file_path) == _file_sha256(save_file_path)
