ddc3371b9ce95788dfb804d35b46b888d42b9028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:30 +0000

    Add package prosperity2/0.0.0/prosperity2-0.0.0.tar.gz

---
+++ packages/prosperity2/prosperity2-0.0.0.tar.gz/prosperity2-0.0.0/README.md
@@ -0,0 +1,9 @@
+# Prosperity2 CLI
+
+## Usage 
+
+```shell
+prosperity2 file_path
+```
+
+Output: "trade_history.json" under the directory where command is executed
---
+++ packages/prosperity2/prosperity2-0.0.0.tar.gz/prosperity2-0.0.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='prosperity2',
+    packages=find_packages(),
+    scripts=['prosperity2/main.py'],
+)
