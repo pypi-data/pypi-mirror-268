8c8d21b4e7f670dab702fb88f8778cd434d8bfc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:03:51 +0000

    Add package dtool-ibeis/1.1.2/dtool_ibeis-1.1.2-py3-none-any.whl

---
+++ packages/dtool-ibeis/dtool_ibeis-1.1.2-py3-none-any.whl/dtool_ibeis/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+from dtool_ibeis import __SQLITE__ as lite
+from dtool_ibeis import base
+from dtool_ibeis import sql_control
+from dtool_ibeis import depcache_control
+from dtool_ibeis import depcache_table
+
+from dtool_ibeis.depcache_control import DependencyCache, make_depcache_decors
+from dtool_ibeis.base import (AlgoResult, MatchResult, Config,
+                        VsManySimilarityRequest, VsOneSimilarityRequest)
+from dtool_ibeis.depcache_table import ExternalStorageException, ExternType
+from dtool_ibeis.base import *  # NOQA
+from dtool_ibeis.sql_control import SQLDatabaseController
+
+__version__ = '1.1.2'
