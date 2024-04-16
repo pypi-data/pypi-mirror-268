a15e8f063bc39e25c2a19cbcadecccb3bf206f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:43 +0000

    Add package pyBlindRL/0.0.2rc62.post1/pyblindrl-0.0.2rc62.post1.tar.gz

---
+++ packages/pyBlindRL/pyblindrl-0.0.2rc62.post1.tar.gz/pyblindrl-0.0.2rc62.post1/tests/test_methods.py
@@ -0,0 +1,12 @@
+#   ---------------------------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   ---------------------------------------------------------------------------------
+"""This is a sample python file for testing functions from the source code."""
+from __future__ import annotations
+
+# from pyBlindRL.hello_world import hello_world
+import pyBlindRL
+
+def test_gaussian():
+    pyBlindRL.gaussian_3d( (100,100,100) )
