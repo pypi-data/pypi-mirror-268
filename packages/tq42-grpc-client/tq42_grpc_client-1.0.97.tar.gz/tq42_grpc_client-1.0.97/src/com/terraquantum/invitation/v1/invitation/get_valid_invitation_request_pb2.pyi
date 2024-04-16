38328077647668a84eed8fcfd69625379ba14e25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 16:03:38 +0000

    Add package tq42-grpc-client/1.0.97/tq42_grpc_client-1.0.97.tar.gz

---
+++ packages/tq42-grpc-client/tq42_grpc_client-1.0.97.tar.gz/tq42_grpc_client-1.0.97/src/com/terraquantum/invitation/v1/invitation/get_valid_invitation_request_pb2.pyi
@@ -0,0 +1,11 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class GetValidInvitationRequest(_message.Message):
+    __slots__ = ("invitation_token",)
+    INVITATION_TOKEN_FIELD_NUMBER: _ClassVar[int]
+    invitation_token: str
+    def __init__(self, invitation_token: _Optional[str] = ...) -> None: ...
