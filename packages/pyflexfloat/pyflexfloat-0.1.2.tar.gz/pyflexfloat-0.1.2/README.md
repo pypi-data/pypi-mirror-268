7a4cc7a9b2bb4ee993db68f4aea5fbe6610d249a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:04:25 +0000

    Add package pyflexfloat/0.1.2/pyflexfloat-0.1.2.tar.gz

---
+++ packages/pyflexfloat/pyflexfloat-0.1.2.tar.gz/pyflexfloat-0.1.2/README.md
@@ -0,0 +1,20 @@
+## PyFlexFloat
+
+A Python wrapper around OPRECOMP's [FlexFloat](https://github.com/oprecomp/flexfloat) library, enabling arbitrary precision floating-point arithmetic emulation in Python.
+
+## Installation
+
+Simply install via pip:
+```
+pip install pyflexfloat
+```
+
+For developers, you can create an editable installation, to test your changes to the source code:
+```
+pip install -e .
+```
+
+Test the installation:
+```
+./test/test.py
+```
---
+++ packages/pyflexfloat/pyflexfloat-0.1.2.tar.gz/pyflexfloat-0.1.2/setup.py
@@ -0,0 +1,28 @@
+# Copyright 2024 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Author: Luca Colagrande <colluca@iis.ee.ethz.ch>
+import setuptools
+from setuptools.command.build import build
+import subprocess
+from pathlib import Path
+
+class CMakeBuild(build):
+
+	def run(self):
+		# Required to generate "flexfloat_config.h"
+		builddir = Path(__file__).parent / 'flexfloat/build'
+		builddir.mkdir(parents=True, exist_ok=True)
+		subprocess.run(['pwd'], cwd=builddir, check=True)
+		subprocess.run(['ls', '..'], cwd=builddir, check=True)
+		subprocess.run(['cmake', '..'], cwd=builddir, check=True)
+
+
+setuptools.setup(
+	packages=setuptools.find_packages(),
+	setup_requires=['cffi'],
+	install_requires=['cffi'],
+	cffi_modules=["pyflexfloat/build_flexfloat_ffi.py:ffibuilder"],
+	cmdclass={'build': CMakeBuild}
+)
