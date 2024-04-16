838d9799585ff0985a5b885a97e3ed30e4fad42d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:03:38 +0000

    Add package accelbyte-py-sdk/0.64.0/accelbyte_py_sdk-0.64.0.tar.gz

---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.64.0.tar.gz/accelbyte_py_sdk-0.64.0/accelbyte_py_sdk/api/achievement/models/__init__.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Achievement Service."""
+
+__version__ = "2.21.14"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .models_achievement_order_update_request import ModelsAchievementOrderUpdateRequest
+from .models_achievement_request import ModelsAchievementRequest
+from .models_achievement_response import ModelsAchievementResponse
+from .models_achievement_update_request import ModelsAchievementUpdateRequest
+from .models_additional_info import ModelsAdditionalInfo
+from .models_contributor_response import ModelsContributorResponse
+from .models_global_achievement_response import ModelsGlobalAchievementResponse
+from .models_icon import ModelsIcon
+from .models_paginated_achievement_response import ModelsPaginatedAchievementResponse
+from .models_paginated_contributor_response import ModelsPaginatedContributorResponse
+from .models_paginated_global_achievement_response import (
+    ModelsPaginatedGlobalAchievementResponse,
+)
+from .models_paginated_tag_response import ModelsPaginatedTagResponse
+from .models_paginated_user_achievement_response import (
+    ModelsPaginatedUserAchievementResponse,
+)
+from .models_paginated_user_contribution_response import (
+    ModelsPaginatedUserContributionResponse,
+)
+from .models_pagination import ModelsPagination
+from .models_public_achievement_response import ModelsPublicAchievementResponse
+from .models_public_achievements_response import ModelsPublicAchievementsResponse
+from .models_tag_response import ModelsTagResponse
+from .models_user_achievement_response import ModelsUserAchievementResponse
+from .models_user_contribution_response import ModelsUserContributionResponse
+from .response_error import ResponseError
+from .service_import_config_response import ServiceImportConfigResponse
