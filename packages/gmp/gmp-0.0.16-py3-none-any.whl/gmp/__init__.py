559b768bbae12697479f0c13a1a2f7ccabe61df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:04 +0000

    Add package gmp/0.0.16/gmp-0.0.16-py3-none-any.whl

---
+++ packages/gmp/gmp-0.0.16-py3-none-any.whl/gmp/__init__.py
@@ -0,0 +1,12 @@
+from .printing import printing
+from .lolcat import rainbow
+from .projecteuler import solve
+from .isprime import isprime
+
+#print(solve(1))
+#style = printing("purple4", "blue")
+#color = rainbow(False)
+
+
+#print(color.lolcat(style.print([1, 67677762, 43], ["fds", "5435", "fds"], [1,2,3])))
+#print(color.lolcat("I told you I was going to do it"))
