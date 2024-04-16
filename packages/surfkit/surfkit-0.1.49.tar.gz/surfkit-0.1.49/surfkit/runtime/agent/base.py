9aba2b98f3f306ae802bddc7345d61da57d8692c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:16 +0000

    Add package surfkit/0.1.49/surfkit-0.1.49.tar.gz

---
+++ packages/surfkit/surfkit-0.1.49.tar.gz/surfkit-0.1.49/surfkit/runtime/agent/base.py
@@ -0,0 +1,69 @@
+from typing import List, TypeVar, Type, Generic, Union, Iterator, Optional
+from abc import ABC, abstractmethod
+
+from pydantic import BaseModel
+from taskara.models import SolveTaskModel
+
+from ...types import AgentType
+
+R = TypeVar("R", bound="AgentRuntime")
+C = TypeVar("C", bound="BaseModel")
+
+
+class AgentRuntime(Generic[R, C], ABC):
+
+    @classmethod
+    def name(cls) -> str:
+        return cls.__name__
+
+    @classmethod
+    @abstractmethod
+    def connect_config_type(cls) -> Type[C]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def connect(cls, cfg: C) -> R:
+        pass
+
+    @abstractmethod
+    def run(
+        self,
+        agent_type: AgentType,
+        name: str,
+        version: Optional[str] = None,
+        env_vars: Optional[dict] = None,
+        llm_providers_local: bool = False,
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def solve_task(
+        self, agent_name: str, task: SolveTaskModel, follow_logs: bool = False
+    ) -> None:
+        pass
+
+    @abstractmethod
+    def list(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def delete(self, name: str) -> None:
+        pass
+
+    @abstractmethod
+    def clean(self) -> None:
+        pass
+
+    @abstractmethod
+    def logs(self, name: str, follow: bool = False) -> Union[str, Iterator[str]]:
+        """
+        Fetches the logs from the specified pod.
+
+        Parameters:
+            name (str): The name of the pod.
+
+        Returns:
+            str: The logs from the pod.
+        """
+        pass
