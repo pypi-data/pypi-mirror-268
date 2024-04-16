23f860eb941e260fbce626eac746fc06afdf39c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:45 +0000

    Add package pyBlindRL/0.0.2rc60.post1/pyblindrl-0.0.2rc60.post1.tar.gz

---
+++ packages/pyBlindRL/pyblindrl-0.0.2rc60.post1.tar.gz/pyblindrl-0.0.2rc60.post1/tests/test_methods.py
@@ -0,0 +1,11 @@
+#   ---------------------------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   ---------------------------------------------------------------------------------
+"""This is a sample python file for testing functions from the source code."""
+from __future__ import annotations
+
+# from pyBlindRL.hello_world import hello_world
+
+def test():
+    return
