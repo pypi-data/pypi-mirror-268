c89fe36a9fd7c63a52617a3ebd2c065c0c62ce12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:01 +0000

    Add package ths-mongo-training-tools/0.1.6/ths_mongo_training_tools-0.1.6.tar.gz

---
+++ packages/ths-mongo-training-tools/ths_mongo_training_tools-0.1.6.tar.gz/ths_mongo_training_tools-0.1.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ths-mongo-training-tools',
+    version='0.1.6',
+    author='Tim Zhou',
+    author_email='zhouyuntao110@gmail.com',
+    description='Speech training status control',
+    packages=find_packages(),
+    install_requires=[
+        'pymongo'
+    ],
+    platforms=["all"],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
