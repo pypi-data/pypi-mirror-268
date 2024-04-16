8aea367c0f7cd30e39832817c0766b326e9f6a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 04:03:15 +0000

    Add package pyDataverseOps/0.1.0/PyDataverseOps-0.1.0.tar.gz

---
+++ packages/pyDataverseOps/PyDataverseOps-0.1.0.tar.gz/PyDataverseOps-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PyDataverseOps",
+    version="0.1.0",
+    author="Akash Yadav",
+    author_email="akash21091999@gmail.com",
+    description="A Python library to facilitate operations on Microsoft Dataverse",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ray2199/PyDataverseOps.git",  # Replace with your repository URL
+    packages=find_packages(),
+    install_requires=["pandas>=1.0", "requests>=2.20"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
