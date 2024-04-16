5f26cdb20d4faeab9259b4d68230b8f54b12c04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:03:38 +0000

    Add package ssb-datadoc/0.10.0/ssb_datadoc-0.10.0.tar.gz

---
+++ packages/ssb-datadoc/ssb_datadoc-0.10.0.tar.gz/ssb_datadoc-0.10.0/src/datadoc/frontend/callbacks/register_callbacks.py
@@ -0,0 +1,367 @@
+"""All decorated callback functions should be defined here.
+
+Implementations of the callback functionality should be in other functions (in other files), to enable unit testing.
+"""
+
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+from dash import ALL
+from dash import MATCH
+from dash import Dash
+from dash import Input
+from dash import Output
+from dash import State
+from dash import ctx
+
+from datadoc import state
+from datadoc.enums import SupportedLanguages
+from datadoc.frontend.callbacks.dataset import accept_dataset_metadata_date_input
+from datadoc.frontend.callbacks.dataset import accept_dataset_metadata_input
+from datadoc.frontend.callbacks.dataset import open_dataset_handling
+from datadoc.frontend.callbacks.utils import update_global_language_state
+from datadoc.frontend.callbacks.variables import accept_variable_metadata_date_input
+from datadoc.frontend.callbacks.variables import accept_variable_metadata_input
+from datadoc.frontend.callbacks.variables import populate_variables_workspace
+from datadoc.frontend.components.builders import build_dataset_edit_section
+from datadoc.frontend.components.dataset_tab import SECTION_WRAPPER_ID
+from datadoc.frontend.components.variables_tab import ACCORDION_WRAPPER_ID
+from datadoc.frontend.components.variables_tab import VARIABLES_INFORMATION_ID
+from datadoc.frontend.fields.display_base import DATASET_METADATA_DATE_INPUT
+from datadoc.frontend.fields.display_base import DATASET_METADATA_INPUT
+from datadoc.frontend.fields.display_base import VARIABLES_METADATA_DATE_INPUT
+from datadoc.frontend.fields.display_base import VARIABLES_METADATA_INPUT
+from datadoc.frontend.fields.display_dataset import NON_EDITABLE_DATASET_METADATA
+from datadoc.frontend.fields.display_dataset import OBLIGATORY_EDITABLE_DATASET_METADATA
+from datadoc.frontend.fields.display_dataset import OPTIONAL_DATASET_METADATA
+from datadoc.frontend.fields.display_dataset import DatasetIdentifiers
+from datadoc.frontend.fields.display_variables import VariableIdentifiers
+
+if TYPE_CHECKING:
+    import dash_bootstrap_components as dbc
+
+    from datadoc.frontend.callbacks.utils import MetadataInputTypes
+
+logger = logging.getLogger(__name__)
+
+
+def register_callbacks(app: Dash) -> None:
+    """Define and register callbacks."""
+
+    @app.callback(
+        Output("progress-bar", "value"),
+        Output("progress-bar", "label"),
+        Input({"type": DATASET_METADATA_INPUT, "id": ALL}, "value"),
+        Input(
+            {
+                "type": VARIABLES_METADATA_INPUT,
+                "variable_short_name": ALL,
+                "id": ALL,
+            },
+            "value",
+        ),
+    )
+    def callback_update_progress(
+        value: MetadataInputTypes,  # noqa: ARG001 argument required by Dash
+        data: list[dict],  # noqa: ARG001 argument required by Dash
+    ) -> tuple[int, str]:
+        """Update the progress bar when new data is entered."""
+        completion = state.metadata.percent_complete
+        return completion, f"{completion}%"
+
+    @app.callback(
+        Output("saved-metadata-success", "is_open"),
+        Input("save-button", "n_clicks"),
+        prevent_initial_call=True,
+    )
+    def callback_save_metadata_file(n_clicks: int) -> bool:
+        """Save the metadata document to disk."""
+        if n_clicks and n_clicks > 0:
+            state.metadata.write_metadata_document()
+            return True
+
+        return False
+
+    @app.callback(
+        Output("dataset-validation-error", "is_open"),
+        Output("dataset-validation-explanation", "children"),
+        Input({"type": DATASET_METADATA_INPUT, "id": ALL}, "value"),
+        prevent_initial_call=True,
+    )
+    def callback_accept_dataset_metadata_input(
+        value: MetadataInputTypes,  # noqa: ARG001 argument required by Dash
+    ) -> tuple[bool, str]:
+        """Save updated dataset metadata values.
+
+        Will display an alert if validation fails.
+        """
+        # Get the ID of the input that changed. This MUST match the attribute name defined in DataDocDataSet
+        return accept_dataset_metadata_input(
+            ctx.triggered[0]["value"],
+            ctx.triggered_id["id"],
+        )
+
+    @app.callback(
+        Output("opened-dataset-success", "is_open"),
+        Output("opened-dataset-error", "is_open"),
+        Output("opened-dataset_warning", "is_open"),
+        Output("opened-dataset-error-explanation", "children"),
+        Output("language-dropdown", "value"),  # Used to force reload of metadata
+        Input("open-button", "n_clicks"),
+        State("dataset-path-input", "value"),
+    )
+    def callback_open_dataset(
+        n_clicks: int,
+        dataset_path: str,
+    ) -> tuple[bool, bool, bool, str, str]:
+        """Open a dataset.
+
+        Shows an alert on success or failure.
+
+        To trigger reload of data in the UI, we update the
+        language dropdown. This is a hack and could be replaced
+        by a more formal mechanism.
+        """
+        return open_dataset_handling(n_clicks, dataset_path)
+
+    @app.callback(
+        Output(VARIABLES_INFORMATION_ID, "children"),
+        Input("language-dropdown", "value"),
+        prevent_initial_call=True,
+    )
+    def callback_populate_variables_info_section(
+        language: str,  # noqa: ARG001 Dash requires arguments for all Inputs
+    ) -> str:
+        return f"Datasettet inneholder {len(state.metadata.variables)} variabler."
+
+    @app.callback(
+        Output(ACCORDION_WRAPPER_ID, "children"),
+        Input("language-dropdown", "value"),
+        Input("search-variables", "value"),
+        prevent_initial_call=True,
+    )
+    def callback_populate_variables_workspace(
+        language: str,
+        search_query: str,
+    ) -> list:
+        """Create variable workspace with accordions for variables.
+
+        Allows for filtering which variables are displayed via the search box.
+        """
+        update_global_language_state(SupportedLanguages(language))
+        logger.debug("Populating variables workspace. Search query: %s", search_query)
+        return populate_variables_workspace(
+            state.metadata.variables,
+            state.current_metadata_language,
+            search_query,
+        )
+
+    @app.callback(
+        Output(SECTION_WRAPPER_ID, "children"),
+        Input("language-dropdown", "value"),
+        State("open-button", "n_clicks"),
+        prevent_initial_call=True,
+    )
+    def callback_populate_dataset_workspace(
+        language: str,
+        n_clicks: int,
+    ) -> list:
+        """Create dataset workspace with sections."""
+        update_global_language_state(SupportedLanguages(language))
+
+        logger.debug("Populating dataset workspace")
+        return [
+            build_dataset_edit_section(
+                "Obligatorisk",
+                OBLIGATORY_EDITABLE_DATASET_METADATA,
+                state.current_metadata_language,
+                state.metadata.dataset,
+                {
+                    "type": "dataset-edit-section",
+                    "id": f"obligatory-{language}-{n_clicks}",
+                },
+            ),
+            build_dataset_edit_section(
+                "Anbefalt",
+                OPTIONAL_DATASET_METADATA,
+                state.current_metadata_language,
+                state.metadata.dataset,
+                {
+                    "type": "dataset-edit-section",
+                    "id": f"recommended-{language}-{n_clicks}",
+                },
+            ),
+            build_dataset_edit_section(
+                "Maskingenerert",
+                NON_EDITABLE_DATASET_METADATA,
+                state.current_metadata_language,
+                state.metadata.dataset,
+                {
+                    "type": "dataset-edit-section",
+                    "id": f"machine-{language}-{n_clicks}",
+                },
+            ),
+        ]
+
+    @app.callback(
+        Output(
+            {
+                "type": VARIABLES_METADATA_INPUT,
+                "variable_short_name": MATCH,
+                "id": MATCH,
+            },
+            "error",
+        ),
+        Output(
+            {
+                "type": VARIABLES_METADATA_INPUT,
+                "variable_short_name": MATCH,
+                "id": MATCH,
+            },
+            "errorMessage",
+        ),
+        Input(
+            {
+                "type": VARIABLES_METADATA_INPUT,
+                "variable_short_name": MATCH,
+                "id": MATCH,
+            },
+            "value",
+        ),
+        prevent_initial_call=True,
+    )
+    def callback_accept_variable_metadata_input(
+        value: MetadataInputTypes,  # noqa: ARG001 argument required by Dash
+    ) -> dbc.Alert:
+        """Save updated variable metadata values."""
+        message = accept_variable_metadata_input(
+            ctx.triggered[0]["value"],
+            ctx.triggered_id["variable_short_name"],
+            ctx.triggered_id["id"],
+        )
+        if not message:
+            # No error to display.
+            return False, ""
+
+        return True, message
+
+    @app.callback(
+        Output(
+            {
+                "type": VARIABLES_METADATA_DATE_INPUT,
+                "variable_short_name": MATCH,
+                "id": VariableIdentifiers.CONTAINS_DATA_FROM.value,
+            },
+            "error",
+        ),
+        Output(
+            {
+                "type": VARIABLES_METADATA_DATE_INPUT,
+                "variable_short_name": MATCH,
+                "id": VariableIdentifiers.CONTAINS_DATA_FROM.value,
+            },
+            "errorMessage",
+        ),
+        Output(
+            {
+                "type": VARIABLES_METADATA_DATE_INPUT,
+                "variable_short_name": MATCH,
+                "id": VariableIdentifiers.CONTAINS_DATA_UNTIL.value,
+            },
+            "error",
+        ),
+        Output(
+            {
+                "type": VARIABLES_METADATA_DATE_INPUT,
+                "variable_short_name": MATCH,
+                "id": VariableIdentifiers.CONTAINS_DATA_UNTIL.value,
+            },
+            "errorMessage",
+        ),
+        Input(
+            {
+                "type": VARIABLES_METADATA_DATE_INPUT,
+                "variable_short_name": MATCH,
+                "id": VariableIdentifiers.CONTAINS_DATA_FROM.value,
+            },
+            "value",
+        ),
+        Input(
+            {
+                "type": VARIABLES_METADATA_DATE_INPUT,
+                "variable_short_name": MATCH,
+                "id": VariableIdentifiers.CONTAINS_DATA_UNTIL.value,
+            },
+            "value",
+        ),
+        prevent_initial_call=True,
+    )
+    def callback_accept_variable_metadata_date_input(
+        contains_data_from: str,
+        contains_data_until: str,
+    ) -> dbc.Alert:
+        """Special case handling for date fields which have a relationship to one another."""
+        return accept_variable_metadata_date_input(
+            VariableIdentifiers(ctx.triggered_id["id"]),
+            ctx.triggered_id["variable_short_name"],
+            contains_data_from,
+            contains_data_until,
+        )
+
+    @app.callback(
+        Output(
+            {
+                "type": DATASET_METADATA_DATE_INPUT,
+                "id": DatasetIdentifiers.CONTAINS_DATA_FROM.value,
+            },
+            "error",
+        ),
+        Output(
+            {
+                "type": DATASET_METADATA_DATE_INPUT,
+                "id": DatasetIdentifiers.CONTAINS_DATA_FROM.value,
+            },
+            "errorMessage",
+        ),
+        Output(
+            {
+                "type": DATASET_METADATA_DATE_INPUT,
+                "id": DatasetIdentifiers.CONTAINS_DATA_UNTIL.value,
+            },
+            "error",
+        ),
+        Output(
+            {
+                "type": DATASET_METADATA_DATE_INPUT,
+                "id": DatasetIdentifiers.CONTAINS_DATA_UNTIL.value,
+            },
+            "errorMessage",
+        ),
+        Input(
+            {
+                "type": DATASET_METADATA_DATE_INPUT,
+                "id": DatasetIdentifiers.CONTAINS_DATA_FROM.value,
+            },
+            "value",
+        ),
+        Input(
+            {
+                "type": DATASET_METADATA_DATE_INPUT,
+                "id": DatasetIdentifiers.CONTAINS_DATA_UNTIL.value,
+            },
+            "value",
+        ),
+        prevent_initial_call=True,
+    )
+    def callback_accept_dataset_metadata_date_input(
+        contains_data_from: str,
+        contains_data_until: str,
+    ) -> dbc.Alert:
+        """Special case handling for date fields which have a relationship to one another."""
+        return accept_dataset_metadata_date_input(
+            DatasetIdentifiers(ctx.triggered_id["id"]),
+            contains_data_from,
+            contains_data_until,
+        )
---
+++ packages/ssb-datadoc/ssb_datadoc-0.10.0.tar.gz/ssb_datadoc-0.10.0/src/datadoc/frontend/callbacks/variables.py
@@ -0,0 +1,225 @@
+"""Callback functions to do with variables metadata."""
+
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+from pydantic import ValidationError
+
+from datadoc import state
+from datadoc.frontend.callbacks.utils import MetadataInputTypes
+from datadoc.frontend.callbacks.utils import find_existing_language_string
+from datadoc.frontend.callbacks.utils import parse_and_validate_dates
+from datadoc.frontend.components.builders import build_edit_section
+from datadoc.frontend.components.builders import build_ssb_accordion
+from datadoc.frontend.fields.display_variables import (
+    MULTIPLE_LANGUAGE_VARIABLES_METADATA,
+)
+from datadoc.frontend.fields.display_variables import OBLIGATORY_VARIABLES_METADATA
+from datadoc.frontend.fields.display_variables import OPTIONAL_VARIABLES_METADATA
+from datadoc.frontend.fields.display_variables import VariableIdentifiers
+
+if TYPE_CHECKING:
+    from datadoc_model import model
+    from datadoc_model.model import LanguageStringType
+
+    from datadoc.enums import SupportedLanguages
+
+logger = logging.getLogger(__name__)
+
+
+def populate_variables_workspace(
+    variables: list[model.Variable],
+    language: SupportedLanguages,
+    search_query: str,
+) -> list:
+    """Create variable workspace with accordions for variables.
+
+    Allows for filtering which variables are displayed via the search box.
+    """
+    return [
+        build_ssb_accordion(
+            variable.short_name or "",
+            {
+                "type": "variables-accordion",
+                "id": f"{variable.short_name}-{language.value}",  # Insert language into the ID to invalidate browser caches
+            },
+            variable.short_name or "",
+            children=[
+                build_edit_section(
+                    OBLIGATORY_VARIABLES_METADATA,
+                    "Obligatorisk",
+                    variable,
+                    language.value,
+                ),
+                build_edit_section(
+                    OPTIONAL_VARIABLES_METADATA,
+                    "Anbefalt",
+                    variable,
+                    language.value,
+                ),
+            ],
+        )
+        for variable in variables
+        if search_query in (variable.short_name or "")
+    ]
+
+
+def handle_multi_language_metadata(
+    metadata_field: str,
+    new_value: MetadataInputTypes | LanguageStringType,
+    updated_row_id: str,
+) -> MetadataInputTypes | LanguageStringType:
+    """Handle updates to fields which support multiple languages."""
+    if new_value is None:
+        # This edge case occurs when the user removes the text in an input field
+        # We want to ensure we only remove the content for the current language,
+        # not create a new blank object!
+        return find_existing_language_string(
+            state.metadata.variables_lookup[updated_row_id],
+            "",
+            metadata_field,
+        )
+
+    if isinstance(new_value, str):
+        return find_existing_language_string(
+            state.metadata.variables_lookup[updated_row_id],
+            new_value,
+            metadata_field,
+        )
+
+    return new_value
+
+
+def accept_variable_metadata_input(
+    value: MetadataInputTypes,
+    variable_short_name: str,
+    metadata_field: str,
+) -> str | None:
+    """Validate and save the value when variable metadata is updated.
+
+    Returns an error message if an exception was raised, otherwise returns None.
+    """
+    logger.debug(
+        "Updating %s, %s with %s",
+        metadata_field,
+        variable_short_name,
+        value,
+    )
+    try:
+        if metadata_field in MULTIPLE_LANGUAGE_VARIABLES_METADATA:
+            new_value = handle_multi_language_metadata(
+                metadata_field,
+                value,
+                variable_short_name,
+            )
+        elif value == "":
+            # Allow clearing non-multiple-language text fields
+            new_value = None
+        else:
+            new_value = value
+
+        # Write the value to the variables structure
+        setattr(
+            state.metadata.variables_lookup[variable_short_name],
+            metadata_field,
+            new_value,
+        )
+    except (ValidationError, ValueError) as e:
+        logger.exception(
+            "Validation failed for %s, %s, %s:",
+            metadata_field,
+            variable_short_name,
+            value,
+        )
+        return str(e)
+    else:
+        if value == "":
+            value = None
+        logger.debug(
+            "Successfully updated %s, %s with %s",
+            metadata_field,
+            variable_short_name,
+            value,
+        )
+        return None
+
+
+def accept_variable_metadata_date_input(
+    variable_identifier: VariableIdentifiers,
+    variable_short_name: str,
+    contains_data_from: str,
+    contains_data_until: str,
+) -> tuple[bool, str, bool, str]:
+    """Validate and save date range inputs."""
+    message = accept_variable_metadata_input(
+        (
+            contains_data_from
+            if variable_identifier == VariableIdentifiers.CONTAINS_DATA_FROM
+            else contains_data_until
+        ),
+        variable_short_name,
+        variable_identifier,
+    )
+
+    try:
+        (
+            parsed_contains_data_from,
+            parsed_contains_data_until,
+        ) = parse_and_validate_dates(
+            str(
+                contains_data_from
+                or state.metadata.variables_lookup[
+                    variable_short_name
+                ].contains_data_from,
+            ),
+            str(
+                contains_data_until
+                or state.metadata.variables_lookup[
+                    variable_short_name
+                ].contains_data_until,
+            ),
+        )
+
+        # Save both values to the model if they pass validation.
+        state.metadata.variables_lookup[
+            variable_short_name
+        ].contains_data_from = parsed_contains_data_from
+        state.metadata.variables_lookup[
+            variable_short_name
+        ].contains_data_until = parsed_contains_data_until
+    except (ValidationError, ValueError) as e:
+        logger.exception(
+            "Validation failed for %s, %s, %s: %s, %s: %s",
+            variable_identifier,
+            variable_short_name,
+            "contains_data_from",
+            contains_data_from,
+            "contains_data_until",
+            contains_data_until,
+        )
+        message = str(e)
+    else:
+        logger.debug(
+            "Successfully updated %s, %s, %s: %s, %s: %s",
+            variable_identifier,
+            variable_short_name,
+            "contains_data_from",
+            contains_data_from,
+            "contains_data_until",
+            contains_data_until,
+        )
+        message = None
+
+    no_error = (False, "")
+    if not message:
+        # No error to display.
+        return no_error + no_error
+
+    error = (True, message)
+    return (
+        error + no_error
+        if variable_identifier == VariableIdentifiers.CONTAINS_DATA_FROM
+        else no_error + error
+    )
