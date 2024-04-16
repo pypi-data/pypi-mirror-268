71a173d3bdb40c1be772678e8d307b6e27b2455d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:03:51 +0000

    Add package switchboard-hw/0.0.40/switchboard_hw-0.0.40-cp312-cp312-macosx_10_13_x86_64.whl

---
+++ packages/switchboard-hw/switchboard_hw-0.0.40-cp312-cp312-macosx_10_13_x86_64.whl/switchboard/verilog/sim/sb_tx_sim.sv
@@ -0,0 +1,53 @@
+// Wrapper module for backwards compatibility (will eventually be removed)
+
+// Copyright (c) 2023 Zero ASIC Corporation
+// This code is licensed under Apache License 2.0 (see LICENSE for details)
+
+`default_nettype none
+
+module sb_tx_sim #(
+    parameter integer READY_MODE_DEFAULT=0,
+    parameter integer DW=416
+) (
+    input clk,
+    input [DW-1:0] data,
+    input [31:0] dest,
+    input last,
+    output ready,
+    input valid
+);
+    `ifdef __ICARUS__
+        `define SB_START_FUNC task
+        `define SB_END_FUNC endtask
+    `else
+        `define SB_START_FUNC function void
+        `define SB_END_FUNC endfunction
+    `endif
+
+    sb_to_queue_sim #(
+        .READY_MODE_DEFAULT(READY_MODE_DEFAULT),
+        .DW(DW)
+    ) tx_i (
+        .*
+    );
+
+    `SB_START_FUNC init(input string uri);
+        /* verilator lint_off IGNOREDRETURN */
+        tx_i.init(uri);
+        /* verilator lint_on IGNOREDRETURN */
+    `SB_END_FUNC
+
+    `SB_START_FUNC set_ready_mode(input integer value);
+        /* verilator lint_off IGNOREDRETURN */
+        tx_i.set_ready_mode(value);
+        /* verilator lint_on IGNOREDRETURN */
+    `SB_END_FUNC
+
+    // clean up macros
+
+    `undef SB_START_FUNC
+    `undef SB_END_FUNC
+
+endmodule
+
+`default_nettype wire
