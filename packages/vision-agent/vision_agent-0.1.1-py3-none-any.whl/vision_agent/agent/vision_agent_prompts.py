c7d781ac4f87494351c2e2b91963afe1c52668d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:26 +0000

    Add package vision-agent/0.1.1/vision_agent-0.1.1-py3-none-any.whl

---
+++ packages/vision-agent/vision_agent-0.1.1-py3-none-any.whl/vision_agent/agent/vision_agent_prompts.py
@@ -0,0 +1,130 @@
+VISION_AGENT_REFLECTION = """You are an advanced reasoning agent that can improve based on self-refection. You will be given a previous reasoning trial in which you were given the user's question, the available tools that the agent has, the decomposed tasks and tools that the agent used to answer the question and the final answer the agent provided. You may also receive an image with the visualized bounding boxes or masks with their associated labels and scores from the tools used. You must determine if the agent's answer was correct or incorrect. If the agent's answer was correct, respond with Finish. If the agent's answer was incorrect, you must diagnose a possible reason for failure or phrasing discrepancy and devise a new, concise, concrete plan that aims to mitigate the same failure with the tools available. Do not make vague steps like re-evaluate the threshold, instead make concrete steps like use a threshold of 0.5 or whatever threshold you think would fix this issue. If the task cannot be completed with the existing tools, respond with Finish. Use complete sentences.
+
+User's question: {question}
+
+Tools available:
+{tools}
+
+Tasks and tools used:
+{tool_results}
+
+Final answer:
+{final_answer}
+
+Reflection: """
+
+TASK_DECOMPOSE = """You need to decompose a user's complex question into some simple subtasks and let the model execute it step by step.
+This is the user's question: {question}
+This is the tool list:
+{tools}
+
+Please note that:
+1. You should only decompose this user's complex question into some simple subtasks which can be executed easily by using one single tool in the tool list.
+2. If one subtask needs the results from another subtask, you should write clearly. For example:
+{{"Tasks": ["Convert 23 km/h to X km/min by 'divide_'", "Multiply X km/min by 45 min to get Y by 'multiply_'"]}}
+3. You must ONLY output in a parsible JSON format. An example output looks like:
+
+{{"Tasks": ["Task 1", "Task 2", ...]}}
+
+Output: """
+
+TASK_DECOMPOSE_DEPENDS = """You need to decompose a user's complex question into some simple subtasks and let the model execute it step by step.
+This is the user's question: {question}
+
+This is the tool list:
+{tools}
+
+This is a reflection from a previous failed attempt:
+{reflections}
+
+Please note that:
+1. You should only decompose this user's complex question into some simple subtasks which can be executed easily by using one single tool in the tool list.
+2. If one subtask needs the results from another subtask, you should write clearly. For example:
+{{"Tasks": ["Convert 23 km/h to X km/min by 'divide_'", "Multiply X km/min by 45 min to get Y by 'multiply_'"]}}
+3. You must ONLY output in a parsible JSON format. An example output looks like:
+
+{{"Tasks": ["Task 1", "Task 2", ...]}}
+
+Output: """
+
+CHOOSE_TOOL = """This is the user's question: {question}
+These are the tools you can select to solve the question:
+{tools}
+
+Please note that:
+1. You should only choose one tool from the Tool List to solve this question.
+2. You must ONLY output the ID of the tool you chose in a parsible JSON format. Two example outputs look like:
+
+Example 1: {{"ID": 1}}
+Example 2: {{"ID": 2}}
+
+Output: """
+
+CHOOSE_TOOL_DEPENDS = """This is the user's question: {question}
+These are the tools you can select to solve the question:
+{tools}
+
+This is a reflection from a previous failed attempt:
+{reflections}
+
+Please note that:
+1. You should only choose one tool from the Tool List to solve this question.
+2. You must ONLY output the ID of the tool you chose in a parsible JSON format. Two example outputs look like:
+
+Example 1: {{"ID": 1}}
+Example 2: {{"ID": 2}}
+
+Output: """
+
+CHOOSE_PARAMETER_DEPENDS = """Given a user's question and an API tool documentation, you need to output parameters according to the API tool documentation to successfully call the API to solve the user's question.
+Please note that:
+1. The Example in the API tool documentation can help you better understand the use of the API.
+2. Ensure the parameters you output are correct. The output must contain the required parameters, and can contain the optional parameters based on the question. If there are no paremters in the required parameters and optional parameters, just leave it as {{"Parameters":{{}}}}
+3. If the user's question mentions other APIs, you should ONLY consider the API tool documentation I give and do not consider other APIs.
+4. The question may have dependencies on answers of other questions, so we will provide logs of previous questions and answers for your reference.
+5. If you need to use this API multiple times, please set "Parameters" to a list.
+6. You must ONLY output in a parsible JSON format. Two example outputs look like:
+
+Example 1: {{"Parameters":{{"input": [1,2,3]}}}}
+Example 2: {{"Parameters":[{{"input": [1,2,3]}}, {{"input": [2,3,4]}}]}}
+
+This is a reflection from a previous failed attempt:
+{reflections}
+
+These are logs of previous questions and answers:
+{previous_log}
+
+This is the current user's question: {question}
+This is the API tool documentation: {tool_usage}
+Output: """
+
+ANSWER_GENERATE_DEPENDS = """You should answer the question based on the response output by the API tool.
+Please note that:
+1. You should try to organize the response into a natural language answer.
+2. We will not show the API response to the user, thus you need to make full use of the response and give the information in the response that can satisfy the user's question in as much detail as possible.
+3. If the API tool does not provide useful information in the response, please answer with your knowledge.
+4. The question may have dependencies on answers of other questions, so we will provide logs of previous questions and answers.
+
+This is a reflection from a previous failed attempt:
+{reflections}
+
+These are logs of previous questions and answers:
+{previous_log}
+
+This is the user's question: {question}
+
+This is the response output by the API tool:
+{call_results}
+
+We will not show the API response to the user, thus you need to make full use of the response and give the information in the response that can satisfy the user's question in as much detail as possible.
+Output: """
+
+ANSWER_SUMMARIZE_DEPENDS = """We break down a user's complex problems into simple subtasks and provide answers to each simple subtask. You need to organize these answers to each subtask and form a self-consistent final answer to the user's question
+This is the user's question: {question}
+
+These are subtasks and their answers:
+{answers}
+
+This is a reflection from a previous failed attempt:
+{reflections}
+Final answer: """
