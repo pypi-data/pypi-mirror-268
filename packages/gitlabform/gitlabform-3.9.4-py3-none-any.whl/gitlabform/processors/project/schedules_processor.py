8242a01bb665ed0074f63d6e9cbd725c62b9308d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:02:51 +0000

    Add package gitlabform/3.9.4/gitlabform-3.9.4-py3-none-any.whl

---
+++ packages/gitlabform/gitlabform-3.9.4-py3-none-any.whl/gitlabform/processors/project/schedules_processor.py
@@ -0,0 +1,143 @@
+from logging import debug
+from typing import Dict, List
+
+from gitlabform.gitlab import GitLab
+from gitlabform.processors.abstract_processor import AbstractProcessor
+
+
+class SchedulesProcessor(AbstractProcessor):
+    def __init__(self, gitlab: GitLab):
+        super().__init__("schedules", gitlab)
+
+    def _process_configuration(self, project_and_group: str, configuration: dict):
+        configured_schedules = configuration.get("schedules", {})
+        enforce_schedules = configuration.get("schedules|enforce", False)
+
+        # Remove 'enforce' key from the config so that it's not treated as a "schedule"
+        if enforce_schedules:
+            configured_schedules.pop("enforce")
+
+        existing_schedules = self.gitlab.get_all_pipeline_schedules(project_and_group)
+        schedule_ids_by_description = self.__group_schedule_ids_by_description(
+            existing_schedules
+        )
+
+        for schedule_description in sorted(configured_schedules):
+            schedule_ids = schedule_ids_by_description.get(schedule_description)
+
+            if configured_schedules[schedule_description].get("delete"):
+                if schedule_ids:
+                    debug("Deleting pipeline schedules '%s'", schedule_description)
+                    for schedule_id in schedule_ids:
+                        self.gitlab.delete_pipeline_schedule(
+                            project_and_group, schedule_id
+                        )
+                else:
+                    debug(
+                        "Not deleting pipeline schedules '%s', because none exist",
+                        schedule_description,
+                    )
+            else:
+                if schedule_ids and len(schedule_ids) == 1:
+                    debug(
+                        "Changing existing pipeline schedule '%s'", schedule_description
+                    )
+                    schedule_id = schedule_ids[0]
+                    self.gitlab.take_ownership(project_and_group, schedule_id)
+                    self.gitlab.update_pipeline_schedule(
+                        project_and_group,
+                        schedule_id,
+                        configuration.get("schedules|" + schedule_description),
+                    )
+                    self.__set_schedule_variables(
+                        project_and_group,
+                        schedule_id,
+                        configuration.get(
+                            "schedules|" + schedule_description + "|variables"
+                        ),
+                    )
+                elif schedule_ids:
+                    debug(
+                        "Replacing existing pipeline schedules '%s'",
+                        schedule_description,
+                    )
+                    for schedule_id in schedule_ids:
+                        self.gitlab.delete_pipeline_schedule(
+                            project_and_group, schedule_id
+                        )
+                    self.create_schedule_with_variables(
+                        configuration, project_and_group, schedule_description
+                    )
+                else:
+                    debug("Creating pipeline schedule '%s'", schedule_description)
+                    self.create_schedule_with_variables(
+                        configuration, project_and_group, schedule_description
+                    )
+
+        if enforce_schedules:
+            debug("Delete unconfigured schedules because enforce is enabled")
+
+            for schedule in existing_schedules:
+                schedule_description = schedule["description"]
+                schedule_id = schedule["id"]
+
+                debug(f"processing {schedule_id}: {schedule_description}")
+                if schedule_description not in configured_schedules:
+                    debug(
+                        "Deleting pipeline schedule named '%s', because it is not in gitlabform configuration",
+                        schedule_description,
+                    )
+                    self.gitlab.delete_pipeline_schedule(project_and_group, schedule_id)
+
+    def create_schedule_with_variables(
+        self, configuration, project_and_group, schedule_description
+    ):
+        data = configuration.get("schedules|" + schedule_description)
+        created_schedule = self.gitlab.create_pipeline_schedule(
+            project_and_group,
+            schedule_description,
+            data.get("ref"),
+            data.get("cron"),
+            optional_data=data,
+        )
+        self.__set_schedule_variables(
+            project_and_group,
+            created_schedule.get("id"),
+            configuration.get("schedules|" + schedule_description + "|variables"),
+        )
+
+    def __set_schedule_variables(self, project_and_group, schedule_id, variables):
+        schedule = self.gitlab.get_pipeline_schedule(project_and_group, schedule_id)
+
+        existing_variables = schedule.get("variables")
+        if existing_variables:
+            debug(
+                "Deleting variables for pipeline schedule '%s'", schedule["description"]
+            )
+
+            for variable in existing_variables:
+                self.gitlab.delete_pipeline_schedule_variable(
+                    project_and_group, schedule_id, variable.get("key")
+                )
+
+        if variables:
+            for variable_key, variable_data in variables.items():
+                self.gitlab.create_pipeline_schedule_variable(
+                    project_and_group,
+                    schedule_id,
+                    variable_key,
+                    variable_data.get("value"),
+                    variable_data,
+                )
+
+    @staticmethod
+    def __group_schedule_ids_by_description(schedules) -> Dict[str, List[str]]:
+        schedule_ids_by_description: Dict[str, List[str]] = {}
+
+        for schedule in schedules:
+            description = schedule["description"]
+            schedule_ids_by_description.setdefault(description, []).append(
+                schedule["id"]
+            )
+
+        return schedule_ids_by_description
---
+++ packages/gitlabform/gitlabform-3.9.4-py3-none-any.whl/tests/acceptance/standard/test_schedules.py
@@ -0,0 +1,306 @@
+import pytest
+
+
+from tests.acceptance import run_gitlabform
+
+
+@pytest.fixture(scope="class")
+def schedules(project):
+    another_branch = "scheduled/new-feature"
+    project.branches.create({"branch": another_branch, "ref": "main"})
+
+    # fmt: off
+    schedules = [
+        ("Existing schedule", "main", "0 * * * *", True),
+        ("Existing schedule with vars", "main", "30 * * * *", False),
+        ("Existing schedule to replace", "main", "30 1 * * *", False),
+        ("Existing schedule to replace", another_branch, "30 2 * * *", False),
+    ]
+    # fmt: on
+
+    gitlab_schedules = []
+
+    for schedule in schedules:
+        gitlab_schedule = project.pipelineschedules.create(
+            {
+                "description": schedule[0],
+                "ref": schedule[1],
+                "cron": schedule[2],
+                "active": schedule[3],
+            }
+        )
+        gitlab_schedules.append(gitlab_schedule)
+
+    redundant_schedule = project.pipelineschedules.create(
+        {
+            "description": "Redundant schedule",
+            "ref": "main",
+            "cron": "0 * * * *",
+        }
+    )
+    redundant_schedule.variables.create({"key": "test_variable", "value": "test_value"})
+
+    schedules.append(redundant_schedule)
+
+    return schedules  # provide fixture value
+
+
+class TestSchedules:
+    def test__add_new_schedule(self, project, schedules):
+        add_schedule = f"""
+        projects_and_groups:
+          {project.path_with_namespace}:
+            schedules:
+              "New schedule":
+                ref: main
+                cron: "0 * * * *"
+                cron_timezone: "London"
+                active: true
+        """
+
+        run_gitlabform(add_schedule, project)
+
+        schedule = self.__find_pipeline_schedule_by_description_and_get_first(
+            project, "New schedule"
+        )
+        assert schedule is not None
+        assert schedule.description == "New schedule"
+        assert schedule.ref == "main"
+        assert schedule.cron == "0 * * * *"
+        assert schedule.cron_timezone == "London"
+        assert schedule.active is True
+
+    def test__add_new_schedule_with_mandatory_fields_only(self, project, schedules):
+        add_schedule_mandatory_fields_only = f"""
+        projects_and_groups:
+          {project.path_with_namespace}:
+            schedules:
+              "New schedule with mandatory fields":
+                ref: main
+                cron: "30 1 * * *"
+        """
+
+        run_gitlabform(add_schedule_mandatory_fields_only, project)
+
+        schedule = self.__find_pipeline_schedule_by_description_and_get_first(
+            project, "New schedule with mandatory fields"
+        )
+        assert schedule is not None
+        assert schedule.description == "New schedule with mandatory fields"
+        assert schedule.ref == "main"
+        assert schedule.cron == "30 1 * * *"
+        assert schedule.cron_timezone == "UTC"
+        assert schedule.active is True
+
+    def test__set_schedule_variables(self, project, schedules):
+        add_schedule_with_variables = f"""
+        projects_and_groups:
+          {project.path_with_namespace}:
+            schedules:
+              "New schedule with variables":
+                ref: main
+                cron: "30 1 * * *"
+                variables:
+                    var1:
+                        value: value123
+                    var2:
+                        value: value987
+                        variable_type: file
+        """
+
+        run_gitlabform(add_schedule_with_variables, project)
+
+        schedule = self.__find_pipeline_schedule_by_description_and_get_first(
+            project, "New schedule with variables"
+        )
+        assert schedule is not None
+        assert schedule.description == "New schedule with variables"
+        assert schedule.ref == "main"
+        assert schedule.cron == "30 1 * * *"
+        assert schedule.cron_timezone == "UTC"
+        assert schedule.active is True
+
+        variables = schedule.attributes["variables"]
+        assert variables is not None
+        assert len(variables) == 2
+
+        assert variables[0]["variable_type"] == "env_var"
+        assert variables[0]["key"] == "var1"
+        assert variables[0]["value"] == "value123"
+
+        assert variables[1]["variable_type"] == "file"
+        assert variables[1]["key"] == "var2"
+        assert variables[1]["value"] == "value987"
+
+    def test__update_existing_schedule(self, project, schedules):
+        edit_schedule = f"""
+        projects_and_groups:
+          {project.path_with_namespace}:
+            schedules:
+              "Existing schedule":
+                ref: scheduled/new-feature
+                cron: "0 */4 * * *"
+                cron_timezone: "Stockholm"
+                active: false
+        """
+
+        run_gitlabform(edit_schedule, project)
+
+        schedule = self.__find_pipeline_schedule_by_description_and_get_first(
+            project, "Existing schedule"
+        )
+        assert schedule is not None
+        assert schedule.description == "Existing schedule"
+        assert schedule.ref == "scheduled/new-feature"
+        assert schedule.cron == "0 */4 * * *"
+        assert schedule.cron_timezone == "Stockholm"
+        assert schedule.active is False
+
+    def test__replace_existing_schedules(self, project, schedules):
+        replace_schedules = f"""
+        projects_and_groups:
+          {project.path_with_namespace}:
+            schedules:
+              "Existing schedule to replace":
+                ref: scheduled/new-feature
+                cron: "0 */3 * * *"
+                cron_timezone: "London"
+                active: true
+        """
+
+        run_gitlabform(replace_schedules, project)
+
+        existing_schedules = self.__find_pipeline_schedules_by_description(
+            project, "Existing schedule to replace"
+        )
+        assert existing_schedules is not None
+        assert len(existing_schedules) == 1
+
+        schedule = existing_schedules[0]
+        assert schedule.description == "Existing schedule to replace"
+        assert schedule.ref == "scheduled/new-feature"
+        assert schedule.cron == "0 */3 * * *"
+        assert schedule.cron_timezone == "London"
+        assert schedule.active is True
+
+    def test__delete_schedule(self, project, schedules):
+        delete_schedule = f"""
+        projects_and_groups:
+          {project.path_with_namespace}:
+            schedules:
+              "Redundant schedule":
+                delete: True
+        """
+
+        run_gitlabform(delete_schedule, project)
+
+        schedule = self.__find_pipeline_schedule_by_description_and_get_first(
+            project, "Redundant schedule"
+        )
+        assert schedule is None
+
+    def test__schedule_enforce_new_schedule(self, project, schedules):
+        new_schedule = f"""
+        projects_and_groups:
+          {project.path_with_namespace}:
+            schedules:
+              enforce: true
+              "New schedule to test enforce config":
+                ref: main
+                cron: "30 1 * * *"
+        """
+
+        schedules_before = project.pipelineschedules.list()
+
+        run_gitlabform(new_schedule, project)
+
+        schedules_after = project.pipelineschedules.list()
+
+        schedule = self.__find_pipeline_schedule_by_description_and_get_first(
+            project, "New schedule to test enforce config"
+        )
+        assert len(schedules_before) == 6
+        assert len(schedules_after) == 1
+        assert schedule is not None
+        assert schedule.description == "New schedule to test enforce config"
+        assert schedule.ref == "main"
+        assert schedule.cron == "30 1 * * *"
+        assert schedule.cron_timezone == "UTC"
+        assert schedule.active is True
+
+    def test__schedule_enforce_new_and_existing_schedule(self, project, schedules):
+        new_schedule = f"""
+        projects_and_groups:
+          {project.path_with_namespace}:
+            schedules:
+              enforce: true
+              "New schedule to test enforce config":
+                ref: main
+                cron: "30 1 * * *"
+              "New schedule with variables":
+                ref: main
+                cron: "30 1 * * *"
+                variables:
+                    var1:
+                        value: value123
+                    var2:
+                        value: value987
+                        variable_type: file
+        """
+
+        schedules_before = project.pipelineschedules.list()
+
+        run_gitlabform(new_schedule, project)
+
+        schedules_after = project.pipelineschedules.list()
+
+        schedule1 = self.__find_pipeline_schedule_by_description_and_get_first(
+            project, "New schedule to test enforce config"
+        )
+        schedule2 = self.__find_pipeline_schedule_by_description_and_get_first(
+            project, "New schedule with variables"
+        )
+        assert len(schedules_before) == 1
+        assert len(schedules_after) == 2
+
+        assert schedule1 is not None
+        assert schedule1.description == "New schedule to test enforce config"
+        assert schedule1.ref == "main"
+        assert schedule1.cron == "30 1 * * *"
+        assert schedule1.cron_timezone == "UTC"
+        assert schedule1.active is True
+
+        assert schedule2 is not None
+        assert schedule2.description == "New schedule with variables"
+        assert schedule2.ref == "main"
+        assert schedule2.cron == "30 1 * * *"
+        assert schedule2.cron_timezone == "UTC"
+        assert schedule2.active is True
+
+        variables = schedule2.attributes["variables"]
+        assert variables is not None
+        assert len(variables) == 2
+        assert variables[0]["variable_type"] == "env_var"
+        assert variables[0]["key"] == "var1"
+        assert variables[0]["value"] == "value123"
+
+        assert variables[1]["variable_type"] == "file"
+        assert variables[1]["key"] == "var2"
+        assert variables[1]["value"] == "value987"
+
+    @classmethod
+    def __find_pipeline_schedule_by_description_and_get_first(
+        cls, project, description
+    ):
+        try:
+            return cls.__find_pipeline_schedules_by_description(project, description)[0]
+        except IndexError:
+            return None
+
+    @staticmethod
+    def __find_pipeline_schedules_by_description(project, description):
+        return [
+            project.pipelineschedules.get(schedule.id)
+            for schedule in project.pipelineschedules.list()
+            if schedule.description == description
+        ]
