ff3c8be521d52272865c9210d539aaf82aa96ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:04:09 +0000

    Add package gpuwidget/0.1.1/gpuwidget-0.1.1-py3-none-any.whl

---
+++ packages/gpuwidget/gpuwidget-0.1.1-py3-none-any.whl/gpuwidget/core.py
@@ -0,0 +1,43 @@
+import gpustat
+from IPython import get_ipython
+from IPython.display import clear_output, display
+
+from .widgets import GPUStatsApp
+
+
+def is_notebook():
+    try:
+        shell = get_ipython()
+        print("###", shell)
+        if shell == "ZMQInteractiveShell":  # jupyter notebook
+            return True
+        elif shell == "TerminalInteractiveShell":  # iPython
+            return False
+        elif shell == "Shell":  # Google Colab
+            return True
+        else:
+            return False
+    except NameError:
+        return False
+
+
+def live(stop_immediately=False):
+    try:
+        if not is_notebook():
+            print(
+                "gpuwidget.live() only works in a Jupyter Notebook, return ascii version instead."
+            )
+            print(gpustat.new_query().print_formatted())
+            return
+        clear_output(wait=True)
+        widget = GPUStatsApp()
+        if stop_immediately:
+            widget.stop_updating(widget.stop_button)
+        display(widget)
+    except Exception as e:
+        print(e)
+        print("It's likely your runtime does not contains GPU instance.")
+
+
+def once():
+    live(stop_immediately=True)
