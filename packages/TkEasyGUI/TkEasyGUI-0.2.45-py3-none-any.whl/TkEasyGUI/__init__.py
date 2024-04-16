c1c5dcfd00fb92f4d980ef8dc19d511b725dc1d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:24 +0000

    Add package TkEasyGUI/0.2.45/TkEasyGUI-0.2.45-py3-none-any.whl

---
+++ packages/TkEasyGUI/TkEasyGUI-0.2.45-py3-none-any.whl/TkEasyGUI/__init__.py
@@ -0,0 +1,12 @@
+"""
+TkEasyGUI - A simple GUI library for Python using tkinter.
+"""
+
+from TkEasyGUI.dialogs import *  # noqa: F403
+from TkEasyGUI.utils import *  # noqa: F403
+from TkEasyGUI.version import __version__
+from TkEasyGUI.widgets import *  # noqa: F403
+
+if __name__ == "__main__":
+    print(__doc__)
+    print("Version: ", __version__)
