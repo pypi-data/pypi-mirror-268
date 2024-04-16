02356e71edb9750d99fb701b3edcce88f9dc8dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:33 +0000

    Add package tinyticker/0.4.11/tinyticker-0.4.11-py3-none-any.whl

---
+++ packages/tinyticker/tinyticker-0.4.11-py3-none-any.whl/tinyticker/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+from .display import Display
+from .ticker import Ticker
+
+logger = logging.getLogger(__name__)
+
+logger.addHandler(logging.NullHandler())
+
+__version__ = "0.4.11"
+__all__ = ["Display", "Ticker"]
