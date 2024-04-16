3942c938da3c1e845309cb4b0f360731d7fbb096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:37 +0000

    Add package arelle-release/2.25.9/arelle_release-2.25.9-py3-none-any.whl

---
+++ packages/arelle-release/arelle_release-2.25.9-py3-none-any.whl/tests/integration_tests/validation/conformance_suite_configurations/kvk_nt17.py
@@ -0,0 +1,35 @@
+from pathlib import PurePath, Path
+
+from tests.integration_tests.validation.assets import NL_PACKAGES
+from tests.integration_tests.validation.conformance_suite_config import ConformanceSuiteConfig, ConformanceSuiteAssetConfig
+
+ZIP_PATH = Path('NT17_KVK_20221214 Berichten.zip')
+# needs to be extracted because arelle can't load a taxonomy package ZIP from within a ZIP
+EXTRACTED_PATH = Path(ZIP_PATH.stem)
+config = ConformanceSuiteConfig(
+    args=[
+        '--disclosureSystem', 'NT17-preview',
+        '--logCodeFilter', '(?!({})$)'.format('|'.join([
+            'xbrlte:closedDefinitionNodeZeroCardinality',
+            'xbrlte:constraintSetAspectMismatch',
+            'xbrlte:invalidDimensionRelationshipSource',
+            'xbrlte:missingAspectValue',
+            'xbrlte:multipleValuesForAspect',
+        ])),
+    ],
+    assets=[
+        ConformanceSuiteAssetConfig.nested_conformance_suite(
+            ZIP_PATH,
+            EXTRACTED_PATH,
+            entry_point_root=EXTRACTED_PATH / 'berichten' / 'NT17_KVK_20221214 - Testsuite.zip',
+            entry_point=Path('testcases.xml'),
+            public_download_url='https://sbr-nl.nl/sites/default/files/bestanden/taxonomie/NT17_KVK_20221214%20Berichten.zip',
+        ),
+        *NL_PACKAGES['NT17'],
+    ],
+    info_url='https://www.sbr-nl.nl/sites/default/files/bestanden/taxonomie/SBR%20Filing%20Rules%20NT17%20-%2020220301__.pdf',
+    name=PurePath(__file__).stem,
+    network_or_cache_required=False,
+    plugins=frozenset({'validate/NL'}),
+    shards=8,
+)
