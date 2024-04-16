bfd9f4979a78815d6a7d184064deb82a3fa4a8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:08 +0000

    Add package ubo-app/0.12.6/ubo_app-0.12.6.tar.gz

---
+++ packages/ubo-app/ubo_app-0.12.6.tar.gz/ubo_app-0.12.6/ubo_app/services/000-sound/setup.py
@@ -0,0 +1,42 @@
+# ruff: noqa: D100, D101, D102, D103, D104, D107, N999
+from __future__ import annotations
+
+from pathlib import Path
+
+from audio_manager import AudioManager
+from constants import SOUND_MIC_STATE_ICON_ID, SOUND_MIC_STATE_ICON_PRIORITY
+
+from ubo_app.store import autorun, dispatch, subscribe_event
+from ubo_app.store.services.sound import SoundPlayChimeEvent
+from ubo_app.store.status_icons import StatusIconsRegisterAction
+
+
+def init_service() -> None:
+    dispatch(
+        StatusIconsRegisterAction(
+            icon='󰍭',
+            priority=SOUND_MIC_STATE_ICON_PRIORITY,
+            id=SOUND_MIC_STATE_ICON_ID,
+        ),
+    )
+
+    audio_manager = AudioManager()
+
+    @autorun(lambda state: state.sound.playback_volume)
+    def _(volume: float) -> None:
+        audio_manager.set_playback_volume(volume)
+
+    @autorun(lambda state: state.sound.capture_volume)
+    def _(volume: float) -> None:
+        audio_manager.set_capture_volume(volume)
+
+    @autorun(lambda state: state.sound.is_playback_mute)
+    def _(is_mute: bool) -> None:  # noqa: FBT001
+        audio_manager.set_playback_mute(mute=is_mute)
+
+    subscribe_event(
+        SoundPlayChimeEvent,
+        lambda event: audio_manager.play(
+            Path(__file__).parent.joinpath(f'sounds/{event.name}.wav').as_posix(),
+        ),
+    )
