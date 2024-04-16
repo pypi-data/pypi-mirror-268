8c788f083dbcb75e1d5ec2f8f8ca735c49648a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:01 +0000

    Add package jaraco.home/3.8.1/jaraco_home-3.8.1.tar.gz

---
+++ packages/jaraco.home/jaraco_home-3.8.1.tar.gz/jaraco_home-3.8.1/conftest.py
@@ -0,0 +1,27 @@
+import platform
+from importlib.resources import files
+
+import pytest
+from mockproc import mockprocess
+
+win_ignore = [
+    'jaraco/home/hdhomerun.py',
+] * (platform.system() == 'Windows')
+
+collect_ignore = [
+    'jaraco/home/relay.py',
+] + win_ignore
+
+
+@pytest.fixture(scope='session', autouse=True)
+def hdhomerun_config_mocked():
+    import jaraco.home.hdhomerun as hd
+
+    hd.hdhomerun_config = 'hdhomerun_config'
+    scripts = mockprocess.MockProc()
+    script = (
+        files('jaraco.home').joinpath('mock hdhomerun.py').read_text(encoding='utf-8')
+    )
+    scripts.append('hdhomerun_config', returncode=0, script=script)
+    with scripts:
+        yield
