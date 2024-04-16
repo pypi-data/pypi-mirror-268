ac00b8205dc28974d81b8460f409cc02824e06ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:03:53 +0000

    Add package inuits-python-logging-loki/1.1.4/inuits_python_logging_loki-1.1.4.tar.gz

---
+++ packages/inuits-python-logging-loki/inuits_python_logging_loki-1.1.4.tar.gz/inuits_python_logging_loki-1.1.4/logging_loki/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from logging_loki.handlers import LokiHandler
+from logging_loki.handlers import LokiQueueHandler
+
+__all__ = ["LokiHandler", "LokiQueueHandler"]
+__version__ = "1.1.4"
+name = "logging_loki"
