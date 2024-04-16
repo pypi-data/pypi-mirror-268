8826eff4b4ce116120e03eac82deef7c6cdd8a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 06:03:23 +0000

    Add package pasta-man/1.0.5/pasta_man-1.0.5-py3-none-any.whl

---
+++ packages/pasta-man/pasta_man-1.0.5-py3-none-any.whl/pasta_man/exceptions.py
@@ -0,0 +1,15 @@
+#
+# This File contains the Exceptions related to PASTA-MAN only.
+#
+
+class InvalidKeyword(Exception):
+    pass
+
+class InvalidExportType(Exception):
+    pass
+
+class NoneTypeVariable(Exception):
+    pass
+
+class OptError(Exception):
+    pass
