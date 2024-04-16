46d5f4f60155714b4a954f785da4769fcf57d677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:03:16 +0000

    Add package flowchat/1.2.0/flowchat-1.2.0-py3-none-any.whl

---
+++ packages/flowchat/flowchat-1.2.0-py3-none-any.whl/flowchat/__init__.py
@@ -0,0 +1,6 @@
+from .autodedent import autodedent
+from .chain import Chain
+from .chain import ImageFormat
+from .chain import Message
+from .chain import RequestParams
+from .chain import ResponseFormat
