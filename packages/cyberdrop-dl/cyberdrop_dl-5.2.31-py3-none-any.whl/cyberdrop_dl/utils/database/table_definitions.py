d12ca4128b0960cd3759ff41774f1b23ec9a7f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:23 +0000

    Add package cyberdrop-dl/5.2.31/cyberdrop_dl-5.2.31-py3-none-any.whl

---
+++ packages/cyberdrop-dl/cyberdrop_dl-5.2.31-py3-none-any.whl/cyberdrop_dl/utils/database/table_definitions.py
@@ -0,0 +1,23 @@
+create_history = """CREATE TABLE IF NOT EXISTS media (domain TEXT,
+                                               url_path TEXT,
+                                               referer TEXT,
+                                               album_id TEXT,
+                                               download_path TEXT,
+                                               download_filename TEXT,
+                                               original_filename TEXT,
+                                               completed INTEGER NOT NULL,
+                                               PRIMARY KEY (domain, url_path, original_filename)
+                                               );"""
+
+create_fixed_history = """CREATE TABLE IF NOT EXISTS media_copy (domain TEXT,
+                                               url_path TEXT,
+                                               referer TEXT,
+                                               album_id TEXT,
+                                               download_path TEXT,
+                                               download_filename TEXT,
+                                               original_filename TEXT,
+                                               completed INTEGER NOT NULL,
+                                               PRIMARY KEY (domain, url_path, original_filename)
+                                               );"""
+
+create_temp = """CREATE TABLE IF NOT EXISTS temp (downloaded_filename TEXT);"""
