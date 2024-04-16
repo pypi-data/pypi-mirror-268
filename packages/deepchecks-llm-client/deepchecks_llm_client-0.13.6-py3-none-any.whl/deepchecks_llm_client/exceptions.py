6a54ad6550978e69a70aec9e495dc615af94cd2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:03:45 +0000

    Add package deepchecks-llm-client/0.13.6/deepchecks_llm_client-0.13.6-py3-none-any.whl

---
+++ packages/deepchecks-llm-client/deepchecks_llm_client-0.13.6-py3-none-any.whl/deepchecks_llm_client/exceptions.py
@@ -0,0 +1,19 @@
+from httpx import HTTPStatusError
+
+
+class DeepchecksLLMClientError(Exception):
+    pass
+
+
+class BaseDeepchecksLLMAPIError(HTTPStatusError):
+    """general API exception"""
+
+
+class DeepchecksLLMBadRequestError(BaseDeepchecksLLMAPIError):
+    """Raised when API return 4xx code"""
+    pass
+
+
+class DeepchecksLLMServerError(BaseDeepchecksLLMAPIError):
+    """Raised when API return 5xx code"""
+    pass
