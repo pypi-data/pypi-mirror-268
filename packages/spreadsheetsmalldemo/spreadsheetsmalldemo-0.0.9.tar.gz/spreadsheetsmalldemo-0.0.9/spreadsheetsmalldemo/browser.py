c9611a1f8ec17fc3955d92c3c3219da80922a69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:14 +0000

    Add package spreadsheetsmalldemo/0.0.9/spreadsheetsmalldemo-0.0.9.tar.gz

---
+++ packages/spreadsheetsmalldemo/spreadsheetsmalldemo-0.0.9.tar.gz/spreadsheetsmalldemo-0.0.9/spreadsheetsmalldemo/browser.py
@@ -0,0 +1,16 @@
+from RPA.Browser.Selenium import Selenium
+
+
+
+class Browser():
+    def __init__(self):
+        self.browser = Selenium()
+    
+    def open_browser(self, url: str):
+        self.browser.open_available_browser(url)
+
+    def change_to_frame(self, frame_xpath: str):
+        self.browser.unselect_frame()
+        self.browser.wait_until_element_is_visible(frame_xpath, timeout=60 * 2)
+        iframe = self.browser.find_element(frame_xpath)
+        self.browser.select_frame(iframe)
