4c4e3698f773dac28ccda799af8dc0fdf2990d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:12 +0000

    Add package DXR-BJ/0.0.3/DXR_BJ-0.0.3.tar.gz

---
+++ packages/DXR-BJ/DXR_BJ-0.0.3.tar.gz/DXR_BJ-0.0.3/DXR_BJ/__init__.py
@@ -0,0 +1,3 @@
+
+version_num='1.0.1'
+print(f'版本号:{version_num}')
---
+++ packages/DXR-BJ/DXR_BJ-0.0.3.tar.gz/DXR_BJ-0.0.3/DXR_BJ/drawer.py
@@ -0,0 +1,13 @@
+
+
+
+def main_zx():
+
+    print(222222)
+    return
+
+
+if __name__ == "__main__":
+    print('main 运行成功')
+    main_zx()
+    pass
