8e437afbe388e8870cd9fd4ae3ee23377e9de1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:03:53 +0000

    Add package nonebot-adapter-minecraft/1.1.2/nonebot_adapter_minecraft-1.1.2-py3-none-any.whl

---
+++ packages/nonebot-adapter-minecraft/nonebot_adapter_minecraft-1.1.2-py3-none-any.whl/nonebot/adapters/minecraft/config.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+from pydantic import BaseModel, Field
+
+
+class Server(BaseModel):
+    enable_rcon: bool = False
+    rcon_port: int = 25575
+    rcon_password: str = "password"
+    timeout: float = 2.0
+
+
+class Config(BaseModel):
+    minecraft_server_rcon: Dict[str, Server] = Field(default_factory=dict)
