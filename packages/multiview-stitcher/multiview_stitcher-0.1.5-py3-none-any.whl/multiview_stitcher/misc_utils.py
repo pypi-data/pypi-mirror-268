dbbedb442ba4c4a1b7b0c568846bf05cda6a1b7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:04:34 +0000

    Add package multiview-stitcher/0.1.5/multiview_stitcher-0.1.5-py3-none-any.whl

---
+++ packages/multiview-stitcher/multiview_stitcher-0.1.5-py3-none-any.whl/multiview_stitcher/misc_utils.py
@@ -0,0 +1,24 @@
+import logging
+from contextlib import contextmanager
+
+
+class DisableLogger:
+    def __enter__(self):
+        logging.disable(logging.CRITICAL)
+
+    def __exit__(self, exit_type, exit_value, exit_traceback):
+        logging.disable(logging.NOTSET)
+
+
+@contextmanager
+def temporary_log_level(logger, level):
+    """
+    Use this in notebooks:
+
+    import logging
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', stream=sys.stdout, force=True)
+    """
+    old_level = logger.level
+    logger.setLevel(level)
+    yield
+    logger.setLevel(old_level)
