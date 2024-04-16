016c44e5600ab61f65546f160787e7c8f9c05b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:03:56 +0000

    Add package cvxpy/1.4.3/cvxpy-1.4.3-cp312-cp312-macosx_10_9_universal2.whl

---
+++ packages/cvxpy/cvxpy-1.4.3-cp312-cp312-macosx_10_9_universal2.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.4.3'
+version = '1.4.3'
+full_version = '1.4.3'
+git_revision = 'f66cfb3'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
