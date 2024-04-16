26c5646ee2d1048971331f0c92486f5b62b3c663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:03:35 +0000

    Add package factory_farm/1.1.2/factory_farm-1.1.2-py3-none-any.whl

---
+++ packages/factory_farm/factory_farm-1.1.2-py3-none-any.whl/factory_farm/procedures/health_scan/dynamic_port.py
@@ -0,0 +1,19 @@
+
+#from factory_farm.topics.process_on.p_expect import process_on
+from factory_farm.topics.process_on.p_expect.implicit import process_on_implicit
+
+def dynamic_port (
+	process_path = "",
+	
+	env = "",
+	name = ""
+):
+	the_health_check = process_on_implicit (
+		"python3 " + process_path,
+		
+		#CWD = CWD,
+		env = env,
+		name = name
+	)
+	
+	return the_health_check
