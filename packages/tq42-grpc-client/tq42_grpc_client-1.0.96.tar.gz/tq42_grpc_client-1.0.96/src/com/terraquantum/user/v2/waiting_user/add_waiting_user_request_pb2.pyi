9b476dfc88f292063f3a6c89b6e7ae0d5818bd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:04:04 +0000

    Add package tq42-grpc-client/1.0.96/tq42_grpc_client-1.0.96.tar.gz

---
+++ packages/tq42-grpc-client/tq42_grpc_client-1.0.96.tar.gz/tq42_grpc_client-1.0.96/src/com/terraquantum/user/v2/waiting_user/add_waiting_user_request_pb2.pyi
@@ -0,0 +1,13 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class AddWaitingUserRequest(_message.Message):
+    __slots__ = ("jwt", "request_id")
+    JWT_FIELD_NUMBER: _ClassVar[int]
+    REQUEST_ID_FIELD_NUMBER: _ClassVar[int]
+    jwt: str
+    request_id: str
+    def __init__(self, jwt: _Optional[str] = ..., request_id: _Optional[str] = ...) -> None: ...
