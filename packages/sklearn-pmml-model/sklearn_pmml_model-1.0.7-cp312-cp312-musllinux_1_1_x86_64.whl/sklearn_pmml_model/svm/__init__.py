6f72174857315a255e212c561333c5333a5e9b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 04:03:44 +0000

    Add package sklearn-pmml-model/1.0.7/sklearn_pmml_model-1.0.7-cp312-cp312-musllinux_1_1_x86_64.whl

---
+++ packages/sklearn-pmml-model/sklearn_pmml_model-1.0.7-cp312-cp312-musllinux_1_1_x86_64.whl/sklearn_pmml_model/svm/__init__.py
@@ -0,0 +1,9 @@
+"""
+The :mod:`sklearn.svm` module includes Support Vector Machine algorithms.
+"""
+
+# License: BSD 2-Clause
+
+from ._classes import PMMLLinearSVC, PMMLLinearSVR, PMMLNuSVC, PMMLNuSVR, PMMLSVC, PMMLSVR
+
+__all__ = ['PMMLLinearSVC', 'PMMLLinearSVR', 'PMMLNuSVC', 'PMMLNuSVR', 'PMMLSVC', 'PMMLSVR']
