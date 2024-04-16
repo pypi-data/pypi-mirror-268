8713abb9a8dd6d1a9febebc409126dcf1ce8c9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:03:40 +0000

    Add package fuxictr/2.2.1/fuxictr-2.2.1-py3-none-any.whl

---
+++ packages/fuxictr/fuxictr-2.2.1-py3-none-any.whl/fuxictr/pytorch/dataloaders/npz_block_dataloader.py
@@ -0,0 +1,89 @@
+# =========================================================================
+# Copyright (C) 2023-2024. FuxiCTR Authors. All rights reserved.
+# Copyright (C) 2022. Huawei Technologies Co., Ltd. All rights reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =========================================================================
+
+
+import numpy as np
+from itertools import chain
+import torch
+from torch.utils import data
+import glob
+
+
+class BlockDataPipe(data.IterDataPipe):
+    def __init__(self, block_datapipe, feature_map):
+        self.feature_map = feature_map
+        self.block_datapipe = block_datapipe
+        
+    def load_data(self, data_path):
+        data_dict = np.load(data_path)
+        data_arrays = []
+        all_cols = list(self.feature_map.features.keys()) + self.feature_map.labels
+        for col in all_cols:
+            array = data_dict[col]
+            if array.ndim == 1:
+                data_arrays.append(array.reshape(-1, 1))
+            else:
+                data_arrays.append(array)
+        data_tensor = torch.from_numpy(np.hstack(data_arrays))
+        return data_tensor
+
+    def read_block(self, data_block):
+        darray = self.load_data(data_block)
+        for idx in range(darray.shape[0]):
+            yield darray[idx, :]
+
+    def __iter__(self):
+        worker_info = data.get_worker_info()
+        if worker_info is None: # single-process data loading
+            block_list = self.block_datapipe
+        else: # in a worker process
+            block_list = [
+                block
+                for idx, block in enumerate(self.block_datapipe)
+                if idx % worker_info.num_workers == worker_info.id
+            ]
+        return chain.from_iterable(map(self.read_block, block_list))
+
+
+class NpzBlockDataLoader(data.DataLoader):
+    def __init__(self, feature_map, data_path, batch_size=32, shuffle=False,
+                 num_workers=1, buffer_size=100000, **kwargs):
+        data_blocks = glob.glob(data_path + "/*.npz")
+        assert len(data_blocks) > 0, f"invalid data_path: {data_path}"
+        if len(data_blocks) > 1:
+            data_blocks.sort() # sort by part name
+        self.data_blocks = data_blocks
+        self.num_blocks = len(self.data_blocks)
+        self.feature_map = feature_map
+        self.batch_size = batch_size
+        self.num_batches, self.num_samples = self.count_batches_and_samples()
+        datapipe = BlockDataPipe(data_blocks, feature_map)
+        if shuffle:
+            datapipe = datapipe.shuffle(buffer_size=buffer_size)
+        super(NpzBlockDataLoader, self).__init__(dataset=datapipe, batch_size=batch_size,
+                                                 num_workers=num_workers)
+
+    def __len__(self):
+        return self.num_batches
+
+    def count_batches_and_samples(self):
+        num_samples = 0
+        for block_path in self.data_blocks:
+            block_size = np.load(block_path)[self.feature_map.labels[0]].shape[0]
+            num_samples += block_size
+        num_batches = int(np.ceil(num_samples / self.batch_size))
+        return num_batches, num_samples
