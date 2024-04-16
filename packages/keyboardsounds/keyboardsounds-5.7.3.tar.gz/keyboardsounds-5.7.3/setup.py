868e854b5e375e40e9cc45f453614c2ae9185b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 04:03:21 +0000

    Add package keyboardsounds/5.7.3/keyboardsounds-5.7.3.tar.gz

---
+++ packages/keyboardsounds/keyboardsounds-5.7.3.tar.gz/keyboardsounds-5.7.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name="keyboardsounds",
+    version="5.7.3",
+    description="Adds the ability to play sounds while typing on any system.",
+    author="Nathan Fiscaletti",
+    author_email="nate.fiscaletti@gmail.com",
+    packages=["keyboardsounds"],
+    install_requires=["pygame", "pynput", "psutil", "imageio-ffmpeg", "pyyaml"],
+    package_data={
+        "keyboardsounds": [
+            "profiles/alpaca/*",
+            "profiles/gateron-black-ink/*",
+            "profiles/gateron-red-ink/*",
+            "profiles/holy-panda/*",
+            "profiles/ios/*",
+            "profiles/mx-black/*",
+            "profiles/mx-blue/*",
+            "profiles/mx-brown/*",
+            "profiles/mx-speed-silver/*",
+            "profiles/telios-v2/*",
+            "profiles/typewriter/*",
+            "external_api/*",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "keyboardsounds = keyboardsounds.main:main",
+            "kbs = keyboardsounds.main:main",
+        ],
+    },
+)
