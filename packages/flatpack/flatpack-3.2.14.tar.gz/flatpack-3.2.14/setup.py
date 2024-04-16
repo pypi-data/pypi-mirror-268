751eeb3f664b8b9e4d337cfa69cb801379ef183e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:03:06 +0000

    Add package flatpack/3.2.14/flatpack-3.2.14.tar.gz

---
+++ packages/flatpack/flatpack-3.2.14.tar.gz/flatpack-3.2.14/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="flatpack",
+    version="3.2.14",
+    license="Apache Software License (Apache-2.0)",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography==42.0.5",
+        "faiss-cpu==1.8.0",
+        "fastapi==0.110.1",
+        "httpx==0.27.0",
+        "ngrok==1.2.0",
+        "sentence-transformers==2.6.1",
+        "toml==0.10.2",
+        "torch==2.2.2",
+        "uvicorn==0.29.0"
+    ],
+    author="Romlin Group AB",
+    author_email="hello@romlin.com",
+    description="Ready-to-assemble AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "flatpack=flatpack.main:main"
+        ],
+    }
+)
