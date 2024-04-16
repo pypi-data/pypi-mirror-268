124cf6a19bb3775d3a18a03304c70cd21a79b512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 04:03:43 +0000

    Add package fennel-ai/1.2.4/fennel_ai-1.2.4.tar.gz

---
+++ packages/fennel-ai/fennel_ai-1.2.4.tar.gz/fennel_ai-1.2.4/fennel/lib/includes/__init__.py
@@ -0,0 +1,6 @@
+from fennel.lib.includes.include_mod import (
+    FENNEL_INCLUDED_MOD,
+    FENNEL_TIER_SELECTOR,
+    TierSelector,
+    includes,
+)
---
+++ packages/fennel-ai/fennel_ai-1.2.4.tar.gz/fennel_ai-1.2.4/fennel/lib/metadata/__init__.py
@@ -0,0 +1,8 @@
+from fennel.lib.metadata.metadata import (
+    meta,
+    Metadata,
+    OWNER,
+    get_meta_attr,
+    set_meta_attr,
+    get_metadata_proto,
+)
