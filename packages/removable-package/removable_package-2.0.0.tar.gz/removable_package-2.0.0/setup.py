3dbabb9016598b3c295ff62ba1747f4df970c82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 16:03:38 +0000

    Add package removable-package/2.0.0/removable_package-2.0.0.tar.gz

---
+++ packages/removable-package/removable_package-2.0.0.tar.gz/removable_package-2.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='removable_package',
+version='2.0.0',
+author='andr3colonel',
+author_email='andr3colonel@gmail.com',
+description='Removable package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
