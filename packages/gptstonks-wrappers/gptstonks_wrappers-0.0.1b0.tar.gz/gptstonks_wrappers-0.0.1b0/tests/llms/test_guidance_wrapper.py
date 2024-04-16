2559d4b3cd8da87c803d22d8eccce0ab40cd3288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 04:03:39 +0000

    Add package gptstonks-wrappers/0.0.1b0/gptstonks_wrappers-0.0.1b0.tar.gz

---
+++ packages/gptstonks-wrappers/gptstonks_wrappers-0.0.1b0.tar.gz/gptstonks_wrappers-0.0.1b0/tests/llms/test_guidance_wrapper.py
@@ -0,0 +1,11 @@
+from unittest.mock import patch
+
+from gptstonks.wrappers.llms.guidance_wrapper import GuidanceWrapper
+
+
+@patch("guidance.models.Transformers")
+@patch("guidance.__call__")
+def test_guidance_wrapper(mocked_transformers, mocked_call):
+    guidance_wrapper = GuidanceWrapper()
+
+    mocked_call.assert_called_once()
