c53ab97bf8efb10a7209dd3eb8cd8a7e13290976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:03:53 +0000

    Add package wikipedia-zh/0.0.6/wikipedia_zh-0.0.6.tar.gz

---
+++ packages/wikipedia-zh/wikipedia_zh-0.0.6.tar.gz/wikipedia_zh-0.0.6/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import setuptools
+from setuptools import find_packages
+
+setuptools.setup(
+    name="wikipedia_zh",
+    version='0.0.6',
+    author="Heng Zhang",
+    author_email="1093869292@qq.com",
+    description="Wikipedia Zh API for Python",
+    url="https://gitee.com/zh19990906/wikipedia_zh",
+    install_requires=[
+        'beautifulsoup4',
+        'requests>=2.0.0,<3.0.0'
+    ],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
+
+
