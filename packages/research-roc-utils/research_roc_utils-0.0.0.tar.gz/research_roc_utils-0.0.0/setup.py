54b851a7956ac3a376cf36c66fb333797de29d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:04:09 +0000

    Add package research-roc-utils/0.0.0/research_roc_utils-0.0.0.tar.gz

---
+++ packages/research-roc-utils/research_roc_utils-0.0.0.tar.gz/research_roc_utils-0.0.0/setup.py
@@ -0,0 +1,38 @@
+"""Python setup.py for project_name package"""
+import io
+import os
+from setuptools import find_packages, setup
+
+def read(*paths, **kwargs):
+    """Read the contents of a text file safely.
+    >>> read("research_roc_utils", "1.0.0")
+    '1.0.0'
+    >>> read("README.md")
+    ...
+    """
+
+    content = ""
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *paths),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as open_file:
+        content = open_file.read().strip()
+    return content
+
+def read_requirements(path):
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(('"', "#", "-", "git+"))
+    ]
+
+setup(
+    name="research_roc_utils",
+    description="Utility functions to assist in the computation of ROC curve comparisons based on academic research.",
+    url="https://github.com/jpbruehw/research-roc-utils",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="JP Bruehwiler",
+    packages=find_packages(exclude=["tests", ".github"]),
+    install_requires=read_requirements("requirements.txt"),
+)
