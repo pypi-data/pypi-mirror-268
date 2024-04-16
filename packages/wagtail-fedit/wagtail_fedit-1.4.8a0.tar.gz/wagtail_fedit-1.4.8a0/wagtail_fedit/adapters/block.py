3584dd03cb9832b5aa38cce3bb97d2dc14712c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 04:03:36 +0000

    Add package wagtail-fedit/1.4.8a0/wagtail_fedit-1.4.8a0.tar.gz

---
+++ packages/wagtail-fedit/wagtail_fedit-1.4.8a0.tar.gz/wagtail_fedit-1.4.8a0/wagtail_fedit/adapters/block.py
@@ -0,0 +1,111 @@
+from django.db import models
+from django.utils import translation
+from django.utils.translation import gettext_lazy as _
+from django.utils.safestring import mark_safe
+from django.http import HttpRequest
+
+from wagtail.log_actions import log
+from wagtail.blocks import (
+    StreamValue,
+    BoundBlock,
+)
+from wagtail.models import (
+    RevisionMixin,
+)
+
+from .base import (
+    BaseAdapter,
+    AdapterError,
+)
+from ..forms import (
+    blocks as block_forms,
+)
+from .. import utils
+
+
+
+
+class BlockAdapter(BaseAdapter):
+    identifier = "block"
+    required_kwargs = ["block"]
+
+    def __init__(self, object: models.Model, field_name: str, request: HttpRequest, **kwargs):
+        super().__init__(object, field_name, request, **kwargs)
+
+        self.block = self.kwargs.pop("block", None)
+        if self.block:
+            if not isinstance(self.block, BoundBlock):
+                raise AdapterError("Invalid block type")
+
+            self.kwargs["block_id"] = self.block.id
+        else:
+            block_id = self.kwargs.get("block_id", None)
+            if block_id is None:
+                raise AdapterError("Block ID is required")
+            
+            self.streamfield: StreamValue = getattr(self.object, self.field_name)
+            result = utils.find_block(block_id, self.streamfield)
+            if not result:
+                raise AdapterError("Block not found; did you provide the correct block ID?")
+            
+            self.block, _ = result
+
+    def get_header_title(self):
+
+        model_string = getattr(self.object, "get_admin_display_title", None)
+        if model_string:
+            model_string = model_string()
+        else:
+            model_string = getattr(self.object, "title", str(self.object))
+
+        return _("Edit block %(block_label)s for %(model_name)s %(model_string)s") % {
+            "block_label": self.block.block.label,
+            "model_name": self.model._meta.verbose_name,
+            "model_string": model_string,
+        }
+
+    def get_form(self):
+
+        self.form_class = block_forms.get_block_form_class(self.block.block)
+
+        if self.request.method == "POST":
+            form = self.form_class(self.request.POST, block=self.block, parent_instance=self.object, request=self.request)
+        else:
+            form = self.form_class(block=self.block, parent_instance=self.object, request=self.request)
+        return form
+    
+    def form_valid(self, form: block_forms.BlockEditForm):
+        self.block = form.save()
+
+        extra_log_kwargs = {}
+        if isinstance(self.object, RevisionMixin):
+            extra_log_kwargs["revision"] = self.object.latest_revision
+
+        with translation.override(None):
+            log(
+                instance=self.object,
+                action="wagtail_fedit.edit_block",
+                user=self.request.user,
+                title=self.get_header_title(),
+                data={
+                    "block_id": self.kwargs["block_id"],
+                    "field_name": self.field_name,
+                    "model_id": self.object.pk,
+                    "model_name": self.object._meta.model_name,
+                    "app_label": self.object._meta.app_label,
+                    "verbose_field_name": str(self.meta_field.verbose_name),
+                    "block_label": str(self.block.block.label),
+                },
+                content_changed=True,
+                **extra_log_kwargs,
+            )
+
+    def render_content(self, parent_context: dict = None) -> str:
+        parent_context = parent_context or {}
+        if hasattr(parent_context, "flatten"):
+            parent_context = parent_context.flatten()
+
+        parent_context.update(self.kwargs)
+
+        return self.block.render(parent_context)
+        
---
+++ packages/wagtail-fedit/wagtail_fedit-1.4.8a0.tar.gz/wagtail_fedit-1.4.8a0/wagtail_fedit/templatetags/fedit.py
@@ -0,0 +1,222 @@
+from typing import Type
+from django.template import library, Node, NodeList, TemplateSyntaxError
+from django.template.loader import render_to_string
+from django.template.base import Parser, Token
+from django.template.base import FilterExpression
+from django.utils.safestring import mark_safe
+from django.http import HttpRequest
+from django.urls import reverse
+from django.core import signing
+from django.db import models
+
+from wagtail.blocks import BoundBlock
+from wagtail import hooks
+from urllib.parse import urlencode
+
+import warnings
+
+from ..toolbar import (
+    FeditBlockEditButton,
+    FeditAdapterEditButton,
+)
+from ..adapters import (
+    adapter_registry,
+    RegistryLookUpError,
+    BaseAdapter,
+)
+from ..utils import (
+    _can_edit,
+    edit_url,
+)
+from ..hooks import (
+    CONSTRUCT_ADAPTER_TOOLBAR,
+)
+
+
+register = library.Library()
+
+
+WARNING_FIELD_NAME_NOT_AVAILABLE = "Field name is not available in the context for %(object)s."
+WARNING_MODEL_INSTANCE_NOT_AVAILABLE = "Model instance is not available in the context for %(object)s."
+
+
+
+class AdapterNode(Node):
+    signer = signing.TimestampSigner()
+
+    def __init__(self, adapter: Type[BaseAdapter], model: FilterExpression, getters: list[str], **kwargs):
+        self.adapter = adapter
+        self.model = model
+        self.getters = getters
+        self.kwargs = kwargs
+
+    def _resolve_expressions(self, context, model, **kwargs):
+        for k, v in kwargs.items():
+            if isinstance(v, FilterExpression):
+                kwargs[k] = v.resolve(context)
+        
+        if isinstance(model, FilterExpression):
+            model = model.resolve(context)
+
+        return model, kwargs
+
+    def render(self, context):
+        model = self.model
+        getters = self.getters
+
+        model, kwargs = self._resolve_expressions(
+            context, model, **self.kwargs,
+        )
+
+        if "wagtail_fedit_field" in context\
+            and "wagtail_fedit_instance" in context\
+            and not model:
+
+            field_name = context["wagtail_fedit_field"]
+            obj = context["wagtail_fedit_instance"]
+
+        else:
+
+            if not model:
+                raise TemplateSyntaxError(
+                    "`instance.field` is required; use `from_context` if "
+                    "this is wrapped by a fedit templatetag or adress a model field."
+                )
+
+            field_name = getters[len(getters)-1]
+            obj = model
+            for i in range(len(getters) - 1):
+                getter = getters[i]
+                try:
+                    obj = getattr(obj, getter)
+                except AttributeError:
+                    raise AttributeError(f"Object {model.__class__.__name__} does not have attribute {getter}")
+
+        request = context.get("request")
+        adapter = self.adapter(
+            object=obj,
+            field_name=field_name,
+            request=request,
+            **kwargs,
+        )
+
+        if not adapter.check_permissions()\
+          or not _can_edit(request, obj):
+            return adapter.render_content(context)
+
+        return wrap_adapter(request, adapter, context)
+
+
+@register.tag(name="fedit")
+def do_render_fedit(parser: Parser, token: Token):
+
+    tokens = token.split_contents()
+
+    _ = tokens.pop(0)
+    adapter_id = tokens.pop(0)
+
+    try:
+        adapter = adapter_registry[adapter_id]
+    except RegistryLookUpError:
+        raise TemplateSyntaxError(f"No adapter found with identifier '{adapter_id}'")
+
+    model, model_tokens = None, None
+    if tokens:
+        model__field = tokens.pop(0)
+        model_tokens = model__field.split(".")
+
+        if len(model_tokens) < 2:
+            if model_tokens[0] != "from_context":
+                raise TemplateSyntaxError(
+                    "Model and field name are required: 'mymodel.myfield' or 'from_context'",
+                )
+
+        if len(model_tokens) > 1:
+            # mymodel.myfield
+            # mymodel.related_field.myfield
+            model = parser.compile_filter(model_tokens.pop(0))
+
+    kwargs = get_kwargs(parser, tokens, adapter.required_kwargs)
+
+    return AdapterNode(
+        adapter=adapter,
+        model=model,
+        getters=model_tokens,
+        **kwargs,
+    )
+
+
+def wrap_adapter(request: HttpRequest, adapter: BaseAdapter, context: dict) -> str:
+    if not context:
+        context = {}
+
+    context["wagtail_fedit_field"] = adapter.field_name
+    context["wagtail_fedit_instance"] = adapter.object
+
+    content = adapter.render_content(context)
+    shared = adapter.encode_shared_context()
+
+    items = [
+        FeditAdapterEditButton(),
+    ]
+
+    for hook in hooks.get_hooks(CONSTRUCT_ADAPTER_TOOLBAR):
+        hook(items=items, adapter=adapter)
+
+    items = [item.render(request) for item in items]
+    items = list(filter(None, items))
+
+    reverse_kwargs = {
+        "adapter_id": adapter.identifier,
+        "field_name": adapter.field_name,
+        "app_label": adapter.object._meta.app_label,
+        "model_name": adapter.object._meta.model_name,
+        "model_id": adapter.object.pk,
+    }
+
+    return render_to_string(
+        "wagtail_fedit/content/editable_adapter.html",
+        {
+            "identifier": adapter.identifier,
+            "content": content,
+            "adapter": adapter,
+            "buttons": items,
+            "shared": shared,
+            "shared_context": adapter.kwargs,
+            "edit_url": reverse(
+                "wagtail_fedit:edit",
+                kwargs=reverse_kwargs,
+            ),
+        },
+        request=request,
+    )
+
+
+def get_kwargs(parser: Parser, tokens: list[str], kwarg_list: list[str] = None) -> dict:
+    had_kwargs = False
+    kwargs = {}
+
+    if not kwarg_list:
+        kwarg_list = []
+
+    for i, token in enumerate(tokens):
+        split = token.split("=")
+        if len(split) == 1 and len(kwarg_list) > i:
+            if had_kwargs:
+                raise ValueError("Unexpected positional argument after keyword argument")
+            
+            kwargs[kwarg_list[i]] = parser.compile_filter(token)
+        else:
+            key = split[0]
+            # if key not in kwargs_names:
+            #     raise ValueError(f"Unexpected keyword argument {key}")
+            
+            kwargs[key] = parser.compile_filter(split[1])
+            had_kwargs = True
+
+    for kwarg in kwarg_list:
+        if kwarg not in kwargs:
+            raise TemplateSyntaxError(f"Missing required keyword argument {kwarg}")
+
+    return kwargs
+
