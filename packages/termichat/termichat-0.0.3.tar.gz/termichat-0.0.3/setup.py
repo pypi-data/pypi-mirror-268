262b1fecbfcd05745c95ede9e40b882ebc11747b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:01 +0000

    Add package termichat/0.0.3/termichat-0.0.3.tar.gz

---
+++ packages/termichat/termichat-0.0.3.tar.gz/termichat-0.0.3/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+setup(
+    name="termichat",
+    packages=["termichat", "termichat.lib"],
+    version="0.0.3",
+    license="MIT",
+    description="Use ChatGPT in terminal",
+    author="GuoDCZ",
+    author_email="guodcz@gmail.com",
+    url="https://github.com/GuoDCZ/termichat",
+    download_url="", # FIXME
+    keywords=[
+        "chat", "chatbot", "gpt", "gpt-3", "openai", "terminal", "cli"
+    ],
+    install_requires=[
+        "openai==0.27.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "termichat=termichat.main:main"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
