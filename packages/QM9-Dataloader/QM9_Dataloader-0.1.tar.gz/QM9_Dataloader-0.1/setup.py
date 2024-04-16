490c5a5126581146b5dd9a3a20fd6bdd69c48cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 06:03:26 +0000

    Add package QM9-Dataloader/0.1/QM9_Dataloader-0.1.tar.gz

---
+++ packages/QM9-Dataloader/QM9_Dataloader-0.1.tar.gz/QM9_Dataloader-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QM9_Dataloader',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Logan Snider',
+    author_email='logop1112@gmail.com',
+    description='Uploads current (4/15/24) version of QM9 to dataframe object',
+)
