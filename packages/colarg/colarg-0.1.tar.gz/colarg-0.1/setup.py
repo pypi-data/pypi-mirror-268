14a75e724abae6952296ca89ee8efdf4c0983bc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 04:03:37 +0000

    Add package colarg/0.1/colarg-0.1.tar.gz

---
+++ packages/colarg/colarg-0.1.tar.gz/colarg-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+DESCRIPTION = ''
+LONG_DESCRIPTION = ''
+setup(
+    name="colarg",
+    version=VERSION,
+    author="nagie",
+    author_email="nagie123@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    package_data={'colorls': ['config/colorls.toml']},
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
