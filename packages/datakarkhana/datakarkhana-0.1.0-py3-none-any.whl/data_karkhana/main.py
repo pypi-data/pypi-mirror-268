44fb9c708d0e09b16407b59a5fbb56062f96fac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:04:01 +0000

    Add package datakarkhana/0.1.0/datakarkhana-0.1.0-py3-none-any.whl

---
+++ packages/datakarkhana/datakarkhana-0.1.0-py3-none-any.whl/data_karkhana/main.py
@@ -0,0 +1,74 @@
+import sys
+from data_karkhana.Tracker import Tracker
+from data_karkhana.Peer import FileTransferHandler
+from data_karkhana.Uploader import uploader
+from data_karkhana.Downloader import Downloader
+import socket
+
+def main():
+    print("Choose a role:")
+    print("1. Tracker")
+    print("2. Uploader")
+    print("3. Downloader")
+    print("4. Normal Peer")
+
+    role_choice = input("Enter your choice (1/2/3/4): ")
+
+ 
+
+    if len(sys.argv) >= 3:
+        tracker_host = sys.argv[1]
+        tracker_port = int(sys.argv[2])
+    else:
+        tracker_host = input("Please input the tracker IP address: ")
+        tracker_port = int(input("Please input the tracker port number: "))
+
+    if role_choice == "1":
+        tracker = Tracker.Tracker(tracker_host, tracker_port)
+        tracker.start()
+
+    elif role_choice == "2":
+        alice_instance = Uploader.uploader()
+
+        # host = input("Please input your IP address properly: ")  # Replace with the actual IP address of the receiver machine
+        # port = int(input("Please enter your port number: "))
+
+        host = get_ip()
+        port = 12345
+        file_path = input("Enter the name of the file you want to upload (for default, type: test.txt): ")
+        peer_id = input("Enter your peer ID: ")
+        alice_instance.send_file( peer_id, host, port, tracker_host, tracker_port, file_path)
+        alice_instance.connect_and_receive_response(peer_id, host, port+1, tracker_host, tracker_port)
+
+    elif role_choice == "3":
+
+        host = get_ip()
+        port = 12345
+
+        # host = input("Please input your IP address properly: ")  # Replace with the actual IP address of the receiver machine
+        # port = int(input("Please enter your port number: "))
+        peer_id = input("Enter your peer ID (any string to recognize you over network): ")
+        downloader = Downloader.Downloader(peer_id, host, port, tracker_host, tracker_port)
+        downloader.connect_and_receive_response()
+
+    elif role_choice == "4":
+
+        host = get_ip()
+        port = 12345
+
+        # host = input("Please input your IP address properly: ")  # Replace with the actual IP address of the receiver machine
+        # port = int(input("Please enter your port number: "))
+        peer_id = input("Enter your peer ID (any string to recognize you over network): ")
+        file_handler = Peer.FileTransferHandler(peer_id, host, port, tracker_host, tracker_port)
+        file_handler.connect_and_receive_response()
+
+    else:
+        print("Invalid choice. Please choose a valid option.")
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+    return s.getsockname()[0]
+
+if __name__ == "__main__":
+    main()
