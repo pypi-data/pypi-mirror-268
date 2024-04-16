4ff85529196e6d5e24be38eac708ca63407dd740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:11 +0000

    Add package lunary/1.0.2/lunary-1.0.2.tar.gz

---
+++ packages/lunary/lunary-1.0.2.tar.gz/lunary-1.0.2/lunary/thread.py
@@ -0,0 +1,31 @@
+import uuid
+from typing import Optional, List, TypedDict
+
+
+class Message(TypedDict, total=False):
+    id: str
+    role: str
+    content: Optional[str]
+    is_retry: Optional[bool]
+    tags: Optional[List[str]]
+
+
+class Thread:
+    def __init__(self, track_event, id: Optional[str] = None, tags: Optional[List[str]] = None):
+        self.id = id or str(uuid.uuid4())
+        self.tags = tags
+        self.track_event = track_event
+
+    def track_message(self, message: Message, feedback=None, user_id=None, user_props=None) -> str:
+        run_id = message.get("id", str(uuid.uuid4()))
+
+        self.track_event("thread", "chat",
+                         run_id=run_id,
+                         parent_run_id=self.id,
+                         thread_tags=self.tags,
+                         feedback=feedback,
+                         message=message,
+                         user_id=user_id,
+                         user_props=user_props
+                         )
+        return run_id
