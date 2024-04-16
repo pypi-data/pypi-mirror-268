b2ba548961e4591ca4fb949b0cdd7cbfbfb1611e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 04:03:06 +0000

    Add package telegram-collector/0.2.16/telegram_collector-0.2.16.tar.gz

---
+++ packages/telegram-collector/telegram_collector-0.2.16.tar.gz/telegram_collector-0.2.16/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='telegram_collector',
+    version='0.2.16',
+    author='fengleicn',
+    author_email='fengleisemail@gmail.com',
+    url='https://github.com/fengleicn/telegram_collector',
+    description=u'收集电报群组的视频图片消息',
+    packages=['telegram_collector'],
+    install_requires=['telethon', 'python_socks', 'async_timeout'],
+    entry_points={
+        'console_scripts': [
+            'tgc=telegram_collector.__main__:main'
+        ]
+    }
+)
