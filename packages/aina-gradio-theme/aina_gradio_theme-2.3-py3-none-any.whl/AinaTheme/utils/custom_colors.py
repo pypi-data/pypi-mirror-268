b6c29aac3048f96c4e40185324fcc7de97abbe90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:04:10 +0000

    Add package aina-gradio-theme/2.3/aina_gradio_theme-2.3-py3-none-any.whl

---
+++ packages/aina-gradio-theme/aina_gradio_theme-2.3-py3-none-any.whl/AinaTheme/utils/custom_colors.py
@@ -0,0 +1,43 @@
+from gradio.themes.utils.colors import Color
+
+primary = Color(
+    c50="#ecf4fb",
+    c100="#c7def2",
+    c200="#a1c8e9",
+    c300="#7bb2e0",
+    c400="#569cd8",
+    c500="#3085cf",
+    c600="#276da9",
+    c700="#1f5584",
+    c800="#163d5e",
+    c900="#0d2438",
+    c950="#040c13",
+)
+
+secondary = Color(
+    c50="#e8f6ff",
+    c100="#b9e3ff",
+    c200="#8bd1ff",
+    c300="#5dbfff",
+    c400="#2eacff",
+    c500="#009aff",
+    c600="#007ed1",
+    c700="#0062a2",
+    c800="#004674",
+    c900="#002a46",
+    c950="#000e17",
+)
+
+neutral = Color(
+    c50="#e8f8ff",
+    c100="#b9e9ff",
+    c200="#8bdbff",
+    c300="#5dccff",
+    c400="#2ebeff",
+    c500="#00afff",
+    c600="#008fd1",
+    c700="#0070a2",
+    c800="#005074",
+    c900="#003046",
+    c950="#001017",
+)
