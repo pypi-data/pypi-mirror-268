474cb02fa09dbcebaf7f7d513552d6fca6efc50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 05:02:47 +0000

    Add package psqlpy/0.5.2/psqlpy-0.5.2-cp39-none-win_amd64.whl

---
+++ packages/psqlpy/psqlpy-0.5.2-cp39-none-win_amd64.whl/psqlpy/extra_types.py
@@ -0,0 +1,25 @@
+from ._internal.extra_types import (
+    BigInt,
+    Integer,
+    PyJSON,
+    PyJSONB,
+    PyMacAddr6,
+    PyMacAddr8,
+    PyText,
+    PyUUID,
+    PyVarChar,
+    SmallInt,
+)
+
+__all__ = [
+    "SmallInt",
+    "Integer",
+    "BigInt",
+    "PyUUID",
+    "PyJSONB",
+    "PyJSON",
+    "PyMacAddr6",
+    "PyMacAddr8",
+    "PyVarChar",
+    "PyText",
+]
