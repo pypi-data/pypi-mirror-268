9d2d51d62c8b80ab3c838d9b95e75a60b5b5a37b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 04:04:15 +0000

    Add package custom-encryption/0.0.2/custom_encryption-0.0.2.tar.gz

---
+++ packages/custom-encryption/custom_encryption-0.0.2.tar.gz/custom_encryption-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+import os
+
+
+base_dir = os.path.abspath(os.path.dirname('encrypt_dir'))
+
+
+setup(
+    name='custom_encryption',
+    version='0.0.2',
+    description='A custom cryptography script using python',
+    packages=find_packages(where='ct_encrypt'),
+    long_description='A custom cryptography script',
+    long_description_content_type='text/markdown',
+    url='https://github.com/Boluex/custom-cryptography-python-script.git',
+    author='Oladeji Olaoluwa',
+    author_email='oladejiolaoluwa46@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators', 
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[],
+    keywords='custom crytography script build',
+)
