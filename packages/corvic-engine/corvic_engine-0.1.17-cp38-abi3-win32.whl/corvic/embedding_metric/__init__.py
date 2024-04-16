057bc8a4a8421dd5fcc0cef6c7d7b9ed0d961a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:03:35 +0000

    Add package corvic-engine/0.1.17/corvic_engine-0.1.17-cp38-abi3-win32.whl

---
+++ packages/corvic-engine/corvic_engine-0.1.17-cp38-abi3-win32.whl/corvic/embedding_metric/__init__.py
@@ -0,0 +1,27 @@
+"""Corvic embedding metrics."""
+
+from corvic.embedding_metric.embeddings import (
+    alpha_req,
+    classification,
+    condition_number,
+    mu0_coherence,
+    mu_coherence,
+    ne_sum,
+    rank_me,
+    regression,
+    self_cluster,
+    stable_rank,
+)
+
+__all__ = [
+    "alpha_req",
+    "classification",
+    "condition_number",
+    "mu_coherence",
+    "mu0_coherence",
+    "ne_sum",
+    "rank_me",
+    "regression",
+    "self_cluster",
+    "stable_rank",
+]
