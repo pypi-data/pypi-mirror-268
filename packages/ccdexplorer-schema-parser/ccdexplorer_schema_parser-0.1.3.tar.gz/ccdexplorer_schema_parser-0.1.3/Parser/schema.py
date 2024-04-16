280ed9c58724778d2047160bc5658758a19fa78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 16:03:23 +0000

    Add package ccdexplorer-schema-parser/0.1.3/ccdexplorer_schema_parser-0.1.3.tar.gz

---
+++ packages/ccdexplorer-schema-parser/ccdexplorer_schema_parser-0.1.3.tar.gz/ccdexplorer_schema_parser-0.1.3/Parser/schema.py
@@ -0,0 +1,27 @@
+from Parser import parser
+import json
+
+
+class Schema:
+    def __init__(self, source, version=None):
+        """Construct a new schema by extracting it from a module source. The
+        module source can be either provided as a serialized versioned module,
+        or a pair of a Wasm module together with a version (which can be either
+        0 or 1). If the optional `version` argument is supplied then this
+        assumes that the `source` is only the Wasm module.
+
+        """
+        if version is not None:
+            self.schema = parser.extract_schema_pair_ffi(version, source)
+        else:
+            self.schema = parser.extract_schema_ffi(source)
+
+    def event_to_json(self, contractName, eventData):
+        response = parser.parse_event_ffi(self.schema, contractName, eventData)
+        return json.loads(response)
+
+    def return_value_to_json(self, contractName, functionName, returnValueData):
+        response = parser.parse_return_value_ffi(
+            self.schema, contractName, functionName, returnValueData
+        )
+        return json.loads(response)
