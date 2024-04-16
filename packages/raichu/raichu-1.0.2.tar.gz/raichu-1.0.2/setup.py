57bc3baaa321003a97f601405173d159140f999d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 04:03:05 +0000

    Add package raichu/1.0.2/raichu-1.0.2.tar.gz

---
+++ packages/raichu/raichu-1.0.2.tar.gz/raichu-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.2'
+DESCRIPTION = 'RAIChU'
+LONG_DESCRIPTION = 'A package to automatically draw natural product biosynthesis pathways.'
+
+print(find_packages())
+
+setup(
+    name="raichu",
+    version=VERSION,
+    
+    author="Barbara Terlouw",
+    author_email="barbara.r.terlouw@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(exclude="build"),
+    package_data={"": ["*.png", "*.svg", "*.txt"]},
+    install_requires=['matplotlib',
+                      'biopython==1.76',
+                      'pikachu-chem>=1.1.1',
+                      'timeout-decorator==0.5.0'],
+    scripts=["bin/raichu"],)
