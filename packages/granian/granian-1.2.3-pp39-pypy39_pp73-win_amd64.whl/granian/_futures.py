000273c3b6d282f890915f49f4652c6a84dd8c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 06:03:31 +0000

    Add package granian/1.2.3/granian-1.2.3-pp39-pypy39_pp73-win_amd64.whl

---
+++ packages/granian/granian-1.2.3-pp39-pypy39_pp73-win_amd64.whl/granian/_futures.py
@@ -0,0 +1,9 @@
+def future_watcher_wrapper(inner):
+    async def future_watcher(watcher):
+        try:
+            await inner(watcher.scope, watcher.proto)
+        except BaseException as exc:
+            watcher.err(exc)
+        watcher.done()
+
+    return future_watcher
