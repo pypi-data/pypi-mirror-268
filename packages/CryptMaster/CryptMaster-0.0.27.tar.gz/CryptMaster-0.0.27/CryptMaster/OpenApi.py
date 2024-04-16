ffa89c182098afd55a92fa694875e38fa8d62504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:33 +0000

    Add package CryptMaster/0.0.27/CryptMaster-0.0.27.tar.gz

---
+++ packages/CryptMaster/CryptMaster-0.0.27.tar.gz/CryptMaster-0.0.27/CryptMaster/OpenApi.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+import httpx
+from .CryptMaster import CryptMaster
+
+from getpass import getpass
+
+ALLOW_EXPIRED_CERTS = False
+
+def open_api():
+    sys_instance = CryptMaster('test')
+    print(f'Current Node ID - {sys_instance.system_id}\n')
+    user = input('Enter email address: ')
+    verify=confirm_cert_skip()
+    domain = user.split('@')[1]
+    url = f'https://secure-api.{domain}:2053/v2/enable_api'
+    user_pass = getpass('Enter user password: ')
+    otp = input('Enter one time passcode: ')
+    payload = {"user_name": user, "user_pass": user_pass, "otp": otp}
+    response = httpx.post(url=url, json=payload, verify=verify, timeout=15)
+    print(response.json())
+    return
+
+def confirm_cert_skip():
+    if not ALLOW_EXPIRED_CERTS:
+        return True
+    verify_input = input('Should the certificate be verified before proceeding? (yes/no): ')
+    return verify_input.lower() not in ['no', 'n']
---
+++ packages/CryptMaster/CryptMaster-0.0.27.tar.gz/CryptMaster-0.0.27/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
+
+setup(
+    name="The Crypt Master",
+    version='0.0.27',
+    author="Huth S0lo",
+    author_email="john@themorphium.io",
+    description="Crypt Master Client for use with a Crypt Master Server",
+    url = "https://github.com/TheCryptMaster/CryptMaster",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pycryptodome', 'httpx', 'argon2-cffi', 'platformdirs', 'pypedreams'],
+    keywords=['pypi', 'cicd', 'python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
