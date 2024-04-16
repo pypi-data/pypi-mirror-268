8d58625699dbdf06379ac33f0afbcb2e30985eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:03:28 +0000

    Add package ParendumLib/0.1.17/ParendumLib-0.1.17.tar.gz

---
+++ packages/ParendumLib/ParendumLib-0.1.17.tar.gz/ParendumLib-0.1.17/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "fastapi",
+    "requests",
+    "motor",
+    "pydantic",
+    "aiohttp",
+    "python-multipart",
+    "fastapi-sessions",
+    "itsdangerous",
+    "pyotp",
+    "jinja2",
+    "pillow"
+]
+
+setup(
+    name='ParendumLib',
+    version='0.1.17',
+    packages=find_packages(),
+    install_requires=requirements,
+    author='Parendum',
+    author_email='info@parendum.com',
+    description='Parendum Official Library',
+    keywords='logger',
+)
