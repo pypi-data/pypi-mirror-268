8a17f8c17184f27b86961bbb0c30360a2e6aba51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:03:40 +0000

    Add package bqskit/1.1.2/bqskit-1.1.2.tar.gz

---
+++ packages/bqskit/bqskit-1.1.2.tar.gz/bqskit-1.1.2/bqskit/ir/gates/composed/__init__.py
@@ -0,0 +1,18 @@
+"""This package contains composed gates."""
+from __future__ import annotations
+
+from bqskit.ir.gates.composed.controlled import ControlledGate
+from bqskit.ir.gates.composed.daggergate import DaggerGate
+from bqskit.ir.gates.composed.embedded import EmbeddedGate
+from bqskit.ir.gates.composed.frozenparam import FrozenParameterGate
+from bqskit.ir.gates.composed.tagged import TaggedGate
+from bqskit.ir.gates.composed.vlg import VariableLocationGate
+
+__all__ = [
+    'ControlledGate',
+    'DaggerGate',
+    'EmbeddedGate',
+    'FrozenParameterGate',
+    'TaggedGate',
+    'VariableLocationGate',
+]
