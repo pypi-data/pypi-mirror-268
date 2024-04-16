7228f8ba30f0f01376b06ff4567094b42c0c629c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 16:03:32 +0000

    Add package jhu/1.5.0/jhu-1.5.0.tar.gz

---
+++ packages/jhu/jhu-1.5.0.tar.gz/jhu-1.5.0/setup.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+
+"""
+制作PIP包教程
+1. 先设置一个setup文件
+2. 运行setup.py sdist
+3. 然后会得到一个dist文件, 这个时候就可用pip install dist/xxxxx 来安装文件了
+4. 到PYPI上创建一个API Token(现在不支持user/passwd模型上传package了)
+5. 在本地的创建一个$HOME/.pypirc文件
+    其中:
+    [pypi]
+    username = __token__
+    password = # 这里改成放申请下来的token
+6. twine upload dist/* 可上传安装,需要输入pypi的用户名和密码
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='jhu',  # 包的名称
+    version='1.5.0',  # 包的版本,每次更新或升级包都需要更新它
+    description='jhu是一个工具包,简化或自动化的做一些任务',  # 包的描述
+    author='J.Hu',  # 作者
+    email='joestarhu@163.com',  # 作者邮箱
+    url='https://github.com/joestarhu/jhu',  # 项目地址
+    packages=find_packages(
+        exclude=['test', 'examples', 'script', 'tutorials']),   # 包内不需要引用的文件
+    classifiers=[
+        "Environment :: Web Environment",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3.12',
+    ],
+    install_requires=[
+        'requests',         # auth webhook
+        'python-jose',      # security.py
+        'bcrypt',           # security.py
+        'pycryptodomex',    # security.py
+        'sqlalchemy',       # data.py
+        ],  # 依赖的包
+    zip_safe=True
+)
