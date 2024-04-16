8785d8d06812a4c121d8b758e40e0582e44427c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:03:57 +0000

    Add package dj-tenants/0.1.0/dj_tenants-0.1.0.tar.gz

---
+++ packages/dj-tenants/dj_tenants-0.1.0.tar.gz/dj_tenants-0.1.0/dj_tenants/__init__.py
@@ -0,0 +1,11 @@
+from dj_tenants.context import (
+    get_current_tenant,
+    tenant_context,
+    tenant_context_disabled,
+)
+
+__all__ = [
+    "get_current_tenant",
+    "tenant_context",
+    "tenant_context_disabled",
+]
