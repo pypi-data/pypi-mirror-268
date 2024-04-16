9348f7c75fbf36905d917aae3572ee3285e038cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 15:03:28 +0000

    Add package webscout/1.3.0/webscout-1.3.0-py3-none-any.whl

---
+++ packages/webscout/webscout-1.3.0-py3-none-any.whl/webscout/__init__.py
@@ -0,0 +1,18 @@
+"""Webscout.
+
+Search for words, documents, images, videos, news, maps and text translation 
+using the Google, DuckDuckGo.com, yep.com, phind.com, you.com, etc Also containes AI models
+"""
+
+import logging
+from .webscout_search import WEBS
+from .webscout_search_async import AsyncWEBS
+from .version import __version__
+from .DWEBS import DeepWEBS
+from .transcriber import transcriber
+from .voice import play_audio
+
+
+__all__ = ["WEBS", "AsyncWEBS", "__version__", "cli"]
+
+logging.getLogger("webscout").addHandler(logging.NullHandler())
