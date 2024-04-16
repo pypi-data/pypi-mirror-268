dc8de5ae1cdac9c6ef82ac7da528f6b7195054cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:03:15 +0000

    Add package waitcondition-hook-for-aws-fargate-task/1.0.6/waitcondition_hook_for_aws_fargate_task-1.0.6-py3-none-any.whl

---
+++ packages/waitcondition-hook-for-aws-fargate-task/waitcondition_hook_for_aws_fargate_task-1.0.6-py3-none-any.whl/waitcondition-hook-for-aws-fargate-task/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "waitcondition-hook-for-aws-fargate-task",
+    "1.0.6",
+    __name__[0:-6],
+    "waitcondition-hook-for-aws-fargate-task@1.0.6.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
