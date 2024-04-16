d5b1161228cf36f8c4f09d07cdab37ef5ddffd12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 06:03:22 +0000

    Add package quant2/0.1.3/quant2-0.1.3-py3-none-any.whl

---
+++ packages/quant2/quant2-0.1.3-py3-none-any.whl/quant/football/transforms/__init__.py
@@ -0,0 +1,14 @@
+from . import table20240326, table20240410, table20240414, table20240415
+
+
+def get_table_factory(table_name):
+    if table_name == "table20240326":
+        return table20240326
+    elif table_name == "table20240410":
+        return table20240410
+    elif table_name == "table20240414":
+        return table20240414
+    elif table_name == "table20240415":
+        return table20240415
+    else:
+        raise NotImplementedError(f"Not supported <{table_name=}>.")
