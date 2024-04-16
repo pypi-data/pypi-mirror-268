4c90fc074a1f78ee1616c05170655f51f699fcb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 14:04:22 +0000

    Add package pynfe_xml2dict/0.0.5/pynfe_xml2dict-0.0.5.tar.gz

---
+++ packages/pynfe_xml2dict/pynfe_xml2dict-0.0.5.tar.gz/pynfe_xml2dict-0.0.5/pynfe_xml2dict/nfe/__tag_ide__.py
@@ -0,0 +1,23 @@
+from typing import Dict
+from pynfe_xml2dict.utils.functions import returnDataInDictOrArray, treatsFieldAsNumber
+
+
+class TagIde():
+    def __init__(self, dataTagIde: dict):
+        self.__dataTagIde = dataTagIde
+        self.__objIde: Dict[str, str] = {}
+
+    def getData(self):
+        self.__objIde['numero_nf'] = treatsFieldAsNumber(returnDataInDictOrArray(self.__dataTagIde, ['nNF']))
+        self.__objIde['modelo_nf'] = treatsFieldAsNumber(returnDataInDictOrArray(self.__dataTagIde, ['mod']))
+        self.__objIde['serie_nf'] = returnDataInDictOrArray(self.__dataTagIde, ['serie'])
+        self.__objIde['data_emissao'] = returnDataInDictOrArray(self.__dataTagIde, ['dhEmi'])
+        self.__objIde['data_saida_entrada'] = returnDataInDictOrArray(self.__dataTagIde, ['dhSaiEnt'])
+        self.__objIde['tipo_documento'] = treatsFieldAsNumber(returnDataInDictOrArray(self.__dataTagIde, 'tpNF'))
+        self.__objIde['identificador_local_destino_operacao'] = treatsFieldAsNumber(
+            returnDataInDictOrArray(self.__dataTagIde, ['idDest']))
+        self.__objIde['municipio_ocorrencia_fato'] = treatsFieldAsNumber(
+            returnDataInDictOrArray(self.__dataTagIde, ['cMunFG']))
+        self.__objIde['finalidade_nf'] = treatsFieldAsNumber(returnDataInDictOrArray(self.__dataTagIde, ['finNFe']))
+
+        return self.__objIde
