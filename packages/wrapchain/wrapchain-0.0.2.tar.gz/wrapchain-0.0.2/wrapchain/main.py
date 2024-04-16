c28e63ce4757f61d7353cb6a88af1ab3aeaa30cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:03:18 +0000

    Add package wrapchain/0.0.2/wrapchain-0.0.2.tar.gz

---
+++ packages/wrapchain/wrapchain-0.0.2.tar.gz/wrapchain-0.0.2/wrapchain/main.py
@@ -0,0 +1,15 @@
+from wrapchain import WrapChain
+
+if __name__ == "__main__":
+
+    wc = WrapChain()
+
+    with open("test.png", "rb") as image_file:
+        result = wc.document(
+            image_file=image_file,
+            prompt="""
+            アップロードされる画像の日本語を読み取って下さい.わからないまたは不明な場合は正直にその旨を返答して下さい.
+            """,
+        )
+
+    print(result)
