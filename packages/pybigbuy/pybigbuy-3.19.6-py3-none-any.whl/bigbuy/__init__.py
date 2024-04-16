b3aa84d9a826d0321e9c91bf5453e370b738fd9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 16:03:33 +0000

    Add package pybigbuy/3.19.6/pybigbuy-3.19.6-py3-none-any.whl

---
+++ packages/pybigbuy/pybigbuy-3.19.6-py3-none-any.whl/bigbuy/__init__.py
@@ -0,0 +1,18 @@
+"""
+Bigbuy
+-------
+
+Bigbuy is a library for Python that wraps the BigBuy API.
+"""
+
+__author__ = 'Bixoto <tech@bixoto.com>'
+__version__ = '3.19.6'
+
+from .api import BigBuy
+from .exceptions import (
+    BBError, BBResponseError, BBPackError, BBExportError, BBProductError, BBStockError,
+    BBNoCarrierError, BBBankWireTooLowError, BBMoneyBoxTooLowError, BBTemporaryOrderError, BBOrderAlreadyExistsError,
+    BBOrderTooLowError, BBIncorrectRefError, BBInvalidPaymentError, BBZipcodeFormatError, BBProductNotFoundError,
+    BBServerError, BBRateLimitError, BBValidationError, BBWarehouseSplitError, BBShippingError, BBTimeoutError,
+)
+from .rate_limit import RateLimit
