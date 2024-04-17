277de30417c8264b21a0134aee984ba877b3b7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 14:04:39 +0000

    Add package spectral-recovery/0.4.1/spectral_recovery-0.4.1-py3-none-any.whl

---
+++ packages/spectral-recovery/spectral_recovery-0.4.1-py3-none-any.whl/spectral_recovery/_utils.py
@@ -0,0 +1,166 @@
+"""Utility functions for spectral-recovery."""
+
+import functools
+import spyndex as spx
+import xarray as xr
+
+from rioxarray.exceptions import MissingCRS
+from prettytable import PrettyTable, ALL
+
+
+def maintain_rio_attrs(func: callable) -> callable:
+    """A wrapper for maintaining rioxarray crs/encoding info.
+
+    Rioxarray information (nodata, CRS, etc.) is lost through
+    operations that create new instances of an xarray object
+    because new accessors (e.g rio) are created for every instance.
+
+    This method ensures tnhat a returned xarray object has the
+    same rio CRS and encodig as the input xarray object.
+
+    Notes
+    -----
+    Similar to https://github.com/pydata/xarray/pull/2482, but specifically
+    for a subset of rio attributes to be maintained.
+
+    """
+
+    @functools.wraps(func)
+    def wrapper_maintain_rio_attrs(*args, **kwargs):
+        """
+        Returns
+        --------
+        result : xr.DataArray
+            DataArray object returned by func with spatial attrs
+
+        Notes
+        -----
+        The first argument passed to a wrapped function (keyword or
+        non-keyword) must be the xarray DataArray whose rio info
+        is to be maintained.
+
+        """
+        kwarg_vals = list(kwargs.values())
+        # Take the first xarray arg
+        arg_da = [isinstance(arg, xr.DataArray) for arg in args]
+        kwarg_da = [isinstance(kwv, xr.DataArray) for kwv in kwarg_vals]
+        if sum(arg_da + kwarg_da) > 1:
+            epsgs = []
+            for i, val in enumerate(arg_da):
+                if val:
+                    crs = args[i].rio.crs
+                    epsgs.append(crs)
+            for i, val in enumerate(kwarg_da):
+                if val:
+                    crs = kwarg_vals[i].rio.crs
+                    epsgs.append(crs)
+            if not epsgs.count(epsgs[0]) == len(epsgs):
+                raise ValueError(f"Ambiguous input for wrapper. CRS on xarray.DataArray inputs do not match.")
+        for i, val in enumerate(arg_da):
+            if val:
+                xarray_obj = args[i]
+        for i, val in enumerate(kwarg_da):
+            if val:
+                xarray_obj = kwarg_vals[i]
+
+        result = func(*args, **kwargs)
+        try:
+            result.rio.write_crs(xarray_obj.rio.crs, inplace=True)
+        except MissingCRS:
+            # TODO: add warning log here?
+            pass
+        result.rio.update_encoding(xarray_obj.encoding, inplace=True)
+        return result
+
+    return wrapper_maintain_rio_attrs
+
+
+def common_and_long_to_short(standard):
+    """Dict of short and common names to standard names
+
+    Notes
+    -----
+    This manually changes the G1, RE1, RE2, and RE3 common
+    names to green1, rededge1, rededge2, and rededge3 respectively
+    to be less ambiguous. This means that the common names returned
+    will be slightly different than those used in spyndex.
+
+    """
+    # make 'green' and 'rededge' common names unambiguous
+    spx.bands["G1"].common_name = "green1"
+    spx.bands["RE1"].common_name = "rededge1"
+    spx.bands["RE2"].common_name = "rededge2"
+    spx.bands["RE3"].common_name = "rededge3"
+
+    common_and_short = {}
+    for band in standard:
+        common_and_short[spx.bands[band].short_name] = band
+        common_and_short[spx.bands[band].common_name] = band
+    return common_and_short
+
+
+def bands_pretty_table():
+    """Create a PrettyTable of all bands (names and id info).
+
+    Returns
+    -------
+    band_table : PrettyTable
+        table for displaying short names, common names, long
+        names, wavelength and platform info for bands in the
+        spyndex package.
+
+    """
+    band_table = PrettyTable()
+    band_table.hrules = ALL
+    band_table.field_names = [
+        "Standard/Short Name",
+        "Common Name",
+        "Long Name",
+        "Wavelength (min, max)",
+        "Platforms",
+    ]
+    for st in list(spx.bands):
+        platforms = _format_platforms(_platforms_from_band(spx.bands[st]), 3)
+        band_table.add_row([
+            st,
+            spx.bands[st].common_name,
+            spx.bands[st].long_name,
+            f"{spx.bands[st].min_wavelength, spx.bands[st].max_wavelength}",
+            platforms,
+        ])
+    return band_table
+
+
+def _platforms_from_band(band_object):
+    """Get list of platform names supported by each band"""
+    platforms = []
+    for p in [
+        "sentinel2a",
+        "sentinel2b",
+        "landsat4",
+        "landsat5",
+        "landsat7",
+        "landsat8",
+        "landsat9",
+        "modis",
+        "planetscope",
+    ]:
+        try:
+            platforms.append(getattr(band_object, p).platform)
+        except AttributeError:
+            continue
+    return platforms
+
+
+def _format_platforms(comment_list, max_items_on_line):
+    """Format list of platform strs into prettier multi-line str"""
+    ACC_length = 0
+    formatted_comment = ""
+    for word in comment_list:
+        if ACC_length + 1 < max_items_on_line:
+            formatted_comment = formatted_comment + word + ", "
+            ACC_length = ACC_length + 1
+        else:
+            formatted_comment = formatted_comment + "\n" + word + ", "
+            ACC_length = +1
+    return formatted_comment
---
+++ packages/spectral-recovery/spectral_recovery-0.4.1-py3-none-any.whl/spectral_recovery/indices.py
@@ -0,0 +1,169 @@
+"""Methods for computing spectral indices.
+
+Most functions are decorated with `compatible_with` and `requires_bands` decorators, 
+which check that the input stack is compatible with the function and that the stack
+contains the required bands.
+
+Most notably, exports the `compute_indices` function, which computes a stack of
+spectral indices from a stack of images and str of index names.
+
+"""
+
+import copy
+import json
+
+import xarray as xr
+import importlib.resources as pkg_resources
+import spyndex as spx
+
+from typing import List, Dict
+
+from spectral_recovery._utils import maintain_rio_attrs
+
+# Set up global index configurations:
+#    1. Only support vegetation and burn indices
+#    2. Init index-specific constant defaults 
+SUPPORTED_DOMAINS = ["vegetation", "burn"]
+with pkg_resources.open_text("spectral_recovery.resources", "constant_defaults.json") as f:
+    INDEX_CONSTANT_DEFAULTS = json.load(f)
+
+@maintain_rio_attrs
+def compute_indices(
+    image_stack: xr.DataArray, indices: list[str], constants: dict = {}, **kwargs
+):
+    """Compute spectral indices using the spyndex package.
+
+
+    Parameters
+    ----------
+    image_stack : xr.DataArray
+        stack of images.
+    indices : list of str
+        list of spectral indices to compute
+    constants : dict of flt
+        constant and value pairs e.g {"L": 0.5}
+    kwargs : dict, optional
+        Additional kwargs for wrapped spyndex.computeIndex function.
+
+    Returns
+    -------jmk,
+        xr.DataArray: stack of images with spectral indices stacked along
+        the band dimension.
+
+    """
+    if _supported_domain(indices):
+        params_dict = _build_params_dict(image_stack)
+        constants_dict = _build_constants_dict(indices, constants)
+            
+        params_dict = params_dict | constants_dict | kwargs
+        index_stack = spx.computeIndex(index=indices, params=params_dict)
+        try:
+            # rename 'index' dim to 'bands' to match tool's expected dims
+            index_stack = index_stack.rename({"index": "band"})
+        except ValueError:
+            # computeIndex will not return an index dim if only 1 index passed
+            index_stack = index_stack.expand_dims(dim={"band": indices})
+    return index_stack
+
+
+def _supported_domain(indices: list[str]):
+    """Determine whether indices application domains are supported by tool.
+
+    Parameters
+    ----------
+    indices : list of str
+        list of indices
+
+    Raises
+    ------
+    ValueError
+        If any index has an unsupported application domain.
+    """
+    for i in indices:
+        if spx.indices[i].application_domain not in SUPPORTED_DOMAINS:
+            raise ValueError(
+                "only application domain 'vegetation' and 'burn' are supported (index"
+                f" {i} has application domain '{spx.indices[i].application_domain}')"
+            ) from None
+    return True
+
+
+def _build_params_dict(image_stack: xr.DataArray):
+    """Build dict of standard names and slices required by computeIndex.
+
+    Slices will be taken along the band dimension of image_stack,
+    selecting for each of the standard band/constant names that computeIndex
+    accepts. Any name that is not in image_stack will not be included
+    in the dictionary.
+
+    Parameters
+    ----------
+    image_stack : xr.DataArray
+        DataArray from which to take slices. Must have a band
+        dimension and band coordinates value should be standard names
+        for the respective band. For more info, see here:
+        https://github.com/awesome-spectral-indices/awesome-spectral-indices
+
+    Returns
+    -------
+    band_dict : dict
+        Dictionary mapping standard names to slice of image_stack.
+
+    """
+    standard_names = list(spx.bands)
+    params_dict = {}
+    for standard in standard_names:
+        try:
+            band_slice = image_stack.sel(band=standard)
+            params_dict[standard] = band_slice
+        except KeyError:
+            continue
+
+    return params_dict
+
+def _build_constants_dict(indices: List, constants: Dict) -> Dict:
+    """ Build dict of constants and values for the requested indices.
+
+    
+    Parameters
+    ----------
+    indices: list of str
+        The requested indices.
+    constants : dict
+        Given (i.e non-default) constants and constant values.
+    
+    Returns
+    -------
+    constants_dict : dict
+        Dict of required constants for requested indices. Default
+        values are used if a required constant is not in `constants`.
+
+    Raises
+    ------
+    ValueError
+        - If a required constant/value is not provided in `constants`
+        and the default value is null/None.
+        - If a required constant/value is not provided in `constants`
+        and more than one index uses a different default value. 
+
+    """
+    constants_dict = copy.deepcopy(constants)
+    given_constants = list(constants_dict.keys())
+    for i in indices:
+        try:
+            default_constants = INDEX_CONSTANT_DEFAULTS[i]
+        except KeyError:
+            continue
+        for c, v in default_constants["defaults"].items():
+            if v is None:
+                raise ValueError(f"No default value for {c} available (required by {i}). Please provide a value for {c} with the `constants` param.")
+            # If more than one index needs the same constant and the constant value wasn't given in `constants` dict
+            # (i.e must use defaults), check that the default values match o.w fail
+            if c not in given_constants:
+                if c in constants_dict:
+                        if constants_dict[c] != v:
+                            raise ValueError(f"Cannot use default values for constants because {c} has more than one default value between the set of indices. Please provide a value for {c} with the `constants` param.")
+                else:
+                    # Set constant value to the default value
+                    constants_dict[c] = v
+    return constants_dict
