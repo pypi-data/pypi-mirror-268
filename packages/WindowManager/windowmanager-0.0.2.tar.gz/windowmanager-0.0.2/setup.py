68ec6471b26085e1fc9f0b4de9c29593a7d1289e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 03:04:11 +0000

    Add package WindowManager/0.0.2/windowmanager-0.0.2.tar.gz

---
+++ packages/WindowManager/windowmanager-0.0.2.tar.gz/windowmanager-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import pathlib
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.rst").read_text(encoding="utf-8")
+setup(
+name='WindowManager',
+version='0.0.2',
+author='Itamar Katzover',
+author_email='itamar43.katzover43@gmail.com',
+description='WindowManager: Simplified window management.',
+long_description_content_type = 'text/x-rst',
+long_description=long_description,
+packages=find_packages(),
+classifiers=[
+"Development Status :: 3 - Alpha",
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+],
+python_requires=">=3.10",
+)
