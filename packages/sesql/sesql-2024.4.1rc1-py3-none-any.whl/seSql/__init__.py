1eb1f6e08fec698cd4cf701ea6b6610ef0d73129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 14:04:39 +0000

    Add package sesql/2024.4.1rc1/sesql-2024.4.1rc1-py3-none-any.whl

---
+++ packages/sesql/sesql-2024.4.1rc1-py3-none-any.whl/seSql/__init__.py
@@ -0,0 +1,9 @@
+from ._version import __version__, __setuptools__
+from .sql import sql
+from .dbc.Utilities import mask, hostIP, hostName
+from seCore.CustomLogging import logger
+
+seSqlVersion = __version__.public_build()
+
+__all__ = ['sql', 'mask', 'seSqlVersion', 'hostIP', 'hostName', 'logger']
+
