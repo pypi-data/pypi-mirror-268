ea1d766293353e4ce182ae0fc783b795be1efc6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 16:04:16 +0000

    Add package istr-python/0.0.3/istr_python-0.0.3.tar.gz

---
+++ packages/istr-python/istr_python-0.0.3.tar.gz/istr_python-0.0.3/istr/sendmoremoney.py
@@ -0,0 +1,12 @@
+import itertools
+from istr import istr
+import istr
+print(istr,istr(8))
+
+for s, e, n, d, m, o, r, y in istr(itertools.permutations("0123456789", 8)):
+    if (m > 0) and ((s | e | n | d) + (m | o | r | e) == (m | o | n | e | y)):
+        print(f" {s|e|n|d}")
+        print(f" {m|o|r|e}")
+        print("-----")
+        print(m | o | n | e | y)
+
