789498b4800a54fe10c52b7f17a8e25536147617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 03:03:50 +0000

    Add package scisuit/1.2.5/scisuit-1.2.5-py3-none-any.whl

---
+++ packages/scisuit/scisuit-1.2.5-py3-none-any.whl/scisuit/integ.py
@@ -0,0 +1,116 @@
+import ctypes as _ct
+import math as _math
+import numbers as _numbers
+import types as _types
+from typing import Iterable
+
+import numpy as _np
+
+from ._ctypeslib import pydll as _pydll
+
+
+def trapz(x:list|_np.ndarray, y:list|_np.ndarray)->float:
+    """
+    Computes area using trapezoidal method and uses Numpy's trapz method.
+
+    ## Input: 
+    x, y: list or ndarray
+    """
+    return _np.trapz(x=x, y=y)
+
+
+
+def cumtrapz(x:list|_np.ndarray, y:list|_np.ndarray)->list:
+	"""
+	Computes the left-tailed cumulative area
+
+	##Input:
+	x, y: list or ndarray
+	"""
+	val = 0.0
+	a, b, f_a, f_b = 0.0, 0.0, 0.0, 0.0
+	retList =[0.0]
+
+	for i in range(len(x)-1):
+		a = x[i]
+		b = x[i + 1]
+
+		if _math.isclose(b, a, abs_tol = 1E-5):
+			raise ValueError("|X(i+1)-X(i)|<1E-5")
+
+		if (b < a):
+			raise ValueError("X(i+1) > X(i) expected.")
+
+		f_a = y[i]
+		f_b = y[i + 1]
+
+		val += (b - a) * (f_a + f_b) / 2.0
+
+		retList.append(val)
+	
+	return retList
+
+
+def simpson(x:Iterable, y:Iterable)->float:
+	"""
+	Uses simpson's method (1/3 and 3/8)
+
+	#Input:
+	x,y: Iterable
+	"""
+
+	return _pydll.c_integ_simpson(x, y)
+
+
+
+def romberg(f:_types.FunctionType, 
+	a:float, 
+	b:float, 
+	tol=1E-5, 
+	maxiter=100)->float:
+	"""
+	Romberg integration
+
+	#Input:
+	f: Univariate function \n
+	a, b: limits of integration (b>a) \n
+	tol:	tolerance for error \n
+	maxiter: Maximum number of iterations
+	"""
+	assert callable(f), "f must be function"
+	assert isinstance(a, _numbers.Real), "a must be real number"
+	assert isinstance(b, _numbers.Real), "b must be real number"
+	assert isinstance(tol, _numbers.Real), "tol must be real number"
+	assert isinstance(maxiter, _numbers.Integral), "maxiter must be int"
+
+	return _pydll.c_integ_romberg(
+		f, 
+		_ct.c_double(a), 
+		_ct.c_double(b), 
+		_ct.c_double(tol), 
+		_ct.c_int(maxiter))
+
+
+
+def fixed_quad(f:_types.FunctionType, 
+	a:float, 
+	b:float, 
+	n:int=5)->float:
+	"""
+	Computes a definite integral using fixed-order Gaussian quadrature.
+
+	#Input:
+	f: Univariate function \n
+	a, b: limits of integration (b>a) \n
+	n: Order of quadrature integration (2<=n<=6)
+	"""
+	assert callable(f), "f must be function"
+	assert isinstance(a, _numbers.Real), "a must be real number"
+	assert isinstance(b, _numbers.Real), "b must be real number"
+	assert isinstance(n, _numbers.Integral), "n must be int"
+
+	return _pydll.c_integ_fixed_quad(
+		f, 
+		_ct.c_double(a), 
+		_ct.c_double(b),  
+		_ct.c_int(n))
---
+++ packages/scisuit/scisuit-1.2.5-py3-none-any.whl/scisuit/plot/_moody.py
@@ -0,0 +1,118 @@
+import math
+from typing import Iterable
+
+from ..roots import brentq
+from ._charts import canvas, title, xlabel, xscale, ylabel, yscale
+from .gdi import arrow, curve, line, rect, text
+
+
+def _Colebrook(f, Re, E_D):
+	temp1 = E_D/3.7 + 2.51/(Re*math.sqrt(f)) 
+	temp2 = 2.0* math.log10(temp1) 
+	return 1/math.sqrt(f) + temp2
+
+
+def _TurbulenceOnset(E_D):	
+	"""
+	compute friction factor
+	A modified version of Colebrook's equation for large Re
+	"""
+	def _vonKarman(E_D):
+		f = -2.0* math.log10(E_D/3.7) 
+		return (1/f)**2
+	
+	"""
+	compute Reynolds number
+	Transactions of the ASME, 66 (8): 671-684 (Page: 676)
+	"""
+	def _Reynolds(f):
+		return 1/math.sqrt(f)*200/E_D
+	
+	f = _vonKarman(E_D)
+	Re = _Reynolds(f)
+	return Re, f
+
+
+
+def moody(
+		Re:Iterable = [1E3, 1E7],
+		friction:Iterable = [0.01, 0.09],
+		showlaminar = True,
+		showtransition = True,
+		showturbulent = True):
+	"""
+	`Re:` Reynolds number range
+	`friction:`  Friction factor range
+	`showlaminar:` show laminar line and related info
+	`showtransition:` show transition region and related info
+	`showturbulent:` show fully turbulent line and related info
+	"""
+	assert isinstance(Re, Iterable), "Re must be iterable"
+	assert len(Re)==2, "Re must have length 2"
+
+	assert isinstance(friction, Iterable), "friction must be iterable"
+	assert len(friction)==2, "friction must have length 2"
+
+	canvas(
+		x=(float(Re[0]), float(Re[1])), 
+		y=(float(friction[0]), float(friction[1])))
+	xscale("log")
+	yscale("log")
+
+	"""
+	laminar flow 0<Re<2300
+	"""
+	if showlaminar:
+		fD = lambda _Re: 64.0/_Re
+		p1 = (1000, fD(1000))
+		p2 = (2300, fD(2300))
+		line(p1=p1, p2=p2, lw=2, ls="--", ec="#FF0000")
+		text(xy=p2, label="Laminar Flow\nf = 64/Re", hanchor="c")
+
+	"""
+	transition region 2300<Re<4000
+	btmleft: x (onset of transition), y(roughly 64/Re)
+	rectangle's width = 4000-2300=1700, height=0.08-0.03
+	"""
+	if showtransition:
+		btmleft = (2300, 0.03)
+		rect(xy=btmleft, width=1700, height=0.05, hatch="solid", alpha=0.5, ls="--", fc="#808080")
+		text(xy=(3300, 0.06), label="Transition Region", rotation=-90, labelcolor="#A52A2A")
+
+	"""
+	Turbulent Region - Re>=4000
+	"""
+	Re_Turbulent = [4000, 6000, 9000, 12E3, 20E3, 30E3, 60E3, 120E3, 250E3, 1E6, 5E6]
+	e_d = [5E-5, 1E-4, 5E-4, 1E-3,2.5E-3, 5E-3, 0.01, 0.02, 0.03, 0.04, 0.05]
+
+	TurbulenceLine = []
+	for _e_d in e_d:
+		x, y = [], []
+		TurbulenceLine.append(_TurbulenceOnset(_e_d))
+		for _Re in Re_Turbulent:
+			_cb = lambda f: _Colebrook(f, _Re, _e_d)
+			friction, _ = brentq(_cb, a=0.001, b=0.08)
+			x.append(_Re)
+			y.append(friction)
+
+		text(xy=(_Re, friction), label=str(_e_d), vanchor="c")
+		curve(x, y, lw=2, ec="#A52A2A")
+
+	"""
+	fully rough turbulent flow line
+	E/D=0.06 appended so that the dashed line will be slightly above E/D=0.05 line
+	"""
+	if showturbulent:
+		TurbulenceLine.append(_TurbulenceOnset(0.06))
+		_turbulence = list(zip(*TurbulenceLine))
+		curve(x=_turbulence[0], y=_turbulence[1], lw=2, ls="--")
+		
+		midpoint = int(len(TurbulenceLine)/2)
+		xx, yy=_turbulence[0][midpoint], _turbulence[1][midpoint]
+		_p1 = (15E3, 0.015)
+		arrow(p1=_p1, p2=(xx,yy), lw=2, length=0.05)
+		text(xy=_p1, label="Complete Turbulence", hanchor="c")
+
+	xlabel("Reynolds Number")
+	ylabel("Friction Factor")
+	title("Moody Diagram")
