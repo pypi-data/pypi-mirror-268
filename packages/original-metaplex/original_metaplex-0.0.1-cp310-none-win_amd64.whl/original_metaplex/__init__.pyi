b3a105879ee0ae0822adb16ad9c03b5b9e75e126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 04:05:05 +0000

    Add package original-metaplex/0.0.1/original_metaplex-0.0.1-cp310-none-win_amd64.whl

---
+++ packages/original-metaplex/original_metaplex-0.0.1-cp310-none-win_amd64.whl/original_metaplex/__init__.pyi
@@ -0,0 +1,13 @@
+from typing import Optional, Tuple
+
+def call_metaplex_function() -> str: ...
+def create_collection_v1(
+    payer: str,
+    name: str,
+    uri: str,
+    latest_blockhash: str,
+    update_delegate: Optional[str] = None,
+) -> Tuple[str, str]: ...
+def create_v1(
+    payer: str, name: str, uri: str, owner: str, collection: str, latest_blockhash: str
+) -> Tuple[str, str]: ...
