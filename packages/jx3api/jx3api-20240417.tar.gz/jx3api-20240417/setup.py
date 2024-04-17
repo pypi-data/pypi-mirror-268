19f1a568c6c17ebf886752bab115d3f36d10d053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 16:04:18 +0000

    Add package jx3api/20240417/jx3api-20240417.tar.gz

---
+++ packages/jx3api/jx3api-20240417.tar.gz/jx3api-20240417/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="jx3api",
+    version="20240417",
+    description="The Python SDK to the JX3API",
+    long_description="The Python SDK to the JX3API",
+    author="JX3API",
+    url="https://www.jx3api.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    install_requires=["aiohttp"],
+)
