0b9ff736c13850dbb69f0fd237c6fff0135a614e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 05:03:20 +0000

    Add package sare/0.1/sare-0.1-py3-none-any.whl

---
+++ packages/sare/sare-0.1-py3-none-any.whl/sare/__init__.py
@@ -0,0 +1,232 @@
+def mm():
+    print("""
+def printBoard(board):
+    print(board[1] + '|' + board[2] + '|' + board[3])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print("\n")
+
+
+def spaceIsFree(position):
+    if board[position] == ' ':
+        return True
+    else:
+        return False
+
+
+def insertLetter(letter, position):
+    if spaceIsFree(position):
+        board[position] = letter
+        printBoard(board)
+        if (checkDraw()):
+            print("Draw!")
+            exit()
+        if checkForWin():
+            if letter == 'X':
+                print("Bot wins!")
+                exit()
+            else:
+                print("Player wins!")
+                exit()
+        return
+
+
+    else:
+        print("Can't insert there!")
+        position = int(input("Please enter new position:  "))
+        insertLetter(letter, position)
+        return
+
+
+def checkForWin():
+    if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] != ' '):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] != ' '):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] != ' '):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] != ' '):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] != ' '):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] != ' '):
+        return True
+    else:
+        return False
+
+
+def checkWhichMarkWon(mark):
+    if board[1] == board[2] and board[1] == board[3] and board[1] == mark:
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] == mark):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] == mark):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] == mark):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] == mark):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] == mark):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] == mark):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] == mark):
+        return True
+    else:
+        return False
+
+
+def checkDraw():
+    for key in board.keys():
+        if (board[key] == ' '):
+            return False
+    return True
+
+
+def playerMove():
+    position = int(input("Enter the position for 'O':  "))
+    insertLetter(player, position)
+    return
+
+
+def compMove():
+    bestScore = -800
+    bestMove = 0
+    for key in board.keys():
+        if (board[key] == ' '):
+            board[key] = bot
+            score = minimax(board, 0, False)
+            board[key] = ' '
+            if (score > bestScore):
+                bestScore = score
+                bestMove = key
+    insertLetter(bot, bestMove)
+    return
+
+
+def minimax(board, depth, isMaximizing):
+    if (checkWhichMarkWon(bot)):
+        return 1
+    elif (checkWhichMarkWon(player)):
+        return -1
+    elif (checkDraw()):
+        return 0
+    if (isMaximizing):
+        bestScore = -800
+        for key in board.keys():
+            if (board[key] == ' '):
+                board[key] = bot
+                score = minimax(board, depth + 1, False)
+                board[key] = ' '
+                if (score > bestScore):
+                    bestScore = score
+        return bestScore
+    else:
+        bestScore = 800
+        for key in board.keys():
+            if (board[key] == ' '):
+                board[key] = player
+                score = minimax(board, depth + 1, True)
+                board[key] = ' '
+                if (score < bestScore):
+                    bestScore = score
+        return bestScore
+
+
+board = {1: ' ', 2: ' ', 3: ' ',
+        4: ' ', 5: ' ', 6: ' ',
+        7: ' ', 8: ' ', 9: ' '}
+
+
+printBoard(board)
+print("Computer goes first! Good luck.")
+print("Positions are as follow:")
+print("1, 2, 3 ")
+print("4, 5, 6 ")
+print("7, 8, 9 ")
+print("\n")
+player = 'O'
+bot = 'X'
+
+
+global firstComputerMove
+firstComputerMove = True
+
+
+while not checkForWin():
+    compMove()
+    playerMove()
+""")
+
+def ab():
+    print("""
+MAX, MIN = 1000, -1000
+
+
+def minimax(depth, nodeIndex, maximizingPlayer,
+      values, alpha, beta):
+
+
+  if depth == 3:
+    return values[nodeIndex]
+
+
+  if maximizingPlayer:
+    best = MIN
+
+
+    for i in range(0, 2):
+      
+      val = minimax(depth + 1, nodeIndex * 2 + i,
+            False, values, alpha, beta)
+      best = max(best, val)
+      alpha = max(alpha, best)
+
+
+      if beta <= alpha:
+        break
+    
+    return best
+  
+  else:
+    best = MAX
+
+
+    for i in range(0, 2):
+    
+      val = minimax(depth + 1, nodeIndex * 2 + i,
+              True, values, alpha, beta)
+      best = min(best, val)
+      beta = min(beta, best)
+
+
+      if beta <= alpha:
+        break
+    
+    return best
+  
+if __name__ == "__main__":
+  values = [3, 5, 6, 9, 1, 2, 0, -1]
+  print("The optimal value is :", minimax(0, 0, True, values, MIN, MAX))
+
+""")
+def lop():
+    print("""
+from kanren import run,var,Relation, facts, conde, eq
+
+
+parent = Relation()
+facts(parent, ("Bob", "Alice"), ("Bob", "Charlie"), ("Charlie", "David"))
+def grandparent(x, z):
+  y = var()
+  return conde((parent(x, y), parent(y, z)))
+x = var()
+results = run(0, x, grandparent(x, "David"))
+print(results)
+""")
