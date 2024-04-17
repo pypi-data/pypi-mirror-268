d30db7bd50ba6dcdc24fa2f65c17d6b2701357d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 17:03:49 +0000

    Add package TkEasyGUI/0.2.47/tkeasygui-0.2.47.tar.gz

---
+++ packages/TkEasyGUI/tkeasygui-0.2.47.tar.gz/tkeasygui-0.2.47/TkEasyGUI/utils.py
@@ -0,0 +1,27 @@
+from typing import Literal, Union, TypeVar
+import pyperclip
+
+# define TypeAlias
+Window = TypeVar("Window")
+Element = TypeVar("Element")
+TextAlign = Literal["left", "right", "center"]
+TextVAlign = Literal["top", "bottom", "center"]
+FontType = Union[tuple[str, int], tuple[str, int, str]]
+PointType = Union[tuple[int, int], tuple[float, float]]
+EventMode = Literal["user", "system"]
+OrientationType = Literal["v", "h", "vertical", "horizontal"]
+ListboxSelectMode = Literal["multiple", "browse", "extended", "single"]
+PadType = Union[int, tuple[int, int], tuple[tuple[int, int], tuple[int, int]]]
+ReliefType = Literal["flat", "groove", "raised", "ridge", "solid", "sunken"]
+
+
+
+def set_clipboard(text):
+    """copy text to clipboard"""
+    pyperclip.copy(text)
+
+def get_clipboard():
+    """get text from clipboard"""
+    return pyperclip.paste()
+
+
