7977e09012fa05a410195a0f1c27254549a5fae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 04:05:09 +0000

    Add package django-kmuhelper/1.8.2/django_kmuhelper-1.8.2.tar.gz

---
+++ packages/django-kmuhelper/django_kmuhelper-1.8.2.tar.gz/django_kmuhelper-1.8.2/kmuhelper/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from kmuhelper.modules.api import admin as apiadmin
+from kmuhelper.modules.emails import admin as emailadmin
+from kmuhelper.modules.main import admin as mainadmin
+from kmuhelper.modules.app import admin as appadmin
+from kmuhelper.modules.settings import admin as settingsadmin
+
+from kmuhelper.modules.integrations.paymentimport import admin as paymentimportadmin
+
+# Disable "view on site" globally (for other apps too!)
+
+admin.site.site_url = None
+
+# List of all models and their corresponding admins
+
+modeladmins = []
+
+for adminclass in [
+    apiadmin,
+    emailadmin,
+    mainadmin,
+    appadmin,
+    settingsadmin,
+    paymentimportadmin,
+]:
+    modeladmins.extend(adminclass.modeladmins)
