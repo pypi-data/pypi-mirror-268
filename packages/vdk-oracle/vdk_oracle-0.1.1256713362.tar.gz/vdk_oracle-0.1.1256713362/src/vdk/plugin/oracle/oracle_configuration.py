4d5c0985bf52d9b42e2490f7e9b20136da9372b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 17:03:47 +0000

    Add package vdk-oracle/0.1.1256713362/vdk_oracle-0.1.1256713362.tar.gz

---
+++ packages/vdk-oracle/vdk_oracle-0.1.1256713362.tar.gz/vdk_oracle-0.1.1256713362/src/vdk/plugin/oracle/oracle_configuration.py
@@ -0,0 +1,133 @@
+# Copyright 2023-2024 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+import json
+import pathlib
+import tempfile
+from typing import Dict
+from typing import Optional
+
+from vdk.internal.core.config import Configuration
+from vdk.internal.core.config import ConfigurationBuilder
+
+ORACLE_USER = "ORACLE_USER"
+ORACLE_PASSWORD = "ORACLE_PASSWORD"
+ORACLE_USE_SECRETS = "ORACLE_USE_SECRETS"
+ORACLE_THICK_MODE = "ORACLE_THICK_MODE"
+ORACLE_THICK_MODE_LIB_DIR = "ORACLE_THICK_MODE_LIB_DIR"
+ORACLE_CONNECTION_STRING = "ORACLE_CONNECTION_STRING"
+ORACLE_HOST = "ORACLE_HOST"
+ORACLE_PORT = "ORACLE_PORT"
+ORACLE_SID = "ORACLE_SID"
+ORACLE_SERVICE_NAME = "ORACLE_SERVICE_NAME"
+ORACLE_INGEST_BATCH_SIZE = "ORACLE_INGEST_BATCH_SIZE"
+
+
+class OracleConfiguration:
+    def __init__(self, configuration: Configuration):
+        self.__config = configuration
+
+    def get_oracle_user(self) -> str:
+        return self.__config.get_value(ORACLE_USER)
+
+    def get_oracle_password(self) -> str:
+        return self.__config.get_value(ORACLE_PASSWORD)
+
+    def get_oracle_connection_string(self) -> str:
+        return self.__config.get_value(ORACLE_CONNECTION_STRING)
+
+    def get_oracle_host(self) -> str:
+        return self.__config.get_value(ORACLE_HOST)
+
+    def get_oracle_port(self) -> int:
+        return int(self.__config.get_value(ORACLE_PORT))
+
+    def get_oracle_sid(self) -> str:
+        return self.__config.get_value(ORACLE_SID)
+
+    def get_oracle_service_name(self) -> str:
+        return self.__config.get_value(ORACLE_SERVICE_NAME)
+
+    def oracle_use_secrets(self) -> bool:
+        return self.__config.get_value(ORACLE_USE_SECRETS)
+
+    def oracle_thick_mode(self) -> bool:
+        return self.__config.get_value(ORACLE_THICK_MODE)
+
+    def oracle_thick_mode_lib_dir(self) -> Optional[str]:
+        return self.__config.get_value(ORACLE_THICK_MODE_LIB_DIR)
+
+    def oracle_ingest_batch_size(self) -> Optional[int]:
+        return int(self.__config.get_value(ORACLE_INGEST_BATCH_SIZE))
+
+    @staticmethod
+    def add_definitions(config_builder: ConfigurationBuilder):
+        config_builder.add(
+            key=ORACLE_USER,
+            default_value=None,
+            is_sensitive=True,
+            description="The Oracle user for the database connection",
+        )
+        config_builder.add(
+            key=ORACLE_PASSWORD,
+            default_value=None,
+            is_sensitive=True,
+            description="The Oracle password for the database connection",
+        )
+        config_builder.add(
+            key=ORACLE_CONNECTION_STRING,
+            default_value=None,
+            is_sensitive=True,
+            description="The Oracle database connection string",
+        )
+        config_builder.add(
+            key=ORACLE_HOST,
+            default_value=None,
+            is_sensitive=True,
+            description="The host of the Oracle database. Note: This option is overridden by ORACLE_CONNECTION_STRING ",
+        )
+        config_builder.add(
+            key=ORACLE_PORT,
+            default_value=1521,
+            is_sensitive=True,
+            description="The port of the Oracle database. Note: This option is overridden by ORACLE_CONNECTION_STRING",
+        )
+        config_builder.add(
+            key=ORACLE_SID,
+            default_value=None,
+            is_sensitive=True,
+            description="The schema id of the Oracle database. Note: This option is overridden by ORACLE_CONNECTION_STRING",
+        )
+        config_builder.add(
+            key=ORACLE_SERVICE_NAME,
+            default_value=None,
+            is_sensitive=True,
+            description="The service name of the Oracle database. Note: This option is overridden by ORACLE_CONNECTION_STRING",
+        )
+        config_builder.add(
+            key=ORACLE_USE_SECRETS,
+            default_value=True,
+            description="Set this flag to use secrets to connect to Oracle. This is protype option. "
+            "Leave default value unless there are issues.",
+        )
+        config_builder.add(
+            key=ORACLE_THICK_MODE,
+            default_value=True,
+            description="Use oracle thick mode. Set to False to disable oracle thick mode."
+            "If set to true you may need to manually install client library. "
+            "Refer to "
+            "https://python-oracledb.readthedocs.io/en/latest/user_guide/initialization.html#enablingthick "
+            "More info: https://python-oracledb.readthedocs.io/en/latest/user_guide/appendix_b.html",
+        )
+        config_builder.add(
+            key=ORACLE_THICK_MODE_LIB_DIR,
+            default_value=None,
+            description="This is the location of the Oracle Client library used when thick mode is enabled. "
+            "This option is ignored if ORACLE_THICK_MODE is false."
+            "Before setting this follow instruction in "
+            "https://python-oracledb.readthedocs.io/en/latest/user_guide/initialization.html#enablingthick ",
+        )
+        config_builder.add(
+            key=ORACLE_INGEST_BATCH_SIZE,
+            default_value=100,
+            description="Batch size when ingesting records into Oracle.",
+        )
---
+++ packages/vdk-oracle/vdk_oracle-0.1.1256713362.tar.gz/vdk_oracle-0.1.1256713362/src/vdk/plugin/oracle/oracle_plugin.py
@@ -0,0 +1,109 @@
+# Copyright 2023-2024 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+import logging
+from typing import List
+
+import click
+import oracledb
+from tabulate import tabulate
+from vdk.api.plugin.hook_markers import hookimpl
+from vdk.api.plugin.plugin_registry import HookCallResult
+from vdk.api.plugin.plugin_registry import IPluginRegistry
+from vdk.internal.builtin_plugins.run.job_context import JobContext
+from vdk.internal.core.config import ConfigurationBuilder
+from vdk.internal.util.decorators import closing_noexcept_on_close
+from vdk.plugin.oracle.ingest_to_oracle import IngestToOracle
+from vdk.plugin.oracle.oracle_configuration import ORACLE_CONNECTION_STRING
+from vdk.plugin.oracle.oracle_configuration import ORACLE_PASSWORD
+from vdk.plugin.oracle.oracle_configuration import ORACLE_USER
+from vdk.plugin.oracle.oracle_configuration import OracleConfiguration
+from vdk.plugin.oracle.oracle_connection import OracleConnection
+
+"""
+Include the plugins implementation. For example:
+"""
+
+log = logging.getLogger(__name__)
+
+
+class OraclePlugin:
+    @hookimpl(tryfirst=True)
+    def vdk_configure(self, config_builder: ConfigurationBuilder):
+        OracleConfiguration.add_definitions(config_builder)
+
+    @hookimpl(trylast=True)
+    def initialize_job(self, context: JobContext):
+        conf = OracleConfiguration(context.core_context.configuration)
+        oracle_user, oracle_pass = conf.get_oracle_user(), conf.get_oracle_password()
+        context.connections.add_open_connection_factory_method(
+            "ORACLE",
+            lambda: OracleConnection(
+                oracle_user,
+                oracle_pass,
+                conf.get_oracle_connection_string(),
+                host=conf.get_oracle_host(),
+                port=conf.get_oracle_port(),
+                sid=conf.get_oracle_sid(),
+                service_name=conf.get_oracle_service_name(),
+                thick_mode=conf.oracle_thick_mode(),
+                thick_mode_lib_dir=conf.oracle_thick_mode_lib_dir(),
+            ),
+        )
+        context.ingester.add_ingester_factory_method(
+            "oracle",
+            lambda: IngestToOracle(
+                context.connections, conf.oracle_ingest_batch_size()
+            ),
+        )
+
+
+@hookimpl
+def vdk_start(plugin_registry: IPluginRegistry, command_line_args: List):
+    plugin_registry.load_plugin_with_hooks_impl(OraclePlugin(), "OraclePlugin")
+
+
+# TODO: https://github.com/vmware/versatile-data-kit/issues/2940
+@click.command(
+    name="oracle-query",
+    help="DEPRECATED: use sql-query, instead."
+    "Execute an Oracle query against an Oracle database (should be configured with env variables)",
+)
+@click.option("-q", "--query", type=click.STRING, required=True)
+@click.pass_context
+def oracle_query(ctx: click.Context, query):
+    """
+    Deprecated: "Use sql-query instead"
+
+    oracle-query: kept for compatibility
+    """
+    log.warning("oracle-query has been deprecated; please use sql-query instead.")
+    conf = ctx.obj.configuration
+    conn = oracledb.connect(
+        user=conf.get_value(ORACLE_USER),
+        password=conf.get_value(ORACLE_PASSWORD),
+        dsn=conf.get_value(ORACLE_CONNECTION_STRING),
+    )
+
+    with closing_noexcept_on_close(conn.cursor()) as cursor:
+        cursor.execute(query)
+        column_names = (
+            [column_info[0] for column_info in cursor.description]
+            if cursor.description
+            else ()  # same as the default value for the headers parameters of the tabulate function
+        )
+        try:
+            res = cursor.fetchall()
+            click.echo(tabulate(res, headers=column_names))
+        except Exception as e:
+            if str(e) == "DPY-1003: the executed statement does not return rows":
+                log.info(
+                    "Query did not produce results, e.g. DROP TABLE, but was successful"
+                )
+            else:
+                raise e
+    conn.commit()
+
+
+@hookimpl
+def vdk_command_line(root_command: click.Group):
+    root_command.add_command(oracle_query)
