e4af0074942038d9906bd59a9b13ebb4cc4573c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 14:04:31 +0000

    Add package onnxscript/0.1.0.dev20240417/onnxscript-0.1.0.dev20240417-py3-none-any.whl

---
+++ packages/onnxscript/onnxscript-0.1.0.dev20240417-py3-none-any.whl/onnxscript/rewriter/__init__.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+from typing import Sequence
+
+__all__ = [
+    # Modules
+    "irbuilder",
+    "protobuilder",
+    "function_rule",
+    "pattern",
+    # Functions
+    "rewrite",
+]
+
+import onnx
+
+from onnxscript._legacy_ir import irbuilder, protobuilder
+from onnxscript.optimizer import remove_unused, remove_unused_function
+from onnxscript.rewriter import function_rule, pattern
+
+PatternRewriteRule = pattern.RewriteRule
+FunctionRewriteRule = function_rule.FunctionRewriteRule
+
+
+def rewrite(
+    model: onnx.ModelProto,
+    function_rewrite_rules: Sequence[type[FunctionRewriteRule]] = (),
+    pattern_rewrite_rules: Sequence[PatternRewriteRule] = (),
+) -> onnx.ModelProto:
+    if function_rewrite_rules:
+        model_ir = irbuilder.build_ir(model)
+        for rule_cls in function_rewrite_rules:
+            rule_cls().apply_to_model(model_ir)
+        model = model_ir.original_model_proto
+    if pattern_rewrite_rules:
+        model_ir = irbuilder.build_ir(model)
+        count = pattern.RewriteRuleSet(pattern_rewrite_rules).apply_to_model(model_ir)
+        print(f"Applied {count} pattern rewrite rules.")
+        model = protobuilder.build_model_proto(model_ir)
+    # TODO: Does it make more sense we run DCE after each rewrite rule applied?
+    # If so, we need IR to support DCE.
+    remove_unused.remove_unused_nodes(model)
+    remove_unused_function.remove_unused_functions(model)
+    return model
