efea38704e1233d846e2ee5d4558904d048fbe75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 15:04:16 +0000

    Add package sodata/0.0.10/sodata-0.0.10.tar.gz

---
+++ packages/sodata/sodata-0.0.10.tar.gz/sodata-0.0.10/sodata/__init__.py
@@ -0,0 +1,9 @@
+from .chunk_clean import *
+from .text_split import *
+from .text_clean import *
+from .chunk_clean import *
+from .file_process import *
+
+def start():
+    print("hhh,let's start sodata...")
+
