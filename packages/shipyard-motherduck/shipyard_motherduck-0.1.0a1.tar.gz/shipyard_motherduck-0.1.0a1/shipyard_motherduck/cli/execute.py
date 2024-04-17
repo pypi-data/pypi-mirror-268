859129ab8ea6b4bd58e92972b3a7c0ad67c6b519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 15:04:03 +0000

    Add package shipyard-motherduck/0.1.0a1/shipyard_motherduck-0.1.0a1.tar.gz

---
+++ packages/shipyard-motherduck/shipyard_motherduck-0.1.0a1.tar.gz/shipyard_motherduck-0.1.0a1/shipyard_motherduck/cli/execute.py
@@ -0,0 +1,36 @@
+import argparse
+import shipyard_bp_utils as shipyard
+import sys
+from shipyard_templates import ShipyardLogger, ExitCodeException, Database
+from shipyard_motherduck import MotherDuckClient
+
+logger = ShipyardLogger().get_logger()
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--token", required=True)
+    parser.add_argument("--query", required=True)
+    parser.add_argument("--database", required=False, dest="database", default="")
+    return parser.parse_args()
+
+
+def main():
+    try:
+        args = get_args()
+        client = MotherDuckClient(args.token)
+        query = args.query
+        client.execute_query(query)
+
+    except ExitCodeException as ec:
+        logger.error(ec.message)
+        sys.exit(ec.exit_code)
+    except Exception as e:
+        logger.error(
+            f"An unexpected error occurred when attempting to execute query against MotherDuck: {e}"
+        )
+        sys.exit(Database.EXIT_CODE_UNKNOWN)
+
+
+if __name__ == "__main__":
+    main()
