dd814ebbdcc5aa87b4142601930cab5b2dee46df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 03:03:18 +0000

    Add package hydamo-validation/1.3.0b1/hydamo_validation-1.3.0b1.tar.gz

---
+++ packages/hydamo-validation/hydamo_validation-1.3.0b1.tar.gz/hydamo_validation-1.3.0b1/hydamo_validation/__init__.py
@@ -0,0 +1,13 @@
+__author__ = ["Het Waterschapshuis", "D2HYDRO", "HKV", "HydroConsult"]
+__copyright__ = "Copyright 2021, HyDAMO ValidatieTool"
+__credits__ = ["D2HYDRO", "HKV", "HydroConsult"]
+__version__ = "1.3.0b1"
+
+__license__ = "MIT"
+__maintainer__ = "Daniel Tollenaar"
+__email__ = "daniel@d2hydro.nl"
+
+from .functions import topologic as topologic_functions
+from .functions import logic as logic_functions
+from .functions import general as general_functions
+from .validator import validator
