3efe5139f398da954365d6383172fddb16226560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 06:04:16 +0000

    Add package msgraph-beta-sdk/1.3.0/msgraph_beta_sdk-1.3.0.tar.gz

---
+++ packages/msgraph-beta-sdk/msgraph_beta_sdk-1.3.0.tar.gz/msgraph_beta_sdk-1.3.0/msgraph_beta/generated/users/item/security/information_protection/sensitivity_labels/microsoft_graph_security_evaluate_removal/microsoft_graph_security_evaluate_removal_request_builder.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+from kiota_abstractions.base_request_builder import BaseRequestBuilder
+from kiota_abstractions.base_request_configuration import RequestConfiguration
+from kiota_abstractions.get_path_parameters import get_path_parameters
+from kiota_abstractions.method import Method
+from kiota_abstractions.request_adapter import RequestAdapter
+from kiota_abstractions.request_information import RequestInformation
+from kiota_abstractions.request_option import RequestOption
+from kiota_abstractions.serialization import Parsable, ParsableFactory
+from typing import Any, Callable, Dict, List, Optional, TYPE_CHECKING, Union
+
+if TYPE_CHECKING:
+    from .......models.o_data_errors.o_data_error import ODataError
+    from .evaluate_removal_post_request_body import EvaluateRemovalPostRequestBody
+    from .evaluate_removal_post_response import EvaluateRemovalPostResponse
+
+class MicrosoftGraphSecurityEvaluateRemovalRequestBuilder(BaseRequestBuilder):
+    """
+    Provides operations to call the evaluateRemoval method.
+    """
+    def __init__(self,request_adapter: RequestAdapter, path_parameters: Union[str, Dict[str, Any]]) -> None:
+        """
+        Instantiates a new MicrosoftGraphSecurityEvaluateRemovalRequestBuilder and sets the default values.
+        param path_parameters: The raw url or the url-template parameters for the request.
+        param request_adapter: The request adapter to use to execute the requests.
+        Returns: None
+        """
+        super().__init__(request_adapter, "{+baseurl}/users/{user%2Did}/security/informationProtection/sensitivityLabels/microsoft.graph.security.evaluateRemoval", path_parameters)
+    
+    async def post(self,body: Optional[EvaluateRemovalPostRequestBody] = None, request_configuration: Optional[RequestConfiguration] = None) -> Optional[EvaluateRemovalPostResponse]:
+        """
+        Indicate to the consuming application what actions it should take to remove the label information. Given contentInfo as an input, which includes existing content metadata key-value pairs, the API returns an informationProtectionAction that contains some combination of one or more of the following: 
+        param body: The request body
+        param request_configuration: Configuration for the request such as headers, query parameters, and middleware options.
+        Returns: Optional[EvaluateRemovalPostResponse]
+        Find more info here: https://learn.microsoft.com/graph/api/security-sensitivitylabel-evaluateremoval?view=graph-rest-1.0
+        """
+        if not body:
+            raise TypeError("body cannot be null.")
+        request_info = self.to_post_request_information(
+            body, request_configuration
+        )
+        from .......models.o_data_errors.o_data_error import ODataError
+
+        error_mapping: Dict[str, ParsableFactory] = {
+            "XXX": ODataError,
+        }
+        if not self.request_adapter:
+            raise Exception("Http core is null") 
+        from .evaluate_removal_post_response import EvaluateRemovalPostResponse
+
+        return await self.request_adapter.send_async(request_info, EvaluateRemovalPostResponse, error_mapping)
+    
+    def to_post_request_information(self,body: Optional[EvaluateRemovalPostRequestBody] = None, request_configuration: Optional[RequestConfiguration] = None) -> RequestInformation:
+        """
+        Indicate to the consuming application what actions it should take to remove the label information. Given contentInfo as an input, which includes existing content metadata key-value pairs, the API returns an informationProtectionAction that contains some combination of one or more of the following: 
+        param body: The request body
+        param request_configuration: Configuration for the request such as headers, query parameters, and middleware options.
+        Returns: RequestInformation
+        """
+        if not body:
+            raise TypeError("body cannot be null.")
+        request_info = RequestInformation(Method.POST, self.url_template, self.path_parameters)
+        request_info.configure(request_configuration)
+        request_info.headers.try_add("Accept", "application/json")
+        request_info.set_content_from_parsable(self.request_adapter, "application/json", body)
+        return request_info
+    
+    def with_url(self,raw_url: Optional[str] = None) -> MicrosoftGraphSecurityEvaluateRemovalRequestBuilder:
+        """
+        Returns a request builder with the provided arbitrary URL. Using this method means any other path or query parameters are ignored.
+        param raw_url: The raw URL to use for the request builder.
+        Returns: MicrosoftGraphSecurityEvaluateRemovalRequestBuilder
+        """
+        if not raw_url:
+            raise TypeError("raw_url cannot be null.")
+        return MicrosoftGraphSecurityEvaluateRemovalRequestBuilder(self.request_adapter, raw_url)
+    
+
