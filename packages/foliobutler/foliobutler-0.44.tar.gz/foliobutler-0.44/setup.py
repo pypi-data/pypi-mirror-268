736e27957a44af884a45c0323461db870ae47063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 15:04:06 +0000

    Add package foliobutler/0.44/foliobutler-0.44.tar.gz

---
+++ packages/foliobutler/foliobutler-0.44.tar.gz/foliobutler-0.44/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='foliobutler',
+	  version='0.44',
+	  description='Sync your Interactive Brokers account with your Folio in Foliobutler',
+	  url='https://github.com/svenotremba/foliobutler',
+	  author='Sven',
+	  include_package_data=True,
+	  install_requires=[
+          'setuptools',
+          'click',
+          'requests',
+          'ib_insync',
+          'python_dotenv',
+      ],
+	  author_email='sven@foliobutler.com',
+	  license='MIT',
+	  packages=['foliobutler'],
+	  zip_safe=False,
+	  entry_points='''
+	  [console_scripts]
+	  foliobutler=foliobutler:click_starter
+	  '''
+	  )
