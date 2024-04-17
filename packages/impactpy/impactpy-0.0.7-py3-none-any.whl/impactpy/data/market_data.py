260d003882a001e4804d76e48bf852f7cdd39192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 06:04:31 +0000

    Add package impactpy/0.0.7/impactpy-0.0.7-py3-none-any.whl

---
+++ packages/impactpy/impactpy-0.0.7-py3-none-any.whl/impactpy/data/market_data.py
@@ -0,0 +1,25 @@
+from collections import OrderedDict
+import polars as pl
+from polars import DataType
+from bin_schedule import BinSchedule
+
+
+class BinnedData:
+
+	@property
+	def df(self) -> pl.DataFrame:
+		return self._df
+
+	@staticmethod
+	def _validate_schema(schema: OrderedDict[str, DataType]) -> None:
+		required_cols = {
+			'stock': pl.Categorical, 'date': pl.Date, 'time': pl.Time,
+			'mid': pl.Float64, 'trade': pl.Int32
+		}
+		assert dict(schema) == required_cols, f'Schema mismatch'
+		     
+	@staticmethod
+	def _fill_missing(df: pl.DataFrame) -> pl.DataFrame: ...
+
+	def __init__(self, df: pl.DataFrame, schedule: BinSchedule) -> None:
+		self._df = df
