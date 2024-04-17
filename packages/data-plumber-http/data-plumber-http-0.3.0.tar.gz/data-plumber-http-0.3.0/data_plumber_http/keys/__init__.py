93bdb72649eaf6f0eb73739b9e69ba3ba822fc24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 06:04:59 +0000

    Add package data-plumber-http/0.3.0/data-plumber-http-0.3.0.tar.gz

---
+++ packages/data-plumber-http/data-plumber-http-0.3.0.tar.gz/data-plumber-http-0.3.0/data_plumber_http/keys/__init__.py
@@ -0,0 +1,11 @@
+class _DPKey:
+    pass
+
+#from .all_of import AllOf
+#from .one_of import OneOf
+from .property import Property
+
+__all__ = [
+    #"AllOf", "OneOf",
+    "Property",
+]
---
+++ packages/data-plumber-http/data-plumber-http-0.3.0.tar.gz/data-plumber-http-0.3.0/data_plumber_http/keys/one_of.py
@@ -0,0 +1,5 @@
+# from . import _DPKey
+# 
+# class OneOf(_DPKey):
+#     pass
+# 
