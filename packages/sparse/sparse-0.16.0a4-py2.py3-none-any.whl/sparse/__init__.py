e63f1be88e5c6cd01dbebce364dc88ed6a394ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 03:03:25 +0000

    Add package sparse/0.16.0a4/sparse-0.16.0a4-py2.py3-none-any.whl

---
+++ packages/sparse/sparse-0.16.0a4-py2.py3-none-any.whl/sparse/__init__.py
@@ -0,0 +1,57 @@
+import os
+from contextvars import ContextVar
+from enum import Enum
+
+from ._version import __version__, __version_tuple__  # noqa: F401
+
+__array_api_version__ = "2022.12"
+
+
+class BackendType(Enum):
+    PyData = "PyData"
+    Finch = "Finch"
+
+
+_ENV_VAR_NAME = "SPARSE_BACKEND"
+
+backend_var = ContextVar("backend", default=BackendType.PyData)
+
+if _ENV_VAR_NAME in os.environ:
+    backend_var.set(BackendType[os.environ[_ENV_VAR_NAME]])
+
+
+class Backend:
+    def __init__(self, backend=BackendType.PyData):
+        self.backend = backend
+        self.token = None
+
+    def __enter__(self):
+        self.token = backend_var.set(self.backend)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        backend_var.reset(self.token)
+        self.token = None
+
+    @staticmethod
+    def get_backend_module():
+        backend = backend_var.get()
+        if backend == BackendType.PyData:
+            import sparse.pydata_backend as backend_module
+        elif backend == BackendType.Finch:
+            import sparse.finch_backend as backend_module
+        else:
+            raise ValueError(f"Invalid backend identifier: {backend}")
+        return backend_module
+
+
+def __getattr__(attr):
+    if attr == "pydata_backend":
+        import sparse.pydata_backend as backend_module
+
+        return backend_module
+    if attr == "finch_backend":
+        import sparse.finch_backend as backend_module
+
+        return backend_module
+
+    return getattr(Backend.get_backend_module(), attr)
