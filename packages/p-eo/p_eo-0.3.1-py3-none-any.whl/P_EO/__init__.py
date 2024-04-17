48f5180b00f1e885e9659021423b0a8058099920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 17:03:49 +0000

    Add package p-eo/0.3.1/p_eo-0.3.1-py3-none-any.whl

---
+++ packages/p-eo/p_eo-0.3.1-py3-none-any.whl/P_EO/__init__.py
@@ -0,0 +1,14 @@
+from P_EO.common.config import Default, LogConfig
+from P_EO.core.elements import Element, Elements, IFrame
+from P_EO.core.page import Page
+
+VERSION = '0.3.1'
+
+__all__ = [
+    Element,
+    Elements,
+    IFrame,
+    Page,
+    Default,
+    LogConfig,
+]
