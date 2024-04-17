3021127c9478694add64a5d0688b519454d40a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 14:04:30 +0000

    Add package threadmem/0.2.15/threadmem-0.2.15-py3-none-any.whl

---
+++ packages/threadmem/threadmem-0.2.15-py3-none-any.whl/threadmem/__init__.py
@@ -0,0 +1,9 @@
+from .role import RoleMessage, RoleThread, RoleModel, RoleThreadsModel, RoleThreadModel
+
+__all__ = ["RoleMessage", "RoleThread", "RoleModel", "RoleThreadsModel", "RoleThreadModel"]
+
+
+class Thread(RoleThread):
+    """A role based chat thread"""
+
+    pass
