04d16a474a13afbfb74510e7239137a3f9ffb1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 06:04:44 +0000

    Add package biotech/1.0.2/biotech-1.0.2.tar.gz

---
+++ packages/biotech/biotech-1.0.2.tar.gz/biotech-1.0.2/venues/stages/biotech/procedures/implicit_procedure/process/moves/format_path.py
@@ -0,0 +1,10 @@
+
+import os
+
+def format_path (sub_path, relative_path):
+	if (type (relative_path) == str):
+		path = os.path.relpath (sub_path, relative_path)
+	else:
+		path = sub_path
+		
+	return path;
