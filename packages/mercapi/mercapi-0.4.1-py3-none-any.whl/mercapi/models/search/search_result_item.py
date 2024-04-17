423ed19d1306b6063dec0ee9b551d6616f2d1dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 06:04:15 +0000

    Add package mercapi/0.4.1/mercapi-0.4.1-py3-none-any.whl

---
+++ packages/mercapi/mercapi-0.4.1-py3-none-any.whl/mercapi/models/search/search_result_item.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from mercapi.models import Item, Profile
+from mercapi.models.base import ResponseModel
+
+
+@dataclass
+class SearchResultItem(ResponseModel):
+    id_: str
+    name: str
+    price: int
+    seller_id: str
+    status: str
+    created: datetime
+    updated: datetime
+    buyer_id: str
+    thumbnails: List[str]
+    item_type: str
+    item_condition_id: int
+    shipping_payer_id: int
+
+    async def full_item(self) -> "Item":
+        return await self._mercapi.item(self.id_)
+
+    async def seller(self) -> "Profile":
+        return await self._mercapi.profile(self.seller_id)
