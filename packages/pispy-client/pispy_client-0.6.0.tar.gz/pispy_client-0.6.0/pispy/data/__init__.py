3bc2408ec0ac54382b726be78b33cd013671a105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 16:04:26 +0000

    Add package pispy-client/0.6.0/pispy_client-0.6.0.tar.gz

---
+++ packages/pispy-client/pispy_client-0.6.0.tar.gz/pispy_client-0.6.0/pispy/data/__init__.py
@@ -0,0 +1,11 @@
+"""Code for getting data from PyPi."""
+
+##############################################################################
+# Local imports.
+from .package import Package, PackageURL
+
+##############################################################################
+# Exprots.
+__all__ = ["Package", "PackageURL"]
+
+### __init__.py ends here
