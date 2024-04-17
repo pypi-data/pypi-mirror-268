4e209ed1ea6b727e32fab972a568e16a264d31c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 17:03:47 +0000

    Add package hao/3.8.7/hao-3.8.7-py3-none-any.whl

---
+++ packages/hao/hao-3.8.7-py3-none-any.whl/hao/spinner.py
@@ -0,0 +1,74 @@
+import threading
+import time
+from typing import List, Optional
+
+from .dates import pretty_time_delta
+
+
+class Spinner(threading.Thread):
+
+    LINE_CLEAR = '\x1b[2K'
+    FRAMES = [
+        '|\\_________',
+        '_|\\________',
+        '__|\\_______',
+        '___|\\______',
+        '____|\\_____',
+        '_____|\\____',
+        '______|\\___',
+        '_______|\\__',
+        '________|\\_',
+        '_________|\\',
+        '_________/|',
+        '________/|_',
+        '_______/|__',
+        '______/|___',
+        '_____/|____',
+        '____/|_____',
+        '___/|______',
+        '__/|_______',
+        '_/|________',
+        '/|_________',
+    ]
+
+    def __init__(self, msg: str, *, ps='>', done: str = '✔️', interval=0.1, frames: Optional[List[str]] = None):
+        super().__init__()
+        self.msg = msg
+        self.ps = ps
+        self.done = done
+        self.interval = interval
+        self.frames = frames or self.FRAMES
+        self.status = threading.Event()
+        self.daemon = True
+
+    def stop(self):
+        self.status.set()
+
+    def is_stopped(self):
+        return self.status.is_set()
+
+    def cursors(self):
+        while True:
+            for cursor in self.frames:
+                yield cursor
+
+    def write(self, text):
+        print(f"{self.LINE_CLEAR}\r{self.ps} {text}", flush=True)
+
+    def run(self):
+        start = int(time.time())
+        cursors = self.cursors()
+        while not self.is_stopped():
+            self.status.wait(self.interval)
+            took = pretty_time_delta(int(time.time()) - start, millis=False)
+            p = f"{self.LINE_CLEAR}\r{self.ps} {self.msg} [{took}] {next(cursors)}"
+            print(p, end='', flush=True)
+        print(f"{self.done} ", end='\n', flush=True)
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+        time.sleep(0.1)
---
+++ packages/hao/hao-3.8.7-py3-none-any.whl/hao/vs.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import json
+import logging
+import os
+import sys
+
+from . import jsons, paths
+
+LOGGER = logging.getLogger(__name__)
+
+
+def run():
+    argv = sys.argv
+    n_args = len(argv)
+    if n_args == 1:
+        print('Usage: h-code {module.name}')
+        return
+
+    try:
+        module_name = argv[-1]
+        path_launch_json = paths.get('.vscode/launch.json')
+        if not os.path.exists(path_launch_json):
+            data = {'version': '0.2.0', 'configurations': []}
+            paths.make_parent_dirs(path_launch_json)
+        else:
+            with open(path_launch_json) as f:
+                data = json.load(f)
+        data.get('configurations').append({
+            "name": module_name,
+            "type": "debugpy",
+            "request": "launch",
+            "program": "-m",
+            "env": {},
+            "console": "integratedTerminal",
+            "args": [module_name],
+            "justMyCode": False
+        })
+        with open(path_launch_json, 'w') as f:
+            f.write(jsons.prettify(data))
+    except Exception as e:
+        LOGGER.exception(e)
