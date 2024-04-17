ff11026175a7ad59e76f4b95f9479b7f903c63ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 17:03:50 +0000

    Add package nonebot-plugin-fhl/0.1.3/nonebot_plugin_fhl-0.1.3-py3-none-any.whl

---
+++ packages/nonebot-plugin-fhl/nonebot_plugin_fhl-0.1.3-py3-none-any.whl/nonebot_plugin_fhl/config.py
@@ -0,0 +1,11 @@
+from nonebot import get_plugin_config
+from pydantic import BaseModel, Field
+
+
+class Config(BaseModel):
+    """Plugin Config Here"""
+
+    feihualing_api: str = Field(default="106.54.63.95:8080")
+    """飞花令api地址配置"""
+
+config = get_plugin_config(Config)
