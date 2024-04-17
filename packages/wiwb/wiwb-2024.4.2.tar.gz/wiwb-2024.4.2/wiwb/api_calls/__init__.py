e1b06eebda3341e46dc0ff410c252b393a6723d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 03:04:11 +0000

    Add package wiwb/2024.4.2/wiwb-2024.4.2.tar.gz

---
+++ packages/wiwb/wiwb-2024.4.2.tar.gz/wiwb-2024.4.2/wiwb/api_calls/__init__.py
@@ -0,0 +1,6 @@
+from wiwb.api_calls.base import Request
+from wiwb.api_calls.get_data_sources import GetDataSources
+from wiwb.api_calls.get_grids import GetGrids
+from wiwb.api_calls.get_variables import GetVariables
+
+__all__ = ["Request", "GetDataSources", "GetVariables", "GetGrids"]
