7ae6ad97cca901555c079ca780b06eaea6c1c8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 17:03:51 +0000

    Add package coolest/0.1.3/coolest-0.1.3.tar.gz

---
+++ packages/coolest/coolest-0.1.3.tar.gz/coolest-0.1.3/coolest/__init__.py
@@ -0,0 +1,25 @@
+"""
+
+This file initializes the coolest module
+and provides some basic information about the package.
+
+"""
+
+# Set the package release version
+version_info = (0, 1, 3)
+__version__ = '.'.join(str(c) for c in version_info)
+
+# Set the package details
+__author__ = 'COOLEST developers'
+__email__ = 'aymeric.galan@gmail.com'
+__year__ = '2021-2024'
+__credits__ = 'COOLEST developers'
+__url__ = 'https://github.com/aymgal/COOLEST'
+__description__ = 'Standard for Strong Gravitational Lens Modeling'
+
+# Default package properties
+__license__ = 'GPL-3.0'
+__about__ = ('{} Author: {}, Email: {}, Year: {}, {}'
+             ''.format(__name__, __author__, __email__, __year__,
+                       __description__))
+
