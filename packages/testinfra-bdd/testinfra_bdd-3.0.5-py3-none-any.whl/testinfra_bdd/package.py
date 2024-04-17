ea9053bc7b5457feae3200d237a584b9323a2d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 05:03:40 +0000

    Add package testinfra-bdd/3.0.5/testinfra_bdd-3.0.5-py3-none-any.whl

---
+++ packages/testinfra-bdd/testinfra_bdd-3.0.5-py3-none-any.whl/testinfra_bdd/package.py
@@ -0,0 +1,53 @@
+"""Then system package fixtures for testinfra-bdd."""
+from pytest_bdd import parsers, then, when
+
+
+@when(parsers.parse('the TestInfra package is {package_name}'))
+def the_package_is(package_name: str, testinfra_bdd_host):
+    """
+    Check the status of a package.
+
+    Parameters
+    ----------
+    package_name : str
+        The package name (e.g. "python3").
+    testinfra_bdd_host : testinfra_bdd.fixture.TestinfraBDD
+        The test fixture.
+    """
+    testinfra_bdd_host.package = testinfra_bdd_host.host.package(package_name.strip('"'))
+
+
+@then(parsers.parse('the TestInfra package state is {expected_status}'))
+@then(parsers.parse('the TestInfra package is {expected_status}'))
+def the_package_status_is(expected_status, testinfra_bdd_host):
+    """
+    Check the status of a package (installed/absent).
+
+    Parameters
+    ----------
+    expected_status : str
+        Can be absent, installed or present.
+    testinfra_bdd_host : testinfra_bdd.fixture.TestinfraBDD
+        The test fixture.
+
+    Raises
+    ------
+    AssertError
+        When the package is not in the expected state.
+    """
+    status_lookup = {
+        'absent': False,
+        'installed': True,
+        'present': True
+    }
+    expected_to_be_installed = status_lookup[expected_status]
+    pkg = testinfra_bdd_host.package
+    actual_status = pkg.is_installed
+
+    if expected_to_be_installed:
+        message = f'Expected {pkg.name} to be {expected_status} on {testinfra_bdd_host.hostname} but it is absent.'
+    else:
+        message = f'Expected {pkg.name} to be absent on {testinfra_bdd_host.hostname} '
+        message += 'but it is installed ({pkg.version}).'
+
+    assert actual_status == expected_to_be_installed, message
