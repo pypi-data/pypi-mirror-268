6c1da031b2dedd4e9f6324d4eea1c476dad79cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 05:03:29 +0000

    Add package mathy-project/0.1.2/mathy-project-0.1.2.tar.gz

---
+++ packages/mathy-project/mathy-project-0.1.2.tar.gz/mathy-project-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="mathy-project",
+    version="0.1.2",
+    packages=find_packages(),
+    description="Package made for an aptitude test, which contains functions for basic math operations",
+    author="Dedeepya Avancha",
+    author_email="Avancha.Dedeepya@iiitb.ac.in",
+    # url="https://github.com/Dd1235/math_operations",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
