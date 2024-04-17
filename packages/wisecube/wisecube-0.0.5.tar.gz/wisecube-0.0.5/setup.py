1cbd2b1b6d163aa2d7fb0b1ab17b741520a0b832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 16:04:03 +0000

    Add package wisecube/0.0.5/wisecube-0.0.5.tar.gz

---
+++ packages/wisecube/wisecube-0.0.5.tar.gz/wisecube-0.0.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+setuptools.setup(
+    name="wisecube",
+    version="0.0.5",
+    author="Wisecube",
+    author_email="info@wisecube.ai",
+    description="Wisecube SDK for graph Search",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wisecubeai/wisecube-python-sdk",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=required,
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    extras_require={
+        "pandas": ["pandas==1.5.3"],
+    }
+)
