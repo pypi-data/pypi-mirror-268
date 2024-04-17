fcad348364ad91acf99053e30eba5cd55cabc253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 17:03:48 +0000

    Add package dbis-relational-calculus/1.0.10/dbis_relational_calculus-1.0.10.tar.gz

---
+++ packages/dbis-relational-calculus/dbis_relational_calculus-1.0.10.tar.gz/dbis_relational_calculus-1.0.10/src/relational_calculus/domain_calculus/Variable.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from copy import deepcopy
+
+from typeguard import typechecked
+
+import relational_calculus.domain_calculus as dc
+
+
+class Variable:
+    @typechecked
+    def __init__(self, name: str) -> None:
+        self.name = name.lower()
+
+    @typechecked
+    def __copy__(self) -> Variable:
+        return deepcopy(self)
+
+    @typechecked
+    def __deepcopy__(self, memo) -> Variable:
+        return Variable(deepcopy(self.name, memo))
+
+    @typechecked
+    def __repr__(self) -> str:
+        return f"\\text{{{self.name}}}"
