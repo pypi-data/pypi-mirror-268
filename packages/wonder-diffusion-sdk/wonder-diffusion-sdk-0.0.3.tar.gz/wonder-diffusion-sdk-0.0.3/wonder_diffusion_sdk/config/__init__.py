88d3711f22a4af934dfabb8292a1973038bcfb4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 16:04:30 +0000

    Add package wonder-diffusion-sdk/0.0.3/wonder-diffusion-sdk-0.0.3.tar.gz

---
+++ packages/wonder-diffusion-sdk/wonder-diffusion-sdk-0.0.3.tar.gz/wonder-diffusion-sdk-0.0.3/wonder_diffusion_sdk/config/__init__.py
@@ -0,0 +1,3 @@
+from .sdk_config import WonderDiffusionSdkConfig
+from .model_config import WonderDiffusionModelConfig
+from .globals import DEVICE
---
+++ packages/wonder-diffusion-sdk/wonder-diffusion-sdk-0.0.3.tar.gz/wonder-diffusion-sdk-0.0.3/wonder_diffusion_sdk/types/pipeline_type.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class WonderPipelineType(Enum):
+    STABLE_DIFFUSION = 'stable_diffusion'
+    STABLE_DIFFUSION_IMG2IMG = 'stable_diffusion_img2img'
+    STABLE_DIFFUSION_INPAINT = 'stable_diffusion_inpaint'
+    STABLE_DIFFUSION_XL = 'stable_diffusion_xl'
+    STABLE_DIFFUSION_XL_IMG2IMG = 'stable_diffusion_xl_img2img'
+    STABLE_DIFFUSION_XL_INPAINT = 'stable_diffusion_xl_inpaint'
