4dbdc2df9e5ab4c7ba520172eec38c63daab6555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 04:05:03 +0000

    Add package pymonzo/2.1.0/pymonzo-2.1.0-py3-none-any.whl

---
+++ packages/pymonzo/pymonzo-2.1.0-py3-none-any.whl/pymonzo/accounts/enums.py
@@ -0,0 +1,27 @@
+"""Monzo API 'accounts' related enums."""
+
+from enum import Enum
+
+
+class MonzoAccountType(str, Enum):
+    """Monzo API 'account type' enum.
+
+    Note:
+        Currently undocumented in Monzo API docs.
+    """
+
+    UK_PREPAID = "uk_prepaid"
+    UK_RETAIL = "uk_retail"
+    UK_REWARDS = "uk_rewards"
+    UK_BUSINESS = "uk_business"
+    UK_LOAN = "uk_loan"
+
+
+class MonzoAccountCurrency(str, Enum):
+    """Monzo API 'account currency' enum.
+
+    Note:
+        Currently undocumented in Monzo API docs.
+    """
+
+    GBP = "GBP"
