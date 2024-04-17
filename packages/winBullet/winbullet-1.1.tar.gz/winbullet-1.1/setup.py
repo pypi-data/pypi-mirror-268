59ede3ea9dff87d6b9e04907933c975504fd3b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 16:04:15 +0000

    Add package winBullet/1.1/winbullet-1.1.tar.gz

---
+++ packages/winBullet/winbullet-1.1.tar.gz/winbullet-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='winBullet',
+      version='1.1',
+      description='Beautiful Python prompts made simple.',
+      long_description="Extensive support for Python list prompts \
+            formatting and colors",
+      url='https://github.com/Likqez/winBullet',
+      keywords = "cli list prompt customize colors",
+      author='Mckinsey666',
+      license='MIT',
+      packages=find_packages(),
+      python_requires=">=3.6"
+)
