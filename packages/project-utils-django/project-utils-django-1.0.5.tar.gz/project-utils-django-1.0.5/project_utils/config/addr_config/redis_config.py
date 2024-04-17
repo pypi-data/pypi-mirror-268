49528101a99da320a1a89c2bcf263b8be885917a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 15:04:03 +0000

    Add package project-utils-django/1.0.5/project-utils-django-1.0.5.tar.gz

---
+++ packages/project-utils-django/project-utils-django-1.0.5.tar.gz/project-utils-django-1.0.5/project_utils/config/addr_config/redis_config.py
@@ -0,0 +1,26 @@
+﻿from typing import Dict, Union
+
+from project_utils.exception import ConfigException
+from .base_config import BaseConfig
+
+
+class RedisConfig(BaseConfig):
+    password: str
+    db: int
+
+    def __init__(self, host: str, password: str, db: str, port: str = "6379"):
+        assert db.isdigit(), ConfigException("params db type required integer!")
+        super().__init__(host=host, port=port)
+        self.password = password
+        self.db = int(db)
+
+    def to_dict(self) -> Dict[str, Union[str, int]]:
+        result: Dict[str, Union[str, int]] = super().to_dict()
+        result.update({
+            "password": self.password,
+            "db": self.db
+        })
+        return result
+
+    def to_url(self) -> str:
+        return f"redis://:{self.password}@{self.host}:{self.port}/{self.db}"
---
+++ packages/project-utils-django/project-utils-django-1.0.5.tar.gz/project-utils-django-1.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.5"
+DESCRIPTION = "python project_utils tools"
+setup(
+    name="project-utils-django",
+    version=VERSION,
+    author="mylx2014",
+    author_email="mylx2014@163.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md', encoding="UTF8").read(),
+    packages=find_packages(),
+    install_requires=[
+        "django==4.2.9", 'loguru', 'dbutils', 'pymysql', "djangorestframework", "drf-yasg", "asyncio",
+                      "aiofiles"],
+    keywords=['python', 'utils', 'project utils', "aiofiles"],
+    data_files=[],
+    entry_points={},
+    license="MIT",
+    url="https://gitee.com/mylx2014/project-utils-django.git",
+    scripts=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ]
+)
