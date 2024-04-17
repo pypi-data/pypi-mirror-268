ff7cb1ad67d248e4c1d2a263a1e2b3bfba3beaf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 05:03:38 +0000

    Add package vortexasdk/0.70.0/vortexasdk-0.70.0.tar.gz

---
+++ packages/vortexasdk/vortexasdk-0.70.0.tar.gz/vortexasdk-0.70.0/tests/endpoints/test_voyages_search_enriched.py
@@ -0,0 +1,137 @@
+from datetime import datetime
+
+from tests.testcases import TestCaseUsingRealAPI
+from vortexasdk import VoyagesSearchEnriched
+
+rotterdam = "68faf65af1345067f11dc6723b8da32f00e304a6f33c000118fccd81947deb4e"
+
+
+class TestVoyagesSearchEnrichedEnriched(TestCaseUsingRealAPI):
+    def test_search_returns_dataframe(self):
+        start = datetime(2021, 6, 17)
+        end = datetime(2021, 6, 21)
+
+        df = (
+            VoyagesSearchEnriched()
+            .search(
+                time_min=start, time_max=end, origins=rotterdam, columns="all"
+            )
+            .to_df()
+            .head(2)
+        )
+
+        assert len(df) == 2
+
+    def test_search_pagination_on_df(self):
+        start = datetime(2021, 1, 1)
+        end = datetime(2021, 2, 28)
+
+        res = (
+            VoyagesSearchEnriched()
+            .search(
+                time_min=start, time_max=end, origins=rotterdam, columns="all"
+            )
+            .to_df()
+        )
+
+        assert len(res) > 1000
+
+    def test_search_pagination_on_lists(self):
+        start = datetime(2021, 1, 1)
+        end = datetime(2021, 2, 28)
+
+        res = (
+            VoyagesSearchEnriched()
+            .search(time_min=start, time_max=end, origins=rotterdam)
+            .to_list()
+        )
+
+        assert len(res) > 1000
+
+    def test_search_returns_some_cols(self):
+        start = datetime(2021, 6, 17)
+        end = datetime(2021, 6, 21)
+
+        df = (
+            VoyagesSearchEnriched()
+            .search(
+                time_min=start,
+                time_max=end,
+                origins=rotterdam,
+                columns=["vessel_name", "imo", "voyage_status", "destination"],
+            )
+            .to_df()
+        )
+
+        assert len(df.columns) == 4
+        assert list(df.columns) == [
+            "VESSEL NAME",
+            "IMO",
+            "VOYAGE STATUS",
+            "DESTINATION",
+        ]
+
+    def test_search_from_description_df(self):
+        start = datetime(2022, 4, 26)
+        end = datetime(2022, 4, 26, 23, 59)
+
+        res = (
+            VoyagesSearchEnriched()
+            .search(
+                time_min=start, time_max=end, origins=rotterdam, columns="all"
+            )
+            .to_df()
+            .head()
+        )
+
+        assert len(res) > 0
+
+    def test_search_from_description_raw(self):
+        start = datetime(2022, 4, 26)
+        end = datetime(2022, 4, 26, 23, 59)
+
+        res = (
+            VoyagesSearchEnriched()
+            .search(time_min=start, time_max=end, origins=rotterdam)
+            .to_list()
+        )
+
+        assert len(res) > 0
+        assert res[0].schema_version == "1.0.0"
+        assert res[0].vessel is not None
+        assert len(res[0].events) > 0
+        assert res[0].events[0].event_group is not None
+
+    def test_has_charterer_param(self):
+        start = datetime(2022, 4, 26)
+        end = datetime(2022, 4, 26, 23, 59)
+
+        res = (
+            VoyagesSearchEnriched()
+            .search(
+                time_min=start,
+                time_max=end,
+                origins=rotterdam,
+                has_charterer="inc",
+            )
+            .to_list()
+        )
+
+        assert len(res) > 0
+
+    def test_has_ship_to_ship_param(self):
+        start = datetime(2022, 4, 26)
+        end = datetime(2022, 4, 26, 23, 59)
+
+        res = (
+            VoyagesSearchEnriched()
+            .search(
+                time_min=start,
+                time_max=end,
+                origins=rotterdam,
+                has_ship_to_ship="inc",
+            )
+            .to_list()
+        )
+
+        assert len(res) > 0
