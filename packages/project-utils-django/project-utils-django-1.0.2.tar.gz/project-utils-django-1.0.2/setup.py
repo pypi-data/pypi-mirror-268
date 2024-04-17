99476db986cea3386852182a86f4f48c8507f1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 03:03:17 +0000

    Add package project-utils-django/1.0.2/project-utils-django-1.0.2.tar.gz

---
+++ packages/project-utils-django/project-utils-django-1.0.2.tar.gz/project-utils-django-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.2"
+DESCRIPTION = "python project_utils tools"
+setup(
+    name="project-utils-django",
+    version=VERSION,
+    author="mylx2014",
+    author_email="mylx2014@163.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open('README.md', encoding="UTF8").read(),
+    packages=find_packages(),
+    install_requires=['loguru', 'dbutils', 'pymysql', "django==3.0", "djangorestframework", "drf-yasg", "asyncio",
+                      "aiofiles"],
+    keywords=['python', 'utils', 'project utils', "aiofiles"],
+    data_files=[],
+    entry_points={},
+    license="MIT",
+    url="https://gitee.com/mylx2014/project-utils-django.git",
+    scripts=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ]
+)
