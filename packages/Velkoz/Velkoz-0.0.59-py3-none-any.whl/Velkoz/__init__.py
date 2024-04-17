def2d36c1abc9202635d153462980f613b645b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 04:04:49 +0000

    Add package Velkoz/0.0.59/Velkoz-0.0.59-py3-none-any.whl

---
+++ packages/Velkoz/Velkoz-0.0.59-py3-none-any.whl/Velkoz/__init__.py
@@ -0,0 +1,44 @@
+
+from .velkoz import(
+        set_api_key,
+        set_ddragon,
+        get_account,
+        get_summoner,
+        get_match_list,
+        get_match,
+        get_all_masteries,
+        get_champion_mastery,
+        get_top_masteries,
+        get_masteryscore,
+        get_champion,
+        get_champ_image,
+        get_champName_by_key
+
+        )
+
+from .settings import *
+
+
+
+from .tentacles import (
+        AccountApi,
+        ChampionMasteryApi,
+        RiotApiService,
+        api_key,
+        MatchApi,
+        SummonerApi
+        )
+
+from .eye import (
+        Summoner,
+        Match,
+        Participant,
+        Account
+        )
+from .data import(
+        DdragonRequest,
+        Champion,
+        RiotJson,
+        Skins
+
+        )
