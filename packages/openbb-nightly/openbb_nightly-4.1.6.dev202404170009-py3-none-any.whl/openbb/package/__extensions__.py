3d792879f3f30a7ccc70465f2b5d6d767ecf6415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 14:04:34 +0000

    Add package openbb-nightly/4.1.6.dev202404170009/openbb_nightly-4.1.6.dev202404170009-py3-none-any.whl

---
+++ packages/openbb-nightly/openbb_nightly-4.1.6.dev202404170009-py3-none-any.whl/openbb/package/__extensions__.py
@@ -0,0 +1,119 @@
+### THIS FILE IS AUTO-GENERATED. DO NOT EDIT. ###
+
+
+from openbb_core.app.static.container import Container
+
+
+class Extensions(Container):
+    # fmt: off
+    """
+Routers:
+    /crypto
+    /currency
+    /derivatives
+    /economy
+    /equity
+    /etf
+    /fixedincome
+    /index
+    /news
+    /regulators
+
+Extensions:
+    - commodity@1.0.2
+    - crypto@1.1.4
+    - currency@1.1.4
+    - derivatives@1.1.4
+    - economy@1.1.4
+    - equity@1.1.4
+    - etf@1.1.4
+    - fixedincome@1.1.4
+    - index@1.1.4
+    - news@1.1.4
+    - regulators@1.1.4
+
+    - benzinga@1.1.4
+    - federal_reserve@1.1.4
+    - fmp@1.1.4
+    - fred@1.1.4
+    - intrinio@1.1.4
+    - oecd@1.1.4
+    - polygon@1.1.4
+    - sec@1.1.4
+    - tiingo@1.1.4
+    - tradingeconomics@1.1.4
+    - yfinance@1.1.4    """
+    # fmt: on
+
+    def __repr__(self) -> str:
+        return self.__doc__ or ""
+
+    @property
+    def crypto(self):
+        # pylint: disable=import-outside-toplevel
+        from . import crypto
+
+        return crypto.ROUTER_crypto(command_runner=self._command_runner)
+
+    @property
+    def currency(self):
+        # pylint: disable=import-outside-toplevel
+        from . import currency
+
+        return currency.ROUTER_currency(command_runner=self._command_runner)
+
+    @property
+    def derivatives(self):
+        # pylint: disable=import-outside-toplevel
+        from . import derivatives
+
+        return derivatives.ROUTER_derivatives(command_runner=self._command_runner)
+
+    @property
+    def economy(self):
+        # pylint: disable=import-outside-toplevel
+        from . import economy
+
+        return economy.ROUTER_economy(command_runner=self._command_runner)
+
+    @property
+    def equity(self):
+        # pylint: disable=import-outside-toplevel
+        from . import equity
+
+        return equity.ROUTER_equity(command_runner=self._command_runner)
+
+    @property
+    def etf(self):
+        # pylint: disable=import-outside-toplevel
+        from . import etf
+
+        return etf.ROUTER_etf(command_runner=self._command_runner)
+
+    @property
+    def fixedincome(self):
+        # pylint: disable=import-outside-toplevel
+        from . import fixedincome
+
+        return fixedincome.ROUTER_fixedincome(command_runner=self._command_runner)
+
+    @property
+    def index(self):
+        # pylint: disable=import-outside-toplevel
+        from . import index
+
+        return index.ROUTER_index(command_runner=self._command_runner)
+
+    @property
+    def news(self):
+        # pylint: disable=import-outside-toplevel
+        from . import news
+
+        return news.ROUTER_news(command_runner=self._command_runner)
+
+    @property
+    def regulators(self):
+        # pylint: disable=import-outside-toplevel
+        from . import regulators
+
+        return regulators.ROUTER_regulators(command_runner=self._command_runner)
