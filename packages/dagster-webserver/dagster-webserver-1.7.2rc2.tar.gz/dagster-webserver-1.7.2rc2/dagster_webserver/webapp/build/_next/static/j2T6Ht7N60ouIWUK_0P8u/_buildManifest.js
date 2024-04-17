aa965b7edbe92d552cfb96a31c6390f804b80565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 06:05:02 +0000

    Add package dagster-webserver/1.7.2rc2/dagster-webserver-1.7.2rc2.tar.gz

---
+++ packages/dagster-webserver/dagster-webserver-1.7.2rc2.tar.gz/dagster-webserver-1.7.2rc2/dagster_webserver/webapp/build/_next/static/j2T6Ht7N60ouIWUK_0P8u/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-fca485987148a9eb.js"],"/_error":["static/chunks/pages/_error-cd3c9f6e58699191.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
