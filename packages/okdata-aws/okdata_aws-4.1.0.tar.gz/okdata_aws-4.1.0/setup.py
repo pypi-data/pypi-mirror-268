8d2a7e83cbcab43e863abc671b2056a1051f9e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 17:03:54 +0000

    Add package okdata-aws/4.1.0/okdata_aws-4.1.0.tar.gz

---
+++ packages/okdata-aws/okdata_aws-4.1.0.tar.gz/okdata_aws-4.1.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="okdata-aws",
+    version="4.1.0",
+    author="Oslo Origo",
+    author_email="dataplattform@oslo.kommune.no",
+    description="Collection of helpers for working with AWS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oslokommune/okdata-aws",
+    packages=setuptools.find_namespace_packages(
+        include="origo.aws.*", exclude=["tests*"]
+    ),
+    namespace_packages=["okdata"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    install_requires=[
+        "boto3",
+        "okdata-sdk>=3.1.1,<4",
+        "requests",
+        "starlette>=0.36.3,<1.0.0",
+        "structlog",
+    ],
+)
