ccb47882cb4df6cf8dc1a310397aafd20698f83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 03:03:59 +0000

    Add package dapla-toolbelt-pseudo/1.8.6/dapla_toolbelt_pseudo-1.8.6.tar.gz

---
+++ packages/dapla-toolbelt-pseudo/dapla_toolbelt_pseudo-1.8.6.tar.gz/dapla_toolbelt_pseudo-1.8.6/src/dapla_pseudo/types.py
@@ -0,0 +1,26 @@
+"""Type declarations for dapla-toolbelt-pseudo."""
+
+import io
+from pathlib import Path
+from typing import TypeAlias
+
+import fsspec
+import gcsfs
+import pandas as pd
+
+from dapla_pseudo.v1.api_models import Field
+
+FieldDecl: TypeAlias = str | dict[str, str] | Field
+BinaryFileDecl: TypeAlias = (
+    io.BufferedReader
+    | fsspec.spec.AbstractBufferedFile
+    | gcsfs.core.GCSFile
+    | io.BytesIO
+)
+
+DatasetDecl: TypeAlias = pd.DataFrame | BinaryFileDecl | str | Path
+FileLikeDatasetDecl: TypeAlias = BinaryFileDecl | str | Path
+FileSpecDecl: TypeAlias = tuple[str | None, BinaryFileDecl | str, str]
+# FileSpecDecl is derived from the "files" argument in multi-part requests from the "Requests"-library
+# The tuple semantically means: ('filename', fileobj, 'content_type')
+# See "files" in https://requests.readthedocs.io/en/latest/api/#requests.request
