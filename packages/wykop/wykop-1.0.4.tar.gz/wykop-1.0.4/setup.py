0283e4a8d8f4e7c1f95f1cd6682d2fd9fde70498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 05:03:36 +0000

    Add package wykop/1.0.4/wykop-1.0.4.tar.gz

---
+++ packages/wykop/wykop-1.0.4.tar.gz/wykop-1.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+__version__ = "1.0.4"
+__author__ = "Michał Skibiński"
+
+
+setup(
+    name="wykop",
+    install_requires=["httpx", "loguru"],
+    version=__version__,
+    author=__author__,
+    description="Python client for interacting with the Wykop API v3.",
+    long_description="docs: https://github.com/michalskibinski109/wykop",
+    author_email="michalskibinski109@gmail.com",
+    url="https://github.com/michalskibinski109/wykop",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+    python_requires=">=3.8",
+)
