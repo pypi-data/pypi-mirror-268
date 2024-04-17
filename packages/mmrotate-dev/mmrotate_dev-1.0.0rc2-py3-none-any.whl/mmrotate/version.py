1fcd1c42e741847470ff8075b35c267df82e5462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 15:03:58 +0000

    Add package mmrotate-dev/1.0.0rc2/mmrotate_dev-1.0.0rc2-py3-none-any.whl

---
+++ packages/mmrotate-dev/mmrotate_dev-1.0.0rc2-py3-none-any.whl/mmrotate/version.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author       : ChenjieXu
+Date         : 2024-04-17 10:27:09
+LastEditors  : ChenjieXu
+LastEditTime : 2024-04-17 10:27:09
+FilePath     : /mmrotate/mmrotate/version.py
+Description  : 
+'''
+# Copyright (c) OpenMMLab. All rights reserved.
+
+__version__ = '1.0.0rc2'
+short_version = __version__
