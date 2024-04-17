c026e0ebdf8215b1d757cd392a26be4ac96fb12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 15:04:06 +0000

    Add package colorprinttest/1.0.1/colorprinttest-1.0.1.tar.gz

---
+++ packages/colorprinttest/colorprinttest-1.0.1.tar.gz/colorprinttest-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'colorprinttest',
+    version = '1.0.1',
+    author = 'Jonatas Miguel',
+    author_email = 'jonatas.miguelss@gmail.com',
+    packages = ['colorprinttest'],
+    description = 'Programa simples de mudança de cor de texto',
+    keywords = 'Mudar cor texto',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Scientific/Engineering :: Physics'
+    ]
+
+)
