84b3e00200e42ab26ae9051291c7cf3416e1f7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 15:04:07 +0000

    Add package ptcr/0.1.2/ptcr-0.1.2.tar.gz

---
+++ packages/ptcr/ptcr-0.1.2.tar.gz/ptcr-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ptcr',
+    version='0.1.2',
+    author='Tiernan Lindauer',
+    author_email='tiernanlind@tamu.edu',
+    description='Constructs and simulates PTCR models.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='==3.9',
+    url='https://github.com/T-Lind/ptcr',
+    license='MIT',
+    keywords='PTCR, FOM, simulation, optimization, decision making, models'
+)
