e8746c4826f503bb4510deb95a659a6a325dae66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 07:07:07 +0000

    Add package maadsbml/1.3.12/maadsbml-1.3.12-py3-none-any.whl

---
+++ packages/maadsbml/maadsbml-1.3.12-py3-none-any.whl/maadsbml/__init__.py
@@ -0,0 +1,8 @@
+from .sendfiles import hyperpredictions
+from .sendfiles import rundemo
+from .sendfiles import algodescription
+from .sendfiles import hypertraining
+from .sendfiles import abort
+
+
+name = "maadsbml"
