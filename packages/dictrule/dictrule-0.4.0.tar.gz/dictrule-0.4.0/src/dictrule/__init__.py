559a6e548e21d9207991120c3f92b1f355d54b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 03:03:48 +0000

    Add package dictrule/0.4.0/dictrule-0.4.0.tar.gz

---
+++ packages/dictrule/dictrule-0.4.0.tar.gz/dictrule-0.4.0/src/dictrule/__init__.py
@@ -0,0 +1,66 @@
+r"""
+       ___      __             __   
+  ____/ (_)____/ /________  __/ /__ 
+ / __  / / ___/ __/ ___/ / / / / _ \
+/ /_/ / / /__/ /_/ /  / /_/ / /  __/
+\__,_/_/\___/\__/_/   \__,_/_/\___/ 
+
+"""
+
+from .generator import Generator
+from .rule import Rule
+from .context import Context
+from .__version__ import (
+    __title__,
+    __description__,
+    __url__,
+    __version__,
+    __author__,
+    __author_email__,
+    __license__,
+    __copyright__,
+)
+
+from .exceptions import (
+    NoneValueException,
+    InvalidTypeException,
+    InvalidValueException,
+)
+
+from .built_in_rules import (
+    BlockRule,
+    CommentRule,
+    EvalRule,
+    InlineRule,
+    IndentRule,
+    ForInRule,
+    JoinBlockRule,
+    JoinEvalRule,
+    FormatRule,
+)
+
+__all__ = [
+    "Generator",
+    "Rule",
+    "Context",
+    "NoneValueException",
+    "InvalidTypeException",
+    "InvalidValueException",
+    "BlockRule",
+    "CommentRule",
+    "EvalRule",
+    "InlineRule",
+    "IndentRule",
+    "ForInRule",
+    "JoinBlockRule",
+    "JoinEvalRule",
+    "FormatRule",
+    "__title__",
+    "__description__",
+    "__url__",
+    "__version__",
+    "__author__",
+    "__author_email__",
+    "__license__",
+    "__copyright__",
+]
