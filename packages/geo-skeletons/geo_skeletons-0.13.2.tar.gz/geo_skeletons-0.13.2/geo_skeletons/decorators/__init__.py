5eca393bf6fe07be95da394b5c76968486a71825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 04:04:33 +0000

    Add package geo-skeletons/0.13.2/geo_skeletons-0.13.2.tar.gz

---
+++ packages/geo-skeletons/geo_skeletons-0.13.2.tar.gz/geo_skeletons-0.13.2/geo_skeletons/decorators/__init__.py
@@ -0,0 +1,4 @@
+from .coordinate_factory import add_coord, add_direction, add_frequency, add_time
+from .datavar_factory import add_datavar
+from .mask_factory import add_mask
+from .magnitude_factory import add_magnitude
