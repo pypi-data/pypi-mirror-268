d38b425ff13e22c73e8b7fa999d4bf29beb60f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 03:03:37 +0000

    Add package not-my-board/0.6.0/not_my_board-0.6.0-py3-none-any.whl

---
+++ packages/not-my-board/not_my_board-0.6.0-py3-none-any.whl/not_my_board/_hub.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python3
+
+import asyncio
+import contextlib
+import contextvars
+import ipaddress
+import itertools
+import logging
+import random
+import traceback
+
+import asgineer
+
+import not_my_board._jsonrpc as jsonrpc
+import not_my_board._models as models
+import not_my_board._util as util
+
+logger = logging.getLogger(__name__)
+client_ip_var = contextvars.ContextVar("client_ip")
+reservation_context_var = contextvars.ContextVar("reservation_context")
+valid_tokens = ("dummy-token-1", "dummy-token-2")
+
+
+def hub():
+    asgineer.run(asgi_app, "uvicorn", ":2092")
+
+
+@asgineer.to_asgi
+async def asgi_app(request):
+    if isinstance(request, asgineer.WebsocketRequest):
+        if request.path == "/ws-agent":
+            return await _handle_agent(request)
+        elif request.path == "/ws-exporter":
+            return await _handle_exporter(request)
+        await request.close()
+        return
+    elif isinstance(request, asgineer.HttpRequest):
+        if request.path == "/api/v1/places":
+            return await _hub.get_places()
+    return 404, {}, "Page not found"
+
+
+async def _handle_agent(ws):
+    await _authorize_ws(ws)
+    client_ip = ws.scope["client"][0]
+    server = jsonrpc.Channel(ws.send, ws.receive_iter())
+    await _hub.agent_communicate(client_ip, server)
+
+
+async def _handle_exporter(ws):
+    await _authorize_ws(ws)
+    client_ip = ws.scope["client"][0]
+    exporter = jsonrpc.Channel(ws.send, ws.receive_iter())
+    await _hub.exporter_communicate(client_ip, exporter)
+
+
+async def _authorize_ws(ws):
+    try:
+        auth = ws.headers["authorization"]
+        scheme, token = auth.split(" ", 1)
+        if scheme != "Bearer":
+            raise ProtocolError(f"Invalid Authorization Scheme: {scheme}")
+        if token not in valid_tokens:
+            raise ProtocolError("Invalid token")
+    except Exception:
+        traceback.print_exc()
+        await ws.close()
+        return
+
+    await ws.accept()
+
+
+class Hub:
+    _places = {}
+    _exporters = {}
+    _available = set()
+    _wait_queue = []
+    _reservations = {}
+
+    def __init__(self):
+        self._id_generator = itertools.count(start=1)
+
+    @jsonrpc.hidden
+    async def get_places(self):
+        return {"places": [p.dict() for p in self._places.values()]}
+
+    @jsonrpc.hidden
+    async def agent_communicate(self, client_ip, rpc):
+        client_ip_var.set(client_ip)
+        async with self._register_agent():
+            rpc.set_api_object(self)
+            await rpc.communicate_forever()
+
+    @jsonrpc.hidden
+    async def exporter_communicate(self, client_ip, rpc):
+        client_ip_var.set(client_ip)
+        async with util.background_task(rpc.communicate_forever()) as com_task:
+            export_desc = await rpc.get_place()
+            with self._register_place(export_desc, rpc, client_ip):
+                await com_task
+
+    @contextlib.contextmanager
+    def _register_place(self, export_desc, rpc, client_ip):
+        id_ = next(self._id_generator)
+        place = models.Place(id=id_, host=_unmap_ip(client_ip), **export_desc)
+
+        try:
+            logger.info("New place registered: %d", id_)
+            self._places[id_] = place
+            self._exporters[id_] = rpc
+            self._available.add(id_)
+            yield self
+        finally:
+            logger.info("Place disappeared: %d", id_)
+            del self._places[id_]
+            del self._exporters[id_]
+            self._available.discard(id_)
+            for candidates, _, future in self._wait_queue:
+                candidates.discard(id_)
+                if not candidates and not future.done():
+                    future.set_exception(Exception("All candidate places are gone"))
+
+    @contextlib.asynccontextmanager
+    async def _register_agent(self):
+        ctx = object()
+        reservation_context_var.set(ctx)
+
+        try:
+            self._reservations[ctx] = set()
+            yield
+        finally:
+            coros = [self.return_reservation(id_) for id_ in self._reservations[ctx]]
+            results = await asyncio.gather(*coros, return_exceptions=True)
+            del self._reservations[ctx]
+            for result in results:
+                if isinstance(result, Exception):
+                    logger.warning("Error while deregistering agent: %s", result)
+
+    async def reserve(self, candidate_ids):
+        ctx = reservation_context_var.get()
+        existing_candidates = {id_ for id_ in candidate_ids if id_ in self._places}
+        if not existing_candidates:
+            raise RuntimeError("None of the candidates exist anymore")
+
+        available_candidates = existing_candidates & self._available
+        if available_candidates:
+            # TODO do something smart to get the best candidate
+            reserved_id = random.choice(list(available_candidates))
+
+            self._available.remove(reserved_id)
+            self._reservations[ctx].add(reserved_id)
+            logger.info("Place reserved: %d", reserved_id)
+        else:
+            logger.debug(
+                "No places available, adding request to queue: %s",
+                str(existing_candidates),
+            )
+            future = asyncio.get_running_loop().create_future()
+            entry = (existing_candidates, ctx, future)
+            self._wait_queue.append(entry)
+            try:
+                reserved_id = await future
+            finally:
+                self._wait_queue.remove(entry)
+
+        client_ip = client_ip_var.get()
+        async with util.on_error(self.return_reservation, reserved_id):
+            rpc = self._exporters[reserved_id]
+            await rpc.set_allowed_ips([_unmap_ip(client_ip)])
+
+        return reserved_id
+
+    async def return_reservation(self, place_id):
+        ctx = reservation_context_var.get()
+        self._reservations[ctx].remove(place_id)
+        if place_id in self._places:
+            for candidates, new_ctx, future in self._wait_queue:
+                if place_id in candidates and not future.done():
+                    self._reservations[new_ctx].add(place_id)
+                    logger.info("Place returned and reserved again: %d", place_id)
+                    future.set_result(place_id)
+                    break
+            else:
+                logger.info("Place returned: %d", place_id)
+                self._available.add(place_id)
+                rpc = self._exporters[place_id]
+                await rpc.set_allowed_ips([])
+        else:
+            logger.info("Place returned, but it doesn't exist: %d", place_id)
+
+
+_hub = Hub()
+
+
+def _unmap_ip(ip_str):
+    """Resolve IPv4-mapped-on-IPv6 to an IPv4 address"""
+    ip = ipaddress.ip_address(ip_str)
+    unmapped = getattr(ip, "ipv4_mapped", None) or ip
+    return str(unmapped)
+
+
+class ProtocolError(Exception):
+    pass
---
+++ packages/not-my-board/not_my_board-0.6.0-py3-none-any.whl/not_my_board/cli/__init__.py
@@ -0,0 +1,214 @@
+import argparse
+import asyncio
+import json
+import logging
+import pathlib
+import sys
+
+import not_my_board._client as client
+import not_my_board._util as util
+from not_my_board._agent import agent
+from not_my_board._export import export
+from not_my_board._hub import hub
+
+try:
+    from ..__about__ import __version__
+except ModuleNotFoundError:
+    __version__ = "dev"
+
+
+# pylint: disable=too-many-statements
+def main():
+    parser = argparse.ArgumentParser(description="Setup, manage and use a board farm")
+    parser.add_argument(
+        "-V", "--version", action="version", version=f"%(prog)s {__version__}"
+    )
+    subparsers = parser.add_subparsers(
+        dest="command",
+        required=True,
+        metavar="command",
+        help="for more info use: %(prog)s <command> -h",
+    )
+
+    def add_subcommand(name, *args, **kwargs):
+        subparser = subparsers.add_parser(name, *args, **kwargs)
+        subparser.set_defaults(func=globals()[f"_{name}_command"])
+        return subparser
+
+    def add_verbose_arg(subparser):
+        subparser.add_argument(
+            "-v", "--verbose", action="store_true", help="Enable debug logs"
+        )
+
+    def add_cacert_arg(subparser):
+        subparser.add_argument(
+            "--cacert",
+            type=pathlib.Path,
+            action="append",
+            help="path to CA certificates, which should be trusted",
+        )
+
+    subparser = add_subcommand("hub", help="start the board farm hub")
+    subparser.set_defaults(verbose=True)
+
+    subparser = add_subcommand(
+        "export", help="make connected boards available in the board farm"
+    )
+    subparser.set_defaults(verbose=True)
+    add_cacert_arg(subparser)
+    subparser.add_argument("hub_url", help="http(s) URL of the hub")
+    subparser.add_argument(
+        "export_description", type=pathlib.Path, help="path to a export description"
+    )
+
+    subparser = add_subcommand("agent", help="start an agent")
+    subparser.set_defaults(verbose=True)
+    add_cacert_arg(subparser)
+    subparser.add_argument("hub_url", help="http(s) URL of the hub")
+
+    subparser = add_subcommand("reserve", help="reserve a place")
+    add_verbose_arg(subparser)
+    subparser.add_argument("-n", "--with-name", help="reserve under a different name")
+    subparser.add_argument(
+        "import_description", help="name or full path of an import description"
+    )
+
+    subparser = add_subcommand("return", help="return a place")
+    add_verbose_arg(subparser)
+    subparser.add_argument("name", help="name of the place to return")
+
+    subparser = add_subcommand("attach", help="attach a reserved place")
+    add_verbose_arg(subparser)
+    subparser.add_argument(
+        "-k",
+        "--keep-others",
+        action="store_true",
+        help="don't return all other reservations",
+    )
+    subparser.add_argument(
+        "name", help="name of a reserved place or an import description"
+    )
+
+    subparser = add_subcommand("detach", help="detach a reserved place")
+    add_verbose_arg(subparser)
+    subparser.add_argument(
+        "-k", "--keep", action="store_true", help="don't return reservation"
+    )
+    subparser.add_argument("name", help="name of the place to attach")
+
+    subparser = add_subcommand("list", help="list reserved places")
+    add_verbose_arg(subparser)
+    subparser.add_argument(
+        "-n", "--no-header", action="store_true", help="Hide table header"
+    )
+
+    subparser = add_subcommand(
+        "status", help="show status of attached places and its interfaces"
+    )
+    add_verbose_arg(subparser)
+    subparser.add_argument(
+        "-n", "--no-header", action="store_true", help="Hide table header"
+    )
+
+    subparser = add_subcommand("uevent", help="handle Kernel uevent for USB devices")
+    add_verbose_arg(subparser)
+    subparser.add_argument("devpath", help="devpath attribute of uevent")
+
+    args = parser.parse_args()
+
+    # Don't use escape sequences, if stdout is not a tty
+    if not sys.stdout.isatty():
+        for attr in dir(Format):
+            if not attr.startswith("_"):
+                setattr(Format, attr, "")
+
+    if args.verbose:
+        level = logging.DEBUG
+
+        # reduce level of verbose loggers
+        logging.getLogger("websockets.client").setLevel(logging.INFO)
+        logging.getLogger("asyncio").setLevel(logging.INFO)
+    else:
+        level = logging.WARNING
+
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s: %(name)s: %(message)s", level=level
+    )
+
+    try:
+        obj = args.func(args)
+        if asyncio.iscoroutine(obj):
+            util.run(obj, debug=True)
+    except KeyboardInterrupt:
+        pass
+
+
+def _hub_command(_):
+    hub()
+
+
+async def _export_command(args):
+    await export(args.hub_url, args.export_description, args.cacert)
+
+
+async def _agent_command(args):
+    await agent(args.hub_url, args.cacert)
+
+
+async def _reserve_command(args):
+    await client.reserve(args.import_description, args.with_name)
+
+
+async def _return_command(args):
+    await client.return_reservation(args.name)
+
+
+async def _attach_command(args):
+    await client.attach(args.name, args.keep_others)
+
+
+async def _detach_command(args):
+    await client.detach(args.name, args.keep)
+
+
+async def _list_command(args):
+    place_list = await client.list_()
+
+    if not args.no_header and place_list:
+        print(f"{Format.BOLD}{'Place':<16} Status{Format.RESET}")
+
+    for entry in place_list:
+        status = (
+            f"{Format.GREEN}Attached"
+            if entry["attached"]
+            else f"{Format.YELLOW}Reserved"
+        )
+        print(f"{entry['place']:<16} {status}{Format.RESET}")
+
+
+async def _status_command(args):
+    status_list = await client.status()
+
+    if not args.no_header and status_list:
+        columns = ["Place", "Part", "Type", "Interface", "Status"]
+        header = " ".join(f"{c:<16}" for c in columns).rstrip()
+        print(f"{Format.BOLD}{header}{Format.RESET}")
+
+    for entry in status_list:
+        keys = ["place", "part", "type", "interface"]
+        status = f"{Format.GREEN}Up" if entry["attached"] else f"{Format.RED}Down"
+        row = " ".join(f"{entry[k]:<16}" for k in keys)
+        row += f" {status}{Format.RESET}"
+        print(row)
+
+
+async def _uevent_command(args):
+    await client.uevent(args.devpath)
+
+
+class Format:
+    RESET = "\033[0m"
+    BOLD = "\033[1m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
