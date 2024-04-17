66754c01938c4b0d76c414a52ccc243939e8d8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 16:03:53 +0000

    Add package pcffont/0.0.6/pcffont-0.0.6.tar.gz

---
+++ packages/pcffont/pcffont-0.0.6.tar.gz/pcffont-0.0.6/src/pcffont/t_scalable_widths.py
@@ -0,0 +1,42 @@
+from collections import UserList
+
+from pcffont.format import PcfTableFormat
+from pcffont.header import PcfHeader
+from pcffont.internal.buffer import Buffer
+from pcffont.table import PcfTable
+
+
+class PcfScalableWidths(PcfTable, UserList[int]):
+    @staticmethod
+    def parse(buffer: Buffer, header: PcfHeader, _strict_level: int) -> 'PcfScalableWidths':
+        table_format = PcfTableFormat.read_and_check(buffer, header)
+        ms_byte_first = PcfTableFormat.ms_byte_first(table_format)
+
+        glyphs_count = buffer.read_uint32(ms_byte_first)
+
+        scalable_widths = PcfScalableWidths(table_format)
+        for _ in range(glyphs_count):
+            scalable_widths.append(buffer.read_int32(ms_byte_first))
+        return scalable_widths
+
+    def __init__(
+            self,
+            table_format: int = PcfTableFormat.build(),
+            scalable_widths: list[int] = None,
+    ):
+        PcfTable.__init__(self, table_format)
+        UserList.__init__(self, scalable_widths)
+
+    def _dump(self, buffer: Buffer, table_offset: int) -> int:
+        ms_byte_first = PcfTableFormat.ms_byte_first(self.table_format)
+
+        glyphs_count = len(self)
+
+        buffer.seek(table_offset)
+        buffer.write_uint32(self.table_format)
+        buffer.write_uint32(glyphs_count, ms_byte_first)
+        for scalable_width in self:
+            buffer.write_int32(scalable_width, ms_byte_first)
+
+        table_size = buffer.tell() - table_offset
+        return table_size
---
+++ packages/pcffont/pcffont-0.0.6.tar.gz/pcffont-0.0.6/tests/test_dump.py
@@ -0,0 +1,72 @@
+import hashlib
+import io
+import os
+
+from pcffont.header import PcfHeader
+from pcffont.internal import util
+from pcffont.internal.buffer import Buffer
+
+project_root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+
+def _sha256(data: bytes) -> str:
+    return hashlib.sha256(data).hexdigest()
+
+
+def _test_dump(file_name: str):
+    file_path = os.path.join(project_root_dir, 'assets', file_name)
+    with open(file_path, 'rb') as file:
+        buffer_in = Buffer(file)
+        buffer_out = Buffer(io.BytesIO())
+
+        headers = PcfHeader.parse(buffer_in)
+        PcfHeader.dump(buffer_out, headers)
+        buffer_in.seek(0)
+        buffer_out.seek(0)
+        headers_data_size = 4 + 4 + (4 * 4) * len(headers)
+        headers_data_in = buffer_in.read(headers_data_size)
+        headers_data_out = buffer_out.read(headers_data_size)
+        assert _sha256(headers_data_in) == _sha256(headers_data_out)
+
+        for header in headers:
+            table = util.parse_table(buffer_in, header)
+            table_offset = header.table_offset
+            table_size = table.dump(buffer_out, table_offset)
+            assert table_size == header.table_size
+            buffer_in.seek(table_offset)
+            buffer_out.seek(table_offset)
+            table_data_in = buffer_in.read(table_size)
+            table_data_out = buffer_out.read(table_size)
+            assert _sha256(table_data_in) == _sha256(table_data_out)
+
+
+def test_unifont():
+    _test_dump('unifont/unifont-15.1.05.pcf')
+
+
+def test_dweep():
+    _test_dump('dweep/dweep.pcf')
+
+
+def test_rock36():
+    _test_dump('sgi/rock36.pcf')
+
+
+def test_raize():
+    _test_dump('raize/raize-normal-19.pcf')
+
+
+def test_anorexia():
+    _test_dump('artwiz/anorexia.pcf')
+
+
+def test_kates():
+    _test_dump('artwiz/kates.pcf')
+
+
+def test_trisk():
+    _test_dump('trisk/trisk.pcf')
+
+
+def test_profont():
+    _test_dump('profont-x11/ProFont_r400-29.pcf')
