5e58863d1eb8127955e9d4c434ffb552453f7ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 04:05:07 +0000

    Add package permit/2.4.1/permit-2.4.1.tar.gz

---
+++ packages/permit/permit-2.4.1.tar.gz/permit-2.4.1/permit/pdp_api/pdp_api_client.py
@@ -0,0 +1,30 @@
+from permit.utils.sync import SyncClass
+
+from ..config import PermitConfig
+from .role_assignments import RoleAssignmentsApi
+
+
+class PermitPdpApiClient:
+    def __init__(self, config: PermitConfig):
+        """
+        Constructs a new instance of the PdpApiClient class with the specified SDK configuration.
+
+        Args:
+            config: The configuration for the Permit SDK.
+        """
+        self._config = config
+        self._headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"bearer {self._config.token}",
+        }
+        self._base_url = self._config.pdp
+
+        self._role_assignments = RoleAssignmentsApi(config)
+
+    @property
+    def role_assignments(self) -> RoleAssignmentsApi:
+        return self._role_assignments
+
+
+class SyncPDPApi(PermitPdpApiClient, metaclass=SyncClass):
+    pass
