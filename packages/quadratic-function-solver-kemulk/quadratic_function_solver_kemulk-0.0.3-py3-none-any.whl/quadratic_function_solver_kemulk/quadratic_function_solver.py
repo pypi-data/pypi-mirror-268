9b8af691069d0cac9e30ed7fca64c30ea18c8631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 06:04:49 +0000

    Add package quadratic-function-solver-kemulk/0.0.3/quadratic_function_solver_kemulk-0.0.3-py3-none-any.whl

---
+++ packages/quadratic-function-solver-kemulk/quadratic_function_solver_kemulk-0.0.3-py3-none-any.whl/quadratic_function_solver_kemulk/quadratic_function_solver.py
@@ -0,0 +1,15 @@
+import math
+
+
+def solve_quadratic_function(a, b, c):
+    discriminant = b ** 2 - 4 * a * c
+
+    if discriminant > 0:
+        root1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        root2 = (-b - math.sqrt(discriminant)) / (2 * a)
+        return root1, root2
+    elif discriminant == 0:
+        root = -b / (2 * a)
+        return root
+    else:
+        return "Ruutvõrrandil puudub lahend"
