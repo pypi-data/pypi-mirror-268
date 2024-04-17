08575febee10e603a99eb2dfe4d83cb86ccf2ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 06:04:12 +0000

    Add package utubes/0.0.8/utubes-0.0.8.tar.gz

---
+++ packages/utubes/utubes-0.0.8.tar.gz/utubes-0.0.8/Utube/functions/function01.py
@@ -0,0 +1,61 @@
+import asyncio
+from ..scripts.eo import Okeys
+from yt_dlp import YoutubeDL, DownloadError
+#==========================================================================
+
+class UDMessages:
+    def __init__(self, **kwargs):
+        self.status = kwargs.get('status', True)
+        self.errors = kwargs.get('errors', None)
+        self.result = kwargs.get('result', None)
+
+#==========================================================================
+
+class UDownloader:
+
+    async def metadata(link, command):
+        with YoutubeDL(command) as ydl:
+            try:
+                moonus = ydl.extract_info(link, download=False)
+                return UDMessages(result=moonus)
+            except Exception as errors:
+                return UDMessages(errors=errors)
+
+#==========================================================================
+
+    async def extracts(link, command):
+        with YoutubeDL(command) as ydl:
+            try:
+                moonus = ydl.extract_info(link, download=False)
+                return UDMessages(result=moonus)
+            except Exception as errors:
+                return UDMessages(errors=errors)
+
+#==========================================================================
+
+    async def filename(link, command):
+        with YoutubeDL(command) as ydl:
+            try:
+                mainos = Okeys.DATA01
+                metase = ydl.extract_info(link, download=False)
+                moonus = ydl.prepare_filename(metase, outtmpl=mainos)
+                return UDMessages(result=moonus)
+            except Exception as errors:
+                return UDMessages(errors=errors)
+
+#==========================================================================
+
+    async def download(link, command, progress):
+        loop = asyncio.get_event_loop()
+        with YoutubeDL(command) as ydl:
+            try:
+                filelink = [link]
+                ydl.add_progress_hook(progress)   
+                await loop.run_in_executor(None, ydl.download, filelink)
+                return UDMessages(status=True)
+            except DownloadError as errors:
+                return UDMessages(status=False, errors=errors)
+            except Exception as errors:
+                return UDMessages(status=False, errors=errors)
+
+#==========================================================================
