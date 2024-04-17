0213638ba3f8f8b9596924aa3301c66cec080bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 06:04:13 +0000

    Add package a9x-webstatistics-package/0.1.1/a9x_webstatistics_package-0.1.1.tar.gz

---
+++ packages/a9x-webstatistics-package/a9x_webstatistics_package-0.1.1.tar.gz/a9x_webstatistics_package-0.1.1/src/a9x_webstatistics/main.py
@@ -0,0 +1,52 @@
+import io, sys, argparse, json, ast
+
+def detectDeviceClass(ua):
+  if ua is None:
+     return 'desktop'
+  if (ua.lower().find('bot') > 0):
+     return 'bots'
+  if (ua.lower().find('python') > 0):
+     return 'bots'
+  if (ua.lower().find('mediapartner') > 0):
+     return 'others'
+  if (ua.lower().find('curl') > 0):
+     return 'others'
+  if (ua.lower().find('ipad') > 0):
+     return 'tablet'
+  if (ua.lower().find('mobile') > 0):
+     return 'mobile'
+  if (ua.lower().find('android') > 0):
+     return 'tablet'
+  return 'desktop'
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--statfile", help="json file that contains calculated statistics", default="webstat.json")
+    parser.add_argument("-i", "--infile", help="filename including path to web server access log that contains input data", default="/var/log/nginx-access.json")
+    args = parser.parse_args()
+
+    if args.cmd == "crtstats":
+        parser.print_help()
+    elif args.cmd == "crtissues":
+        parser.print_help()
+    else:
+        parser.print_help()
+
+    # init statistic file:
+    d = {}
+    d['timelastrec'] = '19991231235959'
+    d['days'] = {}
+
+    # load statistic file if it exists
+    try:
+        f = open(args.statfile) 
+        d = json.load(f) 
+    except FileNotFoundError:   # first call: file does not exists
+        print("-s Statisticfile not found, it will be created")
+    except json.JSONDecodeError:
+        print("-s Json File not valid")
+
+
+
+if __name__ == "__main__":
+    main()
