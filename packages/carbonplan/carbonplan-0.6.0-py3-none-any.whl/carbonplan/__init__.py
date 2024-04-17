65ff44c83ba1948137e7b3f6893510632ec26623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 06:04:08 +0000

    Add package carbonplan/0.6.0/carbonplan-0.6.0-py3-none-any.whl

---
+++ packages/carbonplan/carbonplan-0.6.0-py3-none-any.whl/carbonplan/__init__.py
@@ -0,0 +1,9 @@
+from importlib.metadata import PackageNotFoundError as _PackageNotFoundError
+from importlib.metadata import version as _version
+
+try:
+    version = _version(__name__)
+except _PackageNotFoundError:
+    # package is not installed
+    version = "unknown"
+__version__ = version
