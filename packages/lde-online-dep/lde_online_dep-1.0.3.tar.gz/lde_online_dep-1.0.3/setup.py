596339795887c9fb69204a26e7849be863a9ae20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 04:04:44 +0000

    Add package lde-online-dep/1.0.3/lde_online_dep-1.0.3.tar.gz

---
+++ packages/lde-online-dep/lde_online_dep-1.0.3.tar.gz/lde_online_dep-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lde_online_dep',
+    version='1.0.3',
+    packages=['generated_proto_pycode'],
+    url='https://liangdao.eu/sd/lde_online',
+    license='GNU General Public License v3.0',
+    author='Yang　Wu',
+    author_email='yang.wu@liangdao.de',
+    description='Generated py code which used in lde online service by proto'
+)
