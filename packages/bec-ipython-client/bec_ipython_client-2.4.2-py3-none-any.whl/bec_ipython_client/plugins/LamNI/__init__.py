0597f0f350b31e8da60fa610786fe511e98b8a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 04:05:10 +0000

    Add package bec-ipython-client/2.4.2/bec_ipython_client-2.4.2-py3-none-any.whl

---
+++ packages/bec-ipython-client/bec_ipython_client-2.4.2-py3-none-any.whl/bec_ipython_client/plugins/LamNI/__init__.py
@@ -0,0 +1,2 @@
+from .load_additional_correction import lamni_read_additional_correction
+from .x_ray_eye_align import DataDrivenLamNI, LamNI, MagLamNI, XrayEyeAlign
---
+++ packages/bec-ipython-client/bec_ipython_client-2.4.2-py3-none-any.whl/bec_ipython_client/plugins/cSAXS/__init__.py
@@ -0,0 +1,2 @@
+from .cSAXS_beamline import epics_get, epics_put, fshclose, fshopen, fshstatus
+from .csaxs_bl_checks import cSAXSBeamlineChecks
