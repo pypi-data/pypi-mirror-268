789e01fe34715c1e3a534c5e9c877ab3bf51a00c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 04:04:50 +0000

    Add package saer/1.0.1/saer-1.0.1.tar.gz

---
+++ packages/saer/saer-1.0.1.tar.gz/saer-1.0.1/src/celitech/hooks/hook.py
@@ -0,0 +1,42 @@
+import os
+
+
+class Request:
+    def __init__(self, method, url, headers, body=""):
+        self.method = method
+        self.url = url
+        self.headers = headers
+        self.body = body
+
+    def __str__(self):
+        return f"method={self.method}, url={self.url}, headers={self.headers}, body={self.body})"
+
+
+class Response:
+    def __init__(self, status, headers, body):
+        self.status = status
+        self.headers = headers
+        self.body = body
+
+    def __str__(self):
+        return "Response(status={}, headers={}, body={})".format(
+            self.status, self.headers, self.body
+        )
+
+
+class CustomHook:
+
+    def before_request(self, request: Request, **kwargs):
+        client_id = os.getenv("CLIENT_ID", "")
+        client_secret = os.getenv("CLIENT_SECRET", "")
+        print("Request:", request)
+        print("clientId: ", client_id)
+        print("clientSecret: ", client_secret)
+
+    def after_response(self, request: Request, response: Response, **kwargs):
+        print("after_response")
+
+    def on_error(
+        self, error: Exception, request: Request, response: Response, **kwargs
+    ):
+        print("on_error")
