a38bcfb87e973760361fc76b7030d405c56e182e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 05:03:38 +0000

    Add package libmem/5.0.0rc0/libmem-5.0.0rc0.tar.gz

---
+++ packages/libmem/libmem-5.0.0rc0.tar.gz/libmem-5.0.0rc0/src/libmem/__init__.py
@@ -0,0 +1,196 @@
+#  ----------------------------------
+# |         libmem - by rdbo         |
+# |      Memory Hacking Library      |
+#  ----------------------------------
+#
+# Copyright (C) 2024    Rdbo
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version 3
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import libmem._libmem as _libmem
+from libmem._libmem import lm_process_t, lm_thread_t, lm_module_t, lm_symbol_t, lm_prot_t, lm_page_t, lm_inst_t, lm_vmt_t
+from libmem._libmem import LM_PROT_X, LM_PROT_R, LM_PROT_W, LM_PROT_XR, LM_PROT_XW, LM_PROT_RW, LM_PROT_XRW, LM_BITS
+from typing import Optional, List, Tuple
+
+def LM_FreeMemory(alloc: int, size: int) -> bool:
+    return _libmem.LM_FreeMemory(alloc, size)
+
+def LM_Assemble(code: str) -> Optional[lm_inst_t]:
+    return _libmem.LM_Assemble(code)
+
+def LM_DemangleSymbol(symbol: str) -> Optional[str]:
+    return _libmem.LM_DemangleSymbol(symbol)
+
+def LM_EnumPagesEx(pproc: lm_process_t) -> Optional[List[lm_page_t]]:
+    return _libmem.LM_EnumPagesEx(pproc)
+
+def LM_EnumSymbolsDemangled(pmod: lm_module_t) -> Optional[List[lm_symbol_t]]:
+    return _libmem.LM_EnumSymbolsDemangled(pmod)
+
+def LM_ProtMemory(addr: int, size: int, prot: lm_prot_t) -> Optional[lm_prot_t]:
+    return _libmem.LM_ProtMemory(addr, size, prot)
+
+def LM_IsProcessAlive(pproc: lm_process_t) -> bool:
+    return _libmem.LM_IsProcessAlive(pproc)
+
+def LM_SetMemory(dst: int, byte: bytes, size: int) -> bool:
+    return _libmem.LM_SetMemory(dst, byte, size)
+
+def LM_AllocMemory(size: int, prot: int) -> Optional[int]:
+    return _libmem.LM_AllocMemory(size, prot)
+
+def LM_DeepPointerEx(pproc: lm_process_t, base: int, offsets: List[int]) -> Optional[int]:
+    return _libmem.LM_DeepPointerEx(pproc, base, offsets)
+
+def LM_DataScan(data: bytearray, addr: int, scansize: int) -> Optional[int]:
+    return _libmem.LM_DataScan(data, addr, scansize)
+
+def LM_UnhookCode(from_: int, trampoline: Tuple[int, int]) -> None:
+    return _libmem.LM_UnhookCode(from_, trampoline)
+
+def LM_SetMemoryEx(pproc: lm_process_t, dst: int, byte: bytes, size: int) -> bool:
+    return _libmem.LM_SetMemoryEx(pproc, dst, byte, size)
+
+def LM_EnumThreads() -> Optional[List[lm_thread_t]]:
+    return _libmem.LM_EnumThreads()
+
+def LM_SigScanEx(pproc: lm_process_t, sig: str, addr: int, scansize: int) -> Optional[int]:
+    return _libmem.LM_SigScanEx(pproc, sig, addr, scansize)
+
+def LM_GetThreadProcess(pthr: lm_thread_t) -> Optional[lm_process_t]:
+    return _libmem.LM_GetThreadProcess(pthr)
+
+def LM_EnumPages() -> Optional[List[lm_page_t]]:
+    return _libmem.LM_EnumPages()
+
+def LM_ReadMemoryEx(pproc: lm_process_t, src: int, size: int) -> Optional[bytearray]:
+    return _libmem.LM_ReadMemoryEx(pproc, src, size)
+
+def LM_EnumModulesEx(pproc: lm_process_t) -> Optional[List[lm_module_t]]:
+    return _libmem.LM_EnumModulesEx(pproc)
+
+def LM_HookCodeEx(pproc: lm_process_t, from_: int, to: int) -> Tuple[int, int]:
+    return _libmem.LM_HookCodeEx(pproc, from_, to)
+
+def LM_CodeLength(code: int, minlength: int) -> Optional[int]:
+    return _libmem.LM_CodeLength(code, minlength)
+
+def LM_FreeMemoryEx(pproc: lm_process_t, alloc: int, size: int) -> bool:
+    return _libmem.LM_FreeMemoryEx(pproc, alloc, size)
+
+def LM_DisassembleEx(code: int, bits: int, size: int, count: int, runtime_addr: int) -> Optional[List[lm_inst_t]]:
+    return _libmem.LM_DisassembleEx(code, bits, size, count, runtime_addr)
+
+def LM_CodeLengthEx(pproc: lm_process_t, code: int, minlength: int) -> Optional[int]:
+    return _libmem.LM_CodeLengthEx(pproc, code, minlength)
+
+def LM_Disassemble(code: int) -> Optional[lm_inst_t]:
+    return _libmem.LM_Disassemble(code)
+
+def LM_GetPageEx(pproc: lm_process_t, addr: int) -> Optional[lm_page_t]:
+    return _libmem.LM_GetPageEx(pproc, addr)
+
+def LM_GetProcess() -> Optional[lm_process_t]:
+    return _libmem.LM_GetProcess()
+
+def LM_UnloadModule(pmod : lm_module_t):
+    return _libmem.LM_UnloadModule(pmod )
+
+def LM_WriteMemory(dst: int, src: bytearray) -> bool:
+    return _libmem.LM_WriteMemory(dst, src)
+
+def LM_EnumThreadsEx(pproc: lm_process_t) -> Optional[List[lm_thread_t]]:
+    return _libmem.LM_EnumThreadsEx(pproc)
+
+def LM_GetThread() -> Optional[lm_thread_t]:
+    return _libmem.LM_GetThread()
+
+def LM_LoadModuleEx(pproc: lm_process_t, modpath: str) -> Optional[lm_module_t]:
+    return _libmem.LM_LoadModuleEx(pproc, modpath)
+
+def LM_DataScanEx(pproc: lm_process_t, data: bytearray, addr: int, scansize: int) -> Optional[int]:
+    return _libmem.LM_DataScanEx(pproc, data, addr, scansize)
+
+def LM_HookCode(from_: int, to: int) -> Tuple[int, int]:
+    return _libmem.LM_HookCode(from_, to)
+
+def LM_GetSystemBits() -> int:
+    return _libmem.LM_GetSystemBits()
+
+def LM_LoadModule(modpath: str) -> Optional[lm_module_t]:
+    return _libmem.LM_LoadModule(modpath)
+
+def LM_FindModuleEx(pproc: lm_process_t, name: str) -> Optional[lm_module_t]:
+    return _libmem.LM_FindModuleEx(pproc, name)
+
+def LM_ProtMemoryEx(pproc: lm_process_t, addr: int, size: int, prot: lm_prot_t) -> Optional[lm_prot_t]:
+    return _libmem.LM_ProtMemoryEx(pproc, addr, size, prot)
+
+def LM_GetProcessEx(pid: int) -> Optional[lm_process_t]:
+    return _libmem.LM_GetProcessEx(pid)
+
+def LM_GetPage(addr: int) -> Optional[lm_page_t]:
+    return _libmem.LM_GetPage(addr)
+
+def LM_FindModule(name: str) -> Optional[lm_module_t]:
+    return _libmem.LM_FindModule(name)
+
+def LM_PatternScanEx(pproc: lm_process_t, pattern: bytearray, mask: str, addr: int, scansize: int) -> Optional[int]:
+    return _libmem.LM_PatternScanEx(pproc, pattern, mask, addr, scansize)
+
+def LM_UnloadModuleEx(pproc : lm_process_t, pmod : lm_module_t):
+    return _libmem.LM_UnloadModuleEx(pproc , pmod )
+
+def LM_PatternScan(pattern: bytearray, mask: str, addr: int, scansize: int) -> Optional[int]:
+    return _libmem.LM_PatternScan(pattern, mask, addr, scansize)
+
+def LM_FindProcess(procstr : str):
+    return _libmem.LM_FindProcess(procstr )
+
+def LM_EnumModules() -> Optional[List[lm_module_t]]:
+    return _libmem.LM_EnumModules()
+
+def LM_FindSymbolAddress(pmod: lm_module_t, name: str) -> Optional[int]:
+    return _libmem.LM_FindSymbolAddress(pmod, name)
+
+def LM_AssembleEx(code: str, bits: int, runtime_addr: int) -> Optional[bytearray]:
+    return _libmem.LM_AssembleEx(code, bits, runtime_addr)
+
+def LM_ReadMemory(src: int, size: int) -> Optional[bytearray]:
+    return _libmem.LM_ReadMemory(src, size)
+
+def LM_EnumProcesses() -> Optional[List[lm_process_t]]:
+    return _libmem.LM_EnumProcesses()
+
+def LM_GetThreadEx(pproc: lm_process_t) -> Optional[lm_thread_t]:
+    return _libmem.LM_GetThreadEx(pproc)
+
+def LM_FindSymbolAddressDemangled(pmod: lm_module_t, name: str) -> Optional[int]:
+    return _libmem.LM_FindSymbolAddressDemangled(pmod, name)
+
+def LM_WriteMemoryEx(pproc: lm_process_t, dst: int, src: bytearray) -> bool:
+    return _libmem.LM_WriteMemoryEx(pproc, dst, src)
+
+def LM_SigScan(sig: str, addr: int, scansize: int) -> Optional[int]:
+    return _libmem.LM_SigScan(sig, addr, scansize)
+
+def LM_EnumSymbols(pmod: lm_module_t) -> Optional[lm_symbol_t]:
+    return _libmem.LM_EnumSymbols(pmod)
+
+def LM_DeepPointer(base: int, offsets: List[int]) -> Optional[int]:
+    return _libmem.LM_DeepPointer(base, offsets)
+
+def LM_UnhookCodeEx(pproc: lm_process_t, from_: int, trampoline: Tuple[int, int]) -> None:
+    return _libmem.LM_UnhookCodeEx(pproc, from_, trampoline)
+
+def LM_AllocMemoryEx(pproc: lm_process_t, size: int, prot: int) -> Optional[int]:
+    return _libmem.LM_AllocMemoryEx(pproc, size, prot)
---
+++ packages/libmem/libmem-5.0.0rc0.tar.gz/libmem-5.0.0rc0/tests/tests.py
@@ -0,0 +1,277 @@
+from libmem import *
+import ctypes
+import struct
+
+def separator():
+    print("========================================")
+
+print("[*] libmem-py tests")
+
+separator()
+
+print("[*] Process Enumeration")
+print("\n".join([str(proc) for proc in LM_EnumProcesses()[:5]]))
+
+separator()
+
+print("[*] Current Process")
+curproc = LM_GetProcess()
+print(curproc)
+
+separator()
+
+print("[*] Parent Process of Current Process")
+parent_proc = LM_GetProcessEx(curproc.ppid)
+print(parent_proc)
+
+separator()
+
+print("[*] Remote Process")
+proc = LM_FindProcess("test1")
+print(proc)
+
+separator()
+
+print(f"[*] Is Remote Process Alive? {'Yes' if LM_IsProcessAlive(proc) else 'No'}")
+
+separator()
+
+print(f"[*] System Bits: {LM_GetSystemBits()}")
+
+separator()
+
+print(f"[*] Current Process Threads: {LM_EnumThreads()}")
+
+separator()
+
+print(f"[*] Remote Process Threads: {LM_EnumThreadsEx(proc)}")
+
+separator()
+
+thread = LM_GetThread()
+print(f"[*] Current Thread: {thread}")
+
+separator()
+
+print(f"[*] Remote Thread: {LM_GetThreadEx(proc)}")
+
+separator()
+
+print(f"[*] Process From Thread '{thread}': {LM_GetThreadProcess(thread)}")
+
+separator()
+
+print("[*] Module Enumeration - Current Process")
+print("\n".join([str(mod) for mod in LM_EnumModules()[:5]]))
+
+separator()
+
+print("[*] Module Enumeration - Remote Process")
+print("\n".join([str(mod) for mod in LM_EnumModulesEx(proc)[:5]]))
+
+separator()
+
+curmod = LM_FindModule(curproc.path)
+print(f"[*] Current Process Module: {curmod}")
+
+separator()
+
+mod = LM_FindModuleEx(proc, proc.path)
+print(f"[*] Remote Process Module: {mod}")
+
+separator()
+
+# TODO: Add tests for LM_LoadModule(Ex) and LM_UnloadModule(Ex)
+
+# separator()
+
+print("[*] Symbol Enumeration")
+
+print("\n".join([str(sym) for sym in LM_EnumSymbols(curmod)[:5]]))
+
+separator()
+
+print("[*] Symbol Address Search")
+
+symaddr = LM_FindSymbolAddress(curmod, "Py_BytesMain")
+if symaddr == None:
+    symaddr = LM_FindSymbolAddress(curmod, "_start_c")
+print(f"[*] Py_BytesMain Address: {symaddr}")
+
+separator()
+
+mangled = "_Z15_LM_EnumSymbolsP11lm_module_tPFiP11lm_symbol_tPvES3_"
+demangled = LM_DemangleSymbol(mangled)
+print(f"[*] Demangled '{mangled}': {demangled}")
+
+separator()
+
+print("[*] Demangled Symbol Enumeration")
+
+print("\n".join([str(sym) for sym in LM_EnumSymbolsDemangled(curmod)[:5]]))
+
+# TODO: Add test for 'LM_FindSymbolAddressDemangled'
+
+# separator()
+
+separator()
+
+print("[*] Page Enumeration - Current Process")
+print("\n".join([str(page) for page in LM_EnumPages()[:5]]))
+
+separator()
+
+print("[*] Page Enumeration - Remote Process")
+print("\n".join([str(page) for page in LM_EnumPagesEx(proc)[:5]]))
+
+separator()
+
+print(f"[*] Page From Current Process Module: {LM_GetPage(symaddr)}")
+
+separator()
+
+print(f"[*] Page From Remote Process Module: {LM_GetPageEx(proc, mod.base)}")
+
+separator()
+
+val = ctypes.c_int(10)
+val_addr = ctypes.addressof(val)
+rdbuf = LM_ReadMemory(val_addr, ctypes.sizeof(val))
+rdval = struct.unpack("@i", rdbuf)[0]
+print(f"[*] Read Integer From '{hex(val_addr)}': {str(rdval)}")
+
+separator()
+
+# TODO: Add tests for 'LM_ReadMemoryEx'
+# separator()
+
+LM_WriteMemory(val_addr, bytearray(b"\x39\x05\x00\x00"))
+print(f"[*] Integer After LM_WriteMemory: {val}")
+
+separator()
+
+# TODO: Add tests for 'LM_WriteMemoryEx'
+# separator()
+
+LM_SetMemory(val_addr, b"\x00", ctypes.sizeof(val))
+print(f"[*] Integer After LM_SetMemory: {val}")
+
+separator()
+
+# TODO: Add tests for 'LM_SetMemoryEx'
+# separator()
+
+print("[*] Changing Memory Protection - Current Process")
+old_prot = LM_ProtMemory(curmod.base, 0x1000, LM_PROT_XRW)
+print(f"[*] Old Memory Protection ({hex(curmod.base)}): {old_prot}")
+page = LM_GetPage(curmod.base)
+print(f"[*] Current Memory Protection ({hex(curmod.base)}): {page.prot}")
+
+separator()
+
+print("[*] Changing Memory Protection - Remote Process")
+old_prot = LM_ProtMemoryEx(proc, mod.base, 0x1000, LM_PROT_XRW)
+print(f"[*] Old Memory Protection ({hex(mod.base)}): {old_prot}")
+page = LM_GetPageEx(proc, mod.base)
+print(f"[*] Current Memory Protection ({hex(mod.base)}): {page.prot}")
+
+separator()
+
+alloc_size = 0x1000
+alloc = LM_AllocMemory(alloc_size, LM_PROT_XRW)
+print(f"[*] Allocated Memory - Current Process: {hex(alloc)}")
+LM_FreeMemory(alloc, alloc_size)
+
+separator()
+
+alloc = LM_AllocMemoryEx(proc, alloc_size, LM_PROT_XRW)
+print(f"[*] Allocated Memory - Remote Process: {hex(alloc)}")
+LM_FreeMemoryEx(proc, alloc, alloc_size)
+
+separator()
+
+addr1 = LM_AllocMemory(4, LM_PROT_RW)
+addr2 = LM_AllocMemory(8, LM_PROT_RW)
+
+LM_WriteMemory(addr1, bytearray(b"\x10\x00\x00\x00"))
+LM_WriteMemory(addr2, bytearray(addr1.to_bytes(8, byteorder="little")))
+print("[*] Address 1: ", hex(addr1))
+print("[*] Address 2: ", hex(addr2))
+
+deep_ptr = LM_DeepPointer(addr2, [0, 0])
+print("[*] Deep Pointer result: " + hex(deep_ptr))
+
+value = int.from_bytes(LM_ReadMemory(deep_ptr, 4), byteorder="little")
+print("[*] Deep Pointer value: " + hex(value))
+
+separator()
+
+buf = ctypes.c_buffer(b"\x10\x20\x30\x40\x50\x60\x70\x80\x90\xA0")
+buf_addr = ctypes.addressof(buf)
+print(f"[*] Scanning For Buffer At: {hex(buf_addr)}")
+scan_addr = buf_addr - 0x10
+scan_size = 0x100
+data_scan = LM_DataScan(bytearray(buf.value), scan_addr, scan_size)
+print(f"[*] Data Scan Match: {hex(data_scan)}")
+pattern_scan = LM_PatternScan(bytearray(buf.value), "xxxx?x?x?x", scan_addr, scan_size)
+print(f"[*] Pattern Scan Match: {hex(pattern_scan)}")
+sig_scan = LM_SigScan("10 20 30 40 ?? ?? 70 80 ?? A0", scan_addr, scan_size)
+print(f"[*] Signature Scan Match: {hex(sig_scan)}")
+
+separator()
+
+# TODO: Add tests for 'LM_DataScanEx', 'LM_PatternScanEx', 'LM_SigScanEx'
+# separator()
+
+# TODO: Add tests for 'LM_HookCode' and 'LM_UnhookCode'
+# separator()
+
+# TODO: Add tests for 'LM_HookCodeEx' and 'LM_UnhookCodeEx'
+# separator()
+
+print("[*] Assemblying Instruction")
+inst = LM_Assemble("mov eax, ebx")
+print(inst)
+
+separator()
+
+print("[*] Assemblying Instructions")
+insts = LM_AssembleEx("push ebp; mov ebp, esp; mov esp, ebp; pop ebp; ret", 32, 0)
+print(", ".join([hex(b) for b in insts]))
+
+separator()
+
+print("[*] Disassembly of PyBytesMain Instruction")
+inst = LM_Disassemble(symaddr)
+print(inst)
+
+separator()
+
+print("[*] Disassembly of PyBytesMain Instructions")
+insts = LM_DisassembleEx(symaddr, 32, 0x100, 5, symaddr)
+print("\n".join([str(inst) for inst in insts]))
+
+separator()
+
+minlength = 0x5
+aligned_length = LM_CodeLength(symaddr, minlength)
+print(f"[*] Aligned Length for Minimum '{minlength}' Bytes is '{aligned_length}' Bytes (PyBytesMain)")
+
+separator()
+
+# TODO: Add tests for 'LM_CodeLengthEx'
+# separator()
+
+print("[*] VMT Hooking")
+
+vtable = ctypes.c_ulonglong(0x1020304050607080)
+vmt = lm_vmt_t(ctypes.addressof(vtable))
+print(f"[*] Original Function: {hex(vmt.get_original(0))}")
+vmt.hook(0, 0xdeadbeef)
+print(f"[*] VMT After Hook: {hex(vtable.value)}")
+vmt.unhook(0)
+# vmt.reset()
+print(f"[*] VMT After Unhook: {hex(vtable.value)}")
+
+separator()
+
