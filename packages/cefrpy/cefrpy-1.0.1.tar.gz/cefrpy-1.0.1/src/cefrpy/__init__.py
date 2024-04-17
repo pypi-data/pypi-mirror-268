3a01551c0415384325bac271eb62f4471ce4dde5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 04:04:48 +0000

    Add package cefrpy/1.0.1/cefrpy-1.0.1.tar.gz

---
+++ packages/cefrpy/cefrpy-1.0.1.tar.gz/cefrpy-1.0.1/src/cefrpy/__init__.py
@@ -0,0 +1,9 @@
+from .POSTag import POSTag
+from .CEFRDataReader import CEFRDataReader
+from .CEFRDataProcessor import CEFRDataProcessor
+from .CEFRLevel import CEFRLevel
+from .CEFRAnalyzer import CEFRAnalyzer
+from .CEFRSpaCyAnalyzer import CEFRSpaCyAnalyzer
+
+__version__ = "1.0.1"
+__all__ = ["POSTag", "CEFRDataReader", "CEFRDataProcessor", "CEFRLevel", "CEFRAnalyzer", "CEFRSpaCyAnalyzer"]
