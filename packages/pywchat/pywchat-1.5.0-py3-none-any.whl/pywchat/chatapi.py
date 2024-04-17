6b9915cc314467306dcc5c0f869175f130ad1755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 03:03:47 +0000

    Add package pywchat/1.5.0/pywchat-1.5.0-py3-none-any.whl

---
+++ packages/pywchat/pywchat-1.5.0-py3-none-any.whl/pywchat/chatapi.py
@@ -0,0 +1,8 @@
+chat_api = {
+    'GET_ACCESS_TOKEN': '/cgi-bin/gettoken?corpid={}&corpsecret={}',
+    'MESSAGE_SEND': '/cgi-bin/message/send?access_token={}',
+    'MEDIA_UPLOAD': '/cgi-bin/media/upload?access_token={}&type={}',
+    "IMG_UPLOAD": '/cgi-bin/media/uploadimg?access_token={}',
+    "GET_USERS": "/cgi-bin/user/simplelist",
+    "GET_USERID_EMAIL": "/cgi-bin/user/get_userid_by_email"
+}
