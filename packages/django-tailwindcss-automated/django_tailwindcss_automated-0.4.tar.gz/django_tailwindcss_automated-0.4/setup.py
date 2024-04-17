f1975adab3d5f08303b3e4d2b1fbc84e08f849ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 05:03:30 +0000

    Add package django-tailwindcss-automated/0.4/django_tailwindcss_automated-0.4.tar.gz

---
+++ packages/django-tailwindcss-automated/django_tailwindcss_automated-0.4.tar.gz/django_tailwindcss_automated-0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='django_tailwindcss_automated',
+    version='0.4',
+    author='Jan Leander',
+    packages=find_packages(),
+    install_requires=[
+        # add dependencies here.
+        # e.g 'num>=1.11.1'
+    ],
+    entry_points={
+        "console_scripts": [
+            "automate-django-tailwind = django_tailwindcss_automated:main",
+        ],
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
