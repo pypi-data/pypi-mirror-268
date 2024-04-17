ea3752a922f258ab1526e61a41038171a4d6e526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 14:04:45 +0000

    Add package armis/1.0.16/armis-1.0.16-py3-none-any.whl

---
+++ packages/armis/armis-1.0.16-py3-none-any.whl/armis/__about__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024-present Matthew Lange <mmlange@gmail.com>
+#
+# SPDX-License-Identifier: GPL-2.0-only
+__title__ = "armis"
+__description__ = "Connect and perform actions with the Armis cloud."
+__version__ = "1.0.16"
---
+++ packages/armis/armis-1.0.16-py3-none-any.whl/armis/__init__.py
@@ -0,0 +1,1 @@
+from .armiscloud import ArmisCloud
