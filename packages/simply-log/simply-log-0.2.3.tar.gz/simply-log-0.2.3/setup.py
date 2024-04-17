ed2b9315418be294457ea14caf040ec2a3d69f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 17:03:50 +0000

    Add package simply-log/0.2.3/simply-log-0.2.3.tar.gz

---
+++ packages/simply-log/simply-log-0.2.3.tar.gz/simply-log-0.2.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="simply-log",
+    description="A simple logging.txt library ",
+    version="0.2.3",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
