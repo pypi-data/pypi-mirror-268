aa80458d899c649ad42e7f6ee6169f9c0d0d6b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 16:04:28 +0000

    Add package aws-packages/0.0.25/aws_packages-0.0.25-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aws-packages/aws_packages-0.0.25-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aws_packages/auth/icp/middlewares.py
@@ -0,0 +1,27 @@
+"""Auth middleware."""
+
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver, Response
+from aws_lambda_powertools.event_handler.exceptions import UnauthorizedError
+from aws_lambda_powertools.event_handler.middlewares import NextMiddleware
+
+from aws_packages.auth.exceptions import JWTAuthError
+from aws_packages.auth.icp.auth_backend import icp_auth_backend
+from aws_packages.auth.models import AuthorizationRequest
+
+
+def icp_login_required(
+    app: APIGatewayRestResolver, next_middleware: NextMiddleware
+) -> Response:
+    """Login required middleware
+
+    :param app: application instance
+    :param next_middleware: next middleware
+    :return: Response object after the middleware has been applied
+    """
+
+    request = AuthorizationRequest(**app.current_event.headers)
+    try:
+        icp_auth_backend.authenticate_with_token(request.token)
+    except JWTAuthError:
+        raise UnauthorizedError(f"Unauthorized")
+    return next_middleware(app)
