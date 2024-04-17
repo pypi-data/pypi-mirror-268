1ca0b2a0dd420ad54d41143f45118bc96fb7c74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-17 15:04:02 +0000

    Add package vjepa-encoder/0.0.4/vjepa_encoder-0.0.4.tar.gz

---
+++ packages/vjepa-encoder/vjepa_encoder-0.0.4.tar.gz/vjepa_encoder-0.0.4/setup.py
@@ -0,0 +1,30 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import os
+from setuptools import setup
+
+VERSION = "0.0.4"
+
+from setuptools import setup, find_packages
+
+def get_requirements():
+    with open("./requirements.txt") as reqsf:
+        reqs = reqsf.readlines()
+    return reqs
+
+
+if __name__ == "__main__":
+    setup(
+        name="vjepa_encoder",
+        version=VERSION,
+        description="JEPA research code.",
+        author="Jonathan Koch",
+        author_email="johnnykoch02@gmail.com",
+        python_requires=">=3.7",
+        packages=find_packages(),
+        install_requires=get_requirements(),
+    )
