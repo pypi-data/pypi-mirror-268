2c8b6251750856532e7da8237c2dafc6df1c55e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:03 +0000

    Add package zbx-wxwork-assistant/1.1.0/zbx_wxwork_assistant-1.1.0-py3-none-any.whl

---
+++ packages/zbx-wxwork-assistant/zbx_wxwork_assistant-1.1.0-py3-none-any.whl/ZbxAssistant/ZbxWxworkHots.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+from zabbix_api import ZabbixAPIException
+from .ZbxApis import ZabbixAPIS
+
+
+class ZabbixHosts(ZabbixAPIS):
+    def __init__(self, login_user: str = ""):
+        super().__init__(login_user)
+        self._login_user = login_user.strip()
+
+    @property
+    def hosts(self):
+        """
+            获取登录用户有权查看的所有主机信息：
+                1. 直接使用以登录用户信息封装的 Zabbix API 去获取 Zabbix 主机信息。
+        :return:
+        """
+        try:
+            if self.user_zbxapi:
+                hosts = self.user_zbxapi.host.get(
+                    {
+                        "output": [
+                            "hostid",
+                            "name",
+                            "interfaces",
+                            "host"
+                        ],
+                        "selectInterfaces": ["ip"]
+                    }
+                )
+                return hosts
+        except ZabbixAPIException as err:
+            logging.error(str(err))
