578cb71bd32daaa0b7a1def1255bfab741e82e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 06:04:56 +0000

    Add package py-codeforce/1.3/py_codeforce-1.3.tar.gz

---
+++ packages/py-codeforce/py_codeforce-1.3.tar.gz/py_codeforce-1.3/pycodeforces/abc/__cobjects__.py
@@ -0,0 +1,19 @@
+import msgspec
+import typing as t
+from pycodeforces.abc.__objects__ import (
+    Problem,
+    Contest,
+    RankListRow,
+    ProblemStatistics,
+)
+
+
+class Standings(msgspec.Struct):
+    contest: t.Optional[t.Union[t.List[Contest], Contest]] = None
+    problems: t.Optional[t.Union[t.List[Problem], Problem]] = None
+    rows: t.Optional[t.Union[t.List[RankListRow], RankListRow]] = None
+
+
+class ProblemSetProblems(msgspec.Struct):
+    problems: t.Optional[t.Union[t.List[Problem], Problem]]
+    problemStatistics: t.Optional[t.Union[t.List[ProblemStatistics], ProblemStatistics]]
---
+++ packages/py-codeforce/py_codeforce-1.3.tar.gz/py_codeforce-1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="py-codeforces",
+    packages=["pycodeforces", "pycodeforces.abc"],
+    version="1.3",
+    license="GPL 3.0",
+    description="Py-Codeforces is a high-performance and type-safe Python library designed for seamless interaction with Codeforces. It offers both asynchronous and synchronous client handlers, allowing developers to choose the appropriate method based on their requirements.",
+    author="Parth Mishra",
+    author_email="halfstackpgr@gmail.com",
+    url="https://github.com/halfstackpgr/py-codeforces",
+    keywords=["python", "codeforces", "api", "wrapper", "async", "sync"],
+    install_requires=[
+        "aiohttp",
+        "requests",
+        "msgspec",
+        'tomli; python_version < "3.11"',
+    ],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Development Status :: 1 - Alpha/Unstable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
