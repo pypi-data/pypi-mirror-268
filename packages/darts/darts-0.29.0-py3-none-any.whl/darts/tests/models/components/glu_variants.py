c0f81739e1040df575887af8afe588fd35862245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:30 +0000

    Add package darts/0.29.0/darts-0.29.0-py3-none-any.whl

---
+++ packages/darts/darts-0.29.0-py3-none-any.whl/darts/tests/models/components/glu_variants.py
@@ -0,0 +1,27 @@
+import pytest
+
+from darts.logging import get_logger
+
+logger = get_logger(__name__)
+
+try:
+    import torch
+
+    from darts.models.components import glu_variants
+    from darts.models.components.glu_variants import GLU_FFN
+except ImportError:
+    pytest.skip(
+        f"Torch not available. {__name__} tests will be skipped.",
+        allow_module_level=True,
+    )
+
+
+class TestFFN:
+    def test_ffn(self):
+        for FeedForward_network in GLU_FFN:
+            self.feed_forward_block = getattr(glu_variants, FeedForward_network)(
+                d_model=4, d_ff=16, dropout=0.1
+            )
+
+            inputs = torch.zeros(1, 4, 4)
+            self.feed_forward_block(x=inputs)
---
+++ packages/darts/darts-0.29.0-py3-none-any.whl/darts/utils/__init__.py
@@ -0,0 +1,11 @@
+"""
+Utils
+-----
+"""
+
+from .utils import (
+    _build_tqdm_iterator,
+    _parallel_apply,
+    _with_sanity_checks,
+    n_steps_between,
+)
