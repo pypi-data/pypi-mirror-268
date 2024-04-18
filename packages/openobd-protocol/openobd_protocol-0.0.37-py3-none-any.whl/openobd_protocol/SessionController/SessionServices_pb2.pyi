4141abc8856cebadf0ed7d394a9d7b2ca5dee8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:22 +0000

    Add package openobd-protocol/0.0.37/openobd_protocol-0.0.37-py3-none-any.whl

---
+++ packages/openobd-protocol/openobd_protocol-0.0.37-py3-none-any.whl/openobd_protocol/SessionController/SessionServices_pb2.pyi
@@ -0,0 +1,5 @@
+from openobd_protocol import Status_pb2 as _Status_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
