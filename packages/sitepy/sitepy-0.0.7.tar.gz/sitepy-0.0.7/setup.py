5075c8ca2995a1151296cfc74dfd6b30ffa68998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 06:04:31 +0000

    Add package sitepy/0.0.7/sitepy-0.0.7.tar.gz

---
+++ packages/sitepy/sitepy-0.0.7.tar.gz/sitepy-0.0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sitepy',
+    version='0.0.7',
+    description='A simple web framework.',
+    author='WolfTheDev',
+    author_email='wolfthedev@gmail.com',
+    url='https://github.com/WolfTheDeveloper/sitepy',
+    packages=find_packages(),
+    long_description=open('sitepy/README.md').read(),
+    long_description_content_type='text/markdown',  # Add this line
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.6',
+)
