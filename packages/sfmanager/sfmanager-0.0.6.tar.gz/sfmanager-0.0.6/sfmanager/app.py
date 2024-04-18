c7aea5cc17e1e17393ca429edad28b6f5c4f90d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:38 +0000

    Add package sfmanager/0.0.6/sfmanager-0.0.6.tar.gz

---
+++ packages/sfmanager/sfmanager-0.0.6.tar.gz/sfmanager-0.0.6/sfmanager/app.py
@@ -0,0 +1,58 @@
+class FileManager:
+	filename = None
+	level = None
+
+	def __init__(self, filename, level):
+		self.filename = str(filename)
+		self.level = int(level)
+
+	def readline(self):
+		if self.level >= 1:
+			try:
+				with open(f"{self.filename}", "r") as f:
+					raw = f.readline()
+				return raw
+			except FileNotFoundError:
+				print("Something went wrong...")
+
+	def readlines(self):
+		if self.level >= 1:
+			try:
+				with open(f"{self.filename}", "r") as f:
+					raw = f.readlines()
+				return raw
+			except FileNotFoundError:
+				print("Something went wrong...")
+	def set(self, text):
+		if self.level >= 2:
+			try:
+				with open(f"{self.filename}", "w") as f:
+					f.write(text)
+					return 1
+			except FileNotFoundError:
+				print("Something went wrong...")
+				return 0
+		else:
+			return 0
+	def add(self, text):
+		if self.level >= 2:
+			try:
+				with open(f"{self.filename}", "w") as f:
+					f.write(f.read() + text)
+					return 1
+			except FileNotFoundError:
+				print("Something went wrong...")
+				return 0
+		else:
+			return 0
+	def replace(self, target, text):
+		if self.level >= 2:
+			try:
+				with open(f"{self.filename}", "w") as f:
+					f.write(f.read().replace(target, text))
+					return 1
+			except FileNotFoundError:
+				print("Something went wrong...")
+				return 0
+		else:
+			return 0
