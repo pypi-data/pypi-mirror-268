ee97e812030d4644a63a0cc720dfd640d2434293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:43 +0000

    Add package nobinobi-daily-follow-up/0.1.2.15/nobinobi-daily-follow-up-0.1.2.15.tar.gz

---
+++ packages/nobinobi-daily-follow-up/nobinobi-daily-follow-up-0.1.2.15.tar.gz/nobinobi-daily-follow-up-0.1.2.15/nobinobi_daily_follow_up/templatetags/replacement_classroom_tags.py
@@ -0,0 +1,19 @@
+#      Copyright (C) 2022 <Florian Alu - Prolibre - https://prolibre.com
+#      This program is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU Affero General Public License as
+#      published by the Free Software Foundation, either version 3 of the
+#      License, or (at your option) any later version.
+#
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU Affero General Public License for more details.
+#
+#      You should have received a copy of the GNU Affero General Public License
+#      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+from django.template.defaulttags import register
+
+
+@register.simple_tag
+def get_replacement_classroom_id(child, date, *args, **kwargs):
+    return child.get_now_classroom(date).id
