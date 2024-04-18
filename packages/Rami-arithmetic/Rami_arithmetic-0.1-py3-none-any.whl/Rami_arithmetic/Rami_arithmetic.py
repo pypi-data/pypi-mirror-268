aab78b49b98ea350001c6cf5798e25e769a9403b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:04:59 +0000

    Add package Rami-arithmetic/0.1/Rami_arithmetic-0.1-py3-none-any.whl

---
+++ packages/Rami-arithmetic/Rami_arithmetic-0.1-py3-none-any.whl/Rami_arithmetic/Rami_arithmetic.py
@@ -0,0 +1,15 @@
+# your_package/Rami_arithmetic.py
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError("Division by zero is not allowed")
+    return x / y
