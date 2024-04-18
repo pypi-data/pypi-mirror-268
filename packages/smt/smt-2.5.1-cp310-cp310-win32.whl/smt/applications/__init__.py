b37f95717e46bf202420fc2c4e4620e7b5be7f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 04:05:35 +0000

    Add package smt/2.5.1/smt-2.5.1-cp310-cp310-win32.whl

---
+++ packages/smt/smt-2.5.1-cp310-cp310-win32.whl/smt/applications/__init__.py
@@ -0,0 +1,18 @@
+from .ego import EGO, Evaluator
+from .mfk import MFK, NestedLHS
+from .mfkpls import MFKPLS
+from .mfkplsk import MFKPLSK
+from .moe import MOE, MOESurrogateModel
+from .vfm import VFM
+
+__all__ = [
+    "VFM",
+    "MOE",
+    "MOESurrogateModel",
+    "MFK",
+    "NestedLHS",
+    "MFKPLS",
+    "MFKPLSK",
+    "EGO",
+    "Evaluator",
+]
