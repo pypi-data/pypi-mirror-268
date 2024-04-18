359cf649b6a3fb11738762e359431d1341c25838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:05:01 +0000

    Add package syntho-cli/1.7.0/syntho_cli-1.7.0.tar.gz

---
+++ packages/syntho-cli/syntho_cli-1.7.0.tar.gz/syntho_cli-1.7.0/README.md
@@ -0,0 +1,13 @@
+# syntho-cli
+
+For detailed information on this topic, please refer to the [relevant section in the documentation](https://github.com/syntho-ai/syntho-cli/blob/main/docs/getting-started.md).
+
+## For Developers
+
+After cloning the repo, it can be installed via below command.
+
+> A virtual env is recommended.
+
+```
+make pip-local-install
+```
---
+++ packages/syntho-cli/syntho_cli-1.7.0.tar.gz/syntho_cli-1.7.0/setup.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), "README.md")).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+version = "1.7.0"
+
+setup(
+    name="syntho-cli",
+    version=version,
+    description="Syntho Stack Deployment CLI",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url="https://github.com/syntho-ai/syntho-cli",
+    download_url="https://github.com/syntho-ai/syntho-cli/tarball/%s" % version,
+    author="Syntho B.V.",
+    author_email="info@syntho.ai",
+    license="MIT",
+    keywords="syntho, synthetic data, deployment",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": ["syntho-cli = cli.syntho_cli:cli"]
+    },
+    package_data={"cli": ["scripts/deploy-kubernetes.sh", "scripts/pre-requirements-kubernetes.sh",
+                          "scripts/utils.sh", "scripts/cleanup-kubernetes.sh",
+                          "scripts/get-k8s-cluster-context-name.sh",
+                          "scripts/configuration-questions.sh",
+                          "scripts/download-syntho-charts-release.sh",
+                          "scripts/major-pre-deployment-operations.sh",
+                          "scripts/deploy-ray-and-syntho-stack.sh",
+                          "scripts/k8s-deployment-preparation.sh",
+                          "scripts/cleanup-docker-compose.sh",
+                          "scripts/pre-requirements-dc.sh",
+                          "scripts/configuration-questions-dc.sh",
+                          "scripts/download-syntho-charts-release-dc.sh",
+                          "scripts/deploy-ray-and-syntho-stack-dc.sh"]},
+    include_package_data=True,
+    install_requires=[
+        "click==8.1.7",
+        "pyyaml==6.0.1",
+        "ipdb",
+        "watchdog==4.0.0",
+    ]
+)
