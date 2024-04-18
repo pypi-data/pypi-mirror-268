1b1e1f771b9cf89e1ced674fc81a50accffdbe67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:04:57 +0000

    Add package wipo-gbd-transformation/1.1.49/wipo_gbd_transformation-1.1.49-py3-none-any.whl

---
+++ packages/wipo-gbd-transformation/wipo_gbd_transformation-1.1.49-py3-none-any.whl/gbdtransformation/designs/__init__.py
@@ -0,0 +1,9 @@
+status = ['Ended', 'Expired', 'Pending', 'Registered', 'Unknown', 'Delete']
+
+kinds = ['Design','Design patent']
+
+
+st13_identifier = \
+        { 'national': { 'design': '70'},
+          'multinational': { 'design': '70' }
+          }
