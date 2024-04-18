9f19d78f8165a0a3aa8806a2a2668686360e3619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 14:04:45 +0000

    Add package APIManager-DM-2024/0.0.9/apimanager_dm_2024-0.0.9.tar.gz

---
+++ packages/APIManager-DM-2024/apimanager_dm_2024-0.0.9.tar.gz/apimanager_dm_2024-0.0.9/README.md
@@ -0,0 +1,1 @@
+<h3>Simple API Manipulator</h3>
---
+++ packages/APIManager-DM-2024/apimanager_dm_2024-0.0.9.tar.gz/apimanager_dm_2024-0.0.9/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="APIManager-DM-2024",
+    version="0.0.9",
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.31.0'
+    ]
+)
