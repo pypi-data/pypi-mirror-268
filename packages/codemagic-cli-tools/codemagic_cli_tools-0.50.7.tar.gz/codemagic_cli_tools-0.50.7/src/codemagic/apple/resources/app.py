fd70bb917222c1ca72aa14ceb08377155f735362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:00 +0000

    Add package codemagic-cli-tools/0.50.7/codemagic_cli_tools-0.50.7.tar.gz

---
+++ packages/codemagic-cli-tools/codemagic_cli_tools-0.50.7.tar.gz/codemagic_cli_tools-0.50.7/src/codemagic/apple/resources/app.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Optional
+
+from .enums import ContentRightsDeclaration
+from .enums import Locale
+from .resource import Relationship
+from .resource import Resource
+
+
+class App(Resource):
+    """
+    https://developer.apple.com/documentation/appstoreconnectapi/app
+    """
+
+    attributes: Attributes
+    relationships: Optional[Relationships] = None
+
+    @dataclass
+    class Attributes(Resource.Attributes):
+        bundleId: str
+        name: str
+        primaryLocale: Locale
+        sku: str
+        contentRightsDeclaration: ContentRightsDeclaration
+        isOrEverWasMadeForKids: bool
+
+        def __post_init__(self):
+            if isinstance(self.contentRightsDeclaration, str):
+                self.contentRightsDeclaration = ContentRightsDeclaration(self.contentRightsDeclaration)
+            if isinstance(self.primaryLocale, str):
+                self.primaryLocale = Locale(self.primaryLocale)
+
+    @dataclass
+    class Relationships(Resource.Relationships):
+        _OMIT_IF_NONE_KEYS = ("betaTesters", "ciProduct", "perfPowerMetrics")
+
+        appInfos: Relationship
+        appStoreVersions: Relationship
+        betaAppLocalizations: Relationship
+        betaAppReviewDetail: Relationship
+        betaGroups: Relationship
+        betaLicenseAgreement: Relationship
+        builds: Relationship
+        endUserLicenseAgreement: Relationship
+        gameCenterEnabledVersions: Relationship
+        inAppPurchases: Relationship
+        preOrder: Relationship
+        preReleaseVersions: Relationship
+
+        betaTesters: Optional[Relationship] = None
+        ciProduct: Optional[Relationship] = None
+        perfPowerMetrics: Optional[Relationship] = None
---
+++ packages/codemagic-cli-tools/codemagic_cli_tools-0.50.7.tar.gz/codemagic_cli_tools-0.50.7/tests/tools/app_store_connect/conftest.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import os
+import pathlib
+
+import pytest
+from codemagic.tools.app_store_connect import AppStoreConnect
+from codemagic.tools.app_store_connect.arguments import AppStoreConnectArgument
+from codemagic.tools.app_store_connect.arguments import Types
+
+
+@pytest.fixture(autouse=True)
+def register_args(cli_argument_group):
+    for arg in AppStoreConnect.CLASS_ARGUMENTS:
+        arg.register(cli_argument_group)
+
+
+@pytest.fixture
+def mock_auth_key() -> pathlib.Path:
+    return pathlib.Path(__file__).parent / "mocks" / "AuthKeyMock.p8"
+
+
+@pytest.fixture()
+def namespace_kwargs(mock_auth_key):
+    args = AppStoreConnectArgument
+    ns_kwargs = {
+        "action": "devices",
+        "action_subcommand": "list",
+        args.CERTIFICATES_DIRECTORY.key: args.CERTIFICATES_DIRECTORY.get_default(),
+        args.PROFILES_DIRECTORY.key: args.PROFILES_DIRECTORY.get_default(),
+        args.LOG_REQUESTS.key: True,
+        args.JSON_OUTPUT.key: False,
+        args.ISSUER_ID.key: Types.IssuerIdArgument("issuer-id"),
+        args.KEY_IDENTIFIER.key: Types.KeyIdentifierArgument("key-identifier"),
+        args.PRIVATE_KEY.key: Types.PrivateKeyArgument(mock_auth_key.read_text()),
+        args.UNAUTHORIZED_REQUEST_RETRIES.key: 1,
+        args.SERVER_ERROR_RETRIES.key: 1,
+        args.DISABLE_JWT_CACHE.key: True,
+    }
+    for arg in AppStoreConnect.CLASS_ARGUMENTS:
+        if not hasattr(arg.type, "environment_variable_key"):
+            continue
+        os.environ.pop(arg.type.environment_variable_key, None)
+    return ns_kwargs
