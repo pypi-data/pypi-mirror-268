b3503382e3c7e39d00ba8711f18ce2c5124d09e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 06:04:49 +0000

    Add package abses/0.5.8/abses-0.5.8.tar.gz

---
+++ packages/abses/abses-0.5.8.tar.gz/abses-0.5.8/abses/__init__.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python 3.11.0
+# -*-coding:utf-8 -*-
+# @Author  : Shuang Song
+# @Contact   : SongshGeo@gmail.com
+# GitHub   : https://github.com/SongshGeo
+# Website: https://cv.songshgeo.com/
+
+"""
+ABSESpy - Agent-based Social-ecological System framework in Python
+Copyright (c) 2021-2023 Shuang Song
+
+Documentation: https://absespy.github.io/ABSESpy
+Examples: https://absespy.github.io/ABSESpy/tutorial/user_guide/
+Source: https://github.com/absespy/ABSESpy
+"""
+
+__all__ = [
+    "__version__",
+    "MainModel",
+    "BaseHuman",
+    "BaseNature",
+    "PatchModule",
+    "Actor",
+    "Decision",
+    "ActorsList",
+    "PatchCell",
+    "perception",
+    "alive_required",
+    "time_condition",
+]
+__version__ = "v0.5.8"
+
+from .actor import Actor, alive_required, perception
+from .container import ActorsList
+from .decision import Decision
+from .human import BaseHuman
+from .main import MainModel
+from .nature import BaseNature, PatchCell, PatchModule
+from .time import time_condition
---
+++ packages/abses/abses-0.5.8.tar.gz/abses-0.5.8/abses/dynamic.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python 3.11.0
+# -*-coding:utf-8 -*-
+# @Author  : Shuang (Twist) Song
+# @Contact   : SongshGeo@gmail.com
+# GitHub   : https://github.com/SongshGeo
+# Website: https://cv.songshgeo.com/
+
+from __future__ import annotations
+
+import inspect
+from typing import TYPE_CHECKING, Any, Callable, List
+
+if TYPE_CHECKING:
+    from .objects import _BaseObj
+    from .time import TimeDriver
+
+
+class _DynamicVariable:
+    """Time dependent variable
+
+    A time dependent function will take the model time driver as
+    an input and return its value. The function can also take other
+    variables as inputs. The function can be defined as a static
+    method of a class or a function.
+    """
+
+    def __init__(
+        self, name: str, obj: _BaseObj, data: Any, function: Callable
+    ) -> None:
+        self._name: str = name
+        self._obj: _BaseObj = obj
+        self._data: Any = data
+        self._function: Callable = function
+        self._cached_data: Any = None
+        self.now()
+
+    @property
+    def name(self):
+        """Get the name of the variable
+
+        Returns
+        -------
+        name: str
+        """
+        return self._name
+
+    @property
+    def obj(self) -> _BaseObj:
+        """Returns a base object instance
+
+        Returns:
+            obj:
+                _BaseObj
+        """
+        return self._obj
+
+    @obj.setter
+    def obj(self, obj: _BaseObj):
+        if not isinstance(obj, _BaseObj):
+            raise TypeError("Only accept observer object")
+        self._obj = obj
+
+    @property
+    def data(self) -> Any:
+        """Returns unused data
+
+        Returns:
+            data:
+                Any
+        """
+        return self._data
+
+    @property
+    def function(self) -> Callable:
+        """Get the function that calculates the variable
+
+        Returns:
+            function:
+                Callable
+        """
+        return self._function
+
+    @property
+    def time(self) -> TimeDriver:
+        """Get the model time driver
+
+        Returns:
+            time:
+                abses.time.TimeDriver
+        """
+        return self.obj.time
+
+    def get_required_attributes(self, function: Callable) -> List[str]:
+        """Get the function required attributes
+
+        Returns:
+            required_attributes:
+                list[str]
+        """
+        # Get the source code of the function
+        source_code = inspect.getsource(function)
+        return [
+            attr
+            for attr in ["data", "obj", "time", "name"]
+            if attr in source_code
+        ]
+
+    def now(self) -> Any:
+        """Return the dynamic variable function's output
+
+        Returns:
+            output:
+                Any
+        """
+        required_attrs = self.get_required_attributes(self.function)
+        args = {attr: getattr(self, attr) for attr in required_attrs}
+        result = self.function(**args)
+        self._cached_data = result
+        return result
+
+    @property
+    def cache(self) -> Any:
+        """Return the dynamic variable's cache"""
+        return self._cached_data
