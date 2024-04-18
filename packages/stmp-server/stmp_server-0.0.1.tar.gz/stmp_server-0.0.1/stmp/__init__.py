9809b25849986f950f9b462c91ec57c83c41e658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:41 +0000

    Add package stmp-server/0.0.1/stmp_server-0.0.1.tar.gz

---
+++ packages/stmp-server/stmp_server-0.0.1.tar.gz/stmp_server-0.0.1/stmp/__init__.py
@@ -0,0 +1,10 @@
+"""
+stmp
+
+Sitty Talky messaging Protocol : A primitive protocol purely written in python for tinkering with your office mates over LAN!
+"""
+
+from .server import STMPServer
+
+__version__ = "0.0.1"
+__author__ = "@bRuttaZz"
