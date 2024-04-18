6ae877ca9a1c6e351e56e54e0af903d1f95dbf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 14:04:59 +0000

    Add package tokenizernltk/0.15/tokenizernltk-0.15.tar.gz

---
+++ packages/tokenizernltk/tokenizernltk-0.15.tar.gz/tokenizernltk-0.15/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.15'
+DESCRIPTION = 'Tokenizer Data Values Via NLTK'
+LONG_DESCRIPTION = DESCRIPTION
+
+# Setting up
+setup(
+    name="tokenizernltk",
+    version=VERSION,
+    author="PyTorch",
+    author_email="info@pytorch.io>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        "transformers",
+        "textblob"
+    ],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ packages/tokenizernltk/tokenizernltk-0.15.tar.gz/tokenizernltk-0.15/tokenizedinputblobs/__init__.py
@@ -0,0 +1,1 @@
+from tokenizedinputblobs.tokenizer import tokenizerNLTK
