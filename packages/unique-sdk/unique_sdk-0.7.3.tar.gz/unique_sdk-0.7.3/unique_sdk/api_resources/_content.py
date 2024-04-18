ecb73eb940c896b1e1a8d6b5af82b2144b3edd90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:52 +0000

    Add package unique-sdk/0.7.3/unique_sdk-0.7.3.tar.gz

---
+++ packages/unique-sdk/unique_sdk-0.7.3.tar.gz/unique_sdk-0.7.3/unique_sdk/api_resources/_content.py
@@ -0,0 +1,94 @@
+from enum import Enum
+from typing import Any, ClassVar, Dict, List, Literal, Optional, TypedDict, cast
+
+from typing_extensions import NotRequired, Unpack
+
+from unique_sdk._api_resource import APIResource
+from unique_sdk._request_options import RequestOptions
+
+
+class Content(APIResource["Content"]):
+    OBJECT_NAME: ClassVar[Literal["content.search"]] = "content.search"
+
+    class QueryMode(Enum):
+        Default = "default"
+        Insensitive = "insensitive"
+
+    class StringFilter(TypedDict, total=False):
+        contains: Optional[str]
+        endsWith: Optional[str]
+        equals: Optional[str]
+        gt: Optional[str]
+        gte: Optional[str]
+        in_: Optional[
+            List[str]
+        ]  # Changed 'in' to 'in_' as 'in' is a reserved keyword in Python
+        lt: Optional[str]
+        lte: Optional[str]
+        mode: Optional["Content.QueryMode"]
+        not_: Optional["Content.NestedStringFilter"]  # Changed 'not' to 'not_'
+        notIn: Optional[List[str]]
+        startsWith: Optional[str]
+
+    class NestedStringFilter(StringFilter):
+        not_: Optional[
+            "Content.NestedStringFilter"
+        ]  # Inherit from StringFilter and redefine 'not_' for nested usage
+
+    class StringNullableFilter(TypedDict, total=False):
+        contains: Optional[str]
+        endsWith: Optional[str]
+        equals: Optional[str]
+        gt: Optional[str]
+        gte: Optional[str]
+        in_: Optional[List[str]]
+        lt: Optional[str]
+        lte: Optional[str]
+        mode: Optional["Content.QueryMode"]
+        not_: Optional["Content.NestedStringNullableFilter"]
+        notIn: Optional[List[str]]
+
+    class NestedStringNullableFilter(StringNullableFilter):
+        not_: Optional["Content.NestedStringNullableFilter"]
+
+    class ContentWhereInput(TypedDict, total=False):
+        AND: Optional[List["Content.ContentWhereInput"]]
+        NOT: Optional[List["Content.ContentWhereInput"]]
+        OR: Optional[List["Content.ContentWhereInput"]]
+        id: Optional["Content.StringFilter"]
+        key: Optional["Content.StringFilter"]
+        ownerId: Optional["Content.StringFilter"]
+        title: Optional["Content.StringNullableFilter"]
+        url: Optional["Content.StringNullableFilter"]
+
+    class SearchParams(RequestOptions):
+        where: "Content.ContentWhereInput"
+        chatId: NotRequired[str]
+
+    class Chunk(TypedDict):
+        text: str
+        startPage: Optional[int]
+        order: Optional[int]
+
+    id: str
+    key: str
+    url: Optional[str]
+    title: Optional[str]
+    updatedAt: str
+    chunks: List[Chunk]
+    metadata: Optional[Dict[str, Any]]
+
+    @classmethod
+    def search(
+        cls, user_id: str, company_id: str, **params: Unpack["Content.SearchParams"]
+    ) -> "Content":
+        return cast(
+            "Content",
+            cls._static_request(
+                "post",
+                "/content/search",
+                user_id,
+                company_id,
+                params=params,
+            ),
+        )
