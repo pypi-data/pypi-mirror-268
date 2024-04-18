442fb8a1afe880bc81a923f9e9d9ae6630bb6708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:27 +0000

    Add package mastercard-currency-client/2.0.0/mastercard_currency_client-2.0.0.tar.gz

---
+++ packages/mastercard-currency-client/mastercard_currency_client-2.0.0.tar.gz/mastercard_currency_client-2.0.0/README.md
@@ -0,0 +1,3 @@
+# mastercard-currency-conversion
+
+MasterCard Currency Conversion library, fork of the [mastercard-currency-conversion](https://pypi.org/project/mastercard-currency-conversion/) library.
---
+++ packages/mastercard-currency-client/mastercard_currency_client-2.0.0.tar.gz/mastercard_currency_client-2.0.0/mastercardcurrencyconversion/__init__.py
@@ -0,0 +1,9 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from .resourceconfig import ResourceConfig
+
+# import apis into api package
+from .conversionrate import *
+from .currencies import *
+from .rateissued import *
