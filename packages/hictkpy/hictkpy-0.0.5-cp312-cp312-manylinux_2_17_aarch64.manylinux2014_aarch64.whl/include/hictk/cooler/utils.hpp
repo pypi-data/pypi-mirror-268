c978597c61f62a1b12807ad9d9ac90f67f9dfeba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:40 +0000

    Add package hictkpy/0.0.5/hictkpy-0.0.5-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/hictkpy/hictkpy-0.0.5-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/include/hictk/cooler/utils.hpp
@@ -0,0 +1,58 @@
+// Copyright (C) 2022 Roberto Rossini <roberros@uio.no>
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+#include <filesystem>
+#include <string_view>
+#include <vector>
+
+#include "hictk/cooler/cooler.hpp"
+#include "hictk/cooler/dataset.hpp"
+#include "hictk/cooler/group.hpp"
+#include "hictk/reference.hpp"
+
+namespace hictk::cooler::utils {
+
+/// Iterable of strings
+template <typename N, typename Str>
+void merge(Str first_uri, Str last_uri, std::string_view dest_uri, bool overwrite_if_exists = false,
+           std::size_t chunk_size = 500'000, std::size_t update_frequency = 10'000'000,
+           std::uint32_t compression_lvl = DEFAULT_COMPRESSION_LEVEL);
+
+template <typename PixelIt>
+void merge(const std::vector<PixelIt>& heads, const std::vector<PixelIt>& tails,
+           const BinTable& bins, std::string_view dest_uri, std::string_view assembly = "unknown",
+           bool overwrite_if_exists = false, std::size_t chunk_size = 500'000,
+           std::size_t update_frequency = 10'000'000,
+           std::uint32_t compression_lvl = DEFAULT_COMPRESSION_LEVEL);
+
+[[nodiscard]] bool equal(std::string_view uri1, std::string_view uri2,
+                         bool ignore_attributes = true);
+[[nodiscard]] bool equal(const File& clr1, const File& clr2, bool ignore_attributes = true);
+
+[[nodiscard]] std::vector<std::uint32_t> list_resolutions(const std::filesystem::path& path,
+                                                          bool sorted = true);
+
+void copy(std::string_view uri1, std::string_view uri2);
+void copy(std::string_view uri1, RootGroup dest);
+
+template <typename It>
+void rename_chromosomes(std::string_view uri, It first_mapping, It last_mapping);
+
+template <typename NameMap, typename = std::enable_if_t<is_map_v<NameMap>>>
+void rename_chromosomes(std::string_view uri, const NameMap& mappings);
+
+template <typename NameMap, typename = std::enable_if_t<is_map_v<NameMap>>>
+inline void rename_chromosomes(cooler::Dataset& chrom_dset, const NameMap& mappings);
+
+}  // namespace hictk::cooler::utils
+
+#include "./impl/utils_copy_impl.hpp"           // NOLINT
+#include "./impl/utils_equal_impl.hpp"          // NOLINT
+#include "./impl/utils_impl.hpp"                // NOLINT
+#include "./impl/utils_merge_impl.hpp"          // NOLINT
+#include "./impl/utils_rename_chroms_impl.hpp"  // NOLINT
---
+++ packages/hictkpy/hictkpy-0.0.5-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/include/hictk/fmt/bin.hpp
@@ -0,0 +1,54 @@
+// Copyright (C) 2023 Roberto Rossini <roberros@uio.no>
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <fmt/format.h>
+
+#include <cassert>
+#include <string_view>
+
+#include "hictk/bin.hpp"
+#include "hictk/fmt/common.hpp"
+#include "hictk/fmt/genomic_interval.hpp"
+
+namespace fmt {
+template <>
+struct formatter<hictk::Bin> {
+  enum Presentation { bed, raw, ucsc };
+  Presentation presentation{Presentation::raw};
+
+  constexpr format_parse_context::iterator parse(format_parse_context &ctx) {
+    const auto *it = ctx.begin();
+    const auto *end = ctx.end();
+
+    if (hictk::internal::starts_with(ctx, "bed")) {
+      presentation = Presentation::bed;
+      it += std::string_view{"bed"}.size();  // NOLINT
+    } else if (hictk::internal::starts_with(ctx, "raw")) {
+      presentation = Presentation::raw;
+      it += std::string_view{"raw"}.size();  // NOLINT
+    } else if (hictk::internal::starts_with(ctx, "ucsc")) {
+      presentation = Presentation::ucsc;
+      it += std::string_view{"ucsc"}.size();  // NOLINT
+    }
+
+    if (it != end && *it != '}') {
+      throw fmt::format_error("invalid format");
+    }
+
+    return it;
+  }
+  inline format_context::iterator format(const hictk::Bin &b, format_context &ctx) const {
+    if (presentation == Presentation::bed) {
+      return fmt::format_to(ctx.out(), FMT_STRING("{:bed}"), b.interval());
+    }
+    if (presentation == Presentation::raw) {
+      return fmt::format_to(ctx.out(), FMT_STRING("{}"), b.id());
+    }
+    assert(presentation == Presentation::ucsc);
+    return fmt::format_to(ctx.out(), FMT_STRING("{:ucsc}"), b.interval());
+  }
+};
+}  // namespace fmt
