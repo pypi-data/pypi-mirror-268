2d8161a8373920b11011507901d5a7a9f67139ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:01 +0000

    Add package bens-calculator/0.0.1/bens_calculator-0.0.1.tar.gz

---
+++ packages/bens-calculator/bens_calculator-0.0.1.tar.gz/bens_calculator-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup (
+    name="bens_calculator",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Ben Brownlie",
+    description="A simple calculator for adding and multiplying two numbers",
+    long_description="README.me",
+    long_description_content_type="text/markdown"
+)
