21504dee0f7f3d051dbbbf884150fbc5d5c05b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:46 +0000

    Add package culminator/0.4/culminator-0.4.tar.gz

---
+++ packages/culminator/culminator-0.4.tar.gz/culminator-0.4/culminator/__init__.py
@@ -0,0 +1,3 @@
+from .classification import classification, predict_values_classification
+from.clustering import clustering, predict_values_clustering
+from.regression import regression, predict_values_regression
