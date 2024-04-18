ff4247daf654bb22961f378dc7c1c3d1c71662e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 05:04:26 +0000

    Add package osism/0.20240417.0/osism-0.20240417.0-py3-none-any.whl

---
+++ packages/osism/osism-0.20240417.0-py3-none-any.whl/osism/core/playbooks.py
@@ -0,0 +1,14 @@
+# SPDX-License-Identifier: Apache-2.0
+
+from pathlib import Path
+from typing import Dict, Any
+
+import yaml
+
+MAP_ROLE2ENVIRONMENT: Dict[str, Any] = {}
+
+for path in Path("/interface/playbooks").glob("*-ansible.yml"):
+    with open(path) as fp:
+        data = yaml.load(fp, Loader=yaml.SafeLoader)
+
+    MAP_ROLE2ENVIRONMENT = MAP_ROLE2ENVIRONMENT | data
