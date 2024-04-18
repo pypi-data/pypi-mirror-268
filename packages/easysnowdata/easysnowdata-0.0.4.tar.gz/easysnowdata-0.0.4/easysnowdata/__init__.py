c0d955660aa97a7fba603d3883cd17b05f8f2843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:05:23 +0000

    Add package easysnowdata/0.0.4/easysnowdata-0.0.4.tar.gz

---
+++ packages/easysnowdata/easysnowdata-0.0.4.tar.gz/easysnowdata-0.0.4/easysnowdata/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for easysnowdata."""
+
+__author__ = """Eric Gagliano"""
+__email__ = "egagli@uw.edu"
+__version__ = "0.0.4"
+__all__=['easysnowdata','utils','remote_sensing']
+
+import easysnowdata.easysnowdata
+import easysnowdata.utils
+import easysnowdata.remote_sensing
+import easysnowdata.automatic_weather_stations
+import easysnowdata.topography
