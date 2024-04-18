ea61fb47774e4bb4dc91e63a36fdaf2f73ed4448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:54 +0000

    Add package imio.smartweb.core/1.2.43/imio.smartweb.core-1.2.43-py3-none-any.whl

---
+++ packages/imio.smartweb.core/imio.smartweb.core-1.2.43-py3-none-any.whl/imio/smartweb/core/contents/rest/events/view.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from imio.smartweb.core.contents.rest.view import BaseRestView
+from imio.smartweb.core.interfaces import IOgpViewUtils
+from plone import api
+from zope.interface import implementer
+
+
+@implementer(IOgpViewUtils)
+class EventsViewView(BaseRestView):
+    """EventsView view"""
+
+    @property
+    def propose_url(self):
+        return api.portal.get_registry_record("smartweb.propose_events_url")
+
+    @property
+    def display_map(self):
+        return self.context.display_map
+
+    @property
+    def only_past_events(self):
+        return self.context.only_past_events
+
+    @property
+    def display_agendas_titles(self):
+        return self.context.display_agendas_titles
