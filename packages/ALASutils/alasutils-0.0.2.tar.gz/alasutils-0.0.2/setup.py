94ed1809b459518cbdc353b7ca2aa5cef8a4cae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 04:05:39 +0000

    Add package ALASutils/0.0.2/alasutils-0.0.2.tar.gz

---
+++ packages/ALASutils/alasutils-0.0.2.tar.gz/alasutils-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='ALASutils',
+version='0.0.2',
+author='Juan Pablo Calderon',
+author_email='jpcalderon@fcaglp.unlp.edu.ar',
+description='ALAS utils',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
