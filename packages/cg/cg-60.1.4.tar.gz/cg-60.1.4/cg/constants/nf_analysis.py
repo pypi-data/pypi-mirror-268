3840e84dd5085aa3313271b9ebf808df54ebfe5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 04:04:17 +0000

    Add package cg/60.1.4/cg-60.1.4.tar.gz

---
+++ packages/cg/cg-60.1.4.tar.gz/cg-60.1.4/cg/constants/nf_analysis.py
@@ -0,0 +1,41 @@
+"""Nf-tower related constants."""
+
+from enum import StrEnum
+from typing import Any
+
+
+class NfTowerStatus(StrEnum):
+    """NF-Tower job submission status."""
+
+    SUBMITTED: str = "SUBMITTED"
+    RUNNING: str = "RUNNING"
+    SUCCEEDED: str = "SUCCEEDED"
+    FAILED: str = "FAILED"
+    CANCELLED: str = "CANCELLED"
+    UNKNOWN: str = "UNKNOWN"
+
+
+RNAFUSION_METRIC_CONDITIONS: dict[str, dict[str, Any]] = {
+    "uniquely_mapped_percent": {"norm": "gt", "threshold": 60},
+    "PCT_MRNA_BASES": {"norm": "gt", "threshold": 80},
+    "PCT_RIBOSOMAL_BASES": {"norm": "lt", "threshold": 5},
+    "PERCENT_DUPLICATION": {"norm": "lt", "threshold": 0.7},
+}
+
+TOMTE_METRIC_CONDITIONS: dict[str, dict[str, Any]] = {
+    "uniquely_mapped_percent": {"norm": "gt", "threshold": 60},
+    "PCT_MRNA_BASES": {"norm": "gt", "threshold": 80},
+    "PCT_RIBOSOMAL_BASES": {"norm": "lt", "threshold": 5},
+    "pct_duplication": {"norm": "lt", "threshold": 70},
+}
+
+
+MULTIQC_NEXFLOW_CONFIG = """process {
+    withName:'MULTIQC' {
+        memory = { 1.GB * task.attempt }
+        time   = { 4.h  * task.attempt }
+        cpus = 2
+        ext.args = ' --data-format json '
+    }
+}
+"""
