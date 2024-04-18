a9b3638a34edd7ebbb1de8c08cd415d760dd7b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:04:45 +0000

    Add package radical.pilot/1.52.1/radical_pilot-1.52.1.tar.gz

---
+++ packages/radical.pilot/radical_pilot-1.52.1.tar.gz/radical_pilot-1.52.1/src/radical/pilot/__init__.py
@@ -0,0 +1,69 @@
+
+__copyright__ = "Copyright 2013-2014, http://radical.rutgers.edu"
+__license__   = "MIT"
+
+# ------------------------------------------------------------------------------
+# we *first* import radical.utils, so that the monkeypatching of the logger has
+# a chance to kick in before the logging module is pulled by any other 3rd party
+# module, and also to monkeypatch `os.fork()` for the `atfork` functionality
+import radical.utils as _ru
+
+# ------------------------------------------------------------------------------
+# constants and types
+from .states     import *
+from .constants  import *
+
+
+# ------------------------------------------------------------------------------
+# import API
+from .session                   import Session
+from .proxy                     import Proxy
+
+from .task_manager              import TaskManager
+from .task                      import Task
+from .raptor_tasks              import RaptorMaster, RaptorWorker
+from .pytask                    import PythonTask
+from .task_description          import TaskDescription
+from .task_description          import TASK_EXECUTABLE
+from .task_description          import TASK_METH, TASK_METHOD
+from .task_description          import TASK_FUNC, TASK_FUNCTION
+from .task_description          import TASK_EXEC, TASK_EVAL
+from .task_description          import TASK_PROC, TASK_SHELL
+from .task_description          import RAPTOR_MASTER, RAPTOR_WORKER
+from .task_description          import AGENT_SERVICE
+from .resource_config           import ResourceConfig
+
+from .pilot_manager             import PilotManager
+from .pilot                     import Pilot
+from .pilot_description         import PilotDescription
+
+pythontask = PythonTask.pythontask
+
+
+# ------------------------------------------------------------------------------
+# make submodules available -- mostly for internal use
+from . import utils
+from . import tmgr
+from . import pmgr
+from . import agent
+
+from .agent  import Agent_0
+from .agent  import Agent_n
+
+from .raptor import Master, Worker
+
+
+# ------------------------------------------------------------------------------
+#
+# get version info
+#
+import os as _os
+
+version_short, version_detail, version_base, version_branch, \
+        sdist_name, sdist_path = _ru.get_version(_os.path.dirname(__file__))
+
+version = version_short
+
+
+# ------------------------------------------------------------------------------
+
