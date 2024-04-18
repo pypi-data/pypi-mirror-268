e42559887999b8447f5354b2487fbc4cc645c833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:17 +0000

    Add package pysharek/0.10.0/pysharek-0.10.0-py3-none-any.whl

---
+++ packages/pysharek/pysharek-0.10.0-py3-none-any.whl/pysharek/main.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# pip3 install build
+# python3 -m build
+# python3 -m pip install --upgrade twine
+# python3 -m twine upload --repository testpypi dist/*
+# pip3 install pysharek --no-deps --index-url https://test.pypi.org/simple
+
+import os
+import sys
+
+base_dir = os.path.dirname(__file__)
+sys.path.insert(0, base_dir)
+
+
+from .sup import *
+from .net import *
+from .args import *
+from .work import work_as_sender, work_as_receiver
+from .crypto import PycaAES256CBC, PycaFernet
+
+
+def main():
+    Global.version = "0.10"
+
+    parser = create_and_init_parser()
+    args = parser.parse_args(sys.argv[1:])
+    common_parse(args)
+    plog("Begin pysharek", 1)
+
+    if args.cipher == 1:
+        Global.cipher = PycaFernet()
+    elif args.cipher == 2:
+        Global.cipher = PycaAES256CBC()
+    else:
+        pout("Failed successfully (cipher 1 or cipher 2)")
+        exit()
+
+    Global.file_dir = args.file[0]
+    if args.mode == "send":
+        work_as_sender(args)
+    elif args.mode == "receive":
+        work_as_receiver(args)
+    else:
+        pout("Failed successfully (send or receive)")
+        exit()
+
+if __name__ == "__main__":
+    main()
+
+
