1a1a16c51290b8bc5e20c86f87d560e7ee7b7159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:37 +0000

    Add package displayrws/0.1.4/displayrws-0.1.4-py3-none-any.whl

---
+++ packages/displayrws/displayrws-0.1.4-py3-none-any.whl/displayrws/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Documentation about displayrws"""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "RWS Datalab"
+__email__ = "datalab.codebase@rws.nl"
+__version__ = "0.1.4"
