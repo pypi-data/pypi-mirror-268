2dcb4897111eb039fdd57cfaea09b354caa3f539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:09 +0000

    Add package discord-users-bot/0.0.1/discord-users-bot-0.0.1.tar.gz

---
+++ packages/discord-users-bot/discord-users-bot-0.0.1.tar.gz/discord-users-bot-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="discord-users-bot",
+    version="0.0.1",
+    author="TVOFFicial1",
+    author_email="kirill.shishkov2007@gmail.com",
+    description="Discord user bot library python",
+    long_description_content_type="text/markdown",
+    url="https://github.com/CCCProo/discord-user-bot",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
