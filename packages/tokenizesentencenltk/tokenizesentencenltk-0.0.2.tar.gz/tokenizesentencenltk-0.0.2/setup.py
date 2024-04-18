50f55d945ccda64e6303c5a0ec1f186eb1be7807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:04:57 +0000

    Add package tokenizesentencenltk/0.0.2/tokenizesentencenltk-0.0.2.tar.gz

---
+++ packages/tokenizesentencenltk/tokenizesentencenltk-0.0.2.tar.gz/tokenizesentencenltk-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Tokenizer Data Values Via NLTK'
+LONG_DESCRIPTION = DESCRIPTION
+
+# Setting up
+setup(
+    name="tokenizesentencenltk",
+    version=VERSION,
+    author="PyTorch",
+    author_email="<uts@uts.rf.gd>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=['tokens'],
+    install_requires=[
+        "transformers",
+        "textblob"
+    ],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
