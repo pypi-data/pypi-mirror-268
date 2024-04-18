2d891353c724e1abf7e5f60aaa40f4505fc56351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 06:04:30 +0000

    Add package reward-pkg-23246642/0.0.3/reward_pkg_23246642-0.0.3.tar.gz

---
+++ packages/reward-pkg-23246642/reward_pkg_23246642-0.0.3.tar.gz/reward_pkg_23246642-0.0.3/calculate_rewards_pkg/calculate_rewards.py
@@ -0,0 +1,33 @@
+class Rewards:
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        
+    def calculate_reward(self, item):
+        reward = 0
+        if item.category == "Clothing":
+            reward = 10 
+        elif item.category == "Furniture":
+            reward = 15
+        elif item.category == "Kitchenware":
+            reward = 5
+        else:
+            reward = 2
+        return reward
+    
+    def calculate_total_reward(self, items):
+        total_reward = 0
+        for item in items:
+            total_reward = total_reward + self.calculate_reward(item) 
+        return total_reward
+    
+    def get_reward_for_items(self, items):
+        return self.calculate_total_reward(items) 
+
+class Item:
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+
+
+
