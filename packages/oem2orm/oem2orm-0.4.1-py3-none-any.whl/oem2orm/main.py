c44f7913b057cc51913752f40093185142204f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 05:04:23 +0000

    Add package oem2orm/0.4.1/oem2orm-0.4.1-py3-none-any.whl

---
+++ packages/oem2orm/oem2orm-0.4.1-py3-none-any.whl/oem2orm/main.py
@@ -0,0 +1,35 @@
+import click
+import pathlib
+
+from oem2orm import oep_oedialect_oem2orm
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("metadata-folder", type=click.Path(exists=True))
+def create_tables(metadata_folder):
+    db = oep_oedialect_oem2orm.setup_db_connection()
+    folder = pathlib.Path.cwd() / metadata_folder
+    tables = oep_oedialect_oem2orm.collect_tables_from_oem(db, folder)
+    oep_oedialect_oem2orm.create_tables(db, tables)
+    if len(tables) == 1:
+        # Upload metadata for single table
+        metadata = oep_oedialect_oem2orm.mdToDict(metadata_folder)
+        oep_oedialect_oem2orm.api_updateMdOnTable(metadata)
+
+
+@cli.command()
+@click.argument("metadata-folder", type=click.Path(exists=True))
+def delete_tables(metadata_folder):
+    db = oep_oedialect_oem2orm.setup_db_connection()
+    folder = pathlib.Path.cwd() / metadata_folder
+    tables = oep_oedialect_oem2orm.collect_tables_from_oem(db, folder)
+    oep_oedialect_oem2orm.delete_tables(db, tables)
+
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/oem2orm/oem2orm-0.4.1-py3-none-any.whl/oem2orm/settings.py
@@ -0,0 +1,2 @@
+OEP_URL = "openenergyplatform.org"
+OEP_API_URL = "https://openenergyplatform.org/api/v0/"
