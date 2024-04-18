b889e64de383a39eecfb007a01bb93f483b286b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 07:04:24 +0000

    Add package tukey/0.0.2/tukey-0.0.2.tar.gz

---
+++ packages/tukey/tukey-0.0.2.tar.gz/tukey-0.0.2/src/tukey/tukey.py
@@ -0,0 +1,18 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    else:
+        return a / b
+
+__all__ = ['add', 'subtract', 'multiply', 'divide']
+
+print(divide(8, 4))
