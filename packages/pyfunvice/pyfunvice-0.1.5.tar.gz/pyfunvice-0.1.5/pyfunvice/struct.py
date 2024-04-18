ec2fe7515aa09603000e7693713f108d928f6e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:05:48 +0000

    Add package pyfunvice/0.1.5/pyfunvice-0.1.5.tar.gz

---
+++ packages/pyfunvice/pyfunvice-0.1.5.tar.gz/pyfunvice-0.1.5/pyfunvice/struct.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+class RequestModel(BaseModel):
+    requestId: str
+
+
+class ResponseModel:
+    def __init__(
+        self, requestId: str, code: str, message: str, data: dict = None
+    ) -> None:
+        self.requestId: str = requestId
+        self.code: str = code
+        self.message: str = message
+        self.data: dict = data
+
+    def to_dict(self) -> dict:
+        obj_dict = {
+            "requestId": self.requestId,
+            "code": self.code,
+            "message": self.message,
+            "data": self.data,
+        }
+
+        return obj_dict
