65136f251a5b4e95fa257c8c6f5b16151a6537df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:05:28 +0000

    Add package sfmanager/0.0.4/sfmanager-0.0.4-py3-none-any.whl

---
+++ packages/sfmanager/sfmanager-0.0.4-py3-none-any.whl/sfmanager/app.py
@@ -0,0 +1,25 @@
+class Tensor:
+	filename = None
+	level = None
+
+	def __init__(self, filename, level):
+		self.filename = str(filename)
+		self.level = int(level)
+
+	def readline(self):
+		try:
+			f = open(f"{self.filename}")
+			raw = f.readlines()
+			f.close()
+			return raw
+		except FileNotFoundError:
+			print("Something went wrong...")
+
+	def readlines(self):
+		try:
+			f = open(f"{self.filename}")
+			raw = f.readline()
+			f.close()
+			return raw
+		except FileNotFoundError:
+			print("Something went wrong...")
