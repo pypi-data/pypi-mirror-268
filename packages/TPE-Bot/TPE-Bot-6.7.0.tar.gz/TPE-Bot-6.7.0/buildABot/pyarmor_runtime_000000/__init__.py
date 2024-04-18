261b1b0d87b9ff8ca23f39f1f67debd7eff3e22b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:05:05 +0000

    Add package TPE-Bot/6.7.0/TPE-Bot-6.7.0.tar.gz

---
+++ packages/TPE-Bot/TPE-Bot-6.7.0.tar.gz/TPE-Bot-6.7.0/buildABot/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.4.7 (trial), 000000, 2024-04-18T14:15:17.830666
+from .pyarmor_runtime import __pyarmor__
---
+++ packages/TPE-Bot/TPE-Bot-6.7.0.tar.gz/TPE-Bot-6.7.0/pyarmor_runtime_000000/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.4.7 (trial), 000000, 2024-02-26T19:56:40.842847
+from .pyarmor_runtime import __pyarmor__
