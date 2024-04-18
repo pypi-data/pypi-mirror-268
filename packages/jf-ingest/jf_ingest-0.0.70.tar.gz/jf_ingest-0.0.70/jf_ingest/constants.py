53e6a8370515f48e44741028734aebc2193aadd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 04:04:49 +0000

    Add package jf-ingest/0.0.70/jf_ingest-0.0.70.tar.gz

---
+++ packages/jf-ingest/jf_ingest-0.0.70.tar.gz/jf_ingest-0.0.70/jf_ingest/constants.py
@@ -0,0 +1,10 @@
+class Constants:
+    JELLYFISH_USER_AGENT = f'jellyfish/1.0'
+    # This is how many bytes are in a MB. This is used when
+    # determining how much data to upload to S3 (we generally)
+    # batch files by ~50 MBs
+    MB_SIZE_IN_BYTES = 1048576
+    # 250 is the default largest value we can pull from Jira Server
+    # For cloud it's 100, but we have logic to check what Jira
+    # limits us to and reduce our batch size to that
+    MAX_ISSUE_API_BATCH_SIZE = 250
