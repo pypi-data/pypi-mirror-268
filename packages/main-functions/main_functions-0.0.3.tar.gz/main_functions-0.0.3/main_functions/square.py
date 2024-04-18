1d3c000cc6062cbddda771e8054177d4b3d05d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 05:05:18 +0000

    Add package main-functions/0.0.3/main_functions-0.0.3.tar.gz

---
+++ packages/main-functions/main_functions-0.0.3.tar.gz/main_functions-0.0.3/main_functions/square.py
@@ -0,0 +1,4 @@
+def calculate_square(number):
+    """Calculate the square of the given number."""
+    result = number ** 2
+    return result
---
+++ packages/main-functions/main_functions-0.0.3.tar.gz/main_functions-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='main_functions',
+    version='0.0.3',
+    packages=find_packages(),
+    description='Simple math function package',
+    author='Monkey D. Luffy',
+    author_email='whydoyou@need.it',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
