7e84c6f4796dd2ca47d41c179458317df559b19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:05:07 +0000

    Add package python-cat/0.0.12/python_cat-0.0.12.tar.gz

---
+++ packages/python-cat/python_cat-0.0.12.tar.gz/python_cat-0.0.12/setup.py
@@ -0,0 +1,33 @@
+import os
+import setuptools
+from pysparkcli.core.utils import handle_zip_files
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    project_name = readme.read()
+    modules_list = []
+    handle_zip_files.BuildZipNames(project_name, modules_list)
+
+setuptools.setup(
+    name="python-cat",
+    version="0.0.12",
+    description='A lightweight Python tool for concatenating and displaying text files in the terminal.',
+    url='https://github.com/mtdev/python-cat',
+    author='mtdev',
+    author_email='wuuuuusmall@outlook.com',
+    long_description='',
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'jinja2',
+        'pathlib>1.0.0'
+    ],
+    entry_points='''
+        [console_scripts]
+        pysparkcli=pysparkcli.bin.start:start
+    ''',
+    zip_safe=False
+)
