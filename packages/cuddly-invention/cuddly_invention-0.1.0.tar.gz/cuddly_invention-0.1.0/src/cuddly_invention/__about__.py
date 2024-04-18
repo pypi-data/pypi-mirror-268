5b425bd6a0ad77cf13c0f8a443bf146c22bb9a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:05:02 +0000

    Add package cuddly-invention/0.1.0/cuddly_invention-0.1.0.tar.gz

---
+++ packages/cuddly-invention/cuddly_invention-0.1.0.tar.gz/cuddly_invention-0.1.0/src/cuddly_invention/__about__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024-present MtkN1 <51289448+MtkN1@users.noreply.github.com>
+#
+# SPDX-License-Identifier: MIT
+from importlib.metadata import version
+
+__version__ = version(__package__)
