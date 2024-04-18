7eaaac9192374d639b7f679adb833ce68a1145bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 05:04:01 +0000

    Add package maths-operations-package/0.1/maths_operations_package-0.1.tar.gz

---
+++ packages/maths-operations-package/maths_operations_package-0.1.tar.gz/maths_operations_package-0.1/maths_operations/maths_operations.py
@@ -0,0 +1,30 @@
+import math
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mult(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def rem(a, b):
+    return a % b
+
+def power(a, b):
+    return a ** b
+
+def square_root(a):
+    return pow(a, 0.5)
+
+def absolute(a):
+    return abs(a)
+
+def floor_div(a, b):
+    return math.floor(a / b)
+
+def ceil_div(a, b):
+    return math.ceil(a / b)
---
+++ packages/maths-operations-package/maths_operations_package-0.1.tar.gz/maths_operations_package-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='maths_operations_package',
+    version='0.1',
+    packages=['maths_operations'],
+    install_requires=[],
+    url='https://github.com/Snigdha2005/math_operations',
+    license='IIITB',
+    author='Snigdha',
+    author_email='YS.Snigdha@iiitb.com',
+    description='Basic maths operations implementations'
+)
