782d296ea83f9b7ee183063796aefc0bd2c77f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 14:04:45 +0000

    Add package anycrc/0.3.0/anycrc-0.3.0.tar.gz

---
+++ packages/anycrc/anycrc-0.3.0.tar.gz/anycrc-0.3.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, Extension
+
+setup(
+    name = 'anycrc',
+    version = '0.3.0',
+    ext_modules = [Extension(name='anycrc.anycrc', sources=['src/anycrc/anycrc.pyx', 'lib/crcany/model.c', 'lib/crcany/crc.c'])]
+)
