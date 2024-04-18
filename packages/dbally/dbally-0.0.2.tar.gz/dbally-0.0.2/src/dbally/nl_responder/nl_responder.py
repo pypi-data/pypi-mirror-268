72218d64749684dbcd3a2c65811233718dd7f649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:46 +0000

    Add package dbally/0.0.2/dbally-0.0.2.tar.gz

---
+++ packages/dbally/dbally-0.0.2.tar.gz/dbally-0.0.2/src/dbally/nl_responder/nl_responder.py
@@ -0,0 +1,111 @@
+import copy
+from typing import Dict, List, Optional
+
+import pandas as pd
+
+from dbally.audit.event_tracker import EventTracker
+from dbally.data_models.execution_result import ViewExecutionResult
+from dbally.data_models.prompts.iql_explainer_prompt_template import (
+    IQLExplainerPromptTemplate,
+    default_iql_explainer_template,
+)
+from dbally.data_models.prompts.nl_responder_prompt_template import (
+    NLResponderPromptTemplate,
+    default_nl_responder_template,
+)
+from dbally.llm_client.base import LLMClient
+from dbally.nl_responder.token_counters import count_tokens_for_huggingface, count_tokens_for_openai
+
+
+class NLResponder:
+    """Class used to generate natural language response from the database output."""
+
+    def __init__(
+        self,
+        llm_client: LLMClient,
+        iql_explainer_prompt_template: Optional[IQLExplainerPromptTemplate] = None,
+        nl_responder_prompt_template: Optional[NLResponderPromptTemplate] = None,
+        max_tokens_count: int = 4096,
+    ) -> None:
+        """
+        Args:
+            llm_client: LLM client used to generate natural language response
+            iql_explainer_prompt_template: template for the prompt used to generate the iql explanation\
+            if not set defaults to `default_iql_explainer_template`
+            nl_responder_prompt_template: template for the prompt used to generate the NL response\
+            if not set defaults to `nl_responder_prompt_template`
+            max_tokens_count: maximum number of tokens that can be used in the prompt
+        """
+
+        self._llm_client = llm_client
+        self._nl_responder_prompt_template = nl_responder_prompt_template or copy.deepcopy(
+            default_nl_responder_template
+        )
+        self._iql_explainer_prompt_template = iql_explainer_prompt_template or copy.deepcopy(
+            default_iql_explainer_template
+        )
+        self._max_tokens_count = max_tokens_count
+
+    async def generate_response(
+        self, result: ViewExecutionResult, question: str, filters: str, event_tracker: EventTracker
+    ) -> str:
+        """
+        Uses LLM to generate a response in natural language form.
+
+        Args:
+            result: object representing the result of the query execution
+            question: user question
+            filters: filters used in the query
+            event_tracker: event store used to audit the generation process
+
+        Returns:
+            Natural language response to the user question.
+        """
+
+        rows = _promptify_rows(result.results)
+
+        if "gpt" in self._llm_client.model_name:
+            tokens_count = count_tokens_for_openai(
+                messages=self._nl_responder_prompt_template.chat,
+                fmt={"rows": rows, "question": question},
+                model=self._llm_client.model_name,
+            )
+
+        else:
+            tokens_count = count_tokens_for_huggingface(
+                messages=self._nl_responder_prompt_template.chat,
+                fmt={"rows": rows, "question": question},
+                model=self._llm_client.model_name,
+            )
+
+        if tokens_count > self._max_tokens_count:
+            llm_response = await self._llm_client.text_generation(
+                template=self._iql_explainer_prompt_template,
+                fmt={"question": question, "filters": filters},
+                event_tracker=event_tracker,
+            )
+
+            return llm_response
+
+        llm_response = await self._llm_client.text_generation(
+            template=self._nl_responder_prompt_template,
+            fmt={"rows": _promptify_rows(result.results), "question": question},
+            event_tracker=event_tracker,
+        )
+        return llm_response
+
+
+def _promptify_rows(rows: List[Dict]) -> str:
+    """
+    Formats rows into a markdown table.
+
+    Args:
+        rows: list of rows to be formatted
+
+    Returns:
+        str: formatted rows
+    """
+
+    df = pd.DataFrame.from_records(rows)
+
+    return df.to_markdown(index=False, headers="keys", tablefmt="psql")
---
+++ packages/dbally/dbally-0.0.2.tar.gz/dbally-0.0.2/src/dbally/views/base.py
@@ -0,0 +1,103 @@
+import abc
+import re
+from dataclasses import dataclass
+from typing import _GenericAlias  # type: ignore
+from typing import List, Optional, Union
+
+from dbally.data_models.execution_result import ViewExecutionResult
+from dbally.iql import IQLQuery
+from dbally.similarity import AbstractSimilarityIndex
+
+
+def parse_param_type(param_type: Union[type, _GenericAlias]) -> str:
+    """
+    Parses the type of a method parameter and returns a string representation of it.
+
+    Args:
+        param_type: type of the parameter
+
+    Returns:
+        str: string representation of the type
+    """
+    if param_type in {int, float, str, bool, list, dict, set, tuple}:
+        return param_type.__name__
+    if param_type.__module__ == "typing":
+        return re.sub(r"\btyping\.", "", str(param_type))
+
+    return str(param_type)
+
+
+@dataclass
+class MethodParamWithTyping:
+    """
+    Represents a method parameter with its type.
+    """
+
+    name: str
+    type: Union[type, _GenericAlias]
+
+    def __str__(self) -> str:
+        return f"{self.name}: {parse_param_type(self.type)}"
+
+    @property
+    def similarity_index(self) -> Optional[AbstractSimilarityIndex]:
+        """
+        Returns the SimilarityIndex object if the type is annotated with it.
+        """
+        if hasattr(self.type, "__metadata__"):
+            similarity_indexes = [meta for meta in self.type.__metadata__ if isinstance(meta, AbstractSimilarityIndex)]
+            return similarity_indexes[0] if similarity_indexes else None
+
+        return None
+
+
+@dataclass
+class ExposedFunction:
+    """
+    Represents a function exposed to the AI model.
+    """
+
+    name: str
+    description: str
+    parameters: List[MethodParamWithTyping]
+
+    def __str__(self) -> str:
+        base_str = f"{self.name}({', '.join(str(param) for param in self.parameters)})"
+
+        if self.description != "":
+            return f"{base_str} - {self.description}"
+
+        return base_str
+
+
+class AbstractBaseView(metaclass=abc.ABCMeta):
+    """
+    Base class for all [Views](../../concepts/views.md). All classes implementing this interface has\
+    to be able to list all available filters, apply them and execute queries.
+    """
+
+    @abc.abstractmethod
+    def list_filters(self) -> List[ExposedFunction]:
+        """
+
+        Returns:
+            Filters defined inside the View.
+        """
+
+    @abc.abstractmethod
+    async def apply_filters(self, filters: IQLQuery) -> None:
+        """
+        Applies the chosen filters to the view.
+
+        Args:
+            filters: [IQLQuery](../../concepts/iql.md) object representing the filters to apply
+        """
+
+    @abc.abstractmethod
+    def execute(self, dry_run: bool = False) -> ViewExecutionResult:
+        """
+        Executes the query and returns the result.
+
+        Args:
+            dry_run: if True, should only generate the query without executing it
+        """
