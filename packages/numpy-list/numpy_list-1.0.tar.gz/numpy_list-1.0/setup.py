946eca8ae3174c03d5b962468c5c604303356f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 04:04:06 +0000

    Add package numpy-list/1.0/numpy_list-1.0.tar.gz

---
+++ packages/numpy-list/numpy_list-1.0.tar.gz/numpy_list-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='numpy_list',
+    version='1.0',
+    url='https://github.com/alessionuovo/mionumpy',
+    install_requires=['numpy'],
+    license='GNU',
+    author='Alexei Lisitsa(Alessio Volpe)',
+    author_email='alex123494@gmail.com',
+    description='List module'
+)
