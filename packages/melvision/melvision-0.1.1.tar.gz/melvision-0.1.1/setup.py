5c2e0f4bd52c77c4fb763a658cff3005f33897b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:36 +0000

    Add package melvision/0.1.1/melvision-0.1.1.tar.gz

---
+++ packages/melvision/melvision-0.1.1.tar.gz/melvision-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="melvision",
+    version="0.1.1",
+    author="Melvin Hwang",
+    author_email="huang667399@gmail.com",
+    description="A convenient deep learning and image processing library.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Manuel667399/melvision",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
