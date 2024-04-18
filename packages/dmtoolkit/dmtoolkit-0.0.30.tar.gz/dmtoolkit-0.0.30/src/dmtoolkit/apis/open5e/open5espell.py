aace62ee9e5c862b662d1e528b4e3f538805295a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 05:05:05 +0000

    Add package dmtoolkit/0.0.30/dmtoolkit-0.0.30.tar.gz

---
+++ packages/dmtoolkit/dmtoolkit-0.0.30.tar.gz/dmtoolkit-0.0.30/src/dmtoolkit/apis/open5e/open5espell.py
@@ -0,0 +1,5 @@
+from apis.open5e.open5eapi import Open5e
+
+
+class Open5eSpell(Open5e):
+    endpoints = ["spells"]
---
+++ packages/dmtoolkit/dmtoolkit-0.0.30.tar.gz/dmtoolkit-0.0.30/src/dmtoolkit/dmtools.py
@@ -0,0 +1,32 @@
+from apis.dndbeyondapi import DnDBeyondAPI
+from apis.open5e.open5eitem import Open5eItem
+from apis.open5e.open5emonster import Open5eMonster
+from apis.open5e.open5espell import Open5eSpell
+
+
+def get_dndbeyond_character(dnd_id):
+    return DnDBeyondAPI.getcharacter(dnd_id)
+
+
+def get_monster(obj_id):
+    return Open5eMonster.get(index=obj_id)
+
+
+def get_item(obj_id):
+    return Open5eItem.get(index=obj_id)
+
+
+def get_spell(obj_id):
+    return Open5eSpell.get(index="animate-objects")
+
+
+def search_monster(name):
+    return Open5eMonster.search(name)
+
+
+def search_item(name):
+    return Open5eItem.search(name)
+
+
+def search_spell(name):
+    return Open5eSpell.search(name)
