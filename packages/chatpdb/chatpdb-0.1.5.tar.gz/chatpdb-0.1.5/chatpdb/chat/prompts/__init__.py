d1a89c1fbd880c18286ca9ed15fa571150f55670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:04:52 +0000

    Add package chatpdb/0.1.5/chatpdb-0.1.5.tar.gz

---
+++ packages/chatpdb/chatpdb-0.1.5.tar.gz/chatpdb-0.1.5/chatpdb/chat/prompts/__init__.py
@@ -0,0 +1,12 @@
+from chatpdb.chat.prompts.ask import get_ask_prompt
+from chatpdb.chat.prompts.explain import get_explain_prompt
+from chatpdb.chat.prompts.system import get_system_prompt
+# from chatpdb.chat.prompts.util import format_stack_trace, format_vars
+
+__all__ = [
+    "get_ask_prompt",
+    "get_explain_prompt",
+    "get_system_prompt",
+    # "format_stack_trace",
+    # "format_vars",
+]
