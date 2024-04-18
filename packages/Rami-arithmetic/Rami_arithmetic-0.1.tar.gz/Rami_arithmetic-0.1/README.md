2f1e96a8acad9ee950d6f0676d2358f664d55669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:04:59 +0000

    Add package Rami-arithmetic/0.1/Rami_arithmetic-0.1.tar.gz

---
+++ packages/Rami-arithmetic/Rami_arithmetic-0.1.tar.gz/Rami_arithmetic-0.1/README.md
@@ -0,0 +1,8 @@
+T# Your Package Name
+
+A simple Python package for performing basic arithmetic operations.
+
+## Installation
+
+You can install the package using pip:
+
---
+++ packages/Rami-arithmetic/Rami_arithmetic-0.1.tar.gz/Rami_arithmetic-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Rami_arithmetic',
+    version='0.1',
+    packages=['Rami_arithmetic'],
+    # url='https://github.com/your_username/your_package_name',
+    author='rami.tawadros',
+    author_email='ramikamalabdelsayed@gmail.com',
+    description='A simple Rami_arithmetic package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
