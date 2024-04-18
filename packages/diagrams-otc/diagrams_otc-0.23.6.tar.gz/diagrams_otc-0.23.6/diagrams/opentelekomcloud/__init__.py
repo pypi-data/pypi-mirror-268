46496beb47eebbdb7e5ca526bce3c902f6cca289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:05:21 +0000

    Add package diagrams-otc/0.23.6/diagrams_otc-0.23.6.tar.gz

---
+++ packages/diagrams-otc/diagrams_otc-0.23.6.tar.gz/diagrams_otc-0.23.6/diagrams/opentelekomcloud/__init__.py
@@ -0,0 +1,12 @@
+"""
+_OpenTelekomCloud provides a set of services for Open Telekom Cloud provider.
+"""
+
+from diagrams import Node
+
+
+class _OpenTelekomCloud(Node):
+    _provider = "opentelekomcloud"
+    _icon_dir = "resources/opentelekomcloud"
+
+    fontcolor = "#ffffff"
