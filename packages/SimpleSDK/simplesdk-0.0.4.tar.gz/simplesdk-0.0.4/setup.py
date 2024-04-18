8b55d5e020c2f0ad8a59b70ebfe4bda213da7ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 05:05:12 +0000

    Add package SimpleSDK/0.0.4/simplesdk-0.0.4.tar.gz

---
+++ packages/SimpleSDK/simplesdk-0.0.4.tar.gz/simplesdk-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup  # type: ignore
+
+__project__ = "SimpleSDK"
+__version__ = "0.0.4"
+__description__ = (
+    "SDK for EuskadiTech's Simple Axel (https://git.tech.eus/EuskadiTech/SimpleSDK)"
+)
+__packages__ = ["simplesdk"]
+__url__ = "https://git.tech.eus/EuskadiTech/SimpleSDK"
+__author__ = "EuskadiTech"
+__classifiers__ = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3",
+]
+__requires__ = ["requests"]
+
+setup(
+    name=__project__,
+    version=__version__,
+    description=__description__,
+    packages=__packages__,
+    url=__url__,
+    author=__author__,
+    classifiers=__classifiers__,
+    requires=__requires__,
+)
