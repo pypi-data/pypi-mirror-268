3551b8388dc1cd0386ecba6e9abcf2fae3d1ff46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 04:04:22 +0000

    Add package netsblox/0.6.8/netsblox-0.6.8.tar.gz

---
+++ packages/netsblox/netsblox-0.6.8.tar.gz/netsblox-0.6.8/netsblox/__init__.py
@@ -0,0 +1,16 @@
+'''
+A python interface for accessing NetsBlox services
+'''
+
+from .editor import * # we import editor into global scope
+from . import dev     # users can access dev explicitly if they want
+from . import graphical
+from . import concurrency
+from . import snap
+from . import rooms
+
+from .common import get_location, get_error, nothrow, Namespace
+
+__version__ = '0.6.8'
+__author__ = 'Devin Jean'
+__credits__ = 'Institute for Software Integrated Systems, Vanderbilt University'
