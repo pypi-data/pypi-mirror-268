15e0ad6e2bfca8ee2039a25cd780e1f6edc6c8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:49 +0000

    Add package gempy-legacy/2.3.2/gempy_legacy-2.3.2.tar.gz

---
+++ packages/gempy-legacy/gempy_legacy-2.3.2.tar.gz/gempy_legacy-2.3.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages, Extension
+
+version = '2.3.2'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='gempy_legacy',
+    version=version,
+    packages=find_packages(exclude=('test', 'docs', 'examples')),
+    include_package_data=True,
+    install_requires=[
+        'pandas==2.0.1',
+        'aesara',
+        'pymc',
+        'matplotlib',
+        'numpy',
+        'pytest',
+        'seaborn>=0.9',
+        'networkx',
+        'scikit-image>=0.17',
+        'scikit-learn',
+        'pyvista==0.39.1',
+        'pyvistaqt==0.10.0',
+        'pyqt5',
+        'iPython',
+        'xarray'
+    ],
+    url='https://github.com/cgre-aachen/gempy',
+    license='LGPL v3',
+    author='Miguel de la Varga, Alexander Zimmerman, Elisa Heim, Alexander Schaaf, Fabian Stamm, Florian Wellmann, Jan Niederau, Andrew Annex, Alexander Juestel',
+    author_email='miguel@terranigma-solutions.com',
+    description='An Open-source, Python-based 3-D structural geological modeling software.',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    keywords=['geology', '3-D modeling', 'structural geology', 'uncertainty']
+)
---
+++ packages/gempy-legacy/gempy_legacy-2.3.2.tar.gz/gempy_legacy-2.3.2/test/test_utilities/test_utilities.py
@@ -0,0 +1,19 @@
+import pytest
+
+import aesara
+import numpy as np
+import sys, os
+
+import gempy_legacy.core.data_modules.stack
+
+sys.path.append("../..")
+import gempy_legacy as gp
+
+
+input_path = os.path.dirname(__file__) + '/../../examples/data'
+
+
+
+
+
+
