e7fa3e33d26e52ef97756aad9e86a6f18f0cf7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 06:04:44 +0000

    Add package sphinx-argparse-cli/1.15.0/sphinx_argparse_cli-1.15.0.tar.gz

---
+++ packages/sphinx-argparse-cli/sphinx_argparse_cli-1.15.0.tar.gz/sphinx_argparse_cli-1.15.0/src/sphinx_argparse_cli/__init__.py
@@ -0,0 +1,24 @@
+"""Sphinx generator for argparse."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .version import __version__
+
+if TYPE_CHECKING:
+    from sphinx.application import Sphinx
+
+
+def setup(app: Sphinx) -> None:
+    app.add_css_file("custom.css")
+
+    from ._logic import SphinxArgparseCli
+
+    app.add_directive(SphinxArgparseCli.name, SphinxArgparseCli)
+    app.add_config_value("sphinx_argparse_cli_prefix_document", False, "env")  # noqa: FBT003
+
+
+__all__ = [
+    "__version__",
+]
