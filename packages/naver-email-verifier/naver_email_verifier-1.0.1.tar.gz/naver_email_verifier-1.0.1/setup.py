b948002972f31eed2fcb814cee0c271612d0f85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:04:38 +0000

    Add package naver-email-verifier/1.0.1/naver_email_verifier-1.0.1.tar.gz

---
+++ packages/naver-email-verifier/naver_email_verifier-1.0.1.tar.gz/naver_email_verifier-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import setup
+
+
+# Function to selectively add packages
+def find_pyc_packages():
+    packages = []
+    for dirpath, _, _ in os.walk("naver_email_verifier"):
+        if "__pycache__" in dirpath:
+            package = dirpath.replace(os.path.sep, ".")
+            packages.append(package)
+    return packages
+
+
+setup(
+    name="naver_email_verifier",
+    version="1.0.1",
+    packages=find_pyc_packages(),
+    package_data={"": ["*.pyc"]},
+    exclude_package_data={"": ["*.py"]},
+    install_requires=[
+        "httpx",
+    ],
+    author="Seok Won Choi",
+    author_email="ikr@kakao.com",
+    description="A Python library for verifying Naver email addresses.",
+)
