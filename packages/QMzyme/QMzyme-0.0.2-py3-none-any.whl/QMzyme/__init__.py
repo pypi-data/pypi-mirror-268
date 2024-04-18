4b924bee821cb578f7b32a0f9325cad5b7358234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 14:04:53 +0000

    Add package QMzyme/0.0.2/QMzyme-0.0.2-py3-none-any.whl

---
+++ packages/QMzyme/QMzyme-0.0.2-py3-none-any.whl/QMzyme/__init__.py
@@ -0,0 +1,8 @@
+"""QM-based enzyme model generation and validation."""
+
+# Add imports here
+from QMzyme import *
+from .generate import *
+from .calculate import *
+from .BiopythonWrapper import *
+from ._version import __version__
