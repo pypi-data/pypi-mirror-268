3b5fc64d89a474dd8810c4b017226e22b4489f29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:49 +0000

    Add package myCDEPS/1.0/myCDEPS-1.0.tar.gz

---
+++ packages/myCDEPS/myCDEPS-1.0.tar.gz/myCDEPS-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='myCDEPS',
+    version='1.0',
+    packages=['myCDEPS'],
+    author='ze shi',
+    author_email='your.email@example.com',
+    description='A description of your package',
+    url='https://github.com/zeshidecode/CEDPS',
+)
