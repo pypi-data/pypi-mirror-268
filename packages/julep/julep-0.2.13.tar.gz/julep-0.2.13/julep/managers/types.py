4acb9aa2efcd629efffc548be7a767527d3aef14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:25 +0000

    Add package julep/0.2.13/julep-0.2.13.tar.gz

---
+++ packages/julep/julep-0.2.13.tar.gz/julep-0.2.13/julep/managers/types.py
@@ -0,0 +1,40 @@
+# type: ignore
+# ^^ Added to ignore pytype errors for this file as it uses hacks
+from typing import TypedDict
+from ..api.types import (
+    CreateDoc,
+    AgentDefaultSettings,
+    FunctionDef,
+    CreateToolRequest,
+    ChatSettingsResponseFormat,
+    InputChatMlMessage,
+    Tool,
+)
+
+ChatSettingsResponseFormatDict = TypedDict(
+    "ChatSettingsResponseFormat",
+    **{k: v.outer_type_ for k, v in ChatSettingsResponseFormat.__fields__.items()},
+)
+InputChatMlMessageDict = TypedDict(
+    "InputChatMlMessage",
+    **{k: v.outer_type_ for k, v in InputChatMlMessage.__fields__.items()},
+)
+ToolDict = TypedDict(
+    "Tool",
+    **{k: v.outer_type_ for k, v in Tool.__fields__.items()},
+)
+
+DocDict = TypedDict(
+    "DocDict",
+    **{k: v.outer_type_ for k, v in CreateDoc.__fields__.items()},
+)
+DefaultSettingsDict = TypedDict(
+    "DefaultSettingsDict",
+    **{k: v.outer_type_ for k, v in AgentDefaultSettings.__fields__.items()},
+)
+FunctionDefDict = TypedDict(
+    "FunctionDefDict", **{k: v.outer_type_ for k, v in FunctionDef.__fields__.items()}
+)
+ToolDict = TypedDict(
+    "ToolDict", **{k: v.outer_type_ for k, v in CreateToolRequest.__fields__.items()}
+)
