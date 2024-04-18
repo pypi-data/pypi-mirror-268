6d0cd8feaa23d3059f388406d9d0eb414c4e33eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:08 +0000

    Add package neuromeka/3.2.0/neuromeka-3.2.0.tar.gz

---
+++ packages/neuromeka/neuromeka-3.2.0.tar.gz/neuromeka-3.2.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="neuromeka",
+    version="3.2.0",
+    author="Neuromeka",
+    author_email="youngjin.heo@neuromeka.com",
+    description="Neuromeka client protocols for Indy, IndyEye, Moby, Ecat, and Motor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/neuromeka-robotics/neuromeka-clients3",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "grpcio==1.39.0",
+        "grpcio-tools==1.39.0",
+        "protobuf==3.17.3",
+        "requests==2.22.0",
+        "Pillow==9.5.0",
+        "numpy==1.21.6",
+        "pyModbusTCP==0.2.1",
+        "netifaces==0.11.0"
+    ],
+)
