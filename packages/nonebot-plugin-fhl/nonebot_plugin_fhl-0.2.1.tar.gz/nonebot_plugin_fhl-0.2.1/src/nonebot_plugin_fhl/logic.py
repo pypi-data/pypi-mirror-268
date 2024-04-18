a1495b607f8227549910c40e623bada29f538d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:03:57 +0000

    Add package nonebot-plugin-fhl/0.2.1/nonebot_plugin_fhl-0.2.1.tar.gz

---
+++ packages/nonebot-plugin-fhl/nonebot_plugin_fhl-0.2.1.tar.gz/nonebot_plugin_fhl-0.2.1/src/nonebot_plugin_fhl/logic.py
@@ -0,0 +1,33 @@
+from typing import List
+from .config import config
+from .api import api
+
+
+class FeiHuaLing:
+    subject: str
+    history: List[str]
+    turn_timer: int
+
+    def __init__(self, user_id: str, game_mode: str, poetry_size: int) -> None:
+        self.api = config.feihualing_api
+        self.history = []
+        self.game_id = user_id
+        self.game_mode = game_mode
+        self.poetry_size = poetry_size
+
+    async def init_game(self):
+        resp = await api.get_topic(
+            self.game_mode,
+            self.poetry_size,
+            self.game_id,
+        )
+        self.subject = resp.data.subjectstring
+        return self.subject
+
+    async def answer(self, answer: str):
+        resp = await api.answer(
+            self.game_id,
+            answer,
+        )
+        self.history = resp.data.history
+        return resp
