ababf21f21210300ddc5305e145071c7e564ac68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 05:04:24 +0000

    Add package chkptai/0.1.2/chkptai-0.1.2.tar.gz

---
+++ packages/chkptai/chkptai-0.1.2.tar.gz/chkptai-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chkptai",
+    version="0.1.2",
+    packages=find_packages(),
+)
+
+if __name__ == "__main__":
+    setup()
---
+++ packages/chkptai/chkptai-0.1.2.tar.gz/chkptai-0.1.2/src/chkptai/__init__.py
@@ -0,0 +1,5 @@
+"""
+Python SDK for Checkpoint-AI API
+"""
+__version__ = "0.1.2"
+from .ChkptAI import ChkptAI
