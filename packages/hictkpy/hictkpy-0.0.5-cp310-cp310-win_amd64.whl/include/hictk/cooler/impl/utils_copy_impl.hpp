5b6d9abc9a4cdcd9f1fe80257c87c92f81debab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 16:04:38 +0000

    Add package hictkpy/0.0.5/hictkpy-0.0.5-cp310-cp310-win_amd64.whl

---
+++ packages/hictkpy/hictkpy-0.0.5-cp310-cp310-win_amd64.whl/include/hictk/cooler/impl/utils_copy_impl.hpp
@@ -0,0 +1,120 @@
+// Copyright (C) 2023 Roberto Rossini <roberros@uio.no>
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <fmt/format.h>
+
+#if __has_include(<hdf5/hdf5.h>)
+#include <hdf5/H5Opublic.h>
+#include <hdf5/H5Ppublic.h>
+#else
+#include <H5Opublic.h>
+#include <H5Ppublic.h>
+#endif
+
+#include <exception>
+#include <filesystem>
+#include <highfive/H5File.hpp>
+#include <highfive/H5Group.hpp>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+
+#include "hictk/cooler/group.hpp"
+#include "hictk/cooler/uri.hpp"
+#include "hictk/cooler/validation.hpp"
+
+namespace hictk::cooler::utils {
+
+inline void copy(std::string_view uri1, std::string_view uri2) {
+  const auto [path2, grp2] = cooler::parse_cooler_uri(uri2);
+  if (std::filesystem::exists(uri2) && utils::is_cooler(uri2)) {
+    throw std::runtime_error("destination already contains a Cooler");
+  }
+  const HighFive::File dest(path2, HighFive::File::OpenOrCreate);
+  return copy(uri1, RootGroup{dest.getGroup(grp2)});
+}
+
+inline void copy(std::string_view uri1, RootGroup dest) {
+  try {
+    if (!utils::is_cooler(uri1)) {
+      throw std::runtime_error("input is not a valid Cooler");
+    }
+
+    /* Attempt to open an existing HDF5 file first. Need to open the dst file
+         before the src file just in case that the dst and src are the same file
+    */
+    const auto [path1, grp1] = cooler::parse_cooler_uri(uri1);
+    const HighFive::File fin(path1, HighFive::File::ReadOnly);
+
+    /* create property to pass copy options */
+    auto ocpl_id = H5Pcreate(H5P_OBJECT_COPY);
+    if (ocpl_id < 0) {
+      throw std::runtime_error("H5Pcreate failed");
+    }
+
+    /* Create link creation property list */
+    auto lcpl_id = H5Pcreate(H5P_LINK_CREATE);
+    if (lcpl_id < 0) {
+      throw std::runtime_error("Could not create link creation property list: H5Pcreate failed");
+    }
+
+    /* Set the intermediate group creation property */
+    if (H5Pset_create_intermediate_group(lcpl_id, 1) < 0) {
+      throw std::runtime_error(
+          "Could not set property for creating parent groups: H5Pset_create_intermediate_group "
+          "failed");
+    }
+
+    auto copy_h5_object = [&](const auto &src, const auto &dest_, const auto &obj) {
+      if (H5Ocopy(src.getId(),    /* Source file or group identifier */
+                  obj.data(),     /* Name of the source object to be copied */
+                  dest_.getId(),  /* Destination file or group identifier  */
+                  obj.data(),     /* Name of the destination object  */
+                  ocpl_id,        /* Object copy property list */
+                  lcpl_id) < 0) { /* Link creation property list */
+        throw std::runtime_error(
+            fmt::format(FMT_STRING("H5Ocopy failed for {}::/{} failed"), src.getPath(), obj));
+      }
+    };
+
+    auto copy_h5_attribute = [&](const HighFive::Group &src, HighFive::Group dest_,
+                                 const auto &attr_name) {
+      auto attr = src.getAttribute(attr_name);
+      auto attr_out = dest_.createAttribute(attr_name, attr.getSpace(), attr.getDataType());
+
+      std::string buffer(attr.getStorageSize(), '\0');
+      if (attr.getDataType().isFixedLenStr() || attr.getDataType().isVariableStr()) {
+        // Let HighFive deal with strings
+        attr.read(buffer);
+        attr_out.write(buffer);
+      } else {
+        attr.read(buffer.data(), attr.getDataType());
+        attr_out.write_raw(buffer.data(), attr.getDataType());
+      }
+    };
+
+    for (const auto &obj : fin.getGroup(grp1).listObjectNames()) {
+      copy_h5_object(fin.getGroup(grp1), dest(), obj);
+    }
+
+    for (const auto &attr : fin.getGroup(grp1).listAttributeNames()) {
+      copy_h5_attribute(fin.getGroup(grp1), dest(), std::string{attr});
+    }
+
+    /* close propertis */
+    if (H5Pclose(ocpl_id) < 0) {
+      throw std::runtime_error("H5Pclose failed");
+    }
+    if (H5Pclose(lcpl_id) < 0) {
+      throw std::runtime_error("H5Pclose failed");
+    }
+  } catch (const std::exception &e) {
+    throw std::runtime_error(fmt::format(FMT_STRING("failed to copy Cooler from {} to {}: {}"),
+                                         uri1, dest.uri(), e.what()));
+  }
+}
+
+}  // namespace hictk::cooler::utils
---
+++ packages/hictkpy/hictkpy-0.0.5-cp310-cp310-win_amd64.whl/include/hictk/file.hpp
@@ -0,0 +1,174 @@
+// Copyright (C) 2023 Roberto Rossini <roberros@uio.no>
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#ifdef HICTK_WITH_EIGEN
+#include <Eigen/Dense>
+#include <Eigen/SparseCore>
+#endif
+
+#include <cstddef>
+#include <cstdint>
+#include <iterator>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <variant>
+#include <vector>
+
+#include "hictk/balancing/methods.hpp"
+#include "hictk/bin_table.hpp"
+#include "hictk/cooler/cooler.hpp"
+#include "hictk/cooler/pixel_selector.hpp"
+#include "hictk/genomic_interval.hpp"
+#include "hictk/hic.hpp"
+#include "hictk/hic/pixel_selector.hpp"
+#include "hictk/pixel.hpp"
+#include "hictk/reference.hpp"
+
+namespace hictk {
+
+class PixelSelector {
+  using PixelSelectorVar =
+      std::variant<cooler::PixelSelector, hic::PixelSelector, hic::PixelSelectorAll>;
+  PixelSelectorVar _sel{cooler::PixelSelector{}};
+
+ public:
+  template <typename N>
+  class iterator;
+
+  template <typename PixelSelectorT>
+  explicit PixelSelector(PixelSelectorT selector);
+
+  template <typename N>
+  [[nodiscard]] auto begin(bool sorted = true) const -> iterator<N>;
+  template <typename N>
+  [[nodiscard]] auto end() const -> iterator<N>;
+
+  template <typename N>
+  [[nodiscard]] auto cbegin(bool sorted = true) const -> iterator<N>;
+  template <typename N>
+  [[nodiscard]] auto cend() const -> iterator<N>;
+
+  template <typename N>
+  [[nodiscard]] std::vector<Pixel<N>> read_all() const;
+
+#ifdef HICTK_WITH_EIGEN
+  template <typename N>
+  [[nodiscard]] Eigen::SparseMatrix<N> read_sparse() const;
+  template <typename N>
+  [[nodiscard]] Eigen::Matrix<N, Eigen::Dynamic, Eigen::Dynamic> read_dense() const;
+#endif
+
+  [[nodiscard]] const PixelCoordinates &coord1() const;
+  [[nodiscard]] const PixelCoordinates &coord2() const;
+
+  [[nodiscard]] const BinTable &bins() const;
+
+  template <typename PixelSelectorT>
+  [[nodiscard]] constexpr const PixelSelectorT &get() const noexcept;
+  template <typename PixelSelectorT>
+  [[nodiscard]] constexpr PixelSelectorT &get() noexcept;
+  [[nodiscard]] constexpr auto get() const noexcept -> const PixelSelectorVar &;
+  [[nodiscard]] constexpr auto get() noexcept -> PixelSelectorVar &;
+
+  template <typename N>
+  class iterator {
+    using IteratorVar =
+        std::variant<cooler::PixelSelector::iterator<N>, hic::PixelSelector::iterator<N>,
+                     hic::PixelSelectorAll::iterator<N>>;
+    IteratorVar _it{};
+
+   public:
+    using difference_type = std::ptrdiff_t;
+    using value_type = ThinPixel<N>;
+    using pointer = value_type *;
+    using const_pointer = const value_type *;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using iterator_category = std::forward_iterator_tag;
+
+    iterator() = default;
+    template <typename It>
+    explicit iterator(It it);
+
+    [[nodiscard]] bool operator==(const iterator &other) const noexcept;
+    [[nodiscard]] bool operator!=(const iterator &other) const noexcept;
+
+    [[nodiscard]] auto operator*() const -> const_reference;
+    [[nodiscard]] auto operator->() const -> const_pointer;
+
+    auto operator++() -> iterator &;
+    auto operator++(int) -> iterator;
+
+    template <typename IteratorT>
+    [[nodiscard]] constexpr const IteratorT &get() const noexcept;
+    template <typename IteratorT>
+    [[nodiscard]] constexpr IteratorT &get() noexcept;
+    [[nodiscard]] constexpr auto get() const noexcept -> const IteratorVar &;
+    [[nodiscard]] constexpr auto get() noexcept -> IteratorVar &;
+  };
+};
+
+class File {
+  using FileVar = std::variant<cooler::File, hic::File>;
+  FileVar _fp{cooler::File{}};
+
+ public:
+  using QUERY_TYPE = hictk::GenomicInterval::Type;
+
+  explicit File(cooler::File clr);
+  explicit File(hic::File hf);
+  explicit File(std::string uri, std::uint32_t resolution = 0,
+                hic::MatrixType type = hic::MatrixType::observed,
+                hic::MatrixUnit unit = hic::MatrixUnit::BP);
+
+  [[nodiscard]] std::string uri() const;
+  [[nodiscard]] std::string path() const;
+
+  [[nodiscard]] constexpr bool is_hic() const noexcept;
+  [[nodiscard]] constexpr bool is_cooler() const noexcept;
+
+  [[nodiscard]] auto chromosomes() const -> const Reference &;
+  [[nodiscard]] auto bins() const -> const BinTable &;
+  [[nodiscard]] std::shared_ptr<const BinTable> bins_ptr() const;
+
+  [[nodiscard]] std::uint32_t resolution() const;
+  [[nodiscard]] std::uint64_t nbins() const;
+  [[nodiscard]] std::uint64_t nchroms() const;
+
+  [[nodiscard]] PixelSelector fetch(
+      const balancing::Method &normalization = balancing::Method::NONE()) const;
+  [[nodiscard]] PixelSelector fetch(
+      std::string_view range, const balancing::Method &normalization = balancing::Method::NONE(),
+      QUERY_TYPE query_type = QUERY_TYPE::UCSC) const;
+  [[nodiscard]] PixelSelector fetch(
+      std::string_view chrom_name, std::uint32_t start, std::uint32_t end,
+      const balancing::Method &normalization = balancing::Method::NONE()) const;
+
+  [[nodiscard]] PixelSelector fetch(
+      std::string_view range1, std::string_view range2,
+      const balancing::Method &normalization = balancing::Method::NONE(),
+      QUERY_TYPE query_type = QUERY_TYPE::UCSC) const;
+  [[nodiscard]] PixelSelector fetch(
+      std::string_view chrom1_name, std::uint32_t start1, std::uint32_t end1,
+      std::string_view chrom2_name, std::uint32_t start2, std::uint32_t end2,
+      const balancing::Method &normalization = balancing::Method::NONE()) const;
+
+  [[nodiscard]] bool has_normalization(std::string_view normalization) const;
+  [[nodiscard]] std::vector<balancing::Method> avail_normalizations() const;
+  [[nodiscard]] balancing::Weights normalization(std::string_view normalization_) const;
+
+  template <typename FileT>
+  [[nodiscard]] constexpr const FileT &get() const noexcept;
+  template <typename FileT>
+  [[nodiscard]] constexpr FileT &get() noexcept;
+  [[nodiscard]] constexpr auto get() const noexcept -> const FileVar &;
+  [[nodiscard]] constexpr auto get() noexcept -> FileVar &;
+};
+
+}  // namespace hictk
+
+#include "./impl/file_impl.hpp"  // NOLINT
