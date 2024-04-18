94ab84d5cfa391b32688abac4780c44c583ebf37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 15:05:23 +0000

    Add package swarmauri/0.1.42/swarmauri-0.1.42.tar.gz

---
+++ packages/swarmauri/swarmauri-0.1.42.tar.gz/swarmauri-0.1.42/swarmauri/standard/documents/base/EmbeddedBase.py
@@ -0,0 +1,25 @@
+from abc import ABC
+from typing import List, Any, Optional
+from swarmauri.core.documents.IEmbed import IEmbed
+from swarmauri.core.vectors.IVector import IVector
+
+class EmbeddedBase(IEmbed, ABC):
+    def __init__(self, embedding: Optional[IVector] = None):
+        self._embedding = embedding
+
+    def __str__(self):
+        return f"EmbeddedDocument ID: {self.id}, Content: {self.content}, Metadata: {self.metadata}, embedding={self.embedding}"
+
+    def __repr__(self):
+        return f"EmbeddedDocument(id={self.id}, content={self.content}, metadata={self.metadata}, embedding={self.embedding})"
+
+    def to_dict(self):
+        return self.__dict__
+
+    @property
+    def embedding(self) -> IVector:
+        return self._embedding
+
+    @embedding.setter
+    def embedding(self, value: IVector) -> None:
+        self._embedding = value
