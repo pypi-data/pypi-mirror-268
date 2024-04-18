4e08e8444f8ed893303ca49b479e4e50803cd466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 05:03:58 +0000

    Add package maths-operations-package/0.3/maths_operations_package-0.3.tar.gz

---
+++ packages/maths-operations-package/maths_operations_package-0.3.tar.gz/maths_operations_package-0.3/maths_operations/maths_operations.py
@@ -0,0 +1,29 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def rem(a, b):
+    return a % b
+
+def power(a, b):
+    return a ** b
+
+def square_root(a):
+    return pow(a, 0.5)
+
+def absolute(a):
+    return abs(a)
+
+def floor_div(a, b):
+    return a // b  
+
+def ceil_div(a, b):
+    return -(-a // b)
