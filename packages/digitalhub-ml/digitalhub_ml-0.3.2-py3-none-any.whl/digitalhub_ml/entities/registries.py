67eb400b6cd8b1b8732bcc9a33f775200fc2581a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-18 03:04:23 +0000

    Add package digitalhub-ml/0.3.2/digitalhub_ml-0.3.2-py3-none-any.whl

---
+++ packages/digitalhub-ml/digitalhub_ml-0.3.2-py3-none-any.whl/digitalhub_ml/entities/registries.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from digitalhub_core.entities._base.metadata import MetadataRegistry
+from digitalhub_core.entities._base.spec import SpecRegistry
+from digitalhub_core.entities._base.status import StatusRegistry
+
+metadata_registry = MetadataRegistry()
+spec_registry = SpecRegistry()
+status_registry = StatusRegistry()
+
+spec_registry.register("project", "digitalhub_ml.entities.projects.spec", "ProjectSpecMl", "ProjectParamsMl")
+
+for i in [
+    "model",
+]:
+    root = f"digitalhub_ml.entities.{i}s"
+    metadata_registry.register(i, f"{root}.metadata", f"{i.title()}Metadata")
+    spec_registry.register(i, f"{root}.spec", f"{i.title()}Spec", f"{i.title()}Params")
+    status_registry.register(i, f"{root}.status", f"{i.title()}Status")
