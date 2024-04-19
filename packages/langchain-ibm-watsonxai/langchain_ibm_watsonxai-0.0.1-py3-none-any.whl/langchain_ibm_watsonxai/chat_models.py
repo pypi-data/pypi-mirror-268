28c57b4d71ece573e9dbadf3dd6dd36f455ada95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 14:04:43 +0000

    Add package langchain-ibm-watsonxai/0.0.1/langchain_ibm_watsonxai-0.0.1-py3-none-any.whl

---
+++ packages/langchain-ibm-watsonxai/langchain_ibm_watsonxai-0.0.1-py3-none-any.whl/langchain_ibm_watsonxai/chat_models.py
@@ -0,0 +1,12 @@
+from langchain_core.language_models.chat_models import BaseChatModel
+
+
+class ChatWatsonxAI(BaseChatModel):
+    """IBM watsonx.ai chat models.
+
+    References:
+        [1] https://www.ibm.com/products/watsonx-ai
+        [2] https://cloud.ibm.com/apidocs/watsonx-ai?code=python
+    """
+
+    pass
