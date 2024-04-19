af0557055b9ef9d0d2535ba2f5a2dc8e08f95fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 06:05:07 +0000

    Add package prompt-security-fuzzer/0.1.7/prompt_security_fuzzer-0.1.7.tar.gz

---
+++ packages/prompt-security-fuzzer/prompt_security_fuzzer-0.1.7.tar.gz/prompt_security_fuzzer-0.1.7/ps_fuzz/attack_loader.py
@@ -0,0 +1,13 @@
+from .attacks import (
+    dynamic_test,
+    translation,
+    typoglycemia,
+    dan,
+    aim,
+    self_refine,
+    ethical_compliance,
+    ucar,
+    complimentary_transition,
+    harmful_behavior,
+    base64_injection
+)
