bcfb90268c1366d2874ad50a16919995803b2028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:18 +0000

    Add package xmipy/1.5.0/xmipy-1.5.0-py3-none-any.whl

---
+++ packages/xmipy/xmipy-1.5.0-py3-none-any.whl/xmipy/__init__.py
@@ -0,0 +1,14 @@
+"""
+Package for the eXtendend Model Interface.
+It provides abstract classes, as well as the implementation `XmiWrapper`
+"""
+
+# exports
+from bmipy.bmi import Bmi
+
+from xmipy.xmi import Xmi
+from xmipy.xmiwrapper import XmiWrapper
+
+__all__ = ["Bmi", "Xmi", "XmiWrapper"]
+
+__version__ = "1.5.0"
