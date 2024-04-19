32b97b25697fcf74d91b0b6d993cdc5a823714ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 07:04:32 +0000

    Add package polymatch/0.4.1/polymatch-0.4.1.tar.gz

---
+++ packages/polymatch/polymatch-0.4.1.tar.gz/polymatch-0.4.1/tests/test_glob.py
@@ -0,0 +1,30 @@
+"""Test glob matcher."""
+
+import pytest
+
+from polymatch import pattern_registry
+
+data = (
+    ("glob::*", "", True),
+    ("glob::*?", "", False),
+    ("glob::*?", "a", True),
+    ("glob:cf:*!*@thing", "itd!a@thing", True),
+    (b"glob:ci:*!*@thing", b"itd!a@thing", True),
+    (b"glob:*!*@thing", b"itd!a@thing", True),
+    (b"glob:*!*@thing", b"itd!a@THING", False),
+)
+
+
+@pytest.mark.parametrize(("pattern", "text", "result"), data)
+def test_patterns(pattern: str, text: str, result: bool) -> None:
+    """Ensure various patterns match their expected text."""
+    matcher = pattern_registry.pattern_from_string(pattern)
+    matcher.compile()
+    assert bool(matcher == text) is result
+
+
+def test_invert() -> None:
+    """Ensure parsing inverted pattern works."""
+    pattern = pattern_registry.pattern_from_string("~glob::beep")
+    pattern.compile()
+    assert pattern.inverted
