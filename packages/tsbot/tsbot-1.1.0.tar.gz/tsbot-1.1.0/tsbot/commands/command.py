3ef3e7341039075f95c7d828ba1776f530223c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 14:04:23 +0000

    Add package tsbot/1.1.0/tsbot-1.1.0.tar.gz

---
+++ packages/tsbot/tsbot-1.1.0.tar.gz/tsbot-1.1.0/tsbot/commands/command.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+import asyncio
+import inspect
+import itertools
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING, Callable, Coroutine, cast
+
+from tsbot import exceptions, parsers
+
+if TYPE_CHECKING:
+    from tsbot import bot, context
+
+
+@dataclass(slots=True)
+class TSCommand:
+    commands: tuple[str, ...]
+    handler: Callable[..., Coroutine[None, None, None]]
+
+    help_text: str = field(repr=False, default="")
+    raw: bool = field(repr=False, default=False)
+    hidden: bool = field(repr=False, default=False)
+
+    checks: list[Callable[..., Coroutine[None, None, None]]] = field(
+        default_factory=list, repr=False
+    )
+
+    @property
+    def call_signature(self) -> inspect.Signature:
+        return inspect.signature(self.handler)
+
+    @property
+    def usage(self) -> str:
+        usage: list[str] = []
+
+        for param in itertools.islice(self.call_signature.parameters.values(), 2, None):
+            if param.kind is inspect.Parameter.VAR_POSITIONAL:
+                usage.append(f"[{param.name!r}, ...]")
+
+            elif param.kind is inspect.Parameter.KEYWORD_ONLY:
+                usage.append(
+                    f"-{param.name} {'[!]' if param.default is param.empty else '[?]'}"
+                    f"{f' ({param.default!r})' if param.default not in (param.empty, None) else ''}"
+                )
+
+            else:
+                usage.append(
+                    f"{param.name!r}"
+                    f"""{f" ({param.default or '?'!r})" if param.default is not param.empty else ''}"""
+                )
+
+        return f"Usage: {' | '.join(self.commands)} {' '.join(usage)}"
+
+    async def run_checks(
+        self, bot: bot.TSBot, ctx: context.TSCtx, *args: str, **kwargs: str
+    ) -> None:
+        done, pending = await asyncio.wait(
+            [
+                asyncio.create_task(check(bot, ctx, *args, **kwargs), name="Check-Task")
+                for check in self.checks
+            ],
+            return_when=asyncio.FIRST_EXCEPTION,
+        )
+        for pending_task in pending:
+            pending_task.cancel()
+
+        for done_task in done:
+            if exception := done_task.exception():
+                raise exception
+
+    async def run(self, bot: bot.TSBot, ctx: context.TSCtx, msg: str) -> None:
+        if self.raw:
+            args, kwargs = (msg,) if msg else (), cast(dict[str, str], {})
+        else:
+            args, kwargs = parsers.parse_args_kwargs(msg)
+
+        if self.checks:
+            await self.run_checks(bot, ctx, *args, **kwargs)
+
+        try:
+            binded_arguments = self.call_signature.bind(bot, ctx, *args, **kwargs)
+        except TypeError as e:
+            raise exceptions.TSInvalidParameterError(str(e).capitalize()) from e
+        else:
+            await self.handler(*binded_arguments.args, **binded_arguments.kwargs)
