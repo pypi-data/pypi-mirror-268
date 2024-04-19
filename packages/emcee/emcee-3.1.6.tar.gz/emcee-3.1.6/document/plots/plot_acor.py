395992f9d7e9f6d1c16166dc44fadbd3f6f2f46f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:03 +0000

    Add package emcee/3.1.6/emcee-3.1.6.tar.gz

---
+++ packages/emcee/emcee-3.1.6.tar.gz/emcee-3.1.6/document/plots/plot_acor.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+import matplotlib.pyplot as pl
+import numpy as np
+
+# sys.path.prepend(os.path.abspath(os.path.join(__file__, "..", "..", "..")))
+# import emcee
+
+
+def plot_acor(acorfn):
+    pass
