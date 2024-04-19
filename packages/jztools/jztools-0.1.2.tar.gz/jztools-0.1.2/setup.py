c284f122157ad8703a22c7564a7a6a4474b33b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 06:05:37 +0000

    Add package jztools/0.1.2/jztools-0.1.2.tar.gz

---
+++ packages/jztools/jztools-0.1.2.tar.gz/jztools-0.1.2/setup.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="jztools",
+    packages=find_packages(".", exclude=["tests"]),
+    version="0.1.2",
+    description="General python utilies",
+    scripts=["scripts/test_install"],
+    install_requires=["pygments", "numpy", "climax"],
+    author="Joaquin Zepeda",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zepedaj/jztools",
+)
