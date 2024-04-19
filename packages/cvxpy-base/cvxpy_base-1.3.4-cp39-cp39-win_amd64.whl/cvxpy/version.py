0c6feb9ec0c5999c261d141c030c80e2aa077b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 15:04:33 +0000

    Add package cvxpy-base/1.3.4/cvxpy_base-1.3.4-cp39-cp39-win_amd64.whl

---
+++ packages/cvxpy-base/cvxpy_base-1.3.4-cp39-cp39-win_amd64.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.3.4'
+version = '1.3.4'
+full_version = '1.3.4'
+git_revision = '3e2a68b'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
