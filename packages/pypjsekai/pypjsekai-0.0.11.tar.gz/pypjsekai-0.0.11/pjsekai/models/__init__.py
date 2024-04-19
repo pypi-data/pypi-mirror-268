3ec54df2425fa2de6b133c1478befd33c8dc8253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 05:05:58 +0000

    Add package pypjsekai/0.0.11/pypjsekai-0.0.11.tar.gz

---
+++ packages/pypjsekai/pypjsekai-0.0.11.tar.gz/pypjsekai-0.0.11/pjsekai/models/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2022-present Erik Chan <erikchan002@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .master_data import *
+from .system_info import *
+from .asset_bundle_info import *
+from .game_version import *
+from .information import *
