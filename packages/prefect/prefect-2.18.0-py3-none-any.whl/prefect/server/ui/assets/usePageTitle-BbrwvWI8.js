887b00665540abb9540cd9e4805e680d3b5cb357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 06:05:22 +0000

    Add package prefect/2.18.0/prefect-2.18.0-py3-none-any.whl

---
+++ packages/prefect/prefect-2.18.0-py3-none-any.whl/prefect/server/ui/assets/usePageTitle-BbrwvWI8.js
@@ -0,0 +1,2 @@
+import{b as o,ar as u,u as c}from"./index-DiUaH198.js";function i(...t){const r=[...t,"Prefect Server"],a=o(()=>r.map(e=>c(e)).filter(e=>e!==null).join(" • "));u(()=>document.title=a.value)}export{i as u};
+//# sourceMappingURL=usePageTitle-BbrwvWI8.js.map
