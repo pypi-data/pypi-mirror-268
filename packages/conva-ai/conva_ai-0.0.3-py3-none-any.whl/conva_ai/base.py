a5a4f2bb012f59505025a544ea1444b650945961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 03:04:43 +0000

    Add package conva-ai/0.0.3/conva_ai-0.0.3-py3-none-any.whl

---
+++ packages/conva-ai/conva_ai-0.0.3-py3-none-any.whl/conva_ai/base.py
@@ -0,0 +1,26 @@
+import uuid
+import requests
+
+
+class BaseClient:
+
+    def __init__(self, copilot_id: str, copilot_version: str, api_key: str, host: str = "https://infer.conva.ai"):
+        self.copilot_id: str = copilot_id
+        self.api_key: str = api_key
+        self.copilot_version: str = copilot_version
+        self.host: str = host
+        self.keep_conversation_history: bool = True
+        self.domain: str = ""
+        self.history: str = ""
+
+    def clear_history(self):
+        """
+        Clears the history tracked by the client
+        """
+        self.history: str = ""
+
+    def disable_history(self, use_history: bool):
+        """
+        Disables conversation history tracking by the client
+        """
+        self.keep_conversation_history = use_history
