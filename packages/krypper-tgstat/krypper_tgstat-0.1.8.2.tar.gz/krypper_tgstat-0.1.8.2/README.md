66a35101d3e2601ccc39d622c6fe1f384e1cb2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:05:16 +0000

    Add package krypper-tgstat/0.1.8.2/krypper_tgstat-0.1.8.2.tar.gz

---
+++ packages/krypper-tgstat/krypper_tgstat-0.1.8.2.tar.gz/krypper_tgstat-0.1.8.2/README.md
@@ -0,0 +1,4 @@
+# PyrapperTGStatAPI
+Wrapper for TG Stat API writing on Python 
+TGStat (https://tgstat.ru API wrapper)
+
---
+++ packages/krypper-tgstat/krypper_tgstat-0.1.8.2.tar.gz/krypper_tgstat-0.1.8.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+
+"""
+:authors: Evgeny Momotov (KrymmyEM)
+:license: MIT License
+Copyright: (c) 2024 Evgeny
+"""
+
+version = "0.1.8.2"
+
+long_description = ""
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name = "krypper_tgstat",
+    version = version,
+    description = "Wrapper for TG Stat API writing on Python\nTGStat (https://tgstat.ru API wrapper)",
+    long_description = long_description,
+
+    author = "Evgeny Momotov (KrymmyEM)",
+    author_email = "evgeny.momotov@gmail.com",
+    url = "https://github.com/KrymmyEM/krypper_tgstat",
+    download_url = "https://github.com/KrymmyEM/krypper_tgstat/archive/main.zip",
+
+    license = "MIT License. See LICENSE file",
+    packages = ["krypper_tgstat"],
+    install_requires=[
+        "requests",
+        "aiohttp"
+    ],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
+
+
