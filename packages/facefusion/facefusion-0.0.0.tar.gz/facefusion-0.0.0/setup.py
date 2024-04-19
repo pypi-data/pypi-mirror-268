6aeb3073d6ccaedc8489fcd4a092f2f1a455c947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 03:05:30 +0000

    Add package facefusion/0.0.0/facefusion-0.0.0.tar.gz

---
+++ packages/facefusion/facefusion-0.0.0.tar.gz/facefusion-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='facefusion',
+    version='0.0.0',
+    description='Next generation face swapper and enhancer.',
+    author='Henry Ruhs',
+    author_email='info@henryruhs.com',
+    url='https://facefusion.io',
+)
