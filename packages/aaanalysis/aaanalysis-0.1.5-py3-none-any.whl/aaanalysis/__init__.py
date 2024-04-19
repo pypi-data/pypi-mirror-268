bb449cec004ba23ee5d4f58abd51a6866d25c839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 05:04:49 +0000

    Add package aaanalysis/0.1.5/aaanalysis-0.1.5-py3-none-any.whl

---
+++ packages/aaanalysis/aaanalysis-0.1.5-py3-none-any.whl/aaanalysis/__init__.py
@@ -0,0 +1,70 @@
+from .data_handling import load_dataset, load_scales, load_features, to_fasta
+from .feature_engineering import AAclust, AAclustPlot, SequenceFeature, NumericalFeature, CPP, CPPPlot
+from .pu_learning import dPULearn, dPULearnPlot
+from .pertubation import AAMut, AAMutPlot, SeqMut, SeqMutPlot
+from .plotting import (plot_get_clist, plot_get_cmap, plot_get_cdict,
+                       plot_settings, plot_legend, plot_gcfs)
+from .metrics import (comp_auc_adjusted, comp_bic_score, comp_kld)
+from .config import options
+
+
+# Import of base version features
+__all__ = [
+    "load_dataset",
+    "load_scales",
+    "load_features",
+    "to_fasta",
+    "AAclust",
+    "AAclustPlot",
+    "SequenceFeature",
+    "NumericalFeature",
+    "CPP",
+    "CPPPlot",
+    "dPULearn",
+    "dPULearnPlot",
+    "AAMut",
+    "AAMutPlot",
+    "SeqMut",
+    "SeqMutPlot",
+    "plot_get_clist",
+    "plot_get_cmap",
+    "plot_get_cdict",
+    "plot_settings",
+    "plot_legend",
+    "plot_gcfs",
+    "comp_auc_adjusted",
+    "comp_bic_score",
+    "comp_kld",
+    "options"
+]
+
+
+# Import of professional (pro) version features if dependencies are available
+try:
+    from .explainable_ai import TreeModel, ShapExplainer
+    from .show_html import display_df
+    # Extend the __all__ list with pro features if successful
+    __all__.extend(["TreeModel",
+                    "ShapExplainer",
+                    "display_df"])
+
+except ImportError as e:
+    # Define a factory function to create a class or function placeholder
+    def make_unavailable_feature(feature_name):
+        str_error = (f"'{feature_name}' needs additional dependencies. Install AAanalysis Professional via:"
+                     f"\n\tpip install aaanalysis[pro]")
+
+        class UnavailableFeature:
+            def __init__(self, *args, **kwargs):
+                raise ImportError(str_error)
+
+            def __call__(self, *args, **kwargs):
+                raise ImportError(str_error)
+
+        # Return the custom class for the unavailable feature
+        return UnavailableFeature
+
+    # Use the factory function to create placeholders for pro features
+    TreeModel = make_unavailable_feature("TreeModel")
+    ShapExplainer = make_unavailable_feature("ShapExplainer")
+    display_df = make_unavailable_feature("display_df")
---
+++ packages/aaanalysis/aaanalysis-0.1.5-py3-none-any.whl/aaanalysis/data_handling/_data_read_write.py
@@ -0,0 +1,54 @@
+"""
+This is a script for reading and writing df_seq to the fasta file format, which is the most commonly used format
+in computational biology. This fasta format enables a smooth interaction with the biopython package.
+See https://bioperl.org/formats/sequence_formats/FASTA_sequence_format for description of input fasta format.
+"""
+import time
+import pandas as pd
+import numpy as np
+
+import aaanalysis.utils as ut
+
+
+
+# I Helper Functions
+# TODO add more parsers for often used data formats in computational biology (make overview)
+# TODO implement parser from df to df_seq (remove not necessary columns and adjust naming)
+
+
+# II Main Functions
+def read_fasta():
+    """"""
+
+
+def to_fasta(df=None, fasta_name=None, col_id=None, col_seq=None, cols_info=None):
+        """"""
+        # Check input
+        ut.check_str(name="col_id", val=col_id, accept_none=False)
+        ut.check_str(name="col_seq", val=col_seq, accept_none=False)
+        cols_info = ut.check_list_like(name="cols_info", val=cols_info, accept_str=True, accept_none=True)
+        cols_requiered = [col_id, col_seq]
+        if cols_info is not None:
+            cols_requiered += cols_info
+        ut.check_df(df=df, name="df", cols_requiered=cols_requiered, accept_none=False, accept_nan=False)
+        # Create faste
+        if ".fasta" not in fasta_name:
+            fasta_name += ".fasta"
+        fasta = open(fasta_name, "w")
+        for i, row in df.iterrows():
+            seq = row[col_seq]
+            entry = row[col_id]
+            if cols_info is not None:
+                if isinstance(cols_info, list):
+                    str_info = ", ".join([str(row[c]) for c in cols_info])
+                else:
+                    str_info = row[cols_info]
+                fasta.write(">" + entry + "," + str_info + "\n" + seq + "\n")
+            else:
+                fasta.write(">" + entry + "\n" + seq + "\n")
+        fasta.close()
+
+
+
+def to_df_seq(df=None):
+    """"""
