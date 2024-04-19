75c1a4ca816a530e157a605fedfe3f8f07c0c83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:01 +0000

    Add package th-influx-sdk/1.0.0/th-influx-sdk-1.0.0.tar.gz

---
+++ packages/th-influx-sdk/th-influx-sdk-1.0.0.tar.gz/th-influx-sdk-1.0.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='th-influx-sdk',
+    version='1.0.0',
+    description='A SDK for influxDb',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='xiongyi',
+    author_email='15679191752@163.com',
+    packages=find_packages(),
+    install_requires=[
+        'influxdb',
+        'typing',
+        'dateutil',
+        'datetime',
+        'collections',
+        'json'
+    ]
+)
