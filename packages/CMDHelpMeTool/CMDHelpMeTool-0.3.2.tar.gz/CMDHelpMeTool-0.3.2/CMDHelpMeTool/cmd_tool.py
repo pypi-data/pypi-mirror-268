e7c43aa5f37f152148edfc38108d928582044c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:31 +0000

    Add package CMDHelpMeTool/0.3.2/CMDHelpMeTool-0.3.2.tar.gz

---
+++ packages/CMDHelpMeTool/CMDHelpMeTool-0.3.2.tar.gz/CMDHelpMeTool-0.3.2/CMDHelpMeTool/cmd_tool.py
@@ -0,0 +1,24 @@
+import argparse
+import pyperclip
+from .experiments import *  # Import all variables from experiments.py
+
+def copy_variable_to_clipboard(variable):
+    pyperclip.copy(variable)
+    print(f"Content copied to clipboard: {variable}")
+
+def main():
+    parser = argparse.ArgumentParser(description='Command-line tool for copying variable content to clipboard.')
+    parser.add_argument('command', choices=['copy_experiment'], help='Command to execute')
+    parser.add_argument('variable_name', help='Name of the variable to copy')
+
+    args = parser.parse_args()
+
+    if args.command == 'copy_experiment':
+        variable = globals().get(args.variable_name)
+        if variable is not None:
+            copy_variable_to_clipboard(variable)
+        else:
+            print(f"Variable '{args.variable_name}' not found.")
+
+if __name__ == "__main__":
+    main()
