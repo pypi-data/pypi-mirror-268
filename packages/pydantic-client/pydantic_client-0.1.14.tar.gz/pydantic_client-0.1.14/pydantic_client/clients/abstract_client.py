1dfbd82b1d965be76a5dd73ffb7b06c623d13a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:06:17 +0000

    Add package pydantic-client/0.1.14/pydantic_client-0.1.14.tar.gz

---
+++ packages/pydantic-client/pydantic_client-0.1.14.tar.gz/pydantic_client-0.1.14/pydantic_client/clients/abstract_client.py
@@ -0,0 +1,19 @@
+from typing import Any, Dict, Tuple
+
+from pydantic_client.schema.http_request import HttpRequest
+
+
+class AbstractClient:
+
+    def do_request(self, request: HttpRequest) -> Any:
+        raise NotImplementedError
+
+    @staticmethod
+    def parse_request(request: HttpRequest) -> Tuple[Dict, Dict]:
+        if request.data:
+            data = request.data
+            json = {}
+        else:
+            data = {}
+            json = request.json_body
+        return data, json
