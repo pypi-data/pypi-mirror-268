19f1d92c8d515e980a5b208c8569075ecab18e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:04:57 +0000

    Add package amsdal_utils/0.1.2/amsdal_utils-0.1.2.tar.gz

---
+++ packages/amsdal_utils/amsdal_utils-0.1.2.tar.gz/amsdal_utils-0.1.2/src/amsdal_utils/models/utils/get_subclasses.py
@@ -0,0 +1,20 @@
+from collections.abc import Iterator
+
+from amsdal_utils.models.base import ModelBase
+from amsdal_utils.models.enums import SchemaTypes
+
+
+def get_subclasses(class_item: type[ModelBase] | None) -> Iterator[type[ModelBase]]:
+    if class_item is None:
+        return []
+
+    for parent_class in class_item.mro():
+        schema_type = getattr(parent_class, 'schema_type', None)
+
+        if not schema_type or schema_type in (SchemaTypes.TYPE, SchemaTypes.CORE):
+            break
+
+        if parent_class.__name__ == class_item.__name__:
+            continue
+
+        yield parent_class
