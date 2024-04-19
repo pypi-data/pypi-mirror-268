67c78e8628a4b97a6ba3e9598cbd689b931cd68a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 03:04:47 +0000

    Add package ntqr/0.3.1/ntqr-0.3.1.tar.gz

---
+++ packages/ntqr/ntqr-0.3.1.tar.gz/ntqr-0.3.1/src/ntqr/__init__.py
@@ -0,0 +1,24 @@
+"""
+Evaluate noisy decision makers using logic and algebra.
+
+Classes:
+
+Functions:
+
+Misc variables:
+
+    __version__
+    uci_adult_test_example
+"""
+
+__version__ = "0.3.1"
+
+from ntqr.r2.examples import uciadult_label_counts
+
+from ntqr.r2.datasketches import TrioLabelVoteCounts, TrioVoteCounts
+
+from ntqr.r2.evaluators import (
+    SupervisedEvaluation,
+    ErrorIndependentEvaluation,
+    MajorityVotingEvaluation,
+)
