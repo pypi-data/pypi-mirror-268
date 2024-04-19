0cedd6274e8d2615aebcd709ac67bef1e2e9d295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 14:04:50 +0000

    Add package sagemaker-jupyterlab-extension/0.3.2/sagemaker_jupyterlab_extension-0.3.2.tar.gz

---
+++ packages/sagemaker-jupyterlab-extension/sagemaker_jupyterlab_extension-0.3.2.tar.gz/sagemaker_jupyterlab_extension-0.3.2/sagemaker_jupyterlab_extension/tests/test_error_util.py
@@ -0,0 +1,60 @@
+import botocore.exceptions
+import re
+
+from sagemaker_jupyterlab_extension.utils.error_util import is_fault
+
+
+def test_is_fault_true():
+    """Given"""
+    error = botocore.exceptions.ClientError(
+        {
+            "Error": {
+                "Code": "SomeServiceException",
+                "Message": "Details/context around the exception or error",
+            },
+            "ResponseMetadata": {
+                "RequestId": "1234567890ABCDEF",
+                "HostId": "host ID data will appear here as a hash",
+                "HTTPStatusCode": 500,
+                "HTTPHeaders": {"header metadata key/values will appear here"},
+                "RetryAttempts": 0,
+            },
+        },
+        "AWSSERVICEOPERATION",
+    )
+
+    expected_result = True
+
+    """When"""
+    actual_result = is_fault(error)
+
+    """Then"""
+    assert expected_result == actual_result
+
+
+def test_is_fault_false():
+    """Given"""
+    error = botocore.exceptions.ClientError(
+        {
+            "Error": {
+                "Code": "SomeServiceError",
+                "Message": "Details/context around the exception or error",
+            },
+            "ResponseMetadata": {
+                "RequestId": "1234567890ABCDEF",
+                "HostId": "host ID data will appear here as a hash",
+                "HTTPStatusCode": 400,
+                "HTTPHeaders": {"header metadata key/values will appear here"},
+                "RetryAttempts": 0,
+            },
+        },
+        "AWSSERVICEOPERATION",
+    )
+
+    expected_result = False
+
+    """When"""
+    actual_result = is_fault(error)
+
+    """Then"""
+    assert expected_result == actual_result
