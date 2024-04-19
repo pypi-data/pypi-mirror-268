40cb3f1741637829b6c6c34d16ddfd207501150a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:05:14 +0000

    Add package bharatocr-client/0.1.1/bharatocr_client-0.1.1-py3-none-any.whl

---
+++ packages/bharatocr-client/bharatocr_client-0.1.1-py3-none-any.whl/bharatocrclient/clients/api.py
@@ -0,0 +1,5 @@
+from bharatocrclient.clients.pan import pan
+from bharatocrclient.clients.aadhaar import aadhaar
+from bharatocrclient.clients.driving_licence import driving_licence
+from bharatocrclient.clients.passport import passport
+from bharatocrclient.clients.auth import login, register
