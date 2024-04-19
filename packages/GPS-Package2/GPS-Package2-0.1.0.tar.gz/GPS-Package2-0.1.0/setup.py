126948df04cd488b867892bea1ed53ff4a461fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:06:07 +0000

    Add package GPS-Package2/0.1.0/GPS-Package2-0.1.0.tar.gz

---
+++ packages/GPS-Package2/GPS-Package2-0.1.0.tar.gz/GPS-Package2-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='GPS-Package2',
+version='0.1.0',
+author='Ely Millán',
+author_email='ely141117@gmail.com',
+description='PyPi Upload of the GPS Project',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.10',
+)
