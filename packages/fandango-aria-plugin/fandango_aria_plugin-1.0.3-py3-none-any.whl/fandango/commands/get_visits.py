4d7361a75a79ddf4dfe06fb0f597707eec660774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:04:55 +0000

    Add package fandango-aria-plugin/1.0.3/fandango_aria_plugin-1.0.3-py3-none-any.whl

---
+++ packages/fandango-aria-plugin/fandango_aria_plugin-1.0.3-py3-none-any.whl/fandango/commands/get_visits.py
@@ -0,0 +1,12 @@
+
+import click
+from ..aria_client import AriaClient
+
+
+@click.command()
+@click.option('--vid', prompt='VID Filter', help='Filter to a specific Visit', default='optional')
+def get_visits(vid):
+    vid = None if vid == 'optional' else vid
+    cli = AriaClient()
+    cli.get_visits(vid)
+
