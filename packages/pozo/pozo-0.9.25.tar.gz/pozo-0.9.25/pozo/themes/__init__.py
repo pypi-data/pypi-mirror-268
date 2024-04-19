b35fb2c66720a591474d614ab0da780e205476fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 06:05:32 +0000

    Add package pozo/0.9.25/pozo-0.9.25.tar.gz

---
+++ packages/pozo/pozo-0.9.25.tar.gz/pozo-0.9.25/pozo/themes/__init__.py
@@ -0,0 +1,157 @@
+import colour
+
+import pozo.themes.mnemonic_tables as tables
+
+class Theme(): # Meant to be inherited
+    def __init__(self, *args, **kwargs):
+        self._context = {}
+        super().__init__(*args, **kwargs)
+
+    def set_context(self, data):
+        if data is None: data = {}
+        if not isinstance(data, dict):
+            raise TypeError("Cannot assing context which is not a dict (or None)")
+        self._context = data
+
+    def get_context(self):
+        return self._context
+
+    def resolve(self, key, contexts):
+            raise NotImplementedError("Every Theme inheriter must supply a resolve(self, key, context)")
+
+class ThemeDict(Theme, dict): # Basically a dict
+    def resolve(self, key, contexts=None):
+        if key not in self:
+            return None
+        return self[key]
+
+class DynamicTheme(Theme): # Meant to be inherited
+    def resolve(self, key, contexts):
+        raise NotImplementedError("Dynamic themes must implement get_value(self, key, args)")
+
+# A themeable is something that stores a theme. It accepts either a theme or a dict. It sets its metadata on the theme
+# When
+class Themeable(): # Meant to be inherited by objects
+    def __init__(self, *args, **kwargs):
+        self.set_theme(kwargs.pop("theme", {}))
+        super().__init__(*args, **kwargs)
+
+    def set_theme(self, theme):
+        if isinstance(theme, str):
+            if theme not in themes:
+                raise ValueError(f"{theme} is not a built-in theme, options are: {', '.join(list(themes.keys()))}")
+            theme = themes[theme]
+        if isinstance(theme, dict) and not isinstance(theme, ThemeDict):
+            self._theme = ThemeDict(theme)
+        elif isinstance(theme, Theme):
+            self._theme = theme
+        elif theme is None:
+            self._theme = ThemeDict({})
+        else:
+            raise TypeError(f"Theme was not a dict, ThemeDict, or DynamicTheme, was {type(theme)}")
+
+    def _get_theme(self, context={}):
+        self._theme.set_context(context)
+        return self._theme
+
+    # You may override this
+    def get_theme(self):
+        return self._get_theme()
+
+# Above are inheritables
+from pozo.themes.theme_tools import * # should it be relative?
+# Below is implementations
+themes = {'cangrejo': MnemonicDictionary(tables.cangrejo)}
+
+# ThemeList also has a theme, which is considered an override!
+class ThemeStack(Themeable):
+    def __init__(self, *args, **kwargs):
+        local_default_theme = kwargs.pop("default", default_theme)
+        self._contexts_vertical = [] # parallel con _list
+        self._list = [local_default_theme] if local_default_theme else [] # the themes
+        self._shadow_objects = {} # if themes have things that are initialized when they're added
+        for arg in args:
+            self.append(arg)
+        super().__init__(*[], **kwargs)
+
+    def append(self, theme):
+        if isinstance(theme, Theme):
+            self._list.append(theme)
+            self._contexts_vertical.append(theme.get_context()) # but why not just traverse this at get time
+        else:
+            raise TypeError(f"Found a theme which isn't any type of valid theme: {type(theme)} {theme}")
+
+    def pop(self, index = -1):
+        self._contexts_vertical.pop(index)
+        return self._list.pop(index)
+
+    def __contains__(self, key):
+        try:
+            self.__getitem__(key)
+        except KeyError:
+            return False
+        return True
+
+    def __getitem__(self, key):
+        if not isinstance(key, str):
+            raise TypeError(f"Key must be string, not {type(key)} | value:{key}")
+        # print(f'\ngetitem() Trying to get theme: {key}')
+
+        contexts_horizontal = []
+        eventual_value = self.get_theme() # Self-theme, override
+        eventual_value = self._check_shortcut(eventual_value, key, contexts_horizontal) # something that translates to themelike
+        # print(f'Override: {eventual_value}')
+        while True: # do-while loop using break
+            contexts_horizontal.append(eventual_value.get_context())
+            eventual_value = eventual_value.resolve(key, self._contexts_vertical)
+            # print(f'Override resolved to: {eventual_value}')
+            eventual_value = self._check_shortcut(eventual_value, key, contexts_horizontal) # something that translates to themelike
+            # print(f'Above shortcutted to: {eventual_value}')
+            if not isinstance(eventual_value, Theme):
+                break
+        if eventual_value is None:
+            contexts_horizontal.clear()
+            for eventual_value in reversed(self._list):
+                # print(f'Value from list of length {len(self._list)}: {eventual_value}')
+                while True:# do-while loop using break
+                    contexts_horizontal.append(eventual_value.get_context())
+                    eventual_value = eventual_value.resolve(key, self._contexts_vertical)
+                    # print(f'Override resolved to: {eventual_value}')
+                    eventual_value = self._check_shortcut(eventual_value, key, contexts_horizontal)
+                    # print(f'Above shortcutted to: {eventual_value}')
+                    if not isinstance(eventual_value, Theme):
+                        break
+                if eventual_value is not None:
+                   break
+        # print(f'Final result: {eventual_value}')
+        return self._process_output(eventual_value, key)
+
+    # Take not-themes and return themes. Can't take a regular dict, which is sorta problematic.
+    def _check_shortcut(self, value, key, contexts):
+        context = contexts[-1] if len(contexts) else []
+        if value is None:
+            return None
+        may_be_key = (id(value), id(context))
+        if may_be_key in self._shadow_objects:
+            return self._shadow_objects[may_be_key]
+        elif key == "color" and isinstance(value, list):
+            value = ColorWheel(value, context=context)
+        elif key == "place_holder" and isinstance(value, "placeholder"): # never happen, put other shortcuts here
+            value = "place_holder"
+        else:
+            return value # just send back what we got, do nothing
+        self._shadow_objects[may_be_key] = value
+        return value
+
+    def _process_output(self, value, key):
+        if value is None:
+            raise KeyError(f"{key} does not exist in this theme, and there is no default")
+        if key == "color":
+            if not isinstance(value, colour.Color):
+                return colour.Color(value).hex_l
+            else:
+                return value.hex_l
+        elif key == "range_unit" and value is False:
+            return None
+        return value
+
---
+++ packages/pozo/pozo-0.9.25.tar.gz/pozo-0.9.25/pozo/themes/theme_tools.py
@@ -0,0 +1,89 @@
+import pozo.themes as pzt
+import pozo.units as pzu
+
+
+default_color_list = ['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52']
+default_theme = pzt.ThemeDict({ "color": default_color_list, "track_width": 200, 'force':False, 'hidden':False, 'range':[None,None], 'scale':'linear', 'range_unit':False })
+#
+class MnemonicDictionary(pzt.DynamicTheme):
+    def __init__(self, mnemonic_table, registry=pzu.registry):
+        if not isinstance(mnemonic_table, dict): raise ValueError("menmonic_table must be dictionary")
+        for key, value in mnemonic_table.items():
+            if "range_unit" in value:
+                pzu.registry.parse_units(value["range_unit"]) # just checking to see if its working
+        self._lut = mnemonic_table # renderer will have to do conversons
+
+    def resolve(self, key, contexts):
+        mnemonic = None
+        for context in contexts:
+            if "mnemonic" in context:
+                mnemonic = context["mnemonic"]
+            elif "mnemonics" in context and len(context["mnemonics"]) >= 1:
+                    mnemonic = context["mnemonics"][0]
+        if mnemonic is None or mnemonic not in self._lut:
+            if '-' in self._lut and key in self._lut['-']: return self._lut['-'][key]
+            return None
+        if key in self._lut[mnemonic]:
+            return self._lut[mnemonic][key]
+        if '-' in self._lut and key in self._lut['-']: return self._lut['-'][key]
+        return None
+
+    def get_dictionary(self):
+        return self._lut
+
+    def set_mnemonic(self, mnemonic, dictionary):
+        self._lut[mnemonic] = dictionary
+
+    def get_mnemonic(self, mnemonic):
+        if mnemonic not in self._lut: return None
+        return self._lut[mnemonic]
+
+    def set_value(self, mnemonic, key, value):
+        if mnemonic not in self._lut: self._lut[mnemonic] = {}
+        self._lut[mnemonic][key] = value
+
+    def set_fallback(self, key, value):
+        self.set_value('-', key, value)
+
+class ColorWheel(pzt.DynamicTheme):
+    def __init__(self, color_list=default_color_list, per=None, context=None, each=False):
+        self._color_list=color_list
+        self._each = each
+        if per is None and context is not None and "type" in context:
+            if context["type"] == "axis":
+                self._per = "trace"
+            else:
+                self._per = "axis"
+        elif per is None:
+            self._per= "axis"
+        else:
+            self._per = per
+        self._current_color = color_list[0]
+        self._current_context = None
+        self._i = 0
+        super().__init__()
+
+    def get_current_color(self):
+        return self._current_color
+
+    # ColorWheel is really married to graphs. Think about this during documentation.
+    def resolve(self, key, contexts):
+        if not self._each:
+            for context in contexts:
+                if "type" in context and (context["type"] == self._per or context["type"] in self._per):
+                    if self._current_context is None:
+                        self._current_context = id(context)
+                    elif id(context) != self._current_context:
+                        self._current_context = id(context)
+                        color = self._color_list[self._i % len(self._color_list)]
+                        self._current_color = color
+                    else: # same context, do nothing
+                        continue
+                    self._i += 1
+                    break
+        else:
+            color = self._color_list[self._i % len(self._color_list)]
+            self._current_color = color
+            self._i += 1
+        return self._current_color
+
