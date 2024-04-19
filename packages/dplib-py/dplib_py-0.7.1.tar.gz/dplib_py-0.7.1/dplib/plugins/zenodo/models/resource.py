76f94f205bdbd510866285279e005278a5893a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:14 +0000

    Add package dplib-py/0.7.1/dplib_py-0.7.1.tar.gz

---
+++ packages/dplib-py/dplib_py-0.7.1.tar.gz/dplib_py-0.7.1/dplib/plugins/zenodo/models/resource.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from dplib.helpers.resource import slugify_name
+from dplib.models import Resource
+from dplib.system import Model
+
+
+class ZenodoResource(Model):
+    """Zenodo Resource model"""
+
+    key: str
+    id: Optional[str] = None
+    checksum: Optional[str] = None
+    ext: Optional[str] = None
+    mimetype: Optional[str] = None
+    size: Optional[int] = None
+
+    # Converters
+
+    def to_dp(self) -> Resource:
+        """Convert to Data Package resource
+
+        Returns:
+           Data Resource
+        """
+        resource = Resource(path=self.key, name=slugify_name(self.key))
+
+        # Format
+        if self.ext:
+            resource.format = self.ext.lower()
+
+        # Mediatype
+        if self.mimetype:
+            resource.mediatype = self.mimetype
+
+        # Bytes
+        if self.size:
+            resource.bytes = self.size
+
+        # Hash
+        if self.checksum:
+            resource.hash = self.checksum.replace("md5:", "")
+
+        return resource
+
+    @classmethod
+    def from_dp(cls, resource: Resource) -> Optional[ZenodoResource]:
+        """Create Zenodo Resource from Data Resource
+
+        Parameters:
+            resource: Data Resource
+
+        Returns:
+            Zenodo Resource
+        """
+        if not resource.path or not isinstance(resource.path, str):
+            return
+
+        # Path
+        zenodo = ZenodoResource(key=resource.path)
+
+        # Format
+        if resource.format:
+            zenodo.ext = resource.format
+
+        # Mediatype
+        if resource.mediatype:
+            zenodo.mimetype = resource.mediatype
+
+        # Bytes
+        if resource.bytes:
+            zenodo.size = resource.bytes
+
+        # Hash
+        hash = resource.get_hash()
+        if hash:
+            if hash.type == "md5":
+                zenodo.checksum = hash.long
+
+        return zenodo
