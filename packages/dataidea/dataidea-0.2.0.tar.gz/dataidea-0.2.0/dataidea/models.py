5c5c1f2341e5170d8f22132f22fcf0cad4868c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:14 +0000

    Add package dataidea/0.2.0/dataidea-0.2.0.tar.gz

---
+++ packages/dataidea/dataidea-0.2.0.tar.gz/dataidea-0.2.0/dataidea/models.py
@@ -0,0 +1,10 @@
+# saving and loading the models
+from joblib import load as load_model
+from joblib import dump as save_model
+# models
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
