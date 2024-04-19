744a6a8996516f44fa65c2df1efcf10812c3331c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:13 +0000

    Add package lyk-reservation-nowon/0.0.5/lyk-reservation-nowon-0.0.5.tar.gz

---
+++ packages/lyk-reservation-nowon/lyk-reservation-nowon-0.0.5.tar.gz/lyk-reservation-nowon-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lyk-reservation-nowon',
+    version='0.0.5',
+    description='lyk-reservation-nowon',
+    author='officeyongki',
+    author_email='officeyongki@gmail.com',
+    install_requires=['pyperclip', 'selenium', 'webdriver_manager'],
+    packages=find_packages(exclude=[]),
+    keywords=['reservation', 'lyk', 'pypi'],
+    python_requires='>=3.10',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
