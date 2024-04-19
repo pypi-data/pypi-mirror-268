dd7b0f2a971a8505d8d6a569e04198b4133d488a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 15:04:25 +0000

    Add package pj-basic-operations/0.1/pj_basic_operations-0.1-py3-none-any.whl

---
+++ packages/pj-basic-operations/pj_basic_operations-0.1-py3-none-any.whl/pj_basic_operations/main.py
@@ -0,0 +1,67 @@
+def add_numers(a,b):
+    """
+    Suma dos números y devuelve el resultado.
+
+    Parameters:
+    a (int): El primer número a sumar.
+    b (int): El segundo número a sumar.
+
+    Returns:
+    int: La suma de los dos números.
+
+    Example:
+    >>> add_numers(2, 3)
+    5
+    """
+    return a + b
+
+def subtract_numbers(a,b):
+    """
+    Resta dos números y devuelve el resultado.
+
+    Parameters:
+    a (int): El primer número a restar.
+    b (int): El segundo número a restar.
+
+    Returns:
+    int: La resta de los dos números.
+
+    Example:
+    >>> subtract_numbers(3, 2)
+    1
+    """
+    return a - b
+
+def split_numbers(a,b):
+    """
+    Divide dos números y devuelve el resultado.
+
+    Parameters:
+    a (int): El primer número a dividir.
+    b (int): El segundo número a dividir.
+
+    Returns:
+    int: La divicion de los dos números.
+
+    Example:
+    >>> split_numbers(4, 2)
+    2
+    """
+    return a / b 
+
+def multiply_numbers(a,b):
+    """
+    Multiplica dos números y devuelve el resultado.
+
+    Parameters:
+    a (int): El primer número a multiplicar.
+    b (int): El segundo número a multiplicar.
+
+    Returns:
+    int: La multiplicacion de los dos números.
+
+    Example:
+    >>> multiply_numbers(3, 2)
+    6
+    """
+    return a * b
