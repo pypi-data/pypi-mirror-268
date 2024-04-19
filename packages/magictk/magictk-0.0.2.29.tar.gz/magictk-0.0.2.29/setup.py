fbc601377acb45bd26794a6c3580984323de14ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 03:05:25 +0000

    Add package magictk/0.0.2.29/magictk-0.0.2.29.tar.gz

---
+++ packages/magictk/magictk-0.0.2.29.tar.gz/magictk-0.0.2.29/setup.py
@@ -0,0 +1,34 @@
+import sys
+import os
+from setuptools import setup, find_packages
+
+####### config #######
+VERSION = "0.0.2"
+######################
+
+set_v = VERSION
+if (os.path.exists("runid.conf")):
+    with open("runid.conf", "r") as file:
+        runid = file.read()
+    set_v = set_v+"."+runid
+else:
+    set_v = set_v+"."+"1000"
+
+print("BUILD: version="+set_v)
+
+setup(
+    name="magictk",
+    version=set_v,
+    packages=(
+        find_packages(where="./magictk")
+    ),
+    package_dir={
+        "magictk": "./magictk",
+    },
+    install_requires=["pywin32"],
+    author='cxykevin|git.hmtsai.cn',
+    author_email='cxykevin@yeah.net',
+    description='A tkinter weights looks like element-plus',
+    url='http://git.hmtsai.cn/cxykevin/magictk.git',
+    license='GPLv2',
+)
