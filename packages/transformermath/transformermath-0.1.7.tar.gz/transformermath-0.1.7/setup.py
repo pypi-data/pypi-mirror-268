2e0bd5e7ab3224e1ee7b55d24b2559ac09364011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 03:05:23 +0000

    Add package transformermath/0.1.7/transformermath-0.1.7.tar.gz

---
+++ packages/transformermath/transformermath-0.1.7.tar.gz/transformermath-0.1.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='transformermath',
+    version='0.1.7',  
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib==3.4.3',
+        'numpy==1.21.5',
+        'pandas==1.3.5',
+        'scikit-learn==1.0.2',
+        'plotly==5.6.0',
+        'umap-learn==0.5.3',
+        'torch==1.10.2',
+        'seaborn==0.11.2',
+        'scipy==1.7.3',
+    ],
+)
