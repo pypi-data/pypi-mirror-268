e55df79ff129a6570ddcea77e1c172344c319759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 15:04:50 +0000

    Add package redditharbor/0.2.3/redditharbor-0.2.3.tar.gz

---
+++ packages/redditharbor/redditharbor-0.2.3.tar.gz/redditharbor-0.2.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+  name = 'redditharbor',
+  packages = ['redditharbor'],   
+  version = '0.2.3',      
+  license='MIT',        
+  description = 'Effortlessly collect and store Reddit data in your database.',   
+  author = 'Nick S.H Oh',                   
+  author_email = 'research@socius.org',      
+  url = 'https://github.com/socius-org/RedditHarbor/',  
+  download_url = 'https://github.com/socius-org/RedditHarbor/archive/refs/tags/0.2.3.tar.gz', 
+  keywords = ['Reddit', 'Supabase', 'reddit-api', 'database', 'reddit-crawler', 'reddit-scraper'],
+  include_package_data=True,
+  install_requires=[
+          'praw == 7.7.1',
+          'supabase == 1.0.3', 
+          'rich == 13.4.2',
+          'python-dotenv == 1.0.0',
+          'presidio-analyzer == 2.2.351', 
+          'presidio-anonymizer == 2.2.351',
+          'pillow >= 10.2.0', 
+          'requests == 2.31.0'  
+      ],
+  extras_require={
+    'pii': ['spacy[en_core_web_lg]'],
+    }
+)
