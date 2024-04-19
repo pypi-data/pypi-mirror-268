8d8b74960d48244caf761e54202b1fe620be90dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:05:35 +0000

    Add package degann/1.0/degann-1.0.tar.gz

---
+++ packages/degann/degann-1.0.tar.gz/degann-1.0/degann/networks/optimizers.py
@@ -0,0 +1,23 @@
+from typing import Callable
+
+from tensorflow import keras
+
+_optimizers: dict = {
+    "Adam": keras.optimizers.Adam,
+    "AdamW": keras.optimizers.experimental.AdamW,
+    "Adamax": keras.optimizers.experimental.Adamax,
+    "Adadelta": keras.optimizers.experimental.Adadelta,
+    "Adagrad": keras.optimizers.experimental.Adagrad,
+    "Ftrl": keras.optimizers.experimental.Ftrl,
+    "Nadam": keras.optimizers.experimental.Nadam,
+    "RMSprop": keras.optimizers.experimental.RMSprop,
+    "SGD": keras.optimizers.experimental.SGD,
+}
+
+
+def get_optimizer(name: str):
+    return _optimizers.get(name)
+
+
+def get_all_optimizers() -> dict[str, Callable]:
+    return _optimizers
