7f034fcc4c7e535c75c2f22bae004f2c0ba3e869
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 06:05:06 +0000

    Add package chinmay-fine/0.1/chinmay_fine-0.1.tar.gz

---
+++ packages/chinmay-fine/chinmay_fine-0.1.tar.gz/chinmay_fine-0.1/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='chinmay_fine',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Chinmay',
+    author_email='your@email.com',
+    description='A Python library for interacting with the News API',
+    url='https://github.com/your_username/news-api-python',
+)
