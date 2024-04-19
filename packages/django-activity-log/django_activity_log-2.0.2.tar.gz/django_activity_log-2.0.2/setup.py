54074f4c68d7ec45bcb1329456d043015001837a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 03:05:17 +0000

    Add package django-activity-log/2.0.2/django_activity_log-2.0.2.tar.gz

---
+++ packages/django-activity-log/django_activity_log-2.0.2.tar.gz/django_activity_log-2.0.2/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+DESCRIPTION = open('README.md').read()
+setup(
+    name='django-activity-log',
+    version='2.0.2',
+    description='HTTP queries logger with flexible filters and ip block manager.',
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author='Hossein SayyedMousavi',
+    author_email='hossein.sayyedmousavi@gmail.com',
+    keywords=[
+        "django",
+        "database",
+        "user",
+        "activity log"
+    ],
+    packages=[
+        'activity_log',
+        'activity_log.migrations',
+    ],
+    url='https://github.com/HosseinSayyedMousavi',
+    license='MIT License',
+    readme="README.md",
+    install_requires=[
+        'django<5.0.0',
+        'pprintpp<1.0.0',
+    ],
+
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
