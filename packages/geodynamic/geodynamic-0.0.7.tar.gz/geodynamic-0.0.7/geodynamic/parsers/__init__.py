562fd50fe9ad792d722d90d7fd9aa67e5a837539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:05:22 +0000

    Add package geodynamic/0.0.7/geodynamic-0.0.7.tar.gz

---
+++ packages/geodynamic/geodynamic-0.0.7.tar.gz/geodynamic-0.0.7/geodynamic/parsers/__init__.py
@@ -0,0 +1,11 @@
+from manim import VMobject, VGroup
+from .svg_parser import *
+
+__all__ = ["create_svg_from_vgroup", "create_svg_from_vmobject"]
+
+VMobject.to_svg = create_svg_from_vmobject
+VGroup.to_svg = create_svg_from_vgroup
+
+# Set rich display for manim objects on Jupyter
+VMobject._repr_svg_ = lambda self: create_svg_from_vmobject(self).read_text()
+VGroup._repr_svg_ = lambda self: create_svg_from_vgroup(self).read_text()
