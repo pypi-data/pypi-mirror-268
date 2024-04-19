e227625ea9379636450ef1f392973cbc58d3bc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 05:05:17 +0000

    Add package roblib/2.0/roblib-2.0.tar.gz

---
+++ packages/roblib/roblib-2.0.tar.gz/roblib-2.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='roblib',
+      version='2.0',
+      description='Python Distribution Utilities',
+      author='Luc Jaulin',
+      author_email='lucjaulin@gmail.com ',
+      url='https://www.ensta-bretagne.fr/jaulin/',
+      packages=['roblib'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'matplotlib'
+      ],
+     )
