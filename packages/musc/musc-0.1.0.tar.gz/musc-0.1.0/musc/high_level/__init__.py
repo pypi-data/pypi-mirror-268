c6871b2b7462ec19a48091224e46e99cd525be2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 15:04:40 +0000

    Add package musc/0.1.0/musc-0.1.0.tar.gz

---
+++ packages/musc/musc-0.1.0.tar.gz/musc-0.1.0/musc/high_level/__init__.py
@@ -0,0 +1,44 @@
+from musc.evaluator.high_level import Evaluator
+from musc.hpo.distr.high_level import SingleValue, into_distr
+from musc.hpo.high_level.a import UpdateStrategySearch
+from musc.service.concepts.a import BaseModel, Metric
+from musc.service.concepts.high_level.a import (
+    UpdateStrategyByDriftDetection,
+    UpdateStrategyByPeriodicallyUpdate,
+    UpdateStrategyNone,
+)
+from musc.service.high_level import Service, run_service_http
+
+from . import drift_detectors
+
+__all__ = [
+    'BaseModel',
+    'Evaluator',
+    'Metric',
+    'Service',
+    'SingleValue',
+    'UpdateStrategyByDriftDetection',
+    'UpdateStrategyByPeriodicallyUpdate',
+    'UpdateStrategyNone',
+    'UpdateStrategySearch',
+    'drift_detectors',
+    'into_distr',
+    'run_service_http',
+]
+
+try:
+    import torch as _
+except ImportError:
+    pass
+else:
+    from musc.adaptors.models import ModelPyTorch
+    from musc.hpo.res_pool.torch_device import TorchDevicePool
+    __all__.extend(['ModelPyTorch', 'TorchDevicePool'])
+
+try:
+    import sklearn as _
+except ImportError:
+    pass
+else:
+    from musc.adaptors.models import ModelScikitLearn
+    __all__.extend(['ModelScikitLearn'])
