56f33a42892c03276b88c63b5a9bc1602ecd0b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:15 +0000

    Add package torch-npu/2.1.0.post3/torch_npu-2.1.0.post3-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/torch-npu/torch_npu-2.1.0.post3-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/torch_npu/dynamo/torchair/configs/experimental_config.py
@@ -0,0 +1,35 @@
+from torchair.configs.option_base import OptionValue, IntRangeValue
+from torchair.configs.option_base import NpuBaseConfig
+
+
+class ExperimentalConfig(NpuBaseConfig):
+    """Config for experimental"""
+
+    def __init__(self):
+        self.cc_parallel_enable = OptionValue(False, [False, True])
+        self.keep_inference_input_mutations = OptionValue(True, [True, False])
+        self.memory_efficiency = OptionValue(False, [True, False])
+        self.frozen_parameter = OptionValue(False, [True, False])
+        self.static_model_ops_lower_limit = IntRangeValue(None, -1, 9223372036854775807)
+        self.jit_compile = OptionValue("auto", ["auto"])
+        self.npu_fx_pass = OptionValue(False, [True, False])
+        self.enable_single_stream = OptionValue(False, [True, False])
+        self.topology_sorting_strategy = OptionValue("DFS", ["BFS", "DFS", "RDFS"])
+
+        super(ExperimentalConfig, self).__init__()
+
+    def as_dict(self):
+        global_experiment_option = {}
+        local_experiment_option = {}
+        sorting_strategy_dict = {"BFS": "0", "DFS": "1", "RDFS": "2"}
+
+        global_experiment_option["ge.exec.enableEngineParallel"] = "1" if self.cc_parallel_enable else "0"
+        global_experiment_option["ge.enableSingleStream"] = "true" if self.enable_single_stream else "false"
+        local_experiment_option["ge.featureBaseRefreshable"] = "1" if self.memory_efficiency else "0"
+        local_experiment_option["ge.topoSortingMode"] = sorting_strategy_dict[self.topology_sorting_strategy.value]
+        if self.jit_compile.value == "auto":
+            local_experiment_option["ge.jit_compile"] = "2"
+        if self.static_model_ops_lower_limit.value is not None:
+            local_experiment_option["ge.exec.static_model_ops_lower_limit"] = \
+                str(self.static_model_ops_lower_limit.value)
+        return local_experiment_option, global_experiment_option
---
+++ packages/torch-npu/torch_npu-2.1.0.post3-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/torch_npu/utils/unsupport_api.py
@@ -0,0 +1,27 @@
+import torch
+
+"""
+key: attr_name(str)
+value: parent_module(object)
+"""
+
+unsupported_Tensor_api = {
+    "is_shared": torch.Tensor,
+    "share_memory_": torch.Tensor
+}
+
+unsupported_nn_api = {
+    "__init__": torch.nn.DataParallel,
+    "data_parallel": torch.nn.parallel,
+    "share_memory": torch.nn.Module,
+    "add_module": torch.jit.ScriptModule,
+    "bfloat16": torch.jit.ScriptModule,
+    "register_buffer": torch.jit.ScriptModule,
+    "register_parameter": torch.jit.ScriptModule,
+    "register_module": torch.jit.ScriptModule
+}
+
+unsupported_nested_api = {
+    "nested_tensor": torch.nested,
+    "as_nested_tensor": torch.nested
+}
