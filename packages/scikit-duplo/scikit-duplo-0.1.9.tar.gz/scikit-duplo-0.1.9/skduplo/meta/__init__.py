70bb9e0f502e0308e14b4c64c35403aa29da67d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 07:04:41 +0000

    Add package scikit-duplo/0.1.9/scikit-duplo-0.1.9.tar.gz

---
+++ packages/scikit-duplo/scikit-duplo-0.1.9.tar.gz/scikit-duplo-0.1.9/skduplo/meta/__init__.py
@@ -0,0 +1,8 @@
+__all__ = [
+    "RegressorStack",
+    "QuantileStackRegressor",
+    "BaselineProportionalRegressor"
+]
+from .regressor_stack import RegressorStack
+from .quantile_stack_regressor import QuantileStackRegressor
+from .baseline_proportional_regressor import BaselineProportionalRegressor
