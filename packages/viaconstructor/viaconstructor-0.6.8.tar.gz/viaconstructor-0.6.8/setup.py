89461e6688f2da1c6306d3e7ba3f19dec63f977e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 06:05:19 +0000

    Add package viaconstructor/0.6.8/viaconstructor-0.6.8.tar.gz

---
+++ packages/viaconstructor/viaconstructor-0.6.8.tar.gz/viaconstructor-0.6.8/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+#
+# ls data/* | awk '{print "\""$1"\""}' | tr "\n" ","
+#
+
+import os
+from setuptools import setup
+
+
+setup(
+    name='viaconstructor',
+    version='0.6.8',
+    author='Oliver Dippel',
+    author_email='o.dippel@gmx.de',
+    packages=['viaconstructor', 'viaconstructor.ext.cavaliercontours', 'viaconstructor.ext.HersheyFonts', 'viaconstructor.ext.meshcut', 'viaconstructor.ext.stl', 'viaconstructor.ext.svgpathtools', 'viaconstructor.input_plugins', 'viaconstructor.output_plugins', 'viaconstructor.preview_plugins', 'viaconstructor.tools', 'gcodepreview', 'dxfpreview'],
+    package_data={'viaconstructor.ext.cavaliercontours': ['lib/libCavalierContours.x86_64-linux.so'], 'viaconstructor/ext/nest2D': ['nest2D.cpython-39-x86_64-linux-gnu.so']},
+    scripts=['bin/viaconstructor','bin/gcodepreview','bin/dxfpreview'],
+    url='https://github.com/multigcs/viaconstructor',
+    license='LICENSE',
+    description='python based cam-tool to convert dxf into gcode',
+    long_description=open('README.md').read(),
+    install_requires=["PyQt5", "ezdxf", "PyOpenGL", "Pillow", "pygame", "pyclipper", "setproctitle", "freetype-py", "python-utils", "svgwrite", "matplotlib", "numpy"],
+    include_package_data=True,
+)
+
