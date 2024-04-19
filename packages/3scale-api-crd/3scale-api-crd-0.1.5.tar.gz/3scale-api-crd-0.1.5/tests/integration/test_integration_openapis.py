1d1fa33c0ac3ad3ae8f4be76dfd69cf6cab88561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:05:55 +0000

    Add package 3scale-api-crd/0.1.5/3scale-api-crd-0.1.5.tar.gz

---
+++ packages/3scale-api-crd/3scale-api-crd-0.1.5.tar.gz/3scale-api-crd-0.1.5/tests/integration/test_integration_openapis.py
@@ -0,0 +1,35 @@
+import json
+
+from tests.integration import asserts
+from .asserts import assert_resource, assert_resource_params
+
+# tests important for CRD - CRU + list
+
+
+def test_openapis_list(api, openapi):
+    openapis = api.openapis.list()
+    assert len(openapis) >= 1
+
+
+def test_openapi_can_be_created(api, openapi_params, openapi):
+    assert_resource(openapi)
+    assert_resource_params(openapi, openapi_params)
+
+
+def test_openapi_can_be_read(api, openapi_params, openapi):
+    read = api.openapis.read(openapi.entity_id)
+    asserts.assert_resource(read)
+    asserts.assert_resource_params(read, openapi_params)
+
+
+def test_openapi_can_be_updated(api, openapi):
+    des_changed = (openapi["productSystemName"] or "") + "_changed"
+    openapi["productSystemName"] = des_changed
+    openapi.update()
+    assert openapi["productSystemName"] == des_changed
+    updated = openapi.read()
+    assert updated["productSystemName"] == des_changed
+    assert openapi["productSystemName"] == des_changed
+
+
+# end of tests important for CRD - CRU + list
