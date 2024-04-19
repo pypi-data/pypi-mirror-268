6b29832df773598b408dbb8b443fcf8230cc7bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:04:57 +0000

    Add package proxybandit/1.0.1/proxybandit-1.0.1-py3-none-any.whl

---
+++ packages/proxybandit/proxybandit-1.0.1-py3-none-any.whl/proxybandit/proxy.py
@@ -0,0 +1,50 @@
+class Proxy:
+    PRIVACY_TRANSPARENT = 0
+    PRIVACY_ANONYMOUS = 1
+    PRIVACY_ELITE = 2
+    TYPE_HTTP = 0
+    TYPE_HTTPS = 1
+    TYPE_SOCKS5 = 2
+    ATTRIBUTES = ["host", "port", "type", "privacy", "speed", "origin"]
+
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        type: int = TYPE_HTTP,
+        privacy: int = PRIVACY_TRANSPARENT,
+        speed: float = 1.0,
+        origin=None,
+    ):
+        self.host = host
+        self.port = port
+        self.type = type
+        self.privacy = privacy
+        self.speed = speed
+        self.origin = origin
+
+    def __lt__(self, other):
+        return str(self) < other
+
+    def __gt__(self, other):
+        return str(self) > other
+
+    def __str__(self):
+        prefix = "http://"
+        if self.type == Proxy.TYPE_HTTPS:
+            prefix = "https://"
+        elif self.type == Proxy.TYPE_SOCKS5:
+            prefix = "socks5://"
+        return f"{prefix}{self.host}:{self.port}"
+
+    def to_dict(self) -> dict[str, str]:
+        proxy_dict = {"http": f"http://{self.host}:{self.port}"}
+        if self.type == Proxy.TYPE_SOCKS5:
+            proxy_dict["http"] = str(self)
+            proxy_dict["https"] = str(self)
+        if self.type == Proxy.TYPE_HTTPS:
+            proxy_dict["https"] = f"http://{self.host}:{self.port}"
+        return proxy_dict
+
+    def to_url(self) -> str:
+        return f"http://{self}"
---
+++ packages/proxybandit/proxybandit-1.0.1-py3-none-any.whl/proxybandit/proxylist.py
@@ -0,0 +1,43 @@
+import csv
+
+from .proxy import Proxy
+
+
+class ProxyList:
+    def __init__(self, proxy_list: list[Proxy]):
+        self._index = 0
+        self._list = proxy_list
+
+    def __iter__(self):
+        return iter(self._list)
+
+    def __getitem__(self, item: int) -> Proxy:
+        return self._list[item]
+
+    def __len__(self) -> int:
+        return len(self._list)
+
+    def to_csv(self, path):
+        with open(path, "w", newline="") as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=",")
+            for proxy in self._list:
+                csv_writer.writerow(
+                    [
+                        proxy.type,
+                        proxy.host,
+                        proxy.port,
+                        proxy.privacy,
+                        proxy.origin,
+                        proxy.speed,
+                    ]
+                )
+
+    def add(self, proxy: Proxy) -> None:
+        self.list.append(proxy)
+
+    def remove(self, id: int) -> Proxy:
+        pass
+
+    def clean(self) -> None:
+        # Will be used to remove proxies that no longer work
+        pass
