fa734ed43669ace3cdcce2fb58469bc9188bf304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 14:04:38 +0000

    Add package langchain-ibm-watson/0.0.1/langchain_ibm_watson-0.0.1.tar.gz

---
+++ packages/langchain-ibm-watson/langchain_ibm_watson-0.0.1.tar.gz/langchain_ibm_watson-0.0.1/README.md
@@ -0,0 +1,11 @@
+# langchain-ibm-watson
+
+This package contains the LangChain integrations for IBM Watson.
+
+
+
+## Installation
+
+```bash
+pip install langchain-ibm-watson
+```
---
+++ packages/langchain-ibm-watson/langchain_ibm_watson-0.0.1.tar.gz/langchain_ibm_watson-0.0.1/langchain_ibm_watson/document_transformers.py
@@ -0,0 +1,11 @@
+from langchain_core.documents import BaseDocumentTransformer
+
+
+class WatsonLanguageTranslatorTransformer(BaseDocumentTransformer):
+    """Translate text documents using IBM Watson Language Translator.
+
+    References:
+        [1] https://cloud.ibm.com/apidocs/language-translator?code=python
+    """
+
+    pass
