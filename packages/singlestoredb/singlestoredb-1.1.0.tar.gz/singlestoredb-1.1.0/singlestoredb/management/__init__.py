429a4f7a9c6193253d41e6175a42007cae22a050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:05:22 +0000

    Add package singlestoredb/1.1.0/singlestoredb-1.1.0.tar.gz

---
+++ packages/singlestoredb/singlestoredb-1.1.0.tar.gz/singlestoredb-1.1.0/singlestoredb/management/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from .cluster import manage_cluster
+from .manager import get_organization
+from .manager import get_token
+from .workspace import get_secret
+from .workspace import manage_workspaces
