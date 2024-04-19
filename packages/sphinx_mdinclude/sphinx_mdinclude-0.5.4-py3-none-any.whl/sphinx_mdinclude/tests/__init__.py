91320b6c92a6fc6a25e7c1b2acdd0b2f0d92b51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 15:05:00 +0000

    Add package sphinx_mdinclude/0.5.4/sphinx_mdinclude-0.5.4-py3-none-any.whl

---
+++ packages/sphinx_mdinclude/sphinx_mdinclude-0.5.4-py3-none-any.whl/sphinx_mdinclude/tests/__init__.py
@@ -0,0 +1,15 @@
+from .test_renderer import (
+    TestBasic,
+    TestBlockQuote,
+    TestCodeBlock,
+    TestConplexText,
+    TestDirective,
+    TestFootNote,
+    TestHeading,
+    TestImage,
+    TestInlineMarkdown,
+    TestList,
+    TestRestCode,
+    TestTable,
+)
+from .test_smoke import SmokeTest
