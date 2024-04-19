3264b83718d05a25dedd55f26f39512de06ec875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:05:11 +0000

    Add package custodian/2024.4.18/custodian-2024.4.18.tar.gz

---
+++ packages/custodian/custodian-2024.4.18.tar.gz/custodian-2024.4.18/custodian/__init__.py
@@ -0,0 +1,18 @@
+"""
+Custodian is a simple, robust and flexible just-in-time (JIT) job management
+framework written in Python.
+"""
+
+import os
+
+from .custodian import Custodian
+
+__author__ = (
+    "Shyue Ping Ong, William Davidson Richards, Stephen Dacek, Xiaohui Qu, Matthew Horton, "
+    "Samuel M. Blau, Janosh Riebesell"
+)
+__version__ = "2024.4.18"
+
+
+PKG_DIR = os.path.dirname(__file__)
+ROOT = os.path.dirname(PKG_DIR)
