17d3fe209a6c09abb213e1eaed4f3168a116f216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 03:04:41 +0000

    Add package einhoorntje-llm-lib/0.2/einhoorntje_llm_lib-0.2.tar.gz

---
+++ packages/einhoorntje-llm-lib/einhoorntje_llm_lib-0.2.tar.gz/einhoorntje_llm_lib-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='einhoorntje_llm_lib',
+    version='0.2',
+    description='A brief description of my library',
+    author='Dmitrii Lukianov',
+    author_email='unicornporated@gmil.com',
+    py_modules=['einhoorntje_llm_lib'],
+    install_requires=["openai"],
+)
