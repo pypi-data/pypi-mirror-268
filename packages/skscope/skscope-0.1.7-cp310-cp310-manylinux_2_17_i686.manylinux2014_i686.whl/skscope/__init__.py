52c0198117c6de10d8d0afd5fd450317c4e5d9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 05:05:31 +0000

    Add package skscope/0.1.7/skscope-0.1.7-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/skscope/skscope-0.1.7-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl/skscope/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: Zezhi Wang
+# Copyright (C) 2023 abess-team
+# Licensed under the MIT License.
+
+__version__ = "0.1.7"
+__author__ = "Zezhi Wang, Jin Zhu," "Peng Chen," "Junxian Zhu, Xueqin Wang"
+
+
+from .solver import (
+    ScopeSolver,
+    HTPSolver,
+    GraspSolver,
+    IHTSolver,
+    FobaSolver,
+    ForwardSolver,
+    OMPSolver,
+)
+from .base_solver import BaseSolver
+from .numeric_solver import convex_solver_nlopt
+from .skmodel import PortfolioSelection, NonlinearSelection, RobustRegression
+
+__all__ = [
+    "ScopeSolver",
+    "HTPSolver",
+    "GraspSolver",
+    "IHTSolver",
+    "BaseSolver",
+    "FobaSolver",
+    "ForwardSolver",
+    "OMPSolver",
+    "convex_solver_nlopt",
+    "PortfolioSelection",
+    "NonlinearSelection",
+    "RobustRegression",
+]
