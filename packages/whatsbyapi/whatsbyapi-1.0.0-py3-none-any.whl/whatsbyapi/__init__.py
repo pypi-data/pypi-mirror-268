a74d179e4dc505d126d73caf68ac1ffe6256350b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 05:05:26 +0000

    Add package whatsbyapi/1.0.0/whatsbyapi-1.0.0-py3-none-any.whl

---
+++ packages/whatsbyapi/whatsbyapi-1.0.0-py3-none-any.whl/whatsbyapi/__init__.py
@@ -0,0 +1,11 @@
+"""
+Python wrapper for the WhatsApp Cloud API .
+    - GitHub: https://github.com/abdelhak-zaaim/whatsbyapi
+"""
+
+from whatsbyapi.client import WhatsApp
+from whatsbyapi.utils import Version
+
+__version__ = "1.0.0"
+__author__ = "Abdelhak Zaaim"
+__license__ = "MIT"
