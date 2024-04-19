4da54e1fc2215cb6083bba7e01c346d6206c6d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:06:03 +0000

    Add package premai/0.3.40/premai-0.3.40.tar.gz

---
+++ packages/premai/premai-0.3.40.tar.gz/premai-0.3.40/premai/models/__init__.py
@@ -0,0 +1,148 @@
+""" Contains all the data models used in inputs/outputs """
+
+from .api_response_validation_error import APIResponseValidationError
+from .api_response_validation_error_code_enum import APIResponseValidationErrorCodeEnum
+from .authentication_error import AuthenticationError
+from .authentication_error_code_enum import AuthenticationErrorCodeEnum
+from .blank_enum import BlankEnum
+from .catch_all_error import CatchAllError
+from .catch_all_error_code_enum import CatchAllErrorCodeEnum
+from .chat_completion_input import ChatCompletionInput, ChatCompletionInputDict
+from .chat_completion_input_logit_bias_type_0 import ChatCompletionInputLogitBiasType0
+from .chat_completion_input_response_format_type_0 import ChatCompletionInputResponseFormatType0
+from .chat_completion_input_tools_item import ChatCompletionInputToolsItem
+from .chat_completion_response import ChatCompletionResponse
+from .conflict_error import ConflictError
+from .conflict_error_code_enum import ConflictErrorCodeEnum
+from .data_point import DataPoint, DataPointDict
+from .document_chunks import DocumentChunks
+from .document_input import DocumentInput, DocumentInputDict
+from .document_output import DocumentOutput
+from .document_type_enum import DocumentTypeEnum
+from .embedding import Embedding
+from .embeddings_input import EmbeddingsInput, EmbeddingsInputDict
+from .embeddings_response import EmbeddingsResponse
+from .encoding_format_enum import EncodingFormatEnum
+from .enhancement import Enhancement
+from .fine_tuning_input import FineTuningInput, FineTuningInputDict
+from .fine_tuning_response import FineTuningResponse
+from .fine_tuning_sample import FineTuningSample
+from .input_data_point import InputDataPoint, InputDataPointDict
+from .message import Message
+from .model import Model
+from .model_not_found_error import ModelNotFoundError
+from .model_not_found_error_code_enum import ModelNotFoundErrorCodeEnum
+from .model_provider_enum import ModelProviderEnum
+from .model_type_enum import ModelTypeEnum
+from .patched_data_point import PatchedDataPoint, PatchedDataPointDict
+from .permission_denied_error import PermissionDeniedError
+from .permission_denied_error_code_enum import PermissionDeniedErrorCodeEnum
+from .provider_api_connection_error import ProviderAPIConnectionError
+from .provider_api_connection_error_code_enum import ProviderAPIConnectionErrorCodeEnum
+from .provider_api_status_error import ProviderAPIStatusError
+from .provider_api_status_error_code_enum import ProviderAPIStatusErrorCodeEnum
+from .provider_api_timeout_error import ProviderAPITimeoutError
+from .provider_api_timeout_error_code_enum import ProviderAPITimeoutErrorCodeEnum
+from .provider_internal_server_error import ProviderInternalServerError
+from .provider_internal_server_error_code_enum import ProviderInternalServerErrorCodeEnum
+from .provider_not_found_error import ProviderNotFoundError
+from .provider_not_found_error_code_enum import ProviderNotFoundErrorCodeEnum
+from .rate_limit_error import RateLimitError
+from .rate_limit_error_code_enum import RateLimitErrorCodeEnum
+from .response_choice import ResponseChoice
+from .retrieve_fine_tuning_response import RetrieveFineTuningResponse
+from .role_enum import RoleEnum
+from .status_enum import StatusEnum
+from .unprocessable_entity_error import UnprocessableEntityError
+from .unprocessable_entity_error_code_enum import UnprocessableEntityErrorCodeEnum
+from .usage import Usage
+from .validation_detail import ValidationDetail
+from .validation_detail_error_messages_item import ValidationDetailErrorMessagesItem
+from .validation_error import ValidationError
+from .validation_error_code_enum import ValidationErrorCodeEnum
+from .validation_error_details import ValidationErrorDetails
+
+__all__ = (
+    "APIResponseValidationError",
+    "APIResponseValidationErrorCodeEnum",
+    "AuthenticationError",
+    "AuthenticationErrorCodeEnum",
+    "BlankEnum",
+    "CatchAllError",
+    "CatchAllErrorCodeEnum",
+    "ChatCompletionInput",
+    "ChatCompletionInputLogitBiasType0",
+    "ChatCompletionInputResponseFormatType0",
+    "ChatCompletionInputToolsItem",
+    "ChatCompletionResponse",
+    "ConflictError",
+    "ConflictErrorCodeEnum",
+    "DataPoint",
+    "DocumentChunks",
+    "DocumentInput",
+    "DocumentOutput",
+    "DocumentTypeEnum",
+    "Embedding",
+    "EmbeddingsInput",
+    "EmbeddingsResponse",
+    "EncodingFormatEnum",
+    "Enhancement",
+    "FineTuningInput",
+    "FineTuningResponse",
+    "FineTuningSample",
+    "InputDataPoint",
+    "Message",
+    "Model",
+    "ModelNotFoundError",
+    "ModelNotFoundErrorCodeEnum",
+    "ModelProviderEnum",
+    "ModelTypeEnum",
+    "PatchedDataPoint",
+    "PermissionDeniedError",
+    "PermissionDeniedErrorCodeEnum",
+    "ProviderAPIConnectionError",
+    "ProviderAPIConnectionErrorCodeEnum",
+    "ProviderAPIStatusError",
+    "ProviderAPIStatusErrorCodeEnum",
+    "ProviderAPITimeoutError",
+    "ProviderAPITimeoutErrorCodeEnum",
+    "ProviderInternalServerError",
+    "ProviderInternalServerErrorCodeEnum",
+    "ProviderNotFoundError",
+    "ProviderNotFoundErrorCodeEnum",
+    "RateLimitError",
+    "RateLimitErrorCodeEnum",
+    "ResponseChoice",
+    "RetrieveFineTuningResponse",
+    "RoleEnum",
+    "StatusEnum",
+    "UnprocessableEntityError",
+    "UnprocessableEntityErrorCodeEnum",
+    "Usage",
+    "ValidationDetail",
+    "ValidationDetailErrorMessagesItem",
+    "ValidationError",
+    "ValidationErrorCodeEnum",
+    "ValidationErrorDetails",
+    "ChatCompletionInputDict",
+    "ChatCompletionInputDict",
+    "ChatCompletionInputDict",
+    "InputDataPointDict",
+    "InputDataPointDict",
+    "InputDataPointDict",
+    "DataPointDict",
+    "DataPointDict",
+    "DataPointDict",
+    "PatchedDataPointDict",
+    "PatchedDataPointDict",
+    "PatchedDataPointDict",
+    "EmbeddingsInputDict",
+    "EmbeddingsInputDict",
+    "EmbeddingsInputDict",
+    "FineTuningInputDict",
+    "FineTuningInputDict",
+    "FineTuningInputDict",
+    "DocumentInputDict",
+    "DocumentInputDict",
+    "DocumentInputDict",
+)
