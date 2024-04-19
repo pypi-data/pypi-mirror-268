45d11fbad9cbd22cbb6279d44763ca6937d864df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 05:05:58 +0000

    Add package pypjsekai/0.0.11/pypjsekai-0.0.11-py3-none-any.whl

---
+++ packages/pypjsekai/pypjsekai-0.0.11-py3-none-any.whl/pjsekai/enums/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2022-present Erik Chan <erikchan002@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .enums import *
+from .information import *
+from .platform import *
+from .tutorial_status import *
+from .unknown import *
