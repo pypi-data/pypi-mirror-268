e08e5e39acdd5e661d6c4c4b1e7668e7e6f13b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:16 +0000

    Add package PyDropper/0.1.2/pydropper-0.1.2-py3-none-any.whl

---
+++ packages/PyDropper/pydropper-0.1.2-py3-none-any.whl/pydropper/main.py
@@ -0,0 +1,29 @@
+from .server import Server, check_server
+import asyncio
+
+
+async def main(server):
+    try:
+        while True:
+            command = input("Enter 'exit' to stop the server. \n")
+            if command == "exit":
+                server.stop()
+                break
+    finally:
+        server.join()
+
+    print("server really stopped")
+
+
+async def run():
+    server = Server()
+    server.start()
+    check_server()
+    await main(server)
+
+
+if __name__ == "__main__":
+    server = Server()
+    server.start()
+    check_server()
+    asyncio.run(run())
