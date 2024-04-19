cbc6090cd635002e010697c20d7aa117063347df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 05:04:55 +0000

    Add package cellworld-game/0.0.16/cellworld_game-0.0.16.tar.gz

---
+++ packages/cellworld-game/cellworld_game-0.0.16.tar.gz/cellworld_game-0.0.16/cellworld_game/resources.py
@@ -0,0 +1,11 @@
+from importlib import resources
+import os
+import pkgutil
+
+
+class Resources(object):
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    @staticmethod
+    def file(file_name: str) -> str:
+        return os.path.join(Resources.path, "files", file_name)
