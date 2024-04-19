32519cc5385be56eb1c9d07d72d2b1afad1e1a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 03:05:08 +0000

    Add package mmisp-lib/0.1.0/mmisp_lib-0.1.0.tar.gz

---
+++ packages/mmisp-lib/mmisp_lib-0.1.0.tar.gz/mmisp_lib-0.1.0/src/mmisp/db/config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+from dataclasses import dataclass
+
+@dataclass
+class DatabaseConfig:
+    DATABASE_URL: str
+
+
+load_dotenv(getenv("ENV_FILE", ".env"))
+
+
+config: DatabaseConfig = DatabaseConfig(
+    DATABASE_URL=getenv("DATABASE_URL", "")
+)
