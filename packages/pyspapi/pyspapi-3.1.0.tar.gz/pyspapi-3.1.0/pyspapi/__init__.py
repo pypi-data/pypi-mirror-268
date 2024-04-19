b4193c20bdc30ff5a4210a691ce20f8fd5b16655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 05:05:18 +0000

    Add package pyspapi/3.1.0/pyspapi-3.1.0.tar.gz

---
+++ packages/pyspapi/pyspapi-3.1.0.tar.gz/pyspapi-3.1.0/pyspapi/__init__.py
@@ -0,0 +1,8 @@
+from .spworlds import *
+
+
+__author__ = 'deesiigneer'
+__url__ = 'https://github.com/deesiigneer/pyspapi'
+__description__ = 'API wrapper for SP servers written in Python.'
+__license__ = 'MIT'
+__version__ = "3.1.0"
