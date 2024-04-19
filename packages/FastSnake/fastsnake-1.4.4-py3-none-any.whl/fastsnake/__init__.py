586da020e0ab43e533773336f883498805edd725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 06:05:10 +0000

    Add package FastSnake/1.4.4/fastsnake-1.4.4-py3-none-any.whl

---
+++ packages/FastSnake/fastsnake-1.4.4-py3-none-any.whl/fastsnake/__init__.py
@@ -0,0 +1,16 @@
+__doc__ = """
+A Python Helper CLI for Competitive Programming
+"""
+
+__author__ = "Jean Loui Bernard Silva de Jesus"
+__github__ = "https://github.com/JeanExtreme002"
+__version__ = "1.4.4"
+
+__credits__ = f"""
+ Author: {__author__}
+ GitHub: {__github__}
+ Linktr.ee: https://linktr.ee/jeanlouibernard
+ \n"""
+
+from . import algorithms
+from . import util
