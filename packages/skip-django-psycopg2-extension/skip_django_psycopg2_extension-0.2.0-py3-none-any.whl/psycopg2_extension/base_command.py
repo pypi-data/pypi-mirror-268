8b93435ddafb3246df63919ded59b96bad4e1ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:05:37 +0000

    Add package skip-django-psycopg2-extension/0.2.0/skip_django_psycopg2_extension-0.2.0-py3-none-any.whl

---
+++ packages/skip-django-psycopg2-extension/skip_django_psycopg2_extension-0.2.0-py3-none-any.whl/psycopg2_extension/base_command.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connections
+
+
+class PsycopgBaseCommand(BaseCommand):
+    help = 'Clean database (vacuum full and reindex)'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--database',
+            dest='database',
+            help='Database name in django settings',
+            default='default'
+        )
+
+    def _get_connection(self, alias):
+        connection = connections[alias]
+        if connection.settings_dict['ENGINE'] not in {'django.db.backends.postgresql',
+                                                      'django.contrib.gis.db.backends.postgis',
+                                                      'django_iam_dbauth.aws.postgis'}:
+            raise CommandError('Unsupported database backend!')
+        return connection
