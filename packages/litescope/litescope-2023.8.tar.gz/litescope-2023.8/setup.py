c8ff2d1142afa72532e29a345aaedc484c46213b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 06:05:06 +0000

    Add package litescope/2023.8/litescope-2023.8.tar.gz

---
+++ packages/litescope/litescope-2023.8.tar.gz/litescope-2023.8/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fp:
+    long_description = fp.read()
+
+setup(
+    name="litescope",
+    description="Small footprint and configurable embedded FPGA logic analyzer core",
+    long_description              = long_description,
+    long_description_content_type = "text/markdown",
+    version="2023.08",
+    author="Florent Kermarrec",
+    author_email="florent@enjoy-digital.fr",
+    url="http://enjoy-digital.fr",
+    download_url="https://github.com/enjoy-digital/litescope",
+    test_suite="test",
+    license="BSD",
+    python_requires="~=3.6",
+    packages=find_packages(exclude=("test*", "sim*", "doc*", "examples*")),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "litescope_cli=litescope.software.litescope_cli:main",
+        ],
+    },
+)
