187c3976de3fc713e28b46cea5474c539e54f9ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:19 +0000

    Add package dataidea/0.2.1/dataidea-0.2.1.tar.gz

---
+++ packages/dataidea/dataidea-0.2.1.tar.gz/dataidea-0.2.1/dataidea/packages.py
@@ -0,0 +1,6 @@
+# most used packages
+import scipy as sp
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
---
+++ packages/dataidea/dataidea-0.2.1.tar.gz/dataidea-0.2.1/dataidea/tabular.py
@@ -0,0 +1,25 @@
+# most used packages
+import scipy as sp
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+from .datasets import load
+# saving and loading the models
+from joblib import load as load_model
+from joblib import dump as save_model
+# feature selection
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import f_regression
+from sklearn.feature_selection import RFE
+# model selection
+from sklearn.model_selection import train_test_split
+# models
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
