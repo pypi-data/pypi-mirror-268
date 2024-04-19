e85abec527def5921e357596e733aab759067aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 15:05:00 +0000

    Add package NeelKrypt/7/NeelKrypt-7.tar.gz

---
+++ packages/NeelKrypt/NeelKrypt-7.tar.gz/NeelKrypt-7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '7' 
+DESCRIPTION = 'Text Enkryptor and Dekryptor using PKI'
+LONG_DESCRIPTION = 'USES DIFFIE HELLMAN @ CORE . KEY HANDLING AND CONVERSION IS THE ART HERE'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="NeelKrypt", 
+        version=VERSION,
+        author="Neelanjan Manna",
+        author_email="nm3897mel@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['pycryptodome'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'Diifie Pager', 'Auguste Kerckhoffs'],
+        classifiers= [
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
