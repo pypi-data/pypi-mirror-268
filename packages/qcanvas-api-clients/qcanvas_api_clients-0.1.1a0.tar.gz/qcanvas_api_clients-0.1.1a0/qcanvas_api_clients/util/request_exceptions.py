e9692003ebccef030ac5b49b5d834f2a42d4fe52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 16:05:12 +0000

    Add package qcanvas-api-clients/0.1.1a0/qcanvas_api_clients-0.1.1a0.tar.gz

---
+++ packages/qcanvas-api-clients/qcanvas_api_clients-0.1.1a0.tar.gz/qcanvas_api_clients-0.1.1a0/qcanvas_api_clients/util/request_exceptions.py
@@ -0,0 +1,11 @@
+class RatelimitedError(Exception):
+    pass
+
+
+class UnauthenticatedError(Exception):
+    pass
+
+
+class AuthenticationFailedError(Exception):
+    def __init__(self, msg: str):
+        super().__init__(msg)
