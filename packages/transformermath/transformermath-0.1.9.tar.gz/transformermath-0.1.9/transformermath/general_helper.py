05bfce27721b48c20675df5fbaa59b6b8865c973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 03:05:21 +0000

    Add package transformermath/0.1.9/transformermath-0.1.9.tar.gz

---
+++ packages/transformermath/transformermath-0.1.9.tar.gz/transformermath-0.1.9/transformermath/general_helper.py
@@ -0,0 +1,31 @@
+import sys
+import torch
+from transformermath.run_device import RunDevice
+from typing import List, Tuple
+
+class GeneralHelper:
+    def __init__(self) -> None:
+        self.device = RunDevice().get_device()
+
+    def load_model(self, model_path:str):
+        model = torch.load(model_path)
+        model.eval()
+        return model
+    
+    def tensor_data(self, input_data: List, labels: List) -> Tuple:
+        TX = torch.tensor(input_data, dtype=torch.long).to(self.device)
+        TY = torch.tensor(labels, dtype=torch.long).to(self.device)
+        return TX, TY
+    
+    def decode_examples(self, examples, replacer):
+        decoded_examples = []
+        inv_token_encodings = {v: k for k, v in replacer.items()}
+        for example in examples:
+            decoded_example = [inv_token_encodings.get(token, '') for token in example]
+            decoded_examples.append(decoded_example)
+        return decoded_examples
+    
+    def decode_labels(self, labels, replacer):
+        inv_token_encodings = {v: k for k, v in replacer.items()}
+        decoded_labels = [inv_token_encodings.get(label, '') for label in labels]
+        return decoded_labels
