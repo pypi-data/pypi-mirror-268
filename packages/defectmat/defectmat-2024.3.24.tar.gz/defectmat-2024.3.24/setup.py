0631f46df94fcbd94fc2cd2bde3a4dd8b34907c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 15:05:12 +0000

    Add package defectmat/2024.3.24/defectmat-2024.3.24.tar.gz

---
+++ packages/defectmat/defectmat-2024.3.24.tar.gz/defectmat-2024.3.24/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="defectmat",
+    version="2024.3.24",
+    author="Kamal Choudhary, Daniel Wines",
+    author_email="kamal.choudhary@nist.gov",
+    description="defctmat",
+    install_requires=[
+        "numpy>=1.22.0",
+        "scipy>=1.6.3",
+        "jarvis-tools>=2021.07.19",
+        "pydantic_settings",
+        # "alignn",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/usnistgov/defectmat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
