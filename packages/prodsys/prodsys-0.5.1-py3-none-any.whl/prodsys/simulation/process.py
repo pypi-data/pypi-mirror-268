d7d6b4197249044c22919988656a11da12968a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 04:05:44 +0000

    Add package prodsys/0.5.1/prodsys-0.5.1-py3-none-any.whl

---
+++ packages/prodsys/prodsys-0.5.1-py3-none-any.whl/prodsys/simulation/process.py
@@ -0,0 +1,259 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Union, List, Optional
+
+from pydantic import BaseModel
+
+from prodsys.simulation import time_model, request
+from prodsys.models import processes_data
+
+
+class Process(ABC, BaseModel):
+    """
+    Abstract process base class that defines the interface for all processes.
+    """
+
+    process_data: processes_data.PROCESS_DATA_UNION
+    time_model: Optional[time_model.TimeModel]
+
+    @abstractmethod
+    def matches_request(self, request: request.Request) -> bool:
+        """
+        Returns True if the process matches the request.
+
+        Args:
+            request (request.Request): The request.
+
+        Returns:
+            bool: True if the process matches the request.
+        """
+        pass
+
+    @abstractmethod
+    def get_process_time(self, *args) -> float:
+        """
+        Returns the time it takes to execute the process.
+
+        Returns:
+            float: Time it takes to execute the process.
+        """
+        pass
+
+    @abstractmethod
+    def get_expected_process_time(self, *args) -> float:
+        """
+        Returns the expected time it takes to execute the process.
+
+        Returns:
+            float: Expected time it takes to execute the process.
+        """
+        pass
+
+
+class ProductionProcess(Process):
+    """Class that represents a production process.
+
+    Args:
+        process_data (processes_data.ProductionProcessData): The process data.
+        time_model (time_model.TimeModel): The time model.
+    """
+
+    process_data: processes_data.ProductionProcessData
+
+    def matches_request(self, request: request.Request) -> bool:
+        requested_process = request.process
+        if not isinstance(requested_process, ProductionProcess) and not isinstance(
+            requested_process, CompoundProcess
+        ):
+            return False
+        if isinstance(requested_process, CompoundProcess):
+            return self.process_data.ID in requested_process.process_data.process_ids
+        return requested_process.process_data.ID == self.process_data.ID
+
+    def get_process_time(self) -> float:
+        return self.time_model.get_next_time()
+
+    def get_expected_process_time(self) -> float:
+        return self.time_model.get_expected_time()
+
+
+class CapabilityProcess(Process):
+    """
+    Class that represents a capability process.
+
+    Args:
+        process_data (processes_data.CapabilityProcessData): The process data.
+        time_model (time_model.TimeModel): The time model.
+    """
+    process_data: processes_data.CapabilityProcessData
+
+    def matches_request(self, request: request.Request) -> bool:
+        requested_process = request.process
+        if (
+            not isinstance(requested_process, CapabilityProcess)
+            and not isinstance(requested_process, CompoundProcess)
+            and not isinstance(requested_process, RequiredCapabilityProcess)
+        ):
+            return False
+        if isinstance(requested_process, CompoundProcess):
+            return self.process_data.capability in [
+                p.process_data.capability
+                for p in requested_process.contained_processes_data
+                if isinstance(p, CapabilityProcess)
+            ]
+        return requested_process.process_data.capability == self.process_data.capability
+
+    def get_process_time(self) -> float:
+        return self.time_model.get_next_time()
+
+    def get_expected_process_time(self) -> float:
+        return self.time_model.get_expected_time()
+
+
+class TransportProcess(Process):
+    """
+    Class that represents a transport process.
+
+    Args:
+        process_data (processes_data.TransportProcessData): The process data.
+        time_model (time_model.TimeModel): The time model.
+    """
+    process_data: processes_data.TransportProcessData
+
+    def matches_request(self, request: request.Request) -> bool:
+        requested_process = request.process
+        if not isinstance(requested_process, TransportProcess) and not isinstance(
+            requested_process, CompoundProcess
+        ):
+            return False
+        if isinstance(requested_process, TransportProcess):
+            return requested_process.process_data.ID == self.process_data.ID
+        return self.process_data.ID in requested_process.process_data.process_ids
+
+    def get_process_time(self, origin: List[float], target: List[float]) -> float:
+        return self.time_model.get_next_time(origin=origin, target=target)
+
+    def get_expected_process_time(self, *args) -> float:
+        return self.time_model.get_expected_time(*args)
+
+
+class CompoundProcess(Process):
+    """
+    Class that represents a compound process.
+
+    Args:
+        process_data (processes_data.CompoundProcessData): The process data.
+        processes (List[Union[ProductionProcess, TransportProcess, CapabilityProcess, RequiredCapabilityProcess]]): The processes.
+    """
+    process_data: processes_data.CompoundProcessData
+    contained_processes_data: List[
+        Union[
+            processes_data.ProductionProcessData,
+            processes_data.TransportProcessData,
+            processes_data.CapabilityProcessData,
+            processes_data.RequiredCapabilityProcessData,
+        ]
+    ]
+
+    def matches_request(self, request: request.Request) -> bool:
+        requested_process = request.process
+        if isinstance(requested_process, ProductionProcess) or isinstance(
+            requested_process, TransportProcess
+        ):
+            return requested_process.process_data.ID in self.process_data.process_ids
+        elif isinstance(requested_process, CapabilityProcess) or isinstance(
+            requested_process, RequiredCapabilityProcess
+        ):
+            return requested_process.process_data.capability in [
+                p.process_data.capability
+                for p in self.contained_processes_data
+                if isinstance(p, CapabilityProcess)
+            ]
+        elif isinstance(requested_process, CompoundProcess):
+            return any(
+                p.ID in self.process_data.process_ids
+                for p in requested_process.contained_processes_data
+                if isinstance(p, ProductionProcess)
+            ) or any(
+                p.capability
+                in [
+                    p.process_data.capability
+                    for p in self.contained_processes_data
+                    if isinstance(p, CapabilityProcess)
+                ]
+                for p in requested_process.contained_processes_data
+                if isinstance(p, CapabilityProcess)
+            )
+
+    def get_process_time(self) -> float:
+        raise NotImplementedError("CompoundProcess does not have a process time.")
+
+    def get_expected_process_time(self) -> float:
+        raise NotImplementedError("CompoundProcess does not have a process time.")
+
+
+class RequiredCapabilityProcess(Process):
+    """
+    Class that represents a required capability process.
+
+    Args:
+        process_data (processes_data.RequiredCapabilityProcessData): The process data.
+        time_model (time_model.TimeModel): The time model.
+    """
+
+    process_data: processes_data.RequiredCapabilityProcessData
+
+    def matches_request(self, request: request.Request) -> bool:
+        raise NotImplementedError(
+            "RequiredCapabilityProcess cannot be matched but should only request."
+        )
+
+    def get_process_time(self) -> float:
+        raise NotImplementedError(
+            "RequiredCapabilityProcess does not have a process time."
+        )
+
+    def get_expected_process_time(self) -> float:
+        raise NotImplementedError(
+            "RequiredCapabilityProcess does not have a process time."
+        )
+    
+
+class TransportLinkProcess(Process):
+    """
+    Class that represents a transport link process.
+    """
+
+    # TODO: Implement LinkTransportProcess and RouteTransportProcess and their associatd data models.
+
+    def matches_request(self, request: request.Request) -> bool:
+        # 1. check if request is a transport request (if not, return False)
+        # 2. check if transport request is a link request (if not, return False)
+
+        # 3. check for compatibility -> transport links can links from origin to target of resquest
+        # path: List[Links] = path_finder.find_path(request.origin, request.target, self.links)
+        # return not path:
+        #     return False
+        # 4. set path of request
+        # request.path = path
+        # return True
+        pass
+    
+    def get_process_time(self) -> float:
+        # TODO: calculate based on request and path
+        pass
+
+    def get_expected_process_time(self) -> float:
+        pass
+
+PROCESS_UNION = Union[
+    CompoundProcess,
+    RequiredCapabilityProcess,
+    ProductionProcess,
+    TransportProcess,
+    CapabilityProcess,
+]
+"""
+Union type for all processes.
+"""
---
+++ packages/prodsys/prodsys-0.5.1-py3-none-any.whl/prodsys/simulation/request.py
@@ -0,0 +1,128 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+from prodsys.simulation import process as prodsys_process
+
+if TYPE_CHECKING:
+    from prodsys.simulation import product, process, resources
+
+
+class Request:
+    """
+    Class to represents requests of a product for a process to be executed by a resource.
+
+    Args:
+        process (process.PROCESS_UNION): The process.
+        product (product.Product): The product.
+        resource (resources.Resource): The resource.
+    """
+    def __init__(
+        self,
+        process: process.PROCESS_UNION,
+        product: product.Product,
+    ):
+        self.process = process
+        self.product = product
+        self.resource: Optional[resources.Resource] = None
+
+    def set_resource(self, resource: resources.Resource):
+        """
+        Sets the resource of the request.
+
+        Args:
+            resource (resources.Resource): The resource.
+        """
+        self.resource = resource
+        for process in self.resource.processes:
+            if process.matches_request(self):
+                # TODO: check whether this needs adaption for compound processes...
+                self.process = process
+
+    def get_process(self) -> process.PROCESS_UNION:
+        """
+        Returns the process or the capability process of the request 
+
+        Returns:
+            process.PROCESS_UNION: The process.
+        """
+        return self.process
+
+    def get_product(self) -> product.Product:
+        """
+        Returns the product of the request.
+
+        Returns:
+            product.Product: The product.
+        """
+        return self.product
+
+    def get_resource(self) -> resources.Resource:
+        """
+        Returns the resource of the request.
+
+        Returns:
+            resources.Resource: The resource.
+        """
+        return self.resource
+
+
+class TransportResquest(Request):
+    """
+    Class to represents requests of a product for a transport process to be executed by a transport resource. Additionally, it contains the origin and target locations of the transport.
+
+    Args:
+        process (process.TransportProcess): The transport process.
+        product (product.Product): The product.
+        resource (resources.TransportResource): The transport resource.
+        origin (product.Location): The origin location, either a resource, source or sink.
+        target (product.Location): The target location, either a resource, source or sink.
+    """
+    def __init__(
+        self,
+        process: process.TransportProcess,
+        product: product.Product,
+        origin: product.Location,
+        target: product.Location,
+    ):
+        self.process: process.TransportProcess = process
+        self.product: product.Product = product
+        self.resource: resources.TransportResource = None
+        self.origin: product.Location = origin
+        self.target: product.Location = target
+
+    def get_process(self) -> process.TransportProcess:
+        """
+        Returns the transport process of the transport request.
+
+        Returns:
+            process.TransportProcess: The transport process.
+        """
+        return self.process
+
+    def get_resource(self) -> resources.TransportResource:
+        """
+        Returns the transport resource of the transport request.
+
+        Returns:
+            resources.TransportResource: The transport resource.
+        """
+        return self.resource
+
+    def get_origin(self) -> product.Location:
+        """
+        Returns the origin location of the transport request.
+
+        Returns:
+            product.Location: The origin location.
+        """
+        return self.origin
+
+    def get_target(self) -> product.Location:
+        """
+        Returns the target location of the transport request.
+
+        Returns:
+            product.Location: The target location.
+        """
+        return self.target
