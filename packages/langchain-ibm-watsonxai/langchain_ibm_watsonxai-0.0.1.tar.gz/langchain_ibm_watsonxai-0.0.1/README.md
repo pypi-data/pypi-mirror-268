1dbb38c320208de8c65e8b6e19461490ff3bd326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-19 14:04:43 +0000

    Add package langchain-ibm-watsonxai/0.0.1/langchain_ibm_watsonxai-0.0.1.tar.gz

---
+++ packages/langchain-ibm-watsonxai/langchain_ibm_watsonxai-0.0.1.tar.gz/langchain_ibm_watsonxai-0.0.1/README.md
@@ -0,0 +1,12 @@
+# langchain-ibm-watsonx-ai
+
+This package contains the LangChain integrations for IBM watsonx.ai.
+
+
+
+## Installation
+
+```bash
+pip install langchain-ibm-watsonx-ai
+```
+
---
+++ packages/langchain-ibm-watsonxai/langchain_ibm_watsonxai-0.0.1.tar.gz/langchain_ibm_watsonxai-0.0.1/langchain_ibm_watsonxai/llms.py
@@ -0,0 +1,12 @@
+from langchain_core.language_models.llms import LLM
+
+
+class WatsonxAI(LLM):
+    """IBM watsonx.ai large language models.
+
+    References:
+        [1] https://www.ibm.com/products/watsonx-ai
+        [2] https://cloud.ibm.com/apidocs/watsonx-ai?code=python
+    """
+
+    pass
