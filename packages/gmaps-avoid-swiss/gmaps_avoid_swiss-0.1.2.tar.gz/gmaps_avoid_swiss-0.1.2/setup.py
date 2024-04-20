1af57f98694fce46e0ff195d3473ddbd33191e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 06:05:41 +0000

    Add package gmaps-avoid-swiss/0.1.2/gmaps_avoid_swiss-0.1.2.tar.gz

---
+++ packages/gmaps-avoid-swiss/gmaps_avoid_swiss-0.1.2.tar.gz/gmaps_avoid_swiss-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gmaps_avoid_swiss',
+    version='0.1.2',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    url='https://github.com/xbencat/gmaps_avoid_swiss',
+    license='MIT',
+    author='Gregor Bencat',
+    author_email='bencat.gregor@gmail.com',
+    description='A Python package that customizes Google Maps routing to avoid Swiss routes.',
+    install_requires=[
+        'typer~=0.12.3',
+        'rich~=13.7.1',
+        'google-maps-routing~=0.6.8',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
---
+++ packages/gmaps-avoid-swiss/gmaps_avoid_swiss-0.1.2.tar.gz/gmaps_avoid_swiss-0.1.2/tests/test_gmaps_avoid_swiss.py
@@ -0,0 +1,22 @@
+import unittest
+
+from unittest.mock import patch
+from src.gmaps_avoid_swiss.gmaps_avoid_swiss import GMapsRoutingClient
+
+
+class TestGMapsRoutingClient(unittest.TestCase):
+
+    @patch('src.gmaps_avoid_swiss.gmaps_avoid_swiss.routing_v2.RoutesClient')
+    def test_compute_route(self, mock_routes_client):
+        mock_response = unittest.mock.Mock()
+        mock_response.routes = [unittest.mock.Mock(distance_meters=1000, duration=unittest.mock.Mock(seconds=3600))]
+        mock_routes_client.return_value.compute_routes.return_value = mock_response
+
+        client = GMapsRoutingClient("api_key")
+        origin = {"address": "New York, NY, USA"}
+        destination = {"address": "New Jersey, NJ, USA"}
+        response = client.compute_route(origin, destination)
+
+        self.assertTrue(response.routes)
+        self.assertGreater(response.routes[0].distance_meters, 0)
+        self.assertGreater(response.routes[0].duration.seconds, 0)
