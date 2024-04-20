abafef7229e3a100ef4c3f3fd67e39ecfd23a5b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 06:05:52 +0000

    Add package gmaps-avoid-swiss/0.1.4/gmaps_avoid_swiss-0.1.4.tar.gz

---
+++ packages/gmaps-avoid-swiss/gmaps_avoid_swiss-0.1.4.tar.gz/gmaps_avoid_swiss-0.1.4/src/gmaps_avoid_swiss/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for GMaps Avoid Swiss Routes."""
+
+__author__ = """Gregor Bencat"""
+__email__ = 'bencat.gregor@gmail.com'
+__version__ = '0.1.4'
+
+from gmaps_avoid_swiss.client import GMapsRoutingClient
+
+__all__ = ["GMapsRoutingClient"]
