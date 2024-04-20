ee74d8631fef0adf5f10e6bcbcfa08bae427a49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:04:32 +0000

    Add package rent-counter/0.5.0/rent_counter-0.5.0.tar.gz

---
+++ packages/rent-counter/rent_counter-0.5.0.tar.gz/rent_counter-0.5.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rent_counter',
+    version='0.5.0',
+    py_modules=['countf.countdown'],  # Include countdown.py as a module inside the countf folder
+    include_package_data=True,
+    description='A Python library for managing rent expiration countdowns',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Erick Adikah',
+    author_email='your.email@example.com',
+    license='MIT',
+    url='https://github.com/Erickadikah/count',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    install_requires=[],
+)
