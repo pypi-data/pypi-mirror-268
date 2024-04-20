ebf15f5471924478cbf870cc1efa16ce3009ab59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 03:07:13 +0000

    Add package mycalc-gi/0.0.1/mycalc_gi-0.0.1.tar.gz

---
+++ packages/mycalc-gi/mycalc_gi-0.0.1.tar.gz/mycalc_gi-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+#https://pypi.org/help/#invalid-auth
+#8eb30951-3043-4a5d-8319-c6d014fec2ac api 토큰
+
+setup(
+    name='mycalc_gi',
+    version='0.0.1',
+    description='nice calculator',
+    author='gitaebak',
+    url = 'https://github.com/GitaeBak/unv_oss',
+    download_url = "https://github.com/GitaeBak/unv_oss",
+    install_requires=[" "],
+    package=["mycalc"],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
