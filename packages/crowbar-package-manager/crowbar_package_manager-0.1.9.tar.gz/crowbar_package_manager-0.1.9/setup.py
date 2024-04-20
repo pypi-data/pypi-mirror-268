d9cf45223f53f0e1ac4b33e80e77ae579cefe530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 06:05:22 +0000

    Add package crowbar-package-manager/0.1.9/crowbar_package_manager-0.1.9.tar.gz

---
+++ packages/crowbar-package-manager/crowbar_package_manager-0.1.9.tar.gz/crowbar_package_manager-0.1.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='crowbar_package_manager',
+    version='0.1.9',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'crowbar=crowbar:main',
+            'cb=crowbar:main',
+        ],
+    },
+    description = 'a local-first tool for managing python dependencies with pip',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This line is important for Markdown rendering on PyPI
+)
