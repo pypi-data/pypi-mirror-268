1b721ee014a9e613e1905cf0c02815b79921539e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 04:05:22 +0000

    Add package jaclang/0.5.11/jaclang-0.5.11.tar.gz

---
+++ packages/jaclang/jaclang-0.5.11.tar.gz/jaclang-0.5.11/setup.py
@@ -0,0 +1,26 @@
+"""Jaclang setup file."""
+
+from __future__ import annotations
+
+from setuptools import find_packages, setup  # type: ignore
+
+
+VERSION = "0.5.11"
+
+setup(
+    name="jaclang",
+    version=VERSION,
+    packages=find_packages(include=["jaclang", "jaclang.*"]),
+    install_requires=[],
+    package_data={
+        "": ["*.ini"],
+    },
+    entry_points={
+        "console_scripts": [
+            "jac = jaclang.cli.cli:start_cli",
+        ],
+    },
+    author="Jason Mars",
+    author_email="jason@jaseci.org",
+    url="https://github.com/Jaseci-Labs/jaclang",
+)
