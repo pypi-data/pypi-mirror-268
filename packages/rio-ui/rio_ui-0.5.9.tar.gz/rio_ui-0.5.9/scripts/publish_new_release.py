f4fef9f7b08c71ca49925779a1807abdd87a997c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 04:05:12 +0000

    Add package rio-ui/0.5.9/rio_ui-0.5.9.tar.gz

---
+++ packages/rio-ui/rio_ui-0.5.9.tar.gz/rio_ui-0.5.9/scripts/publish_new_release.py
@@ -0,0 +1,54 @@
+import subprocess
+from pathlib import Path
+
+import requests
+import revel
+
+PROJECT_DIR = Path(__file__).absolute().parent.parent
+
+
+def main() -> None:
+    # Make sure we're on the correct branch
+    process = subprocess.run(
+        ["git", "branch"], check=True, capture_output=True, text=True
+    )
+    if process.stdout.strip("\n") != "dev":
+        revel.fatal("You must checkout the 'dev' branch")
+
+    # Make sure the version number got bumped
+    version = get_current_version()
+    if version == get_latest_published_version():
+        revel.fatal("You forgot to increment the version number")
+
+    # Build the TS code
+    subprocess.run(["rye", "run", "build"], check=True)
+
+    # Run the test suite
+    if not tests_pass():
+        revel.fatal("The test don't pass")
+
+    # Merge dev into main and push
+    subprocess.run(["git", "fetch", ".", "dev:main"], check=True)
+    subprocess.run(["git", "push", "-u", "origin", "main"], check=True)
+
+    # Publish
+    subprocess.run(["rye", "publish", "--skip-existing"], check=True)
+
+
+def get_latest_published_version() -> str:
+    response = requests.get(f"https://pypi.org/pypi/rio-ui/json")
+    return response.json()["info"]["version"]
+
+
+def get_current_version() -> str:
+    process = subprocess.run(
+        ["rye", "version"], capture_output=True, text=True, check=True
+    )
+    return process.stdout.strip()
+
+
+def tests_pass() -> bool:
+    return subprocess.run(["rye", "test"], check=True) == 0
+
+
+main()
