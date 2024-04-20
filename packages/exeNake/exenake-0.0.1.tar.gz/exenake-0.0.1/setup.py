8d426c9ce50f2af7b64dd49cdf6abf10375eea52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 03:06:57 +0000

    Add package exeNake/0.0.1/exenake-0.0.1.tar.gz

---
+++ packages/exeNake/exenake-0.0.1.tar.gz/exenake-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="exeNake",
+    version="0.0.1",
+    author="Comet",
+    author_email="9su0425@gmail.com",
+    description="strong Multiple-Run-Token System",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
