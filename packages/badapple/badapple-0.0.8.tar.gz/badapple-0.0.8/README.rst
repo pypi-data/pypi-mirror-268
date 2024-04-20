546802e0261b14a56526cabca510bf5d0dd2280e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 05:07:41 +0000

    Add package badapple/0.0.8/badapple-0.0.8.tar.gz

---
+++ packages/badapple/badapple-0.0.8.tar.gz/badapple-0.0.8/README.rst
@@ -0,0 +1,65 @@
+BadApple
+========
+
+Python Module: Play the video in the console as ASCII art.
+
+Where there is light, there is 
+`Bad Apple!! <https://www.youtube.com/watch?v=FtutLA63Cp8>`_
+
+Installation
+------------
+
+.. code-block:: shell
+
+    python -m pip install badapple
+
+with audio:
+
+.. code-block:: shell
+
+    python -m pip install badapple[audio]
+
+with any supported player:
+
+.. code-block:: shell
+
+    python -m pip install badapple[dev]
+
+Run
+---
+
+.. code-block:: shell
+
+    python -m badapple
+
+Help Message
+------------
+
+.. code-block:: md
+
+    usage: badapple [options] ... 
+
+    BadApple-Python-OS-ISA-Version
+
+    options:
+      -h, --help            show this help message and exit
+      --help_audio          show avaliable players (default: False)
+      -i INPUT, --input INPUT
+                            video file (use `_BA_MP4` `_BA_BA` to load built-in video) (default: _BA_MP4)
+      -o OUTPUT, --output OUTPUT
+                            preload output file (default: )
+      -s SCALE, --scale SCALE
+                            width:height (0 means auto) (default: 0:0)
+      -r RATE, --rate RATE  frame rate (default: 1024.0)
+      --audio AUDIO         audio file (use `_BA_WAV` `_BA_MP3` `_BA_MP4` to load built-in audio) (default: )
+      --audio_player AUDIO_PLAYER
+                            audio player [ffplay mpv vlc mpg123 cmus simpleaudio pyaudio playsound auto] (default: )
+      -c COLOR, --color COLOR
+                            color type [ascii rgb24 halfwidth fullwidth x256e x256w x232e x232w] (default: ascii)
+      -m MESSAGE, --message MESSAGE
+                              print message (default: )
+      --font FONT           font data file (use `_BA_CONSOLA` to load built-in font data) (default: _BA_CONSOLA)
+      --not_clear           not clear screen (with ANSI) before each frame (default: False)
+      --contrast            contrast enhancement (default: False)
+      --preload             preload video (not play) (default: False)
+      --debug               debug (default: False)
---
+++ packages/badapple/badapple-0.0.8.tar.gz/badapple-0.0.8/src/badapple/play.py
@@ -0,0 +1,144 @@
+import os
+import time
+import cv2
+
+from .util import get_func
+from .audio import get_player
+from .replay import replay
+from .color import get_buffer
+
+
+def play(
+    p_list: list,
+    video: str, output: str,
+    x: int, y: int, fps: int,
+    audio: str, player: str,
+    color: str, message: str, font: str,
+    need_clear: bool = True, contrast: bool = False, preload: bool = False,
+    debug: bool = False
+) -> None:
+    if video.endswith('.badapple'):
+        return replay(
+            p_list,
+            video, audio, player,
+            message, need_clear, debug
+        )
+
+    video = os.path.abspath(video)
+    open(video, 'rb').close()
+    p = get_player(audio, player, video)
+
+    capture = cv2.VideoCapture(video)
+    width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+    width = int(width + 0.5)
+    height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    height = int(height + 0.5)
+    nb_frames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+    nb_frames = int(nb_frames + 0.5)
+    rate = capture.get(cv2.CAP_PROP_FPS)
+    if rate < 0.1:
+        raise Exception("The frame rate is %s!" % str(rate))
+    duration = nb_frames / rate
+
+    mo = max(int(0.5 + rate / fps), 1)
+    clk = mo / rate
+
+    max_x, max_y = os.get_terminal_size()
+    max_y = (max_y-1)*2
+
+    x = int(x)
+    y = int(y)
+
+    if x > 0:
+        if y > 0:
+            # x = x
+            # y = y
+            pass
+        else:
+            # x = x
+            y = int(height*x/width + 0.5)
+    else:
+        if y > 0:
+            x = int(width*y/height + 0.5)
+            # y = y
+        else:
+            x = min(max_x, int(width*max_y/height + 0.5))
+            y = min(max_y, int(height*max_x/width + 0.5))
+
+    if y % 2:
+        if y == max_y+1:
+            y = max_y
+        else:
+            y += 1
+
+    print('[%d:%d %.2lfHz] -%s-> [%d:%d %.2lfHz] %.3lfs/%dms%s %s' % (
+        width, height, rate,
+        p.name if p else '',
+        x, y, rate / mo,
+        duration, clk*1000+0.5,
+        ' [debug]' if debug else '',
+        message
+    ), flush=True)
+    # [1444:1080 29.97Hz] -ffplay-> [72:54 9.99Hz] 232.065s
+
+    rewind, clear, console_resize = get_func(need_clear)
+    fontmap = open(font, 'r').read().split('\n')
+
+    if output or preload:
+        if not output:
+            output = video + '.badapple'
+        output = os.path.abspath(output)
+
+        with open(output, 'w') as fp:
+            fp.write('%d %d %d\n\n' % (x, y, int(clk*1000+0.5)))
+            fp.flush()
+
+            for i in range(nb_frames):
+                succ, img = capture.read()
+                if not succ:
+                    raise Exception(i)
+                if i % mo:
+                    continue
+                buffer = get_buffer(
+                    img, x, y, color, message,
+                    fontmap, contrast
+                )
+
+                fp.write(buffer + '\n')
+                fp.flush()
+
+    else:
+        # print('BEGINNING...', flush=True)
+        time.sleep(1)
+        if p:
+            p_list.append(p)
+            p.start()
+        if debug:
+            time.sleep(5)
+        rewind()
+        clear()
+        if not debug:
+            # console_resize(x, y//2+1)
+            rewind()
+            clear()
+
+        t0 = time.time()
+        for i in range(nb_frames):
+            succ, img = capture.read()
+            if not succ:
+                raise Exception(i)
+            if i % mo:
+                continue
+            buffer = get_buffer(
+                img, x, y, color, message,
+                fontmap, contrast
+            )
+
+            rewind()
+            # clear()
+            print(buffer, end='', flush=True)
+
+            t1 = time.time()
+            while t1 - t0 < clk:
+                t1 = time.time()
+            t0 = t1
