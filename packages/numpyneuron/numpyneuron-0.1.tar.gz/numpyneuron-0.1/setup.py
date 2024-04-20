a9df6e6bb64f92f62d88dd38a2581109b6e3e633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 06:05:26 +0000

    Add package numpyneuron/0.1/numpyneuron-0.1.tar.gz

---
+++ packages/numpyneuron/numpyneuron-0.1.tar.gz/numpyneuron-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="numpyneuron",
+    version="0.1",
+    author="Jensen Holm",
+    author_email="jensen.dev.01@gmail.com",
+    description="Simple, lightweight neural network framework built in numpy",
+    long_description=open("about_package.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jensen-holm/Numpy-Neuron",
+    project_urls={"Bug Tracker": "https://github.com/Jensen-holm/Numpy-Neuron/issues"},
+    package_dir={"": "nn"},
+    packages=find_packages(where="nn"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
