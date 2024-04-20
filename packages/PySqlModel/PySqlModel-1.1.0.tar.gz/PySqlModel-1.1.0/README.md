7df8f19930cfa13266710e2d5b9920b625bdbf19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 04:05:01 +0000

    Add package PySqlModel/1.1.0/PySqlModel-1.1.0.tar.gz

---
+++ packages/PySqlModel/PySqlModel-1.1.0.tar.gz/PySqlModel-1.1.0/README.md
@@ -0,0 +1,9 @@
+# PySqlModel
+
+**目前支持 MySql、SQLite3 数据库查询**
+
+**example 目录下为示例程序**
+
+
+**[使用手册](./PySqlModel/使用手册.md)**
+
---
+++ packages/PySqlModel/PySqlModel-1.1.0.tar.gz/PySqlModel-1.1.0/setup.py
@@ -0,0 +1,30 @@
+"""
+@Project:PySqlModel
+@File:setup.py
+@Author:函封封
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="PySqlModel",
+    version="1.1.0",
+    author="HanFengFeng",
+    author_email="mr_jia_han@qq.com",
+    description="简单方便的数据库查询包",
+    # 项目主页
+    url="https://github.com/NeverStopDreamingWang/pysqlmodel",
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages(),
+    install_requires=[
+        "pymysql",
+        "sqlite3"
+    ],
+    project_urls={
+        "github": "https://github.com/NeverStopDreamingWang/pysqlmodel",
+        "gitee": "https://gitee.com/NeverStopDreamingWang/pysqlmodel",
+    }
+)
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
