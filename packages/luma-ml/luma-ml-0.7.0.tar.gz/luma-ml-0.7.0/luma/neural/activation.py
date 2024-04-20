f711370cea6c34a65f0ae4177bcb314638006892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 03:07:17 +0000

    Add package luma-ml/0.7.0/luma-ml-0.7.0.tar.gz

---
+++ packages/luma-ml/luma-ml-0.7.0.tar.gz/luma-ml-0.7.0/luma/neural/activation.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+__all__ = ("ReLU", "LeakyReLU", "ELU", "Tanh", "Sigmoid", "Softmax")
+
+
+type Matrix = Matrix
+
+
+class ReLU:
+    def func(self, X: Matrix) -> Matrix:
+        return np.maximum(0, X)
+
+    def grad(self, X: Matrix) -> Matrix:
+        return (X > 0).astype(float)
+
+
+class LeakyReLU:
+    def __init__(self, alpha=0.01):
+        self.alpha = alpha
+
+    def func(self, X: Matrix) -> Matrix:
+        return np.where(X > 0, X, X * self.alpha)
+
+    def grad(self, X: Matrix) -> Matrix:
+        return np.where(X > 0, 1, self.alpha)
+
+
+class ELU:
+    def __init__(self, alpha=1.0):
+        self.alpha = alpha
+
+    def func(self, X: Matrix) -> Matrix:
+        return np.where(X > 0, X, self.alpha * (np.exp(X) - 1))
+
+    def grad(self, X: Matrix) -> Matrix:
+        return np.where(X > 0, 1, self.func(X) + self.alpha)
+
+
+class Tanh:
+    def func(self, X: Matrix) -> Matrix:
+        return np.tanh(X)
+
+    def grad(self, X: Matrix) -> Matrix:
+        return 1 - np.tanh(X) ** 2
+
+
+class Sigmoid:
+    def func(self, X: Matrix) -> Matrix:
+        return 1 / (1 + np.exp(-X))
+
+    def grad(self, X: Matrix) -> Matrix:
+        return X * (1 - X)
+
+
+class Softmax:
+    def func(self, X: Matrix) -> Matrix:
+        exps = np.exp(X - np.max(X, axis=1, keepdims=True))
+        return exps / np.sum(exps, axis=1, keepdims=True)
+
+    def grad(self, _: Matrix) -> Matrix:
+        NotImplemented
