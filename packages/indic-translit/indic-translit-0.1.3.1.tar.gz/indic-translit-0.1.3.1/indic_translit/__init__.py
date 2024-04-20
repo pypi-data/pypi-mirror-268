594be256c003deca44a1bfcd42f1777437435a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 04:05:25 +0000

    Add package indic-translit/0.1.3.1/indic-translit-0.1.3.1.tar.gz

---
+++ packages/indic-translit/indic-translit-0.1.3.1.tar.gz/indic-translit-0.1.3.1/indic_translit/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.3.1'
+
+from indic_translit.Transliterator import Transliterator
+from indic_translit.DeTransliterator import DeTransliterator
+from indic_translit.transliterate import transliterate
+from indic_translit.detransliterate import detransliterate
