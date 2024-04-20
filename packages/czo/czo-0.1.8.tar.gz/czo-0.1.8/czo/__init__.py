01fbe16eac7701fed8f705469f8751843d715a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 04:05:37 +0000

    Add package czo/0.1.8/czo-0.1.8.tar.gz

---
+++ packages/czo/czo-0.1.8.tar.gz/czo-0.1.8/czo/__init__.py
@@ -0,0 +1,11 @@
+"""
+辅助测试的一些常用功能
+"""
+from ._date import FastDate
+from ._faker import Faker
+from ._net import NetLib
+from ._path import DirLib, PathLib
+from ._rand import Random
+
+__all__: list[str] = ["FastDate", "Faker",
+                      "PathLib", "DirLib", "Random", "NetLib"]
