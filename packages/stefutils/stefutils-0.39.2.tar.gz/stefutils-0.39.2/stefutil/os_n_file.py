5b19410f9d1965788e3f6f8b15963da26013f779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:04:25 +0000

    Add package stefutils/0.39.2/stefutils-0.39.2.tar.gz

---
+++ packages/stefutils/stefutils-0.39.2.tar.gz/stefutils-0.39.2/stefutil/os_n_file.py
@@ -0,0 +1,60 @@
+"""
+os-related
+"""
+
+import os
+from os.path import join as os_join
+from pathlib import Path
+from typing import List, Union
+
+
+__all__ = ['get_hostname', 'stem']
+
+
+def get_hostname() -> str:
+    return os.uname().nodename
+
+
+def stem(
+        path: Union[str, Path], keep_ext=False, top_n: int = None, as_list: bool = False
+) -> Union[str, List[str]]:
+    """
+    :param path: A potentially full path to a file
+    :param keep_ext: If True, file extensions is preserved
+    :param top_n: If given, keep the top `top_n` parent directories
+    :param as_list: If True, return as a list
+        Relevant only when `top_n` is given
+    :return: The file name, without parent directories
+    """
+    if top_n:
+        ret = stem(path=path, keep_ext=keep_ext, top_n=None)
+        if isinstance(path, Path):
+            dirs = []
+            for _ in range(top_n):
+                path = path.parent
+                dirs.append(path.name)
+            dirs.reverse()
+        else:
+            dirs = path.split(os.sep)
+            dirs = dirs[-top_n-1:-1]
+        dirs += [ret]
+        return dirs if as_list else os_join(*dirs)
+    else:
+        # if it's a directory, always keep extension
+        if os.path.exists(path) and os.path.isdir(path):
+            keep_ext = True
+        return os.path.basename(path) if keep_ext else Path(path).stem
+
+
+if __name__ == '__main__':
+    def check_stem():
+        n = 3
+        # path = __file__
+        path = '../dir-with.dot'
+        path_ = Path(path)
+        print(path)
+        print(stem(path, top_n=n))
+        print(stem(path, top_n=n, as_list=True))
+        print(path_)
+        print(stem(path_, top_n=n))
+    check_stem()
