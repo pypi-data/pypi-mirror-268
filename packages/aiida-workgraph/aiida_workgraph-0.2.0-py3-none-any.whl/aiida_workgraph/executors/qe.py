d5be229ca4c267315d282611d9811a984642b3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 04:05:20 +0000

    Add package aiida-workgraph/0.2.0/aiida_workgraph-0.2.0-py3-none-any.whl

---
+++ packages/aiida-workgraph/aiida_workgraph-0.2.0-py3-none-any.whl/aiida_workgraph/executors/qe.py
@@ -0,0 +1,25 @@
+from aiida import load_profile
+
+load_profile()
+
+
+def get_pseudo_from_structure(pseudo_family, structure):
+    """for input_namespace"""
+    from aiida.orm import Group, QueryBuilder
+
+    pseudo_group = (
+        QueryBuilder().append(Group, filters={"label": pseudo_family}).one()[0]
+    )
+    elements = [kind.name for kind in structure.kinds]
+    pseudos = {}
+    print("elements: ", elements)
+    for ele in elements:
+        for node in pseudo_group.nodes:
+            if ele == node.element:
+                pseudos[ele] = node
+    print("pseudos: ", pseudos)
+    return {"Pseudo": pseudos}
+
+
+if __name__ == "__main__":
+    pass
