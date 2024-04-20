7b9bf7ddd56c8419b795c05c2b2130ab8551aa39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:04:27 +0000

    Add package pydiverse-transform/0.1.5/pydiverse_transform-0.1.5.tar.gz

---
+++ packages/pydiverse-transform/pydiverse_transform-0.1.5.tar.gz/pydiverse_transform-0.1.5/src/pydiverse/transform/core/expressions/lambda_getter.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from pydiverse.transform.core.expressions import LambdaColumn
+from pydiverse.transform.core.expressions.symbolic_expressions import SymbolicExpression
+
+
+class LambdaColumnGetter:
+    """
+    An instance of this object can be used to instantiate a LambdaColumn.
+    """
+
+    def __getattr__(self, item):
+        if item.startswith("__"):
+            raise AttributeError(
+                f"'{type(self).__name__}' object has no attribute '{item}'"
+            )
+        return SymbolicExpression(LambdaColumn(item))
+
+    def __getitem__(self, item):
+        return SymbolicExpression(LambdaColumn(item))
+
+
+# Global instance of LambdaColumnGetter.
+λ = LambdaColumnGetter()
---
+++ packages/pydiverse-transform/pydiverse_transform-0.1.5.tar.gz/pydiverse_transform-0.1.5/src/pydiverse/transform/ops/string.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from pydiverse.transform.ops.core import ElementWise, OperatorExtension, Unary
+from pydiverse.transform.ops.logical import Logical
+from pydiverse.transform.ops.numeric import Add, RAdd
+
+__all__ = [
+    "StringAdd",
+    "StringRAdd",
+    "Strip",
+    "StringLength",
+    "Upper",
+    "Lower",
+    "Replace",
+    "StartsWith",
+    "EndsWith",
+    "Contains",
+]
+
+
+class StringAdd(OperatorExtension):
+    operator = Add
+    signatures = [
+        "str, str -> str",
+    ]
+
+
+class StringRAdd(OperatorExtension):
+    operator = RAdd
+    signatures = [
+        "str, str -> str",
+    ]
+
+
+####
+
+
+class StringUnary(ElementWise, Unary):
+    signatures = [
+        "str -> str",
+    ]
+
+
+class Strip(StringUnary):
+    name = "strip"
+
+
+class StringLength(StringUnary):
+    name = "len"
+    signatures = [
+        "str -> int",
+    ]
+
+
+class Upper(StringUnary):
+    name = "upper"
+
+
+class Lower(StringUnary):
+    name = "lower"
+
+
+class Replace(ElementWise):
+    name = "replace"
+    signatures = [
+        "str, const str, const str -> str",
+    ]
+
+
+class StartsWith(ElementWise, Logical):
+    name = "startswith"
+    signatures = [
+        "str, const str -> bool",
+    ]
+
+
+class EndsWith(ElementWise, Logical):
+    name = "endswith"
+    signatures = [
+        "str, const str -> bool",
+    ]
+
+
+class Contains(ElementWise, Logical):
+    name = "contains"
+    signatures = [
+        "str, const str -> bool",
+    ]
