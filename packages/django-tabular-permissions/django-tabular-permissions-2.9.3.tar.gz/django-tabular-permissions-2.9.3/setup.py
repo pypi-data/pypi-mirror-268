4aac17bf1a31802c04359ef9c0231a8fcc5ecabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 05:07:19 +0000

    Add package django-tabular-permissions/2.9.3/django-tabular-permissions-2.9.3.tar.gz

---
+++ packages/django-tabular-permissions/django-tabular-permissions-2.9.3.tar.gz/django-tabular-permissions-2.9.3/setup.py
@@ -0,0 +1,42 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-tabular-permissions',
+    version='2.9.3',
+    packages=['tabular_permissions'],
+    include_package_data=True,
+    license='BSD License',
+    description='Display django permissions in a tabular format that is user friendly, and highly customisable',
+    long_description=README,
+    url='https://github.com/RamezIssac/django-tabular-permissions',
+    author='Ramez Ashraf',
+    author_email='ramez@rasystems.io',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
---
+++ packages/django-tabular-permissions/django-tabular-permissions-2.9.3.tar.gz/django-tabular-permissions-2.9.3/tabular_permissions/admin.py
@@ -0,0 +1,60 @@
+from django.contrib import admin
+from django.contrib.auth.admin import Group, GroupAdmin as DjGroupAdmin, UserAdmin as DjUserAdmin
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ImproperlyConfigured
+from tabular_permissions.widgets import TabularPermissionsWidget
+from . import app_settings
+
+User = get_user_model()
+
+
+class UserTabularPermissionsMixin:
+
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        field = super().formfield_for_manytomany(db_field, request, **kwargs)
+        if db_field.name == 'user_permissions':
+            field.widget = TabularPermissionsWidget(db_field.verbose_name, db_field.name in self.filter_vertical)
+            field.help_text = ''
+        return field
+
+
+class GroupTabularPermissionsMixin:
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        field = super().formfield_for_manytomany(db_field, request, **kwargs)
+        if db_field.name == 'permissions':
+            field.widget = TabularPermissionsWidget(db_field.verbose_name, db_field.name in self.filter_vertical,
+                                                    'permissions')
+            field.help_text = ''
+        return field
+
+
+try:
+    UserAdminModel = admin.site._registry[User].__class__
+except:  # pragma: no cover
+    UserAdminModel = DjUserAdmin
+
+try:
+    GroupAdminModel = admin.site._registry[Group].__class__
+except:  # pragma: no cover
+    GroupAdminModel = DjGroupAdmin
+
+
+class TabularPermissionsUserAdmin(UserTabularPermissionsMixin, UserAdminModel):
+    pass
+
+
+class TabularPermissionsGroupAdmin(GroupTabularPermissionsMixin, GroupAdminModel):
+    pass
+
+
+if app_settings.AUTO_IMPLEMENT:
+    try:
+        admin.site.unregister(User)
+        admin.site.register(User, TabularPermissionsUserAdmin)
+        admin.site.unregister(Group)
+        admin.site.register(Group, TabularPermissionsGroupAdmin)
+
+    except:
+        raise ImproperlyConfigured(
+            'Please make sure that django.contrib.auth (Or the app containing your custom User model) '
+            'comes before tabular_permissions in INSTALLED_APPS; Or set AUTO_IMPLEMENT to False in your settings.')
