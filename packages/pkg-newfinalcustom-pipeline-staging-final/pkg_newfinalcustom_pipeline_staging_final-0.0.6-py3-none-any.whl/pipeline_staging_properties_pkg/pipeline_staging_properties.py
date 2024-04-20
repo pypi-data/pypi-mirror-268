90f441b1d98bc72f65fe4430d35cdaf709f00bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 03:07:11 +0000

    Add package pkg-newfinalcustom-pipeline-staging-final/0.0.6/pkg_newfinalcustom_pipeline_staging_final-0.0.6-py3-none-any.whl

---
+++ packages/pkg-newfinalcustom-pipeline-staging-final/pkg_newfinalcustom_pipeline_staging_final-0.0.6-py3-none-any.whl/pipeline_staging_properties_pkg/pipeline_staging_properties.py
@@ -0,0 +1,42 @@
+import boto3
+import os
+from botocore.exceptions import ClientError
+
+class PipelineStagingManager:
+    def __init__(self, bucket_name):
+        self.bucket_name = bucket_name
+        self.s3_client = boto3.client('s3')
+
+    def list_stages(self):
+        try:
+            response = self.s3_client.list_objects_v2(Bucket=self.bucket_name)
+            stages = [obj['Key'] for obj in response.get('Contents', [])]
+            return stages
+        except ClientError as e:
+            print(f"An error occurred while listing stages: {e}")
+            return []
+
+    def add_stage(self, stage_name):
+        try:
+            self.s3_client.put_object(Bucket=self.bucket_name, Key=stage_name)
+        except ClientError as e:
+            print(f"An error occurred while adding stage {stage_name}: {e}")
+
+    def delete_stage(self, stage_name):
+        try:
+            self.s3_client.delete_object(Bucket=self.bucket_name, Key=stage_name)
+        except ClientError as e:
+            print(f"An error occurred while deleting stage {stage_name}: {e}")
+            
+    def upload_stage(self, stage_name):
+        try:
+            response = self.s3_client.get_object(Bucket=self.bucket_name, Key=stage_name)
+            current_content = response['Body'].read().decode('utf-8')
+            modified_content = modify_content(current_content)
+            self.s3_client.put_object(Bucket=self.bucket_name, Key=stage_name, Body=modified_content.encode('utf-8'))
+        except ClientError as e:
+            print(f"An error occurred while editing stage {stage_name}: {e}")
+
+def modify_content(current_content):
+    modified_content = current_content + "\nModified content"
+    return modified_content
