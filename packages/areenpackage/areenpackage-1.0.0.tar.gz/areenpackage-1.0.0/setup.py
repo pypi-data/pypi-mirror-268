f6a72f157d9989c6955f348246b1572bce72b571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 05:05:31 +0000

    Add package areenpackage/1.0.0/areenpackage-1.0.0.tar.gz

---
+++ packages/areenpackage/areenpackage-1.0.0.tar.gz/areenpackage-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='areenpackage',
+    version='1.0.0',
+    packages=find_packages(),
+    author='Areen Patil',
+    author_email='Areen.Patil@iiitb.ac.in',
+    description='This is a package that contains the basic mathematical functions like addition,subtract and multiplication',
+    url='https://github.com/areen-patil/areen_python_package',
+)
