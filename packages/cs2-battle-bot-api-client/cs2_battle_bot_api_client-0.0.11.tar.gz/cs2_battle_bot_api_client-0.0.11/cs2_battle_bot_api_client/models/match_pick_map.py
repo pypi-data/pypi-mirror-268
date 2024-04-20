bf57f6de7efa0eecb97d12017ecd82599660fef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 05:05:31 +0000

    Add package cs2-battle-bot-api-client/0.0.11/cs2_battle_bot_api_client-0.0.11.tar.gz

---
+++ packages/cs2-battle-bot-api-client/cs2_battle_bot_api_client-0.0.11.tar.gz/cs2_battle_bot_api_client-0.0.11/cs2_battle_bot_api_client/models/match_pick_map.py
@@ -0,0 +1,66 @@
+from typing import Any, Dict, List, Type, TypeVar
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+T = TypeVar("T", bound="MatchPickMap")
+
+
+@_attrs_define
+class MatchPickMap:
+    """
+    Attributes:
+        user_id (str):
+        map_tag (str):
+    """
+
+    user_id: str
+    map_tag: str
+    additional_properties: Dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+    def to_dict(self) -> Dict[str, Any]:
+        user_id = self.user_id
+
+        map_tag = self.map_tag
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        field_dict.update(
+            {
+                "user_id": user_id,
+                "map_tag": map_tag,
+            }
+        )
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
+        d = src_dict.copy()
+        user_id = d.pop("user_id")
+
+        map_tag = d.pop("map_tag")
+
+        match_pick_map = cls(
+            user_id=user_id,
+            map_tag=map_tag,
+        )
+
+        match_pick_map.additional_properties = d
+        return match_pick_map
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
