20dd6f557d561d29a1ecefb86d2ef916fab54a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 04:05:40 +0000

    Add package aeroterra-ds/1.0.0/aeroterra_ds-1.0.0.tar.gz

---
+++ packages/aeroterra-ds/aeroterra_ds-1.0.0.tar.gz/aeroterra_ds-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aeroterra_ds',
+    version='1.0.0',
+    author='Data Science',
+    author_email='pedrofuentes7799@gmail.com',
+    description='Python Functions To Work With GeoSpatial Data & ArcGis in a simpler way',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://example.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords='arcgis',
+    python_requires='>=3.11',
+    install_requires=[
+        "gssapi",
+        "dask",
+        "arcgis",
+        "pyproj"
+        "shapely",
+        "matplotlib",
+        "geopandas",
+        "pandas",
+    ],
+)
