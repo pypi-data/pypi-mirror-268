796df8e0fa012258dffd58c3958334a303f43d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:04:37 +0000

    Add package aisploit/0.0.19/aisploit-0.0.19-py3-none-any.whl

---
+++ packages/aisploit/aisploit-0.0.19-py3-none-any.whl/aisploit/classifiers/__init__.py
@@ -0,0 +1,9 @@
+from .markdown import MarkdownInjectionClassifier
+from .text import RegexClassifier, SubstringClassifier, TextTokenClassifier
+
+__all__ = [
+    "MarkdownInjectionClassifier",
+    "RegexClassifier",
+    "SubstringClassifier",
+    "TextTokenClassifier",
+]
