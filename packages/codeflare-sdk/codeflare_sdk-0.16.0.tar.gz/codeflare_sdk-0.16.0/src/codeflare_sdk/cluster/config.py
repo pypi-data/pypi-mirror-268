ac0683d9d08bb981cef8030520d330bc7683e20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 04:05:33 +0000

    Add package codeflare-sdk/0.16.0/codeflare_sdk-0.16.0.tar.gz

---
+++ packages/codeflare-sdk/codeflare_sdk-0.16.0.tar.gz/codeflare_sdk-0.16.0/src/codeflare_sdk/cluster/config.py
@@ -0,0 +1,63 @@
+# Copyright 2022 IBM, Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+The config sub-module contains the definition of the ClusterConfiguration dataclass,
+which is used to specify resource requirements and other details when creating a
+Cluster object.
+"""
+
+from dataclasses import dataclass, field
+import pathlib
+
+dir = pathlib.Path(__file__).parent.parent.resolve()
+
+
+@dataclass
+class ClusterConfiguration:
+    """
+    This dataclass is used to specify resource requirements and other details, and
+    is passed in as an argument when creating a Cluster object.
+    """
+
+    name: str
+    namespace: str = None
+    head_info: list = field(default_factory=list)
+    head_cpus: int = 2
+    head_memory: int = 8
+    head_gpus: int = 0
+    machine_types: list = field(default_factory=list)  # ["m4.xlarge", "g4dn.xlarge"]
+    min_cpus: int = 1
+    max_cpus: int = 1
+    num_workers: int = 1
+    min_memory: int = 2
+    max_memory: int = 2
+    num_gpus: int = 0
+    template: str = f"{dir}/templates/base-template.yaml"
+    instascale: bool = False
+    mcad: bool = False
+    envs: dict = field(default_factory=dict)
+    image: str = ""
+    image_pull_secrets: list = field(default_factory=list)
+    dispatch_priority: str = None
+    write_to_file: bool = False
+    verify_tls: bool = True
+
+    def __post_init__(self):
+        if not self.verify_tls:
+            print(
+                "Warning: TLS verification has been disabled - Endpoint checks will be bypassed"
+            )
+
+    local_queue: str = None
