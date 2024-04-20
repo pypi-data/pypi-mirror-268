87216728edb658d208a92bb9a7254cc670c23358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 03:06:04 +0000

    Add package mindgard/0.23.0/mindgard-0.23.0.tar.gz

---
+++ packages/mindgard/mindgard-0.23.0.tar.gz/mindgard-0.23.0/src/mindgard/api_service.py
@@ -0,0 +1,68 @@
+from typing import List, Dict, Any
+from .utils import standard_headers
+import requests
+from .constants import API_BASE
+
+class ApiService():
+    def get_tests(self, access_token: str) ->  List[Dict[str, Any]]:
+        url = f"{API_BASE}/assessments?ungrouped=true"
+
+        res = requests.get(url, headers=standard_headers(access_token))
+        
+        res.raise_for_status()
+        
+        data: List[Dict[str, Any]] = res.json()
+
+        # augment the output with the shortened url
+        for test in data:
+            test_id = test["id"]
+            test["url"] = f"https://sandbox.mindgard.ai/r/test/{test_id}"
+            for attack in test["attacks"]:
+                attack_id = attack["id"]
+                attack["url"] = f"https://sandbox.mindgard.ai/r/attack/{attack_id}"
+
+        return data
+
+    def get_test(self, access_token: str, test_id:str) -> Dict[str, Any]:
+        url = f"{API_BASE}/assessments/{test_id}"
+
+        res = requests.get(url, headers=standard_headers(access_token))
+        
+        res.raise_for_status()
+        
+        data: Dict[str, Any] = res.json()
+
+        data["url"] = f"https://sandbox.mindgard.ai/r/test/{test_id}"
+        for attack in data["attacks"]:
+            attack_id = attack["id"]
+            attack["url"] = f"https://sandbox.mindgard.ai/r/attack/{attack_id}"
+
+        return data
+
+    def submit_test(self, access_token: str, target_name:str) -> Dict[str, Any]:
+        url = f"{API_BASE}/assessments"
+        post_body = {"mindgardModelName": target_name}
+        res = requests.post(url, headers=standard_headers(access_token), json=post_body)
+        res.raise_for_status()
+        data: Dict[str, Any] = res.json()
+        return data
+
+    def fetch_llm_prompts(self, access_token: str) -> Dict[str, Any]:
+        url = f"{API_BASE}/llm_tests/prompts"
+
+        res = requests.get(url, headers=standard_headers(access_token))
+        
+        res.raise_for_status()
+        
+        data: Dict[str, Any] = res.json()
+        return data
+    
+    def submit_llm_responses(self, access_token: str, responses:Dict[str, Any]) -> Dict[str, Any]:
+        url = f"{API_BASE}/llm_tests/responses"
+
+        res = requests.post(url, headers=standard_headers(access_token), json=responses)
+        
+        res.raise_for_status()
+        
+        data: Dict[str, Any] = res.json()
+        return data
