dae60ea45dcc2d3bf9a47621d19bf95fb9b9097c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 06:06:00 +0000

    Add package outputstyles/0.1.3/outputstyles-0.1.3.tar.gz

---
+++ packages/outputstyles/outputstyles-0.1.3.tar.gz/outputstyles-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+# La descripción larga va a ser el mismo fichero README.md
+long_desc = Path("README.md").read_text()
+
+# Datos del paquete
+setup(
+    name="outputstyles",
+    version="0.1.3",
+    author="Duniesky Salazar Pérez",
+    author_email="<duniesky.salazar@gmail.com>",
+    description="Applying styles to CLI output",
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dunieskysp/output_styles',
+    packages=find_packages(
+        exclude=["module"]
+    ),
+    keywords=['python', 'outputstyles', 'CLI styles', 'text styles'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+
+)
