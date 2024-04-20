fb7aeb53ba26f6789dc1e3427fa1c4e9dabec50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 04:05:01 +0000

    Add package telegrinder/0.1.dev163/telegrinder-0.1.dev163-py3-none-any.whl

---
+++ packages/telegrinder/telegrinder-0.1.dev163-py3-none-any.whl/telegrinder/bot/dispatch/return_manager/inline_query.py
@@ -0,0 +1,14 @@
+from telegrinder.bot.cute_types import InlineQueryCute
+from telegrinder.bot.dispatch.context import Context
+
+from .abc import BaseReturnManager, register_manager
+
+
+class InlineQueryReturnManager(BaseReturnManager[InlineQueryCute]):         
+    @register_manager(dict)
+    @staticmethod
+    async def dict_manager(value: dict, event: InlineQueryCute, ctx: Context) -> None:
+        await event.answer(**value)
+
+
+__all__ = ("InlineQueryReturnManager",)
