f6a32b06e49190fb4fbc542d71704d6dfa751ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:04:29 +0000

    Add package wdl-lsp/0.0.84/wdl-lsp-0.0.84.tar.gz

---
+++ packages/wdl-lsp/wdl-lsp-0.0.84.tar.gz/wdl-lsp-0.0.84/setup.py
@@ -0,0 +1,48 @@
+import os
+
+from setuptools import find_packages, setup
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(cwd, '..', 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='wdl-lsp',
+    author='Broad Institute',
+    description='Language Server Protocol (LSP) implementation for Workflow Definition Language (WDL)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='BSD 3-clause "New" or "Revised" License',
+    url='https://github.com/broadinstitute/wdl-ide',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development :: Interpreters',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.7.0',
+    packages=find_packages(
+        exclude=[
+            'tests',
+        ],
+    ),
+    setup_requires=[
+        'setuptools_scm',
+    ],
+    use_scm_version={
+        'root': '..',
+        'relative_to': __file__,
+    },
+    install_requires=[
+        'cromwell-tools == 2.3.0',
+        "miniwdl >= 1.1.5",
+        'pygls == 0.8.1',
+    ],
+    entry_points={
+        'console_scripts': ['wdl-lsp = wdl_lsp.__main__:main'],
+    },
+)
