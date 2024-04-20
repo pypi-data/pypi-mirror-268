e71f6d69efd7b1fe7b008df747bda581c5ea5059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 05:07:20 +0000

    Add package blues-util/1.0.2/blues_util-1.0.2.tar.gz

---
+++ packages/blues-util/blues_util-1.0.2.tar.gz/blues_util-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+  name="blues_util", # package name
+  version="1.0.2", # package version
+  long_description="blues util demo1",
+  packages=find_packages(), # package module
+  install_requires=[] # package dependency
+)
