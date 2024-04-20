cf90458456fb433291fa547260b9b675c4c17ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-20 14:04:25 +0000

    Add package taskiq-redis/0.5.6/taskiq_redis-0.5.6.tar.gz

---
+++ packages/taskiq-redis/taskiq_redis-0.5.6.tar.gz/taskiq_redis-0.5.6/taskiq_redis/__init__.py
@@ -0,0 +1,21 @@
+"""Package for redis integration."""
+from taskiq_redis.redis_backend import (
+    RedisAsyncClusterResultBackend,
+    RedisAsyncResultBackend,
+)
+from taskiq_redis.redis_broker import ListQueueBroker, PubSubBroker
+from taskiq_redis.redis_cluster_broker import ListQueueClusterBroker
+from taskiq_redis.schedule_source import (
+    RedisClusterScheduleSource,
+    RedisScheduleSource,
+)
+
+__all__ = [
+    "RedisAsyncClusterResultBackend",
+    "RedisAsyncResultBackend",
+    "ListQueueBroker",
+    "PubSubBroker",
+    "ListQueueClusterBroker",
+    "RedisScheduleSource",
+    "RedisClusterScheduleSource",
+]
